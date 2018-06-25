c394f1ac20a250335a7f5ec5944899a704e8cfd0
Varun Wadekar <vwadekar@nvidia.com>
2018-06-25 11:36:47 -0700

    cpus: denver: reset power state to 'C1' on boot
    Denver CPUs expect the power state field to be reset to 'C1'
    during boot. This patch updates the reset handler to reset the
    ACTLR_.PMSTATE field to 'C1' state during CPU boot.
    
    Change-Id: I7cb629627a4dd1a30ec5cbb3a5e90055244fe30c
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- include/lib/cpus/aarch64/denver.h
+++ include/lib/cpus/aarch64/denver.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015-2018, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020, NVIDIA Corporation. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
