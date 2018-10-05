5d4a52fae07d6b98341c33ea8ebe6b637e95297e
Andre Przywara <andre.przywara@arm.com>
2018-10-05 01:25:40 +0100

    feat(allwinner): calculate MAC address from serial number
    Each Allwinner SoC contains a unique serial number, stored in the SID
    device. This number is used by U-Boot to generate a MAC address for
    network interfaces. Now technically the SID is a secure-only device,
    which would prevent U-Boot (running in NS-EL2) to access it. But on most
    devices the TrustZone device protection is not in effect, because they
    are using the standard, non-secure "eGON" boot method.
    However when a device is booted with some secure fuse burnt, the Secure
    Peripheral Controller starts to work, preventing U-Boot from accessing
    the SID device (reading all zeros for the serial number).
    
    To avoid a random MAC address to be generated by U-Boot, do the MAC
    address calculation already in BL31, where we can always access the SID.
    Copy the U-Boot algorithm that generates the MAC address from the first
    four SID registers, and store the resulting MAC in the device tree, where
    U-Boot and kernels can pick it up.
    
    The addition of the read/write functions of libfdt adds quite some code
    to BL31 (>5KB), which exceeds the SRAM limit on some SoCs (A64/H5),
    when compiled with DEBUG=1. Since secure devices are not very
    widespread, make this code optional, by requiring the Makefile variable
    SUNXI_GENERATE_MAC_ADDRESSES to be set at build time. To still
    accommodate debug builds with MAC address generation, drop the inclusion
    of ASSERTIONS on the affected SoCs.
    
    Change-Id: I469fe37c28d1961a97a5e21eb67b1d4b98fa9007
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- plat/allwinner/common/sunxi_prepare_dtb.c
+++ plat/allwinner/common/sunxi_prepare_dtb.c
@@ -9,8 +9,73 @@
 #include <common/debug.h>
 #include <common/fdt_fixup.h>
 #include <common/fdt_wrappers.h>
+#include <common/tf_crc32.h>
 
+#include <lib/mmio.h>
+
+#include <sunxi_mmap.h>
 #include <sunxi_private.h>
+
+#ifdef SUNXI_GENERATE_MAC_ADDRESSES
+/*
+ * Generate a MAC address based on the SID ID numbers.
+ * The SID is a secure device holding, among other information, a few registers
+ * with apparently unique serial numbers.
+ * U-Boot has for long years used this to generate a MAC address.
+ * If secure boot is enabled, U-Boot (running in EL2) has no longer access
+ * to the secure SID device, so it fails to generate the MAC address.
+ * Let us do this in EL3 instead, and write the generated number into the
+ * DT below.
+ * The algorithm here is identical to U-Boot's version, so is generating the
+ * very same MAC address.
+ */
+static void sunxi_generate_mac_address(unsigned int index, uint8_t *mac_addr)
+{
+	uint32_t sid[4], crc;
+	int i;
+
+	for (i = 0; i < 4; i++) {
+		sid[i] = mmio_read_32(SUNXI_SID_BASE + 0x200 + i * 4);
+	}
+
+	crc = tf_crc32(0, (uint8_t *)&sid[1], 12);
+
+	mac_addr[0] = (index << 4) | BIT_32(1);
+	mac_addr[1] = sid[0] & 0xff;
+	mac_addr[2] = crc >> 24;
+	mac_addr[3] = (crc >> 16) & 0xff;
+	mac_addr[4] = (crc >> 8) & 0xff;
+	mac_addr[5] = crc & 0xff;
+}
+
+static void fdt_add_mac_addresses(void *fdt)
+{
+	uint8_t mac_addr[6];
+	int ret, index;
+
+	/* Up to two MAC addresses, for Ethernet and WiFi. */
+	for (index = 0; index < 2; index++) {
+		sunxi_generate_mac_address(index, mac_addr);
+		ret = fdt_set_mac_address(fdt, index, mac_addr);
+		if (ret && ret != -FDT_ERR_NOTFOUND) {
+			WARN("BL31: cannot write MAC address into device tree: %d\n",
+			     ret);
+		}
+		if (ret) {
+			return;
+		}
+
+		INFO("BL31: MAC address #%d: %02x:%02x:%02x:%02x:%02x:%02x\n",
+		     index, mac_addr[0], mac_addr[1], mac_addr[2],
+		     mac_addr[3], mac_addr[4], mac_addr[5]);
+	}
+}
+
+#else	/* SUNXI_GENERATE_MAC_ADDRESSES */
+static void fdt_add_mac_addresses(void *fdt)
+{
+}
+#endif
 
 void sunxi_prepare_dtb(void *fdt)
 {
@@ -34,6 +99,8 @@ void sunxi_prepare_dtb(void *fdt)
 		return;
 	}
 #endif
+
+	fdt_add_mac_addresses(fdt);
 
 	ret = fdt_pack(fdt);
 	if (ret < 0) {
