1f32f79fb630037d948986d62751474bf1c205a6
Rajan Vaja <rajan.vaja@xilinx.com>
2018-10-05 04:42:57 -0700

    zynqmp: pm: Update PM version and support PM version check
    ATF is not checking PM version. Add version check in such
    a way that it is compatible with current and newer version
    of PM.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    Change-Id: Ia095d118121e6f75e8d320e87d5e2018068fa079

--- plat/xilinx/zynqmp/sip_svc_setup.c
+++ plat/xilinx/zynqmp/sip_svc_setup.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2018, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2013-2020, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -41,9 +41,7 @@ DEFINE_SVC_UUID2(zynqmp_sip_uuid,
 static int32_t sip_svc_setup(void)
 {
 	/* PM implementation as SiP Service */
-	pm_setup();
-
-	return 0;
+	return pm_setup();
 }
 
 /**
