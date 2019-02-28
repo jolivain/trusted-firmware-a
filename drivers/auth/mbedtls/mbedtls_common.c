8e59210df24a63b92e1b2fa7a0879d8a4814805c
John Tsichritzis <john.tsichritzis@arm.com>
2019-02-28 11:14:03 +0000

    Remove Mbed TLS dependency from plat_bl_common.c
    Due to the shared Mbed TLS heap optimisation introduced in 6d01a463,
    common code files were depending on Mbed TLS specific headers. This
    dependency is now removed by moving the default, unoptimised heap
    implementation inside the Mbed TLS specific files.
    
    Change-Id: I11ea3eb4474f0d9b6cb79a2afd73a51a4a9b8994
    Signed-off-by: John Tsichritzis <john.tsichritzis@arm.com>

--- drivers/auth/mbedtls/mbedtls_common.c
+++ drivers/auth/mbedtls/mbedtls_common.c
@@ -16,6 +16,8 @@
 #include <drivers/auth/mbedtls/mbedtls_config.h>
 #include <plat/common/platform.h>
 
+#pragma weak plat_get_mbedtls_heap
+
 static void cleanup(void)
 {
 	ERROR("EXIT from BL2\n");
@@ -53,4 +55,20 @@ void mbedtls_init(void)
 #endif
 		ready = 1;
 	}
+}
+
+/*
+ * The following default implementation of the function simply returns the
+ * by default allocated heap.
+ */
+int plat_get_mbedtls_heap(void **heap_addr, size_t *heap_size)
+{
+	static unsigned char heap[TF_MBEDTLS_HEAP_SIZE];
+
+	assert(heap_addr != NULL);
+	assert(heap_size != NULL);
+
+	*heap_addr = heap;
+	*heap_size = sizeof(heap);
+	return 0;
 }
