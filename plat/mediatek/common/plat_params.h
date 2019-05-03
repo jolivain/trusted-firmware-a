d9b6b93af73f1e3d9d84f561754fbd25a25991b3
kenny liang <kenny.liang@mediatek.com>
2019-05-03 16:59:07 +0800

    mediatek: mt8183: pass platform parameters
    Add plat parameter structs to support BL2 to pass
    variable-length, variable-type parameters to BL31.
    The parameters are structured as a link list.
    During BL31 setup time, we traverse the list to process
    each parameter.
    
    Signed-off-by: kenny liang <kenny.liang@mediatek.com>
    Change-Id: Ie84cfc9606656fb1d2780a68cadf27e09afa6628

---
+++ plat/mediatek/common/plat_params.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PLAT_PARAMS_H
+#define PLAT_PARAMS_H
+
+#include <lib/bl_aux_params/bl_aux_params.h>
+#include <stdint.h>
+
+/* param type */
+enum bl_aux_mtk_param_type {
+	BL_AUX_PARAM_MTK_RESET_GPIO = BL_AUX_PARAM_VENDOR_SPECIFIC_FIRST,
+};
+
+#endif
