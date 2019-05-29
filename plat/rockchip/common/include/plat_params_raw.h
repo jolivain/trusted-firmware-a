64ef1d1ccd0fca83f40f4621fe993ffd20a40cd5
Julius Werner <jwerner@chromium.org>
2019-05-28 21:03:58 -0700

    Factor out cross-BL API into raw headers suitable for other environments
    This patch splits a bunch of headers into a header.h and header_raw.h
    part. The goal of this is to allow third-party BL2 implementations (like
    coreboot) to directly include the "raw" headers so they don't need to
    duplicate all the handoff API constant and type definitions. This isn't
    possible with the current headers because handoff API definitions are
    too deeply intertwined with other TF code/headers and chain-include
    other headers that will not be available in the other environment.
    
    The new approach aims to solve this by separating only the parts that
    are really needed into these special headers that may not chain-include
    any others and may only use ISO C integer types (e.g. uint64_t but not
    u_register_t). Trusted Firmware code should never include them directly
    but should instead always include the respective wrapper header, which
    will include the required prerequisites (like <stdint.h>) before
    including the raw header. Third-party code can include the raw headers
    directly (making sure it provides the required types in its own way).
    
    Change-Id: Ifd769320ba51371439a8e5dd5b79c2516c3b43ab
    Signed-off-by: Julius Werner <jwerner@chromium.org>

---
+++ plat/rockchip/common/include/plat_params_raw.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2019, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef ARM_TRUSTED_FIRMWARE_PLAT_ROCKCHIP_COMMON_PLAT_PARAMS_RAW_H
+#define ARM_TRUSTED_FIRMWARE_PLAT_ROCKCHIP_COMMON_PLAT_PARAMS_RAW_H
+
+/**************** NOTE ************** NOTE ************** NOTE ****************
+ * This file is intended to be included by third-party projects. It should    *
+ * not contain any #include directives or function prototypes. It should only *
+ * contain constant and type definitions, only rely on ISO C fixed-width      *
+ * integer types and not depend on the definition of any macros other than    *
+ * U(), AARCH32/64 and __ASSEMBLY__. It should never be included directly,    *
+ * but only through the corresponding wrapper header, which must include      *
+ * relevant headers (like <stdint.h>) before this file.                       *
+ **************** NOTE ************** NOTE ************** NOTE ****************/
+
+/* param type */
+enum bl31_rk_param_type {
+	BL31_PLAT_PARAM_RK_RESET_GPIO = BL31_PLAT_PARAM_VENDOR_SPECIFIC_FIRST,
+	BL31_PLAT_PARAM_RK_POWEROFF_GPIO,
+	BL31_PLAT_PARAM_RK_SUSPEND_GPIO,
+	BL31_PLAT_PARAM_RK_SUSPEND_APIO,
+};
+
+typedef struct {
+	uint8_t apio1 : 1;
+	uint8_t apio2 : 1;
+	uint8_t apio3 : 1;
+	uint8_t apio4 : 1;
+	uint8_t apio5 : 1;
+} bl31_rk_apio_info_t;
+
+typedef struct {
+	bl31_plat_param_header_t h;
+	bl31_rk_apio_info_t apio;
+} bl31_plat_param_rk_apio_t;
+
+#endif /* ARM_TRUSTED_FIRMWARE_PLAT_ROCKCHIP_COMMON_PLAT_PARAMS_RAW_H */
