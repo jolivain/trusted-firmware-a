c6970baa3b73d9c4da2c1d8651df70fa4aaf6d06
Julius Werner <jwerner@chromium.org>
2019-05-28 21:03:58 -0700

    Factor out cross-BL API into raw headers suitable for other environments
    This patch splits a bunch of headers into a header.h and header_raw.h
    part. The goal of this is to allow third-party BL2 implementations (like
    coreboot) to directly include the "raw" headers so they don't need to
    duplicate all the handoff API constant and type definitions. This isn't
    possible with the current headers because handoff API definitions are
    too deeply intertwined with other TF code/headers and chain-include
    other headers that will not be available in the other environment.
    
    The new approach aims to solve this by separating only the parts that
    are really needed into these special headers that may not chain-include
    any others and may only use ISO C integer types (e.g. uint64_t but not
    u_register_t). Trusted Firmware code should never include them directly
    but should instead always include the respective wrapper header, which
    will include the required prerequisites (like <stdint.h>) before
    including the raw header. Third-party code can include the raw headers
    directly (making sure it provides the required types in its own way).
    
    Change-Id: Ifd769320ba51371439a8e5dd5b79c2516c3b43ab
    Signed-off-by: Julius Werner <jwerner@chromium.org>

--- plat/rockchip/common/include/plat_params.h
+++ plat/rockchip/common/include/plat_params.h
@@ -10,25 +10,6 @@
 #include <plat/common/bl31_params.h>
 #include <stdint.h>
 
-/* param type */
-enum bl31_rk_param_type {
-	BL31_PLAT_PARAM_RK_RESET_GPIO = BL31_PLAT_PARAM_VENDOR_SPECIFIC_FIRST,
-	BL31_PLAT_PARAM_RK_POWEROFF_GPIO,
-	BL31_PLAT_PARAM_RK_SUSPEND_GPIO,
-	BL31_PLAT_PARAM_RK_SUSPEND_APIO,
-};
-
-typedef struct {
-	uint8_t apio1 : 1;
-	uint8_t apio2 : 1;
-	uint8_t apio3 : 1;
-	uint8_t apio4 : 1;
-	uint8_t apio5 : 1;
-} bl31_rk_apio_info_t;
-
-typedef struct {
-	bl31_plat_param_header_t h;
-	bl31_rk_apio_info_t apio;
-} bl31_plat_param_rk_apio_t;
+#include "plat_params_raw.h"
 
 #endif /* PLAT_PARAMS_H */
