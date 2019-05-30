3843788db96cb7ba765369a0d7944c2b8a7e37f0
Julius Werner <jwerner@chromium.org>
2019-05-29 17:01:46 -0700

    plat/common: Add helper to parse BL31 parameters (both versions)
    BL31 used to take a single bl31_params_t parameter structure with entry
    point information in arg0. In commit 726002263 (Add new version of image
    loading.) this API was changed to a more flexible linked list approach,
    and the old parameter structure was copied into all platforms that still
    used the old format. This duplicated code unnecessarily among all these
    platforms.
    
    This patch adds a helper function that platforms can optionally link to
    outsource the task of interpreting arg0. Many platforms are just
    interested in the BL32 and BL33 entry point information anyway. The new
    helper will transparently detect if version 1 or 2 of the BL31
    parameters is used (allowing BL2 implementations to update on their own
    time) and extract the relevant information, allowing platforms to drop
    a bunch of duplicated boilerplate code.
    
    Change-Id: I9e6475adb1a7d4bccea666118bd1c54962e9fc38
    Signed-off-by: Julius Werner <jwerner@chromium.org>

--- plat/common/bl31_params.c
+++ plat/common/bl31_params.c
@@ -4,9 +4,48 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <assert.h>
+#include <common/bl_common.h>
 #include <common/debug.h>
+#include <common/tbbr/tbbr_img_def.h>
 #include <lib/coreboot.h>
 #include <plat/common/bl31_params.h>
+
+struct bl31_params_v1 {
+       param_header_t h;
+       image_info_t *bl31_image_info;
+       entry_point_info_t *bl32_ep_info;
+       image_info_t *bl32_image_info;
+       entry_point_info_t *bl33_ep_info;
+       image_info_t *bl33_image_info;
+};
+
+void bl31_params_parse(u_register_t param, entry_point_info_t *bl32_ep_info_out,
+		       entry_point_info_t *bl33_ep_info_out)
+{
+	struct bl31_params_v1 *v1 = (void *)(uintptr_t)param;
+	bl_params_t *v2 = (void *)(uintptr_t)param;
+
+	if (v1->h.version == PARAM_VERSION_1) {
+		assert(v1->h.type == PARAM_BL31);
+		if (bl32_ep_info_out)
+			*bl32_ep_info_out = *v1->bl32_ep_info;
+		if (bl33_ep_info_out)
+			*bl33_ep_info_out = *v1->bl33_ep_info;
+	} else {
+		bl_params_node_t *node;
+		assert(v2->h.version == PARAM_VERSION_2);
+		assert(v2->h.type == PARAM_BL_PARAMS);
+		for (node = v2->head; node; node = node->next_params_info) {
+			if (node->image_id == BL32_IMAGE_ID)
+				if (bl32_ep_info_out)
+					*bl32_ep_info_out = *node->ep_info;
+			if (node->image_id == BL33_IMAGE_ID)
+				if (bl33_ep_info_out)
+					*bl33_ep_info_out = *node->ep_info;
+		}
+	}
+}
 
 void bl31_plat_params_parse(u_register_t plat_param,
 			    bl31_plat_param_handler_t handler)
