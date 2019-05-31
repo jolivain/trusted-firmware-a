742ce41d469f15df2509fa23d417d002ec3fd5ee
Julius Werner <jwerner@chromium.org>
2019-05-30 17:34:08 -0700

    plat/mediatek/mt81*: Use new bl31_params_parse() helper
    The Mediatek MT8173/MT8183 SoCs are prime candidates for switching to
    the new bl31_params_parse() helper, so switch them over. This will allow
    BL2 implementations on these platforms to transparently switch over to
    the version 2 parameter structure.
    
    Change-Id: I0d17ba6c455102d325a06503d2078a76d12b5deb
    Signed-off-by: Julius Werner <jwerner@chromium.org>

--- plat/mediatek/mt8173/bl31_plat_setup.c
+++ plat/mediatek/mt8173/bl31_plat_setup.c
@@ -79,6 +79,7 @@ entry_point_info_t *bl31_plat_get_next_image_ep_info(uint32_t type)
 	entry_point_info_t *next_image_info;
 
 	next_image_info = (type == NON_SECURE) ? &bl33_ep_info : &bl32_ep_info;
+	assert(next_image_info->h.type == PARAM_EP);
 
 	/* None of the images on this platform can have 0x0 as the entrypoint */
 	if (next_image_info->pc)
@@ -98,18 +99,11 @@ entry_point_info_t *bl31_plat_get_next_image_ep_info(uint32_t type)
 void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 				u_register_t arg2, u_register_t arg3)
 {
-	struct mtk_bl31_params *arg_from_bl2 = (struct mtk_bl31_params *)arg0;
-
 	console_init(MT8173_UART0_BASE, MT8173_UART_CLOCK, MT8173_BAUDRATE);
 
 	VERBOSE("bl31_setup\n");
 
-	assert(arg_from_bl2 != NULL);
-	assert(arg_from_bl2->h.type == PARAM_BL31);
-	assert(arg_from_bl2->h.version >= VERSION_1);
-
-	bl32_ep_info = *arg_from_bl2->bl32_ep_info;
-	bl33_ep_info = *arg_from_bl2->bl33_ep_info;
+	bl31_params_parse(arg0, &bl32_ep_info, &bl33_ep_info);
 }
 
 /*******************************************************************************
