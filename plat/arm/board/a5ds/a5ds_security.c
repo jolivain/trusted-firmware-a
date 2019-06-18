51b39660c89ec98d445c8defe78eb32d8b2cc5fe
Usama Arif <usama.arif@arm.com>
2019-06-18 16:46:05 +0100

    plat/arm: Introduce FVP DesignStart platform.
    This patch adds support for Cortex-A5 FVP for the
    DesignStart program. DesignStart aims at providing
    low cost and fast access to Arm IP.
    
    Signed-off-by: Usama Arif <usama.arif@arm.com>
    Change-Id: I3a2281ce6de2402dda4610a89939ed53aa045fab

---
+++ plat/arm/board/a5ds/a5ds_security.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*
+ * We assume that all security programming is done by the primary core.
+ */
+void plat_arm_security_setup(void)
+{
+	/*
+	 * The platform currently does not have any security setup.
+	 */
+}
