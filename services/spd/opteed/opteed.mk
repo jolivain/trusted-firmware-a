c222651882744bc2619ae7181e54536314b0ba16
Sumit Garg <sumit.garg@linaro.org>
2019-08-05 14:34:48 +0530

    spd: opteed: enable NS_TIMER_SWITCH
    Enable dispatcher to save/restore unbanked timer registers. So that
    both secure (OP-TEE) and non-secure (Linux) worlds can have independent
    access control over timer registers.
    
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
    Change-Id: I4d58d5ff8298587ed478c8433fcbc3aef538d668

--- services/spd/opteed/opteed.mk
+++ services/spd/opteed/opteed.mk
@@ -13,3 +13,6 @@ SPD_SOURCES		:=	services/spd/opteed/opteed_common.c	\
 				services/spd/opteed/opteed_pm.c
 
 NEED_BL32		:=	yes
+
+# required so that optee code can control access to the timer registers
+NS_TIMER_SWITCH		:=	1
