abf7ab54479e280fa47fa2855789e5b0a0155b96
Balint Dobszay <balint.dobszay@arm.com>
2019-08-23 16:05:57 +0200

    Add CMake build system
    First version of the CMake based build system. This version is able to build
    AArch64 version of TF-A targeting the fvp platform. The resulting binary can
    boot to Linux fine, and passes standard TFTF tests.
    
    Change-Id: I865535d06cc0a788c0625406db95c2bc6ff9a519
    Signed-off-by: Balint Dobszay <balint.dobszay@arm.com>
    Co-authored-by: Bence Szépkúti <bence.szepkuti@arm.com>
    Co-authored-by: Gyorgy Szing <gyorgy.szing@arm.com>

---
+++ plat/arm/board/common/board_common.cmake
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2019-2020, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+if(NOT DEFINED TFA_ROOT_DIR)
+	message(FATAL_ERROR "TF-A root dir not defined")
+endif()
+
+#plat_bl_common
+stgt_add_src_param(NAME bl1 bl2 bl31 KEY ARCH SRC
+	${TFA_ROOT_DIR}/drivers/arm/pl011/@ARCH@/pl011_console.S
+	${TFA_ROOT_DIR}/plat/arm/board/common/@ARCH@/board_arm_helpers.S
+)
+
+#plat_bl1, plat_bl2, plat_bl31
+stgt_add_src(NAME bl1 bl2 bl31 SRC
+	${TFA_ROOT_DIR}/drivers/cfi/v2m/v2m_flash.c
+)
+
+#TODO: TRUSTED_BOARD_BOOT
