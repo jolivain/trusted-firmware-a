77f895eb94f656978397fb7c9877af703127d768
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2019-09-16 17:05:08 +0530

    gic/gic600: add support for multichip configuration
    Add support to configure GIC-600's multichip routing table registers.
    Introduce a new gic600 multichip structure in order to support platforms
    to pass their GIC-600 multichip information such as routing table owner,
    SPI blocks ownership.
    
    Change-Id: Id409d0bc07843e271ead3fc2f6e3cb38b317878d
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>

---
+++ drivers/arm/gic/v3/gic600_multichip.c
@@ -0,0 +1,193 @@
+/*
+ * Copyright (c) 2019, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*
+ * GIC-600 driver extension for multichip setup
+ */
+
+#include <assert.h>
+#include <common/debug.h>
+
+#include <drivers/arm/gicv3.h>
+#include <drivers/arm/gic600_multichip.h>
+
+#include "gic600_multichip_private.h"
+#include "../common/gic_common_private.h"
+
+/*******************************************************************************
+ * GIC-600 multichip operation related helper functions
+ ******************************************************************************/
+static void gicd_dchipr_wait_for_power_update_progress(uintptr_t gicd_base)
+{
+	unsigned int retry = GICD_PUP_UPDATE_RETRIES;
+
+	while ((gicd_read_dchipr(gicd_base) & GICD_DCHIPR_PUP_BIT) != 0U) {
+		if (retry-- == 0) {
+			ERROR("GIC-600 connection to Routing Table Owner timed "
+					 "out\n");
+			panic();
+		}
+	}
+}
+
+static void gicd_chipr_n_wait_for_power_update_progress(
+					uintptr_t gicd_base,
+					unsigned int chip_id)
+{
+	unsigned int retry = GICD_PUP_UPDATE_RETRIES;
+
+	while ((gicd_read_chipr_n(gicd_base, chip_id) &
+				GICD_CHIPRx_PUP_BIT) != 0U) {
+		if (retry-- == 0) {
+			ERROR("GIC-600 Routing Table register CHIPR%u failed "
+					"to update\n", chip_id);
+			panic();
+		}
+	}
+}
+
+static void gicd_chipsr_wait_for_rts_update(uintptr_t base)
+{
+	unsigned int retry = GICD_PUP_UPDATE_RETRIES;
+
+	while (((gicd_read_chipsr(base) & GICD_CHIPSR_RTS_MASK) >>
+				GICD_CHIPSR_RTS_SHIFT) ==
+				GICD_CHIPSR_RTS_STATE_UPDATING) {
+		if (retry-- == 0) {
+			ERROR("GIC-600 failed to come to Consistent State\n");
+			panic();
+		}
+	}
+}
+
+/*******************************************************************************
+ * Sets up the routing table owner.
+ ******************************************************************************/
+static void gicd_set_dchipr_rt_owner(uintptr_t base, unsigned int rt_owner)
+{
+	/*
+	 * Ensure that Group enables in GICD_CTLR are disabled and no pending
+	 * register writes to GICD_CTLR.
+	 */
+	if ((gicd_read_ctlr(base) &
+			(CTLR_ENABLE_G0_BIT | CTLR_ENABLE_G1S_BIT |
+			 CTLR_ENABLE_G1NS_BIT | GICD_CTLR_RWP_BIT)) != 0) {
+		ERROR("GICD_CTLR group enables is either enabled or have "
+				"pending writes. Cannot set RT owner.\n");
+		panic();
+	}
+
+	gicd_write_dchipr(base, gicd_read_dchipr(base) |
+			(rt_owner << GICD_DCHIPR_RT_OWNER_SHIFT));
+
+	/* Poll till PUP is zero */
+	gicd_dchipr_wait_for_power_update_progress(base);
+}
+
+/*******************************************************************************
+ * Configures the Chip Register to make connections to GICDs on
+ * a multichip platform.
+ ******************************************************************************/
+static void gicd_set_chipr_n(uintptr_t base,
+				unsigned int chip_id,
+				uint64_t chip_addr,
+				unsigned int spi_id_min,
+				unsigned int spi_id_max)
+{
+	unsigned int spi_block_min, spi_blocks;
+	uint64_t chipr_n_val;
+
+	/*
+	 * Ensure that group enables in GICD_CTLR are disabled and no pending
+	 * register writes to GICD_CTLR.
+	 */
+	if ((gicd_read_ctlr(base) &
+			(CTLR_ENABLE_G0_BIT | CTLR_ENABLE_G1S_BIT |
+			 CTLR_ENABLE_G1NS_BIT | GICD_CTLR_RWP_BIT)) != 0) {
+		ERROR("GICD_CTLR group enables is either enabled or have "
+				"pending writes. Cannot set CHIPR register.\n");
+		panic();
+	}
+
+	/* SPI interrupt id ranges from 32 to 1019 */
+	assert((spi_id_min >= GICv3_SPI_ID_MIN) &&
+			(spi_id_min <= GICv3_SPI_ID_MAX));
+	assert((spi_id_max >= GICv3_SPI_ID_MIN) &&
+			(spi_id_max <= GICv3_SPI_ID_MAX));
+
+	spi_block_min = SPI_BLOCK_MIN_VALUE(spi_id_min);
+	spi_blocks    = SPI_BLOCKS_VALUE(spi_id_min, spi_id_max);
+
+	chipr_n_val = GICD_CHIPR_VALUE(chip_addr, spi_block_min, spi_blocks);
+
+	/*
+	 * Wait for DCHIPR.PUP to be zero before commencing writes to
+	 * GICD_CHIPRx
+	 */
+	gicd_dchipr_wait_for_power_update_progress(base);
+
+	/*
+	 * Assign chip addr, spi min block, number of spi blocks and bring chip
+	 * online by setting SocketState
+	 */
+	gicd_write_chipr_n(base, chip_id,
+			(chipr_n_val | GICD_CHIPRx_SOCKET_STATE));
+
+	/* Wait for CHIPR Power Update Progress */
+	gicd_chipr_n_wait_for_power_update_progress(base, chip_id);
+
+	/*
+	 * Ensure that write to GICD_CHIPRx is successful and the chip_n came
+	 * online.
+	 */
+	if (gicd_read_chipr_n(base, chip_id) !=
+			(chipr_n_val | GICD_CHIPRx_SOCKET_STATE)) {
+		ERROR("GICD_CHIPR%u write failed\n", chip_id);
+		panic();
+	}
+
+	/*
+	 * Poll until DCHIP.PUP is zero to verify connection to rt_owner chip
+	 * is complete
+	 */
+	gicd_dchipr_wait_for_power_update_progress(base);
+
+	/* Wait till the chip is in consistent state */
+	gicd_chipsr_wait_for_rts_update(base);
+}
+
+void gic600_multichip_init(gic600_multichip_data_t *multichip_data)
+{
+	unsigned int i;
+
+	/*
+	 * Clear the "enable" bits for G0/G1S/G1NS interrupts before configuring
+	 * the routing table registers.
+	 */
+	gicd_clr_ctlr(multichip_data->gicd_base,
+		      CTLR_ENABLE_G0_BIT |
+		      CTLR_ENABLE_G1S_BIT |
+		      CTLR_ENABLE_G1NS_BIT,
+		      RWP_TRUE);
+
+	/* Initialize the GICD which is marked as routing table owner first */
+	gicd_set_dchipr_rt_owner(multichip_data->gicd_base,
+			multichip_data->rt_owner);
+	gicd_set_chipr_n(multichip_data->gicd_base, multichip_data->rt_owner,
+			multichip_data->chip_addrs[multichip_data->rt_owner],
+			multichip_data->spi_ids[multichip_data->rt_owner][0],
+			multichip_data->spi_ids[multichip_data->rt_owner][1]);
+
+	for (i = 0; i < multichip_data->chip_count; i++) {
+		if (i == multichip_data->rt_owner)
+			continue;
+
+		gicd_set_chipr_n(multichip_data->gicd_base, i,
+				multichip_data->chip_addrs[i],
+				multichip_data->spi_ids[i][0],
+				multichip_data->spi_ids[i][1]);
+	}
+}
---
+++ include/drivers/arm/gic600_multichip.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef GIC600_MULTICHIP_H
+#define GIC600_MULTICHIP_H
+
+#include <stdint.h>
+
+/*
+ * GIC-600 microarchitecture supports coherent multichip environments containing
+ * up to 16 chips of up to 64 cores each.
+ */
+#define GIC600_MAX_MULTICHIP	16
+
+/*******************************************************************************
+ * GIC-600 multichip data structure describes platform specific attributes
+ * related to GIC-600 multichip. Platform port is expected to define these
+ * attributes to initialize the multichip related registers and create
+ * successful connections between the GIC-600s in a multichip system.
+ *
+ * The 'gicd_base' field contains the base address of the Distributor interface
+ * programmer's view.
+ *
+ * The 'rt_owner' field contains the chip number which owns the routing table.
+ * Chip number or chip_id starts from 0.
+ *
+ * The 'chip_count' field contains the total number of chips in a multichip
+ * system. This should match the number of entries in 'chip_addrs' and 'spi_ids'
+ * fields.
+ *
+ * The 'chip_addrs' field contains array of chip addresses. These addresses are
+ * implementation specific values.
+ *
+ * The 'spi_ids' field contains array of minimum and maximum SPI interrupt ids
+ * that each chip owns. Note that SPI interrupt ids can range for 32 to 1019.
+ * If a chip doesn't own any SPI interrupts a value of {32, 32} should be
+ * passed.
+ ******************************************************************************/
+typedef struct gic600_multichip_data {
+	uintptr_t gicd_base;
+	unsigned int rt_owner;
+	unsigned int chip_count;
+	uint64_t chip_addrs[GIC600_MAX_MULTICHIP];
+	unsigned int spi_ids[GIC600_MAX_MULTICHIP][2];
+} gic600_multichip_data_t;
+
+void gic600_multichip_init(gic600_multichip_data_t *multichip_data);
+#endif /* GIC600_MULTICHIP_H */
+
