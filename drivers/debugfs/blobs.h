a7ab3ab2775428ded1ecb9bdfae50146a3e35986
Olivier Deprez <olivier.deprez@arm.com>
2019-09-19 17:46:46 +0200

    debugfs: add 9p device interface
    The 9p interface provides abstraction layers allowing the software
    that uses devices to be independent from the hardware.
    
    This patch provides a file system abstraction to link drivers to their
    devices and propose a common interface to expose driver operations to
    higher layers. This file system can be used to access and configure a
    device by doing read/write operations.
    
    Signed-off-by: Ambroise Vincent <ambroise.vincent@arm.com>
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: Ia9662393baf489855dc0c8f389fe4a0afbc9c255

---
+++ drivers/debugfs/blobs.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2019, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+static const dirtab_t blobtab[] = {
+	{"ctl", DEV_ROOT_QBLOBCTL, 0, O_READ}
+};
