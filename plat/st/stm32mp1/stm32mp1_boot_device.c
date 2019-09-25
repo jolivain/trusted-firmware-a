36191553474c7f3a739574e619d289b708a606be
Lionel Debieve <lionel.debieve@st.com>
2019-09-25 09:11:31 +0200

    stm32mp1: Add support for SPI-NOR boot device
    STM32MP1 platform is able to boot from SPI-NOR devices.
    These modifications add this support using the new
    SPI-NOR framework.
    
    Change-Id: I75ff9eba4661f9fb87ce24ced2bacbf8558ebe44
    Signed-off-by: Lionel Debieve <lionel.debieve@st.com>

--- plat/st/stm32mp1/stm32mp1_boot_device.c
+++ plat/st/stm32mp1/stm32mp1_boot_device.c
@@ -149,3 +149,21 @@ int plat_get_spi_nand_data(struct spinand_device *device)
 }
 #endif
 
+#if STM32MP_SPI_NOR
+int plat_get_nor_data(struct nor_device *device)
+{
+	device->size = 0x4000000U;
+
+	zeromem(&device->read_op, sizeof(struct spi_mem_op));
+	device->read_op.cmd.opcode = SPI_NOR_OP_READ_1_1_4;
+	device->read_op.cmd.buswidth = SPI_MEM_BUSWIDTH_1_LINE;
+	device->read_op.addr.nbytes = 3U;
+	device->read_op.addr.buswidth = SPI_MEM_BUSWIDTH_1_LINE;
+	device->read_op.dummy.nbytes = 1U;
+	device->read_op.dummy.buswidth = SPI_MEM_BUSWIDTH_1_LINE;
+	device->read_op.data.buswidth = SPI_MEM_BUSWIDTH_4_LINE;
+	device->read_op.data.dir = SPI_MEM_DATA_IN;
+
+	return 0;
+}
+#endif
