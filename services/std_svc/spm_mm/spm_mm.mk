ddc11e02dfc42905bb84ac2ea9c111abcf45408b
Paul Beesley <paul.beesley@arm.com>
2019-10-15 16:14:14 +0000

    spm-mm: Rename aarch64 assembly files
    Change-Id: I2bab67f319758dd033aa689d985227cad796cdea
    Signed-off-by: Paul Beesley <paul.beesley@arm.com>

--- services/std_svc/spm_mm/spm_mm.mk
+++ services/std_svc/spm_mm/spm_mm.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2017-2018, ARM Limited and Contributors. All rights reserved.
+# Copyright (c) 2017-2019, ARM Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -12,8 +12,8 @@ ifneq (${ARCH},aarch64)
 endif
 
 SPM_SOURCES	:=	$(addprefix services/std_svc/spm_mm/,	\
-			${ARCH}/spm_helpers.S			\
-			${ARCH}/spm_shim_exceptions.S		\
+			${ARCH}/spm_mm_helpers.S			\
+			${ARCH}/spm_mm_shim_exceptions.S		\
 			spm_mm_main.c				\
 			spm_mm_setup.c				\
 			spm_mm_xlat.c)
