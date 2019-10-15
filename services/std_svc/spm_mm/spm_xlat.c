e7f331f2aebbe9895709f679394a30f4aa400814
Paul Beesley <paul.beesley@arm.com>
2019-10-15 10:57:42 +0000

    spm-mm: Refactor secure_partition.h and its contents
    Before adding any new SPM-related components we should first do
    some cleanup around the existing SPM-MM implementation. The aim
    is to make sure that any SPM-MM components have names that clearly
    indicate that they are MM-related. Otherwise, when adding new SPM
    code, it could quickly become confusing as it would be unclear to
    which component the code belongs.
    
    The secure_partition.h header is a clear example of this, as the
    name is generic so it could easily apply to any SPM-related code,
    when it is in fact SPM-MM specific.
    
    This patch renames the file and the two structures defined within
    it, and then modifies any references in files that use the header.
    
    Change-Id: I44bd95fab774c358178b3e81262a16da500fda26
    Signed-off-by: Paul Beesley <paul.beesley@arm.com>

--- services/std_svc/spm_mm/spm_xlat.c
+++ services/std_svc/spm_mm/spm_xlat.c
@@ -11,7 +11,7 @@
 #include <lib/xlat_tables/xlat_tables_v2.h>
 #include <platform_def.h>
 #include <plat/common/platform.h>
-#include <services/secure_partition.h>
+#include <services/spm_mm_partition.h>
 #include <services/spm_svc.h>
 
 #include "spm_private.h"
