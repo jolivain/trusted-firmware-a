515d9c51bb5e3f6e9437a31ea6398835b96e3196
Louis Mayencourt <louis.mayencourt@arm.com>
2019-10-17 14:46:51 +0100

    fconf: Load config dtb from bl1
    Move the loading of the dtb from arm_dym_cfg to fconf. The new loading
    function is not associated to arm platform anymore, and can be moved
    to bl_main if wanted.
    
    Change-Id: I847d07eaba36d31d9d3ed9eba8e58666ea1ba563
    Signed-off-by: Louis Mayencourt <louis.mayencourt@arm.com>

--- drivers/fconf/fconf.c
+++ drivers/fconf/fconf.c
@@ -9,7 +9,47 @@
 #include <common/debug.h>
 #include <drivers/fconf/fconf.h>
 #include <libfdt.h>
+#include <plat/common/platform.h>
 #include <platform_def.h>
+
+static uintptr_t tb_fw_cfg_dtb;
+static size_t tb_fw_cfg_dtb_size;
+
+void fconf_load_config(void)
+{
+	int err;
+	uintptr_t config_base = 0UL;
+	image_desc_t *desc;
+
+	image_desc_t arm_tb_fw_info = {
+		.image_id = TB_FW_CONFIG_ID,
+		SET_STATIC_PARAM_HEAD(image_info, PARAM_IMAGE_BINARY,
+				VERSION_2, image_info_t, 0),
+		.image_info.image_base = ARM_TB_FW_CONFIG_BASE,
+		.image_info.image_max_size = (uint32_t)
+			(ARM_TB_FW_CONFIG_LIMIT - ARM_TB_FW_CONFIG_BASE)
+	};
+
+	VERBOSE("FCONF: Loading FW_CONFIG\n");
+	err = load_auth_image(TB_FW_CONFIG_ID, &arm_tb_fw_info.image_info);
+	if (err != 0) {
+		/* Return if FW_CONFIG is not loaded */
+		VERBOSE("Failed to load FW_CONFIG\n");
+		return;
+	}
+
+	/* At this point we know that a DTB is indeed available */
+	config_base = arm_tb_fw_info.image_info.image_base;
+	tb_fw_cfg_dtb = config_base;
+	tb_fw_cfg_dtb_size = (size_t)arm_tb_fw_info.image_info.image_max_size;
+
+	/* The BL2 ep_info arg0 is modified to point to FW_CONFIG */
+	desc = bl1_plat_get_image_desc(BL2_IMAGE_ID);
+	assert(desc != NULL);
+	desc->ep_info.args.arg0 = config_base;
+
+	INFO("FCONF: FW_CONFIG loaded at address = 0x%lx\n", config_base);
+}
 
 void fconf_populate(uintptr_t config)
 {
