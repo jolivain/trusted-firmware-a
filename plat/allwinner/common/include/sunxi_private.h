7089f0397c9d89de56b8590c9f874e76173bafb3
Samuel Holland <samuel@sholland.org>
2019-10-20 15:06:57 -0500

    allwinner: Merge duplicate code in sunxi_power_down
    The action of last resort isn't going to change between SoCs. This moves
    that code back to the PSCI implementation, where it more obviously
    matches the code in sunxi_system_reset().
    
    The two error messages say essentially the same thing anyway.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>
    Change-Id: I62ac35fdb5ed78a016e9b18281416f1dcea38a4a

--- plat/allwinner/common/include/sunxi_private.h
+++ plat/allwinner/common/include/sunxi_private.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2018, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2017-2019, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -12,7 +12,7 @@ void sunxi_configure_mmu_el3(int flags);
 void sunxi_cpu_on(u_register_t mpidr);
 void sunxi_cpu_off(u_register_t mpidr);
 void sunxi_disable_secondary_cpus(u_register_t primary_mpidr);
-void __dead2 sunxi_power_down(void);
+void sunxi_power_down(void);
 
 int sunxi_pmic_setup(uint16_t socid, const void *fdt);
 void sunxi_security_setup(void);
