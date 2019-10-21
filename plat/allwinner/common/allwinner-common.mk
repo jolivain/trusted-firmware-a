a1a0266f9ac9c89dc844f1877aeb7993031b67d9
Samuel Holland <samuel@sholland.org>
2019-10-20 21:34:38 -0500

    allwinner: h6: power: Switch to using the AXP driver
    Chip ID checking and poweroff work just like they did before.
    Regulators are now enabled just like on A64/H5.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>
    Change-Id: I14d63d171a094fa1375904928270fa3e21761646

--- plat/allwinner/common/allwinner-common.mk
+++ plat/allwinner/common/allwinner-common.mk
@@ -20,7 +20,8 @@ PLAT_BL_COMMON_SOURCES	:=	drivers/ti/uart/${ARCH}/16550_console.S	\
 				${AW_PLAT}/common/plat_helpers.S	\
 				${AW_PLAT}/common/sunxi_common.c
 
-BL31_SOURCES		+=	drivers/arm/gic/common/gic_common.c	\
+BL31_SOURCES		+=	drivers/allwinner/axp/common.c		\
+				drivers/arm/gic/common/gic_common.c	\
 				drivers/arm/gic/v2/gicv2_helpers.c	\
 				drivers/arm/gic/v2/gicv2_main.c		\
 				drivers/delay_timer/delay_timer.c	\
--- plat/allwinner/sun50i_h6/sunxi_power.c
+++ plat/allwinner/sun50i_h6/sunxi_power.c
@@ -10,6 +10,7 @@
 
 #include <arch_helpers.h>
 #include <common/debug.h>
+#include <drivers/allwinner/axp.h>
 #include <drivers/delay_timer.h>
 #include <drivers/mentor/mi2cv.h>
 #include <lib/mmio.h>
@@ -26,42 +27,43 @@ static enum pmic_type {
 	AXP805,
 } pmic;
 
-int axp_i2c_read(uint8_t chip, uint8_t reg, uint8_t *val)
+int axp_read(uint8_t reg)
 {
+	uint8_t val;
 	int ret;
 
-	ret = i2c_write(chip, 0, 0, &reg, 1);
+	ret = i2c_write(AXP805_ADDR, 0, 0, &reg, 1);
 	if (ret)
 		return ret;
 
-	return i2c_read(chip, 0, 0, val, 1);
+	ret = i2c_read(AXP805_ADDR, 0, 0, &val, 1);
+	if (ret)
+		return ret;
+
+	return val;
 }
 
-int axp_i2c_write(uint8_t chip, uint8_t reg, uint8_t val)
+int axp_write(uint8_t reg, uint8_t val)
 {
-	return i2c_write(chip, reg, 1, &val, 1);
+	return i2c_write(AXP805_ADDR, reg, 1, &val, 1);
 }
 
 static int axp805_probe(void)
 {
 	int ret;
-	uint8_t val;
 
-	ret = axp_i2c_write(AXP805_ADDR, 0xff, 0x0);
+	ret = axp_write(0xff, 0x0);
 	if (ret) {
 		ERROR("PMIC: Cannot put AXP805 in master mode\n");
 		return -EPERM;
 	}
 
-	ret = axp_i2c_read(AXP805_ADDR, AXP805_ID, &val);
-	if (ret) {
+	ret = axp_check_id();
+	if (ret < 0) {
 		ERROR("PMIC: Cannot communicate with AXP805\n");
 		return -EPERM;
-	}
-
-	val &= 0xcf;
-	if (val != 0x40) {
-		ERROR("PMIC: Unknown PMIC 0x%02x detected\n", val);
+	} else if (ret) {
+		ERROR("PMIC: Unknown PMIC 0x%02x detected\n", ret);
 		return -EINVAL;
 	}
 
@@ -78,20 +80,18 @@ void sunxi_pmic_setup(uint16_t socid, const void *fdt)
 		return;
 	pmic = AXP805;
 	NOTICE("PMIC: Detected AXP805 on I2C\n");
+	axp_setup_regulators(fdt);
 }
 
 void sunxi_power_down(void)
 {
-	uint8_t val;
-
 	switch (pmic) {
 	case AXP805:
 		/* Re-initialise after rich OS might have used it. */
 		sunxi_init_platform_r_twi(SUNXI_SOC_H6, false);
 		/* initialise mi2cv driver */
 		i2c_init((void *)SUNXI_R_I2C_BASE);
-		axp_i2c_read(AXP805_ADDR, 0x32, &val);
-		axp_i2c_write(AXP805_ADDR, 0x32, val | 0x80);
+		axp_power_off();
 		break;
 	default:
 		break;
