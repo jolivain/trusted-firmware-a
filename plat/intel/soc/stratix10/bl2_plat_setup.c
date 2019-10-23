62f898b3f1a15ac1cb539d2a18fa4d3eb4310c9b
Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
2019-10-23 17:09:51 +0800

    intel: Refactor common platform code [4/5]
    socfpga_storage.c
    
    Signed-off-by: Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
    Change-Id: If758dc9f8fd59e3c301bba57fe1b22321e1a8153

--- plat/intel/soc/stratix10/bl2_plat_setup.c
+++ plat/intel/soc/stratix10/bl2_plat_setup.c
@@ -27,9 +27,9 @@ include <arch.h>
 #include "s10_reset_manager.h"
 #include "s10_clock_manager.h"
 #include "s10_pinmux.h"
-#include "include/s10_mailbox.h"
 #include "qspi/cadence_qspi.h"
 #include "socfpga_handoff.h"
+#include "socfpga_mailbox.h"
 #include "socfpga_private.h"
 #include "wdt/watchdog.h"
 
@@ -114,7 +114,7 @@ void bl2_el3_plat_arch_setup(void)
 	switch (boot_source) {
 	case BOOT_SOURCE_SDMMC:
 		dw_mmc_init(&params, &info);
-		stratix10_io_setup(boot_source);
+		socfpga_io_setup(boot_source);
 		break;
 
 	case BOOT_SOURCE_QSPI:
@@ -123,7 +123,7 @@ void bl2_el3_plat_arch_setup(void)
 		cad_qspi_init(0, QSPI_CONFIG_CPHA, QSPI_CONFIG_CPOL,
 			QSPI_CONFIG_CSDA, QSPI_CONFIG_CSDADS,
 			QSPI_CONFIG_CSEOT, QSPI_CONFIG_CSSOT, 0);
-		stratix10_io_setup(boot_source);
+		socfpga_io_setup(boot_source);
 		break;
 
 	default:
