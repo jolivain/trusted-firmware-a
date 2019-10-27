d0c4758c6c1db190a000ed5e5ba40ba29b527ff3
Samuel Holland <samuel@sholland.org>
2019-10-27 14:07:52 -0500

    allwinner: Move the NOBITS region to SRAM A1
    This frees up space in SRAM A2 that will be used by the SCP firmware and
    SCPI shared memory.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>
    Change-Id: I8ce035257451e2d142666fe0cd045e59d4d57b35

--- plat/allwinner/common/allwinner-common.mk
+++ plat/allwinner/common/allwinner-common.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2017-2018, ARM Limited and Contributors. All rights reserved.
+# Copyright (c) 2017-2019, ARM Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -54,6 +54,9 @@ PROGRAMMABLE_RESET_ADDRESS	:=	1
 
 # Allow mapping read-only data as execute-never.
 SEPARATE_CODE_AND_RODATA	:=	1
+
+# Put NOBITS memory in SRAM A1, overwriting U-Boot's SPL.
+SEPARATE_NOBITS_REGION		:=	1
 
 # BL31 gets loaded alongside BL33 (U-Boot) by U-Boot's SPL
 RESET_TO_BL31			:=	1
