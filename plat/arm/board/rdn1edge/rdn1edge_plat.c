81f884d3cbb8d6d546f4a43e73abe41215372961
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2019-10-28 14:49:48 +0530

    board/rdn1edge: add support for dual-chip configuration
    RD-N1-Edge based platforms can operate in dual-chip configuration
    wherein two rdn1edge SoCs are connected through a high speed coherent
    CCIX link.
    
    This patch adds a function to check if the RD-N1-Edge platform is
    operating in multi-chip mode by reading the SID register's NODE_ID
    value. If operating in multi-chip mode, initialize GIC-600 multi-chip
    operation by overriding the default GICR frames with array of GICR
    frames and setting the chip 0 as routing table owner.
    
    The address space of the second RD-N1-Edge chip (chip 1) starts from the
    address 4TB. So increase the physical and virtual address space size to
    43 bits to accommodate the multi-chip configuration. If the multi-chip
    mode configuration is detected, dynamically add mmap entry for the
    peripherals memory region of the second RD-N1-Edge SoC. This is required
    to let the BL31 platform setup stage to configure the devices in the
    second chip.
    
    The max chip count is increased to 2 and topology changes are added to
    represent the dual-chip configuration.
    
    In order the build the dual-chip platform, PLAT_ARM_CSS_NUM_CHIPS macro
    should be set to 2:
    export CROSS_COMPILE=<path-to-cross-compiler>
    make PLAT=rdn1edge BL31_CFLAGS+=-DPLAT_ARM_CSS_NUM_CHIPS=2 ARCH=aarch64 all
    
    Change-Id: I576cdaf71f0b0e41b9a9181fa4feb7091f8c7bb4
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>

--- plat/arm/board/rdn1edge/rdn1edge_plat.c
+++ plat/arm/board/rdn1edge/rdn1edge_plat.c
@@ -4,7 +4,35 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <common/debug.h>
+#include <drivers/arm/gic600_multichip.h>
+#include <plat/arm/common/plat_arm.h>
 #include <plat/common/platform.h>
+#include <sgi_base_platform_def.h>
+
+#if defined(IMAGE_BL31)
+static struct gic600_multichip_data rdn1e1_multichip_data __init = {
+	.rt_owner_base = PLAT_ARM_GICD_BASE,
+	.rt_owner = 0,
+	.chip_count = 2,
+	.chip_addrs = {
+		PLAT_ARM_GICD_BASE >> 16,
+		(PLAT_ARM_GICD_BASE +
+		 PLAT_ARM_SGI_REMOTE_CHIP_MEM_OFFSET(1)) >> 16
+	},
+	.spi_ids = {
+		{32, 255},
+		{0, 0}
+	}
+};
+
+static uintptr_t rdn1e1_multichip_gicr_frames[] = {
+	PLAT_ARM_GICR_BASE,				/* Chip 0's GICR Base */
+	PLAT_ARM_GICR_BASE +
+		PLAT_ARM_SGI_REMOTE_CHIP_MEM_OFFSET(1),	/* Chip 1's GICR BASE */
+	0U						/* Zero Termination */
+};
+#endif
 
 unsigned int plat_arm_sgi_get_platform_id(void)
 {
@@ -24,7 +52,34 @@ unsigned int plat_arm_sgi_get_multi_chip_mode(void)
 }
 
 #if defined(IMAGE_BL31)
+static const mmap_region_t rdn1edge_dynamic_mmap[] = {
+	CSS_SGI_MAP_DEVICE_REMOTE_CHIP(1),
+	SOC_CSS_MAP_DEVICE_REMOTE_CHIP(1)
+};
+
 void plat_arm_sgi_bl31_board_setup(void)
 {
+	int i, ret;
+
+	if (plat_arm_sgi_get_multi_chip_mode() == 1 &&
+			PLAT_ARM_CSS_NUM_CHIPS > 1) {
+		INFO("BL31: Enabling support for multi-chip\n");
+
+		for (i = 0; i < ARRAY_SIZE(rdn1edge_dynamic_mmap); i++) {
+			ret = mmap_add_dynamic_region(
+					rdn1edge_dynamic_mmap[i].base_pa,
+					rdn1edge_dynamic_mmap[i].base_va,
+					rdn1edge_dynamic_mmap[i].size,
+					rdn1edge_dynamic_mmap[i].attr
+					);
+			if (ret != 0) {
+				ERROR("Failed to add dynamic mmap entry\n");
+				panic();
+			}
+		}
+
+		plat_arm_override_gicr_frames(rdn1e1_multichip_gicr_frames);
+		gic600_multichip_init(&rdn1e1_multichip_data);
+	}
 }
 #endif
