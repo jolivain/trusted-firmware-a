2647ba082733ea3c829e78e665c930f46d89f88d
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2019-10-28 14:49:48 +0530

    board/rdn1edge: add support for dual-chip configuration
    RD-N1-Edge based platforms can operate in dual-chip configuration
    wherein two rdn1edge SoCs are connected through a high speed coherent
    CCIX link.
    
    This patch adds a function to check if the RD-N1-Edge platform is
    operating in multi-chip mode by reading the SID register's NODE_ID
    value. If operating in multi-chip mode, initialize GIC-600 multi-chip
    operation by overriding the default GICR frames with array of GICR
    frames and setting the chip 0 as routing table owner.
    
    The address space of the second RD-N1-Edge chip (chip 1) starts from the
    address 4TB. So increase the physical and virtual address space size to
    43 bits to accommodate the multi-chip configuration. If the multi-chip
    mode configuration is detected, dynamically add mmap entry for the
    peripherals memory region of the second RD-N1-Edge SoC. This is required
    to let the BL31 platform setup stage to configure the devices in the
    second chip.
    
    The max chip count is increased to 2 and topology changes are added to
    represent the dual-chip configuration.
    
    In order the build the dual-chip platform, PLAT_ARM_CSS_NUM_CHIPS build
    flag should be set to 2:
    export CROSS_COMPILE=<path-to-cross-compiler>
    make PLAT=rdn1edge BL31_CFLAGS+=-DPLAT_ARM_CSS_NUM_CHIPS=2 ARCH=aarch64 all
    
    Change-Id: I576cdaf71f0b0e41b9a9181fa4feb7091f8c7bb4
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>

--- plat/arm/board/rdn1edge/rdn1edge_topology.c
+++ plat/arm/board/rdn1edge/rdn1edge_topology.c
@@ -5,14 +5,22 @@
  */
 
 #include <plat/arm/common/plat_arm.h>
+#include <plat/arm/css/common/css_pm.h>
+
+#define RD_N1_EDGE_MAX_PE	PLAT_ARM_CSS_NUM_CHIPS * CSS_SGI_MAX_PE_PER_CHIP
+#define SCMI_CHANNEL_ID(n)	(n << SCMI_CHANNEL_ID_SHIFT)
 
 /******************************************************************************
  * The power domain tree descriptor.
  ******************************************************************************/
 const unsigned char rdn1edge_pd_tree_desc[] = {
-	PLAT_ARM_CLUSTER_COUNT,
+	PLAT_ARM_CLUSTER_COUNT * PLAT_ARM_CSS_NUM_CHIPS,
+	CSS_SGI_MAX_CPUS_PER_CLUSTER,
+	CSS_SGI_MAX_CPUS_PER_CLUSTER,
+#if (PLAT_ARM_CSS_NUM_CHIPS > 1)
 	CSS_SGI_MAX_CPUS_PER_CLUSTER,
 	CSS_SGI_MAX_CPUS_PER_CLUSTER
+#endif
 };
 
 /*******************************************************************************
@@ -27,6 +35,15 @@ const unsigned char *plat_get_power_domain_tree_desc(void)
  * The array mapping platform core position (implemented by plat_my_core_pos())
  * to the SCMI power domain ID implemented by SCP.
  ******************************************************************************/
-const uint32_t plat_css_core_pos_to_scmi_dmn_id_map[CSS_SGI_MAX_PE_PER_CHIP] = {
-	0, 1, 2, 3, 4, 5, 6, 7
+const uint32_t plat_css_core_pos_to_scmi_dmn_id_map[RD_N1_EDGE_MAX_PE] = {
+	(SCMI_CHANNEL_ID(0) | 0x0), (SCMI_CHANNEL_ID(0) | 0x1),
+	(SCMI_CHANNEL_ID(0) | 0x2), (SCMI_CHANNEL_ID(0) | 0x3),
+	(SCMI_CHANNEL_ID(0) | 0x4), (SCMI_CHANNEL_ID(0) | 0x5),
+	(SCMI_CHANNEL_ID(0) | 0x6), (SCMI_CHANNEL_ID(0) | 0x7),
+#if (PLAT_ARM_CSS_NUM_CHIPS > 1)
+	(SCMI_CHANNEL_ID(1) | 0x0), (SCMI_CHANNEL_ID(1) | 0x1),
+	(SCMI_CHANNEL_ID(1) | 0x2), (SCMI_CHANNEL_ID(1) | 0x3),
+	(SCMI_CHANNEL_ID(1) | 0x4), (SCMI_CHANNEL_ID(1) | 0x5),
+	(SCMI_CHANNEL_ID(1) | 0x6), (SCMI_CHANNEL_ID(1) | 0x7),
+#endif
 };
