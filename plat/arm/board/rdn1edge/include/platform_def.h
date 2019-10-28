ca1ce9b62f8dd516711159fcf23094db88982ed4
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2019-10-28 14:49:48 +0530

    board/rdn1edge: add support for dual-chip configuration
    RD-N1-Edge based platforms can operate in dual-chip configuration
    wherein two rdn1edge SoCs are connected through a high speed coherent
    CCIX link.
    
    This patch adds a function to check if the RD-N1-Edge platform is
    operating in multi-chip mode by reading the SID register's NODE_ID
    value. If operating in multi-chip mode, initialize GIC-600 multi-chip
    operation by overriding the default GICR frames with array of GICR
    frames and setting the chip 0 as routing table owner.
    
    The address space of the second RD-N1-Edge chip (chip 1) starts from the
    address 4TB. So increase the physical and virtual address space size to
    43 bits to accommodate the multi-chip configuration. If the multi-chip
    mode configuration is detected, dynamically add mmap entry for the
    peripherals memory region of the second RD-N1-Edge SoC. This is required
    to let the BL31 platform setup stage to configure the devices in the
    second chip.
    
    The max chip count is increased to 2 and topology changes are added to
    represent the dual-chip configuration.
    
    In order the build the dual-chip platform, PLAT_ARM_CSS_NUM_CHIPS macro
    should be set to 2:
    export CROSS_COMPILE=<path-to-cross-compiler>
    make PLAT=rdn1edge BL31_CFLAGS+=-DPLAT_ARM_CSS_NUM_CHIPS=2 ARCH=aarch64 all
    
    Change-Id: I576cdaf71f0b0e41b9a9181fa4feb7091f8c7bb4
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>

--- plat/arm/board/rdn1edge/include/platform_def.h
+++ plat/arm/board/rdn1edge/include/platform_def.h
@@ -11,7 +11,7 @@
 
 #include <sgi_base_platform_def.h>
 
-#define CSS_SGI_MAX_CHIP_COUNT		U(1)
+#define CSS_SGI_MAX_CHIP_COUNT		U(2)
 #define PLAT_ARM_CLUSTER_COUNT		U(2)
 #define CSS_SGI_MAX_CPUS_PER_CLUSTER	U(4)
 #define CSS_SGI_MAX_PE_PER_CPU		U(1)
@@ -28,12 +28,15 @@
 
 #define PLAT_MAX_PWR_LVL		ARM_PWR_LVL1
 
+/* Virtual address used by dynamic mem_protect for chunk_base */
+#define PLAT_ARM_MEM_PROTEC_VA_FRAME	UL(0xc0000000)
+
 /*
  * Physical and virtual address space limits for MMU in AARCH64 & AARCH32 modes
  */
 #ifdef __aarch64__
-#define PLAT_PHY_ADDR_SPACE_SIZE	(1ULL << 36)
-#define PLAT_VIRT_ADDR_SPACE_SIZE	(1ULL << 36)
+#define PLAT_PHY_ADDR_SPACE_SIZE	(1ULL << 43)
+#define PLAT_VIRT_ADDR_SPACE_SIZE	(1ULL << 43)
 #else
 #define PLAT_PHY_ADDR_SPACE_SIZE	(1ULL << 32)
 #define PLAT_VIRT_ADDR_SPACE_SIZE	(1ULL << 32)
