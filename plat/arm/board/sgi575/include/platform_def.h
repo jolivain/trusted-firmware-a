22b63b5ed03c9d9e43beb1b604f59f9b8c4dbaa3
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2019-10-29 15:51:00 +0530

    plat/arm/sgi: add macros for maximum chip count
    In preparation to support multi-chip configurations, introduce
    CSS_SGI_MAX_CHIP_COUNT macro which defines maximum chip count supported
    by a platform.
    
    Change-Id: I145b73869cfdd7a58547930bdf00237a837e50d8
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>

--- plat/arm/board/sgi575/include/platform_def.h
+++ plat/arm/board/sgi575/include/platform_def.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2019, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2018-2020, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -11,6 +11,7 @@
 
 #include <sgi_base_platform_def.h>
 
+#define CSS_SGI_MAX_CHIP_COUNT		U(1)
 #define PLAT_ARM_CLUSTER_COUNT		U(2)
 #define CSS_SGI_MAX_CPUS_PER_CLUSTER	U(4)
 #define CSS_SGI_MAX_PE_PER_CPU		U(1)
