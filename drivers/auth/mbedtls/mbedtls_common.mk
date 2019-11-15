92e67bf906f404fa0d6a576a3332a50e6a1a9fd6
Sumit Garg <sumit.garg@linaro.org>
2019-11-15 10:43:00 +0530

    drivers: crypto: Add authenticated decryption framework
    Add framework for autheticated decryption of data. Currently this
    patch optionally imports mbedtls library as a backend if build option
    "DECRYPTION_SUPPORT = aes-gcm" is set to perform authenticated decryption
    using AES-GCM algorithm.
    
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
    Change-Id: I2966f0e79033151012bf4ffc66f484cd949e7271

--- drivers/auth/mbedtls/mbedtls_common.mk
+++ drivers/auth/mbedtls/mbedtls_common.mk
@@ -45,6 +45,10 @@ LIBMBEDTLS_SRCS		:= $(addprefix ${MBEDTLS_DIR}/library/,	\
 					rsa_internal.c				\
 					x509.c 					\
 					x509_crt.c 				\
+					gcm.c 					\
+					cipher.c 				\
+					cipher_wrap.c 				\
+					aes.c 					\
 					)
 
 # The platform may define the variable 'TF_MBEDTLS_KEY_ALG' to select the key
@@ -92,6 +96,13 @@ $(eval $(call add_define,TF_MBEDTLS_KEY_ALG_ID))
 $(eval $(call add_define,TF_MBEDTLS_KEY_SIZE))
 $(eval $(call add_define,TF_MBEDTLS_HASH_ALG_ID))
 
+ifeq (${DECRYPTION_SUPPORT}, aes-gcm)
+    TF_MBEDTLS_USE_AES_GCM	:=	1
+else
+    TF_MBEDTLS_USE_AES_GCM	:=	0
+endif
+
+$(eval $(call add_define,TF_MBEDTLS_USE_AES_GCM))
 
 $(eval $(call MAKE_LIB,mbedtls))
 
