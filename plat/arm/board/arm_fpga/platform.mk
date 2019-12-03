ae2e3c40e70cdc1ba3c20b1c3dffe899f13674c5
Oliver Swede <oli.swede@arm.com>
2019-12-03 14:08:21 +0000

    plat/arm/board/arm_fpga: Initialize the Generic Interrupt Controller
    This initializes the GIC using the Arm GIC drivers in TF-A.
    The initial FPGA image uses a GIC600 implementation, and so that its
    power controller is enabled, this platform port calls the corresponding
    implementation-specific routines.
    
    Signed-off-by: Oliver Swede <oli.swede@arm.com>
    Change-Id: I88d5a073eead4b653b1ca73273182cd98a95e4c5

--- plat/arm/board/arm_fpga/platform.mk
+++ plat/arm/board/arm_fpga/platform.mk
@@ -39,7 +39,10 @@ FPGA_CPU_LIBS		:=	lib/cpus/${ARCH}/aem_generic.S		\
 
 FPGA_GIC_SOURCES	:=	drivers/arm/gic/common/gic_common.c	\
 				drivers/arm/gic/v3/gicv3_main.c		\
-				plat/common/plat_gicv3.c
+				drivers/arm/gic/v3/gicv3_helpers.c	\
+				plat/common/plat_gicv3.c		\
+				plat/arm/board/arm_fpga/fpga_gicv3.c	\
+				drivers/arm/gic/v3/gic600.c
 
 PLAT_INCLUDES		:=	-Iplat/arm/board/arm_fpga/include
 
