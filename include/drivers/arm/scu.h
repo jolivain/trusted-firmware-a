bd86a858554b985b74bd0b9a723a13bf9e5ae425
Vishnu Banavath <vishnu.banavath@arm.com>
2019-12-13 17:18:15 +0000

    drivers: add a driver for snoop control unit
    Snoop Control Unit will enable to snoop on other CPUs caches.
    This is very important when it comes to synchronizing data between
    CPUs. As an example, there is a high chance that data might be
    cache'd and other CPUs can't see the change. In such cases,
    if snoop control unit is enabled, data is synchoronized immediately
    between CPUs and the changes are visible to other CPUs.
    
    This driver also enables user to know
    - number of CPUs present
    - is a particular CPU operating in SMP mode or AMP mode
    - data cache size of a particular CPU
    - does SCU has ACP port
    - is L2CPRESENT
    
    Change-Id: I0d977970154fa60df57caf449200d471f02312a0
    Signed-off-by: Vishnu Banavath <vishnu.banavath@arm.com>

---
+++ include/drivers/arm/scu.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCU_H
+#define SCU_H
+
+#include <stdint.h>
+
+#define SCU_CTRL_REG	0x00
+#define SCU_CFG_REG	0x04
+
+#define SCU_ENABLE_BIT	(1 << 0)
+
+typedef struct scu_conf_desc {
+	unsigned int num_cpus:2;
+	unsigned int cpu_smp:4;
+	unsigned int cpu0_data_cache_size:4;
+	unsigned int cpu1_data_cache_size:4;
+	unsigned int cpu2_data_cache_size:4;
+	unsigned int cpu3_data_cache_size:4;
+	/* accelerator coherrency port */
+	unsigned int acp_present:1;
+	/* Indicates the presence of a
+	 * PL310 L2 cache controller connected
+	 * to the processor
+	 */
+	unsigned int l2_cache_ctrl_present:1;
+} scu_conf_desc_t;
+
+void enable_snoop_ctrl_unit(uintptr_t base);
+scu_conf_desc_t *config_snoop_ctrl_unit(uintptr_t base);
+
+#endif /* SCU_H */
