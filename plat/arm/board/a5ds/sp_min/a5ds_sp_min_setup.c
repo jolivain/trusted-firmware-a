833112c721e17cc7d97f13170f741f08107d9961
Vishnu Banavath <vishnu.banavath@arm.com>
2019-12-13 17:18:15 +0000

    plat: a5ds: enable snoop control unit on power on
    Snoop control unit will enable to snoop on other CPU's caches.
    This is very important when it comes to synchronizing data between
    cpu's. As an example, there is a high chance that data might be
    cache'd and other CPU's can't see the change. In such cases,
    if snoop control unit is enabled, data is synchoronized immediately
    between CPU's and the changes are visible to other CPU's.
    
    Change-Id: I0d977970154fa60df57caf449200d471f02312a0
    Signed-off-by: Vishnu Banavath <vishnu.banavath@arm.com>

--- plat/arm/board/a5ds/sp_min/a5ds_sp_min_setup.c
+++ plat/arm/board/a5ds/sp_min/a5ds_sp_min_setup.c
@@ -4,12 +4,16 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <a5ds_private.h>
 #include <plat/arm/common/plat_arm.h>
 
 void plat_arm_sp_min_early_platform_setup(u_register_t arg0, u_register_t arg1,
 			u_register_t arg2, u_register_t arg3)
 {
 	arm_sp_min_early_platform_setup((void *)arg0, arg1, arg2, (void *)arg3);
+
+	/*enable snoop control unit */
+	a5ds_enable_scu();
 }
 
 /*
