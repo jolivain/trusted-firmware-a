87dd6ac040a9e02045d9049c71b4e1651729dd42
Madhukar Pappireddy <madhukar.pappireddy@arm.com>
2019-12-27 12:02:34 -0600

    fconf: Extract topology node properties from HW_CONFIG dtb
    1. Create a new node in the dts to reflect the topology of DynamIQ
       based FVP platform.
    2. Create, register( and implicitly invoke) fconf_populate_topology()
       function which extracts the properties from this topology node into
       the newly created fconf based configuration structure 'soc_topology'
    
    Change-Id: I9eb406da3ba4732008a66c01afec7c9fa8ef59bf
    Signed-off-by: Madhukar Pappireddy <madhukar.pappireddy@arm.com>

--- plat/arm/board/fvp/include/fconf_hw_config_getter.h
+++ plat/arm/board/fvp/include/fconf_hw_config_getter.h
@@ -12,13 +12,23 @@
 /* Hardware Config related getter */
 #define hw_config__gicv3_config_getter(prop) gicv3_config.prop
 
+#define hw_config__topology_getter(prop) soc_topology.prop
+
 struct gicv3_config_t {
 	void *gicd_base;
 	void *gicr_base;
 };
 
+struct hw_topology_t {
+	int plat_cluster_count;
+	int cluster_cpu_count;
+	int plat_cpu_count;
+	int plat_max_pwr_level;
+};
+
 int fconf_populate_gicv3_config(uintptr_t config);
 
 extern struct gicv3_config_t gicv3_config;
+extern struct hw_topology_t soc_topology;
 
 #endif /* FCONF_HW_CONFIG_GETTER_H */
