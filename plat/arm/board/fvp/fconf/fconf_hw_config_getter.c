5fa600d2c0712c7ee244145cab569f28f47df956
Madhukar Pappireddy <madhukar.pappireddy@arm.com>
2019-12-27 12:02:34 -0600

    fconf: Extract topology node properties from HW_CONFIG dtb
    1. Create a new node in the dts to reflect the topology of DynamIQ
       based FVP platform.
    2. Create, register( and implicitly invoke) fconf_populate_topology()
       function which extracts the properties from this topology node into
       the newly created fconf based configuration structure 'soc_topology'
    
    Change-Id: I9eb406da3ba4732008a66c01afec7c9fa8ef59bf
    Signed-off-by: Madhukar Pappireddy <madhukar.pappireddy@arm.com>

--- plat/arm/board/fvp/fconf/fconf_hw_config_getter.c
+++ plat/arm/board/fvp/fconf/fconf_hw_config_getter.c
@@ -13,6 +13,7 @@
 #include <plat/common/platform.h>
 
 struct gicv3_config_t gicv3_config;
+struct hw_topology_t soc_topology;
 
 int fconf_populate_gicv3_config(uintptr_t config)
 {
@@ -46,5 +47,86 @@ int fconf_populate_gicv3_config(uintptr_t config)
 	return err;
 }
 
+int fconf_populate_topology(uintptr_t config)
+{
+	int err = 0, cluster_count = 0, cpu_count = 0, total_cpu_count = 0;
+	int node, cluster_node, core_node;
+	const void *hw_config_dtb = (void *)config;
+
+	/* Assert the node offset point to "arm,psci-1.0" compatible property */
+	node = fdt_node_offset_by_compatible(hw_config_dtb, -1, "arm,psci-1.0");
+	if ( node < 0) {
+		ERROR("Unrecognized hardware configuration dtb (%d)\n", node);
+		panic();
+	}
+
+	err = fdtw_read_cells(hw_config_dtb, node, "max-pwr-lvl", 1,
+		&soc_topology.plat_max_pwr_level);
+	if (err < 0) {
+		ERROR("FCONF failed to read cluster-count property\n");
+		return err;
+	}
+
+	/* Find the offset of the "cpus" node */
+	node = fdt_path_offset(hw_config_dtb, "/cpus");
+	if ( node < 0) {
+		ERROR("Node 'cpus' not found in hardware configuration dtb \n");
+		panic();
+	}
+
+	/* Locate the cpu-map child node */
+	node = fdt_subnode_offset(hw_config_dtb, node, "cpu-map");
+	if ( node < 0) {
+		ERROR("Node 'cpu-map' not found in hardware configuration dtb \n");
+		panic();
+	}
+
+	/* A typical cpu-map node in a device tree is shown here for reference
+	cpu-map {
+		cluster0 {
+			core0 {
+				cpu = <&CPU0>;
+			};
+			core1 {
+				cpu = <&CPU1>;
+			};
+		};
+
+		cluster1 {
+			core0 {
+				cpu = <&CPU2>;
+			};
+			core1 {
+				cpu = <&CPU3>;
+			};
+		};
+	};
+	*/
+
+	int cpus_per_cluster[PLAT_ARM_CLUSTER_COUNT] = {0};
+
+	/* Iterate through cluster nodes */
+	fdt_for_each_subnode(cluster_node, hw_config_dtb, node) {
+		/* Iterate through core nodes */
+		fdt_for_each_subnode(core_node, hw_config_dtb, cluster_node) {
+			cpus_per_cluster[cluster_count]++;
+		}
+		INFO("CLUSTER ID: %d cpu-count: %d\n", cluster_count, cpus_per_cluster[cluster_count]);
+
+		/* Find the maximum number of cpus in any cluster */
+		if (cpu_count < cpus_per_cluster[cluster_count]) {
+			cpu_count = cpus_per_cluster[cluster_count];
+		}
+		cluster_count++;
+		total_cpu_count += cpus_per_cluster[cluster_count];
+	}
+
+	soc_topology.plat_cluster_count = cluster_count;
+	soc_topology.cluster_cpu_count = cpu_count;
+	soc_topology.plat_cpu_count = total_cpu_count;
+
+	return 0;
+}
 
 FCONF_REGISTER_POPULATOR(HW_CONFIG, gicv3_config, fconf_populate_gicv3_config);
+FCONF_REGISTER_POPULATOR(HW_CONFIG, topology, fconf_populate_topology);
