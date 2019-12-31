f4397c34102c53d8b241689d96783541d82ed6e5
Aditya Angadi <aditya.angadi@arm.com>
2019-12-31 14:23:53 +0530

    drivers/arm/scmi: allow use of multiple SCMI channels
    On systems that have multiple platform components that can interpret the
    SCMI messages, there is a need to support multiple SCMI channels (one
    each to those platform components). Extend the existing SCMI interface
    that currently supports only a single SCMI channel to support multiple
    SCMI channels.
    
    Change-Id: Ice4062475b903aef3b5e5bc37df364c9778a62c5
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

--- include/plat/arm/css/common/css_pm.h
+++ include/plat/arm/css/common/css_pm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2018, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2015-2020, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -43,5 +43,9 @@ int css_node_hw_state(u_register_t mpidr, unsigned int power_level);
  * a given index maps that core to an SCMI power domain.
  */
 extern const uint32_t plat_css_core_pos_to_scmi_dmn_id_map[];
+
+#define SCMI_DOMAIN_ID_MASK					0x0000FFFF
+#define SCMI_CHANNEL_ID_MASK				0xFFFF0000
+#define SCMI_CHANNEL_ID_SHIFT				U(16)
 
 #endif /* CSS_PM_H */
