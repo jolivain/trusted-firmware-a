2353bd44c1e8aaea561db42da0a2e7d4dc4a24de
Manish Pandey <manish.pandey2@arm.com>
2020-01-07 17:05:28 +0000

    SPM: modify sptool to generate individual SP blobs
    Currently sptool generates a single blob containing all the Secure
    Partitions, with latest SPM implementation, it is desirable to have
    individual blobs for each Secure Partition. It allows to leverage
    packaging and parsing of SP on existing FIP framework. It also allows
    SP packages coming from different sources.
    
    This patch modifies sptool so that it takes number of SP payload pairs
    as input and generates number of SP blobs instead of a single blob.
    
    Each SP blob will have its own header containing offests and sizes of
    different payloads along with a SP magic number and version. It is also
    associated in FIP with a UUID, provided by SP owner.
    
    Usage example:
    sptool -i sp1.bin:sp1.dtb -o sp1.pkg -i sp2.bin:sp2.dtb -o sp2.pkg ...
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: Ie2db8e601fa1d4182d0a1d22e78e9533dce231bc

--- include/tools_share/sptool.h
+++ include/tools_share/sptool.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Arm Limited. All rights reserved.
+ * Copyright (c) 2018-2020, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -9,21 +9,16 @@
 
 #include <stdint.h>
 
-/* Header for a secure partition package. There is one per package. */
-struct sp_pkg_header {
-	uint64_t version;
-	uint64_t number_of_sp;
-};
+#define SECURE_PARTITION_MAGIC		0x5A5A5A5A
 
-/*
- * Entry descriptor in a secure partition package. Each entry comprises a
- * secure partition and its resource description.
- */
-struct sp_pkg_entry {
-	uint64_t sp_offset;
-	uint64_t sp_size;
-	uint64_t rd_offset;
-	uint64_t rd_size;
+/* Header for a secure partition package. */
+struct sp_pkg_header {
+	uint32_t magic;
+	uint32_t version;
+	uint64_t img_offset;
+	uint64_t img_size;
+	uint64_t pm_offset;
+	uint64_t pm_size;
 };
 
 #endif /* SPTOOL_H */
