24cbb47145b4e4b5b25be48aa2abcec887c652c5
laurenw-arm <lauren.wehrmeister@arm.com>
2020-02-06 11:42:18 -0600

    fconf: Extract Timer properties from HW_CONFIG dtb
    Extract Timer properties from the timer and timer@2a810000 nodes in
    HW_CONFIG dtb. The first timer is a per-core architected timer attached
    to a GIC to deliver its per-processor interrupts via PPIs. The second
    timer is a memory mapped architected timer attached to a GIC to deliver
    its interrupts via SPIs.
    
    Signed-off-by: Lauren Wehrmeister <lauren.wehrmeister@arm.com>
    Change-Id: I2f4b27c48e4c79208dab9f03c768d9221ba6ca86

--- plat/arm/board/fvp/include/fconf_hw_config_getter.h
+++ plat/arm/board/fvp/include/fconf_hw_config_getter.h
@@ -11,8 +11,9 @@
 
 /* Hardware Config related getter */
 #define hw_config__gicv3_config_getter(prop) gicv3_config.prop
-
 #define hw_config__topology_getter(prop) soc_topology.prop
+#define hw_config__cpu_timer_getter(prop) soc_cpu_timer.prop
+#define hw_config__mm_timer_getter(prop) soc_mm_timer.prop
 
 struct gicv3_config_t {
 	uint64_t gicd_base;
@@ -37,10 +38,42 @@ struct hw_topology_t {
 	uint32_t plat_max_pwr_level;
 };
 
+struct cpu_timer_t {
+	uint32_t interrupt_stype;
+	uint32_t interrupt_snum;
+	uint32_t interrupt_sflags;
+	uint32_t interrupt_nstype;
+	uint32_t interrupt_nsnum;
+	uint32_t interrupt_nsflags;
+	uint32_t interrupt_vtype;
+	uint32_t interrupt_vnum;
+	uint32_t interrupt_vflags;
+	uint32_t interrupt_htype;
+	uint32_t interrupt_hnum;
+	uint32_t interrupt_hflags;
+	int clock_freq;
+};
+
+struct mm_timer_t {
+	uint64_t cframe_base;
+	uint64_t cframe_offset;
+	int clock_freq;
+	int frame_num;
+	uint32_t pinterrupt_type;
+	uint32_t pinterrupt_num;
+	uint32_t pinterrupt_flags;
+	uint64_t fframe_base;
+	uint64_t fframe_offset;
+};
+
 int fconf_populate_gicv3_config(uintptr_t config);
 int fconf_populate_topology(uintptr_t config);
+int fconf_populate_cpu_timer(uintptr_t config);
+int fconf_populate_mm_timer(uintptr_t mm_timer);
 
 extern struct gicv3_config_t gicv3_config;
 extern struct hw_topology_t soc_topology;
+extern struct cpu_timer_t cpu_timer;
+extern struct mm_timer_t mm_timer;
 
 #endif /* FCONF_HW_CONFIG_GETTER_H */
