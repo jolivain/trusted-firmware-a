e8cf3c681a0c4a495cd44a27de7d69a830d7d75c
Aditya Angadi <aditya.angadi@arm.com>
2020-02-11 16:05:58 +0530

    drivers/arm/scmi: fix always true comparison in coverity scan
    Change type of 'core_pos' variable from 'unsigned int' to 'int' to fix
    always true comparison as reported by coverity tool.
    
    Change-Id: I68a6406b5c4b0177195502fda5b4361387faa3a8
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

--- drivers/arm/css/scp/css_pm_scmi.c
+++ drivers/arm/css/scp/css_pm_scmi.c
@@ -214,8 +214,8 @@ void css_scp_off(const struct psci_power_state *target_state)
  */
 void css_scp_on(u_register_t mpidr)
 {
-	unsigned int lvl = 0, channel_id, core_pos, domain_id;
-	int ret;
+	unsigned int lvl = 0, channel_id, domain_id;
+	int core_pos, ret;
 	uint32_t scmi_pwr_state = 0;
 
 	for (; lvl <= PLAT_MAX_PWR_LVL; lvl++)
