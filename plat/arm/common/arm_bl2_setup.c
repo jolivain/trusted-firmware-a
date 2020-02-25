b087cf4f6a2c6636366a7aaca60538cd23b33ad0
Manish Pandey <manish.pandey2@arm.com>
2020-02-25 11:38:19 +0000

    SPMD: loading Secure Partition payloads
    This patch implements loading of Secure Partition packages using
    existing framework of loading other bl images.
    
    The current framework uses a statically defined array to store all the
    possible image types and at run time generates a link list and traverse
    through it to load different images.
    
    To load SPs, a new array of fixed size is introduced which will be
    dynamically populated based on number of SPs available in the system
    and it will be appended to the loadable images list.
    
    Change-Id: I8309f63595f2a71b28a73b922d20ccba9c4f6ae4
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>

--- plat/arm/common/arm_bl2_setup.c
+++ plat/arm/common/arm_bl2_setup.c
@@ -205,7 +205,16 @@ int arm_bl2_handle_post_image_load(unsigned int image_id)
  ******************************************************************************/
 int arm_bl2_plat_handle_post_image_load(unsigned int image_id)
 {
+#if defined(SPD_spmd)
+	/* For Secure Partitions we don't need post processing */
+	if (image_id >= (MAX_NUMBER_IDS - MAX_SP_IDS) && image_id < MAX_NUMBER_IDS) {
+		return 0;
+	} else {
+		return arm_bl2_handle_post_image_load(image_id);
+	}
+#else
 	return arm_bl2_handle_post_image_load(image_id);
+#endif
 }
 
 int bl2_plat_handle_post_image_load(unsigned int image_id)
