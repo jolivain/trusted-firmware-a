ad334acc1e4f692739dfd6b6e9be7009218d0415
Masahiro Yamada <yamada.masahiro@socionext.com>
2020-03-25 20:54:09 +0900

    linker_script: move more common code to bl_common.ld.h
    These are mostly used to collect data from special structure,
    and repeated in many linker scripts.
    
    To differentiate the alignment size, I added a new header linkage.h
    in aarch32/aarch64, and defined STRUCT_ALIGN in it.
    
    While I moved the PMF_SVC_DESCS, I dropped #if ENABLE_PMF conditional.
    As you can see in include/lib/pmf/pmf_helpers.h, PMF_REGISTER_SERVICE*
    are no-op when ENABLE_PMF=0. So, pmf_svc_descs and pmf_timestamp_array
    data are not populated.
    
    Change-Id: I3f4ab7fa18f76339f1789103407ba76bda7e56d0
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

---
+++ include/arch/aarch64/linkage.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef LINKAGE_H
+#define LINKAGE_H
+
+#define STRUCT_ALIGN	8
+
+#endif /* LINKAGE_H */
