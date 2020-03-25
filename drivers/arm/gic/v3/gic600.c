bdb58e4d1cfd7aea9f7680007ec4b4b2c2f86278
Andre Przywara <andre.przywara@arm.com>
2020-03-25 15:50:38 +0000

    GICv3: GIC-600: Detect GIC-600 at runtime
    The only difference between GIC-500 and GIC-600 relevant to TF-A is the
    differing power management sequence.
    A certain GIC implementation is detectable at runtime, for instance by
    checking the IIDR register. Let's add that test before initiating the
    GIC-600 specific sequence, so the code can be used on both GIC-600 and
    GIC-500 chips alike, without deciding on a GIC chip at compile time.
    
    Change-Id: I17ea97d9fb05874772ebaa13e6678b4ba3415557
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- drivers/arm/gic/v3/gic600.c
+++ drivers/arm/gic/v3/gic600.c
@@ -103,10 +103,7 @@ void gicv3_distif_post_restore(unsigned int proc_num)
 	arm_gicv3_distif_post_restore(proc_num);
 }
 
-/*
- * Power off GIC-600 redistributor
- */
-void gicv3_rdistif_off(unsigned int proc_num)
+static uintptr_t get_gicr_base(unsigned int proc_num)
 {
 	uintptr_t gicr_base;
 
@@ -117,8 +114,30 @@ void gicv3_rdistif_off(unsigned int proc_num)
 	gicr_base = gicv3_driver_data->rdistif_base_addrs[proc_num];
 	assert(gicr_base);
 
+	return gicr_base;
+}
+
+#define GICR_IIDR_PRODUCT_MASK		0xff000fff
+#define GICR_IIDR_PRODUCT_ARM_GIC_600	0x0200043b
+
+static bool gicv3_is_gic600(uintptr_t gicr_base)
+{
+	uint32_t reg = mmio_read_32(gicr_base + GICR_IIDR);
+
+	return (reg & GICR_IIDR_PRODUCT_MASK) == GICR_IIDR_PRODUCT_ARM_GIC_600;
+}
+
+/*
+ * Power off GIC-600 redistributor
+ */
+void gicv3_rdistif_off(unsigned int proc_num)
+{
+	uintptr_t gicr_base = get_gicr_base(proc_num);
+
 	/* Attempt to power redistributor off */
-	gic600_pwr_off(gicr_base);
+	if (gicv3_is_gic600(gicr_base)) {
+		gic600_pwr_off(gicr_base);
+	}
 }
 
 /*
@@ -126,15 +145,10 @@ void gicv3_rdistif_off(unsigned int proc_num)
  */
 void gicv3_rdistif_on(unsigned int proc_num)
 {
-	uintptr_t gicr_base;
-
-	assert(gicv3_driver_data);
-	assert(proc_num < gicv3_driver_data->rdistif_num);
-	assert(gicv3_driver_data->rdistif_base_addrs);
-
-	gicr_base = gicv3_driver_data->rdistif_base_addrs[proc_num];
-	assert(gicr_base);
+	uintptr_t gicr_base = get_gicr_base(proc_num);
 
 	/* Power redistributor on */
-	gic600_pwr_on(gicr_base);
+	if (gicv3_is_gic600(gicr_base)) {
+		gic600_pwr_on(gicr_base);
+	}
 }
