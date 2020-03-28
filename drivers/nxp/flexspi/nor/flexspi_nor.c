baaa74a4f7828d5b482008284ae4c372c7011ad2
Ruchika Gupta <ruchika.gupta@nxp.com>
2020-03-28 08:54:13 +0000

    nxp: Add flex-spi controller driver for QorIQ Layerscape platforms
    Change-Id: Idaea11cd1734010011bf91879ae1f229e4806888
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>

---
+++ drivers/nxp/flexspi/nor/flexspi_nor.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include <platform_def.h>
+#include <assert.h>
+#include <common/debug.h>
+#include <plat_common.h>
+#include <mmio.h>
+#include <lib/xlat_tables/xlat_tables_v2.h>
+
+int flexspi_init(void)
+{
+	return 0;
+}
+
+int flexspi_nor_io_setup(void)
+{
+
+	flexspi_init();
+	/* Adding NOR Memory Map in XLAT Table */
+	mmap_add_region(NXP_FLEXSPI_FLASH_ADDR, NXP_FLEXSPI_FLASH_ADDR,
+			NXP_FLEXSPI_FLASH_SIZE, MT_MEMORY | MT_RW);
+
+	return plat_io_memmap_setup(NXP_FLEXSPI_FLASH_ADDR + PLAT_FIP_OFFSET);
+}
