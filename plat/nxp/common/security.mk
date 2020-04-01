0e185b7a6d99be13a8d911272f8fc38c29991078
Pankaj Gupta <pankaj.gupta@nxp.com>
2020-04-01 22:41:09 +0530

    nxp: Add support for QorIQ LayerscapePlatform lx2160a/lx2162a
    The patch series establishes common bootflow across NXP
    QorIQ Layerscape ARMv8 platforms where ROM loads BL2
    in SoC Internal Memory. BL2 binary runs in EL3 mode and
    further loads BL31, BL32 and BL33.
    
    Following feature set are supported:
            - LX2160A RDB
            - Boot from Flex-SPI NOR flash
    	- Support for complete PSCI functionality
    	- OPTEE Boot
    
    Signed-off-by: rocket <rod.dorris@nxp.com>
    Signed-off-by: York Sun <york.sun@nxp.com>
    Signed-off-by: Ruchika Gupta <ruchika.gupta@nxp.com>
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Change-Id: I1711999be5ffc8ca99fccc8c96ee541587aae674

---
+++ plat/nxp/common/security.mk
@@ -0,0 +1,27 @@
+#
+# Copyright 2018-2020 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#------------------------------------------------------------------------------
+#
+# Select the SECURITY files
+#
+# -----------------------------------------------------------------------------
+
+ifeq ($(TZC_ID), TZC400)
+SECURITY_SOURCES 	+=	drivers/arm/tzc/tzc400.c
+else ifeq ($(TZC_ID), NONE)
+    $(info -> No TZC present on platform)
+else
+    $(error -> TZC type not set!)
+endif
+
+
+#GET SEC_SOURCES defined
+include $(PLAT_DRIVERS_PATH)/fsl_sec/sec.mk
+
+SECURITY_SOURCES	+= 	${SEC_SOURCES}
+
+# -----------------------------------------------------------------------------
+
