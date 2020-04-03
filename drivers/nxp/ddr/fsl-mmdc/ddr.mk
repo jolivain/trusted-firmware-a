f9f0f7ac37a773c49613bedf7571634942f8b1bc
York Sun <york.sun@nxp.com>
2020-04-03 16:01:55 +0530

    nxp: add generic driver for Freescale MMDC(Multi Mode DDR Controller)
    Signed-off-by: Rajesh Bhagat <rajesh.bhagat@nxp.com>
    Signed-off-by: York Sun <york.sun@nxp.com>
    Change-Id: I20543abe64c3870d2537b40f872a1b0f1df68e3f

---
+++ drivers/nxp/ddr/fsl-mmdc/ddr.mk
@@ -0,0 +1,16 @@
+#
+# Copyright 2017-2020 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-----------------------------------------------------------------------------
+
+# MMDC ddr cntlr driver files
+
+DDR_DRIVERS_PATH	:=	drivers/nxp/ddr/fsl-mmdc
+
+DDR_CNTLR_SOURCES  = ${DDR_DRIVERS_PATH}/fsl_mmdc.c
+DDR_CNTLR_H  = fsl_mmdc.h
+
+#------------------------------------------------
+
