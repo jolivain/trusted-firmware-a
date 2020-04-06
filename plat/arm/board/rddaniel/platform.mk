6ef738041abce31117e3717227aa50e194c5c6fc
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2020-04-06 17:54:42 +0530

    plat/arm/rddaniel: set GIC_VERSION to 4
    RD-Daniel uses GIC-Clayton as its interrupt controller which is an
    implementation of GICv4.1 architecture. Hence for RD-Daniel, set
    GIC_VERSION macro to 4.
    
    Change-Id: I45ae8c82376f8fe8fc0666306822ae2db74e71b8
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>

--- plat/arm/board/rddaniel/platform.mk
+++ plat/arm/board/rddaniel/platform.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
+# RD-Daniel platform uses GIC-Clayton which is based on GICv4.
+GIC_VERSION		:=	4
+
 include plat/arm/css/sgi/sgi-common.mk
 
 RDDANIEL_BASE		=	plat/arm/board/rddaniel
