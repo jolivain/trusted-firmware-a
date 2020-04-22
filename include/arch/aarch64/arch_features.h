e90906a87b338cfba2e1b253d39cf30a4beaca46
johpow01 <john.powell@arm.com>
2020-04-22 14:05:13 -0500

    Enable v8.6 WFE trap delays
    This patch enables the v8.6 extension to add a delay before WFE traps
    are taken. A weak hook plat_arm_set_twedel_scr_el3 has been added in
    plat/common/aarch64/plat_common.c that disables this feature by default
    but platform-specific code can override it when needed.
    
    The only hook provided sets the TWED fields in SCR_EL3, there are similar
    fields in HCR_EL2, SCTLR_EL2, and SCTLR_EL1 to control WFE trap delays in
    lower ELs but these should be configured by code running at EL2 and/or EL1
    depending on the platform configuration and is outside the scope of TF-A.
    
    Signed-off-by: John Powell <john.powell@arm.com>
    Change-Id: I0a9bb814205efeab693a3d0a0623e62144abba2d

--- include/arch/aarch64/arch_features.h
+++ include/arch/aarch64/arch_features.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Arm Limited. All rights reserved.
+ * Copyright (c) 2020, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -50,6 +50,12 @@ static inline unsigned int get_armv8_5_mte_support(void)
 {
 	return ((read_id_aa64pfr1_el1() >> ID_AA64PFR1_EL1_MTE_SHIFT) &
 		ID_AA64PFR1_EL1_MTE_MASK);
+}
+
+static inline bool is_armv8_6_twed_present(void)
+{
+	return (((read_id_aa64mmfr1_el1() >> ID_AA64MMFR1_EL1_TWED_SHIFT) &
+		ID_AA64MMFR1_EL1_TWED_MASK) == ID_AA64MMFR1_EL1_TWED_SUPPORTED);
 }
 
 #endif /* ARCH_FEATURES_H */
