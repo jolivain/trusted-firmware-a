95e6faa4fa7bcae6e197c975693e2bd4c8339867
Saurabh Gorecha <sgorecha@codeaurora.org>
2020-04-22 21:31:24 +0530

    sc7180 platform support
    Adding support for QTI CHIP SC7180 on ATF
    
    Change-Id: I0d82d3a378036003fbd0bc4784f61464bb76ea82
    Signed-off-by: Saurabh Gorecha <sgorecha@codeaurora.org>
    Co-authored-by: Maulik Shah <mkshah@codeaurora.org>

---
+++ plat/qti/common/inc/qti_cpu.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __QTI_CPU_H__
+#define __QTI_CPU_H__
+
+/* KRYO-4xx Gold MIDR */
+#define QTI_KRYO4_GOLD_MIDR	0x517F804D
+
+/* KRYO-4xx Silver MIDR */
+#define QTI_KRYO4_SILVER_MIDR	0x517F805D
+
+#endif /* __QTI_CPU_H__ */
