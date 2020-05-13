1b71461210aec6e017b44d18f93278aad9062900
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2020-05-13 14:09:58 +0100

    FPGA Support: Add support for topology self-discovery
    As secondary cores show up, they populate the topology
    tree by increasing the count of cores for the cluster
    where they belong. This has to be done after the primary
    core has initialized the C runtime.
    
    The primary core will be responsible for initializing
    the count of clusters as well as power domains.
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I8fd64761a2296714ce0f37c46544f3e6f13b5f61

--- plat/arm/board/arm_fpga/fpga_topology.c
+++ plat/arm/board/arm_fpga/fpga_topology.c
@@ -9,11 +9,25 @@
 #include "fpga_private.h"
 #include <platform_def.h>
 
-static unsigned char fpga_power_domain_tree_desc[FPGA_MAX_CLUSTER_COUNT + 2];
+#include <common/debug.h>
+#include <lib/spinlock.h>
+
+unsigned char fpga_power_domain_tree_desc[PLATFORM_CORE_COUNT + 2];
+unsigned char fpga_cpu_topology_tree[FPGA_MAX_CLUSTER_COUNT * \
+				     FPGA_MAX_CPUS_PER_CLUSTER];
+unsigned char fpga_max_cpu_affvl;
+unsigned char fpga_max_pe_affvl;
+
+spinlock_t topology_tree_spinlock;
+
+void fpga_update_affinity_boundaries(void);
 
 const unsigned char *plat_get_power_domain_tree_desc(void)
 {
-	int i;
+	unsigned int i, cluster_count;
+	void *src;
+	void *dst;
+
 	/*
 	* The highest level is the system level. The next level is constituted
 	* by clusters and then cores in clusters.
@@ -22,11 +36,66 @@ const unsigned char *plat_get_power_domain_tree_desc(void)
 	* indices returned by the plat_core_pos_by_mpidr() and plat_my_core_pos()
 	* APIs.
 	*/
-	fpga_power_domain_tree_desc[0] = 1;
-	fpga_power_domain_tree_desc[1] = FPGA_MAX_CLUSTER_COUNT;
+	if (fpga_power_domain_tree_desc[0] == 0U) {
+		/*
+		 * As fpga_power_domain_tree_desc[0] == 0, assume that the
+		 * first two levels have not been initialized and neither has
+		 * the primary CPU information, so perform the final
+		 * initialization here.
+		 */
 
-	for (i = 0; i < FPGA_MAX_CLUSTER_COUNT; i++) {
-		fpga_power_domain_tree_desc[i + 2] = FPGA_MAX_CPUS_PER_CLUSTER;
+		spin_lock(&topology_tree_spinlock);
+		dmbsy(); /* Ensure all writes by other cores are commited. */
+
+		fpga_power_domain_tree_desc[0] = 1;
+
+		/* Get the cluster to which this CPU belongs */
+		i = fpga_get_cpu_affinity(2);
+
+		if (i >= FPGA_MAX_CLUSTER_COUNT) {
+			panic();
+		}
+		if (++fpga_power_domain_tree_desc[i + 2] >=
+		    FPGA_MAX_CPUS_PER_CLUSTER) {
+			panic();
+		}
+
+		/*
+		 * Update the thread count on the current CPU
+		 */
+		fpga_cpu_topology_tree[FPGA_MAX_CLUSTER_COUNT * i +
+					fpga_get_cpu_affinity(1)]++;
+		/*
+		 * Find out the number of clusters on the system by walking
+		 * through the array of clusters and assuming that they are not
+		 * sparse, so as soon as we find an empty one, stop.
+		 */
+		for (i = 0; i < FPGA_MAX_CLUSTER_COUNT; i++) {
+			if (fpga_power_domain_tree_desc[i + 2] == 0U) {
+				break;
+			}
+			fpga_power_domain_tree_desc[1]++;
+		}
+		if (fpga_power_domain_tree_desc[1] == 0U) {
+			panic();
+		}
+
+		/*
+		 * Populate the rest of the topology tree whith the threads per
+		 * core
+		 */
+		cluster_count = fpga_power_domain_tree_desc[1];
+		for (i = 0; i < cluster_count; i++) {
+			src = (void *)&fpga_cpu_topology_tree[
+						FPGA_MAX_CPUS_PER_CLUSTER * i];
+			dst = (void *)&fpga_power_domain_tree_desc[
+						2 + cluster_count + i];
+			memcpy(dst, src, fpga_power_domain_tree_desc[2 + i]);
+		}
+
+		fpga_update_affinity_boundaries();
+
+		spin_unlock(&topology_tree_spinlock);
 	}
 
 	return fpga_power_domain_tree_desc;
@@ -36,10 +105,10 @@ int plat_core_pos_by_mpidr(u_register_t mpidr)
 {
 	unsigned int cluster_id, cpu_id, thread_id;
 
-	mpidr &= MPIDR_AFFINITY_MASK;
-	if (mpidr & ~(MPIDR_CLUSTER_MASK | MPIDR_CPU_MASK)) {
-		return -1;
-	}
+	/* Ensure that the power domain tree is updated */
+	(void)plat_get_power_domain_tree_desc();
+
+	mpidr &= (MPIDR_AFFINITY_MASK | MPIDR_MT_MASK);
 
 	if (mpidr & MPIDR_MT_MASK) {
 		thread_id = MPIDR_AFFLVL0_VAL(mpidr);
@@ -50,11 +119,11 @@ int plat_core_pos_by_mpidr(u_register_t mpidr)
 	cpu_id = MPIDR_AFFLVL1_VAL(mpidr);
 	cluster_id = MPIDR_AFFLVL2_VAL(mpidr);
 
-	if (cluster_id >= FPGA_MAX_CLUSTER_COUNT) {
+	if (cluster_id >= fpga_power_domain_tree_desc[1]) {
 		return -1;
-	} else if (cpu_id >= FPGA_MAX_CPUS_PER_CLUSTER) {
+	} else if (cpu_id > fpga_max_cpu_affvl) {
 		return -1;
-	} else if (thread_id >= FPGA_MAX_PE_PER_CPU) {
+	} else if (thread_id > fpga_max_pe_affvl) {
 		return -1;
 	}
 
