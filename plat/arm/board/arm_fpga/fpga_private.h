6fdd2c174a6fe0e3e073d8f6fae495a98c6600ba
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2020-05-13 14:09:58 +0100

    FPGA Support: Add support for topology self-discovery
    As secondary cores show up, they populate the topology
    tree by increasing the count of cores for the cluster
    where they belong. This has to be done after the primary
    core has initialized the C runtime.
    
    The primary core will be responsible for initializing
    the count of clusters as well as power domains.
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I8fd64761a2296714ce0f37c46544f3e6f13b5f61

--- plat/arm/board/arm_fpga/fpga_private.h
+++ plat/arm/board/arm_fpga/fpga_private.h
@@ -7,6 +7,14 @@
 #ifndef FPGA_PRIVATE_H
 #define FPGA_PRIVATE_H
 
+#include "../fpga_def.h"
+#include <platform_def.h>
+
+#define C_RUNTIME_READY_KEY (0xaa55aa55)
+
+#ifndef __ASSEMBLER__
+extern unsigned char fpga_power_domain_tree_desc[PLATFORM_CORE_COUNT + 2];
+
 unsigned int plat_fpga_calc_core_pos(u_register_t mpidr);
 
 void fpga_console_init(void);
@@ -14,5 +22,7 @@ void fpga_console_init(void);
 void plat_fpga_gic_init(void);
 void fpga_pwr_gic_on_finish(void);
 void fpga_pwr_gic_off(void);
+unsigned int fpga_get_cpu_affinity(unsigned int);
+#endif /* __ASSEMBLER__ */
 
-#endif
+#endif /* FPGA_PRIVATE_H */
