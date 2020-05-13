28fb387b1a596d092b683cd3d01e8dc5214ef145
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2020-05-13 14:09:58 +0100

    FPGA Support: Add support for topology self-discovery
    As secondary cores show up, they populate the topology
    tree by increasing the count of CPUs for the cluster
    where they belong as well as the thread count.
    This has to be done after the primary CPU has
    initialized the C runtime.
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I8fd64761a2296714ce0f37c46544f3e6f13b5f61

--- plat/arm/board/arm_fpga/fpga_topology.c
+++ plat/arm/board/arm_fpga/fpga_topology.c
@@ -9,11 +9,88 @@
 #include "fpga_private.h"
 #include <platform_def.h>
 
-static unsigned char fpga_power_domain_tree_desc[FPGA_MAX_CLUSTER_COUNT + 2];
+#include <common/debug.h>
+#include <lib/spinlock.h>
+
+unsigned char fpga_power_domain_tree_desc[PLATFORM_CORE_COUNT + 2];
+uint32_t fpga_mpids[PLATFORM_CORE_COUNT];
+
+spinlock_t topology_tree_spinlock;
+
+bool plat_is_my_cpu_primary(void);
+unsigned int plat_fpga_calc_provisional_core_pos(uint32_t mpid);
+int get_cpuid_by_mpidr(u_register_t mpidr);
+
+static void fpga_generate_power_topology_tree(void)
+{
+	/*
+	 * Iterates over fpga_mpids to generate the power domain tree
+	 * description.
+	 */
+
+	int max_cluster = -1;
+	unsigned int cluster_count, i, j;
+
+	/* First pass: identify clusters and cores on the tree */
+	for (i = 0; i < PLATFORM_CORE_COUNT; i++) {
+		unsigned int cluster;
+
+		if (!(fpga_mpids[i] & DISCOVERED_MPID_MASK)) {
+			continue;
+		}
+
+		cluster = MPIDR_AFFLVL2_VAL(fpga_mpids[i]);
+
+		if ((int)cluster > max_cluster) {
+			max_cluster = cluster;
+			if (++fpga_power_domain_tree_desc[1] >
+						FPGA_MAX_CLUSTER_COUNT) {
+				panic();
+			}
+		}
+
+		if (++fpga_power_domain_tree_desc[2 + cluster] >
+						FPGA_MAX_CPUS_PER_CLUSTER) {
+			panic();
+		}
+	}
+
+	/* Second pass: add the threads for each core on the tree */
+	cluster_count = fpga_power_domain_tree_desc[1];
+	j = 0; /* CPUs on previous cluster */
+	for (i = 0; i < PLATFORM_CORE_COUNT; i++) {
+		unsigned int cpu, cluster;
+
+		if (!(fpga_mpids[i] & DISCOVERED_MPID_MASK)) {
+			continue;
+		}
+
+		cpu = MPIDR_AFFLVL1_VAL(fpga_mpids[i]);
+		cluster = MPIDR_AFFLVL2_VAL(fpga_mpids[i]);
+
+		/*
+		 * Get the position of the PE count for the current cluster:
+		 * - 2: Index on the tree where afflvl1 starts (CPUs per
+		 *      cluster)
+		 * - cluster_count: Afflvl0 description is adjactent to afflvl1
+		 * - j: Afflvl0 for the current cluster is shifted by the
+		 *      number of CPUs (afflvl1) on the prvious one.
+		 * - CPU: CPU shift on the current cluster.
+		 */
+		fpga_power_domain_tree_desc[2 + cluster_count + j + cpu]++;
+		if (fpga_power_domain_tree_desc[2 + cluster_count + cpu] >
+							FPGA_MAX_PE_PER_CPU) {
+			panic();
+		}
+		j = fpga_power_domain_tree_desc[2 + cluster];
+	}
+}
 
 const unsigned char *plat_get_power_domain_tree_desc(void)
 {
-	int i;
+	uint64_t index;
+	uint32_t mpidr;
+
 	/*
 	* The highest level is the system level. The next level is constituted
 	* by clusters and then cores in clusters.
@@ -21,53 +98,56 @@ const unsigned char *plat_get_power_domain_tree_desc(void)
 	* This description of the power domain topology is aligned with the CPU
 	* indices returned by the plat_core_pos_by_mpidr() and plat_my_core_pos()
 	* APIs.
+	*
+	* A description of the topology tree can be found at
+	* https://trustedfirmware-a.readthedocs.io/en/latest/design/psci-pd-tree.html#design
 	*/
-	fpga_power_domain_tree_desc[0] = 1;
-	fpga_power_domain_tree_desc[1] = FPGA_MAX_CLUSTER_COUNT;
 
-	for (i = 0; i < FPGA_MAX_CLUSTER_COUNT; i++) {
-		fpga_power_domain_tree_desc[i + 2] = FPGA_MAX_CPUS_PER_CLUSTER * FPGA_MAX_PE_PER_CPU;
+	/* To avoid concurrent calls whilst the tree is being generated */
+	spin_lock(&topology_tree_spinlock);
+
+	if (fpga_power_domain_tree_desc[0] == 0U) {
+		/*
+		 * As fpga_power_domain_tree_desc[0] == 0, assume that the
+		 * Power Domain Topology Tree has not been initialized, so
+		 * perform the initialization here.
+		 */
+
+		if (!plat_is_my_cpu_primary()) {
+			/* Only the primary CPU can build the tree */
+			panic();
+		}
+
+		dsbsy(); /* Ensure all writes by other cores are commited. */
+
+		mpidr = (read_mpidr_el1() & MPID_MASK &
+				~(MPIDR_AFFLVL_MASK << MPIDR_AFF3_SHIFT));
+
+		/* Add this core to the mpids list */
+		index = plat_fpga_calc_provisional_core_pos(mpidr);
+		fpga_mpids[index] = mpidr | DISCOVERED_MPID_MASK;
+
+		fpga_generate_power_topology_tree();
+
+		fpga_power_domain_tree_desc[0] = 1;
 	}
+
+	spin_unlock(&topology_tree_spinlock);
 
 	return fpga_power_domain_tree_desc;
 }
 
 int plat_core_pos_by_mpidr(u_register_t mpidr)
 {
-	unsigned int cluster_id, cpu_id, thread_id;
-
 	/*
-	 * The image running on the FPGA may or may not implement
-	 * multithreading, and it shouldn't be assumed this is consistent
-	 * across all CPUs.
-	 * This ensures that any passed mpidr values reflect the status of the
-	 * primary CPU's MT bit.
+	 * Check if the topology three has been initialized. If not, initialize
+	 * it, as it is needed to get the valid MPIDR array
 	 */
-	mpidr |= (read_mpidr_el1() & MPIDR_MT_MASK);
-	mpidr &= MPID_MASK;
-
-	if (mpidr & MPIDR_MT_MASK) {
-		thread_id = MPIDR_AFFLVL0_VAL(mpidr);
-		cpu_id = MPIDR_AFFLVL1_VAL(mpidr);
-		cluster_id = MPIDR_AFFLVL2_VAL(mpidr);
-	} else {
-		thread_id = 0;
-		cpu_id = MPIDR_AFFLVL0_VAL(mpidr);
-		cluster_id = MPIDR_AFFLVL1_VAL(mpidr);
+	if (fpga_power_domain_tree_desc[0] == 0) {
+		(void)plat_get_power_domain_tree_desc();
 	}
 
-	if (cluster_id >= FPGA_MAX_CLUSTER_COUNT) {
-		return -1;
-	}
-
-	if (cpu_id >= FPGA_MAX_CPUS_PER_CLUSTER) {
-		return -1;
-	}
-
-	if (thread_id >= FPGA_MAX_PE_PER_CPU) {
-		return -1;
-	}
-
-	/* Calculate the correct core, catering for multi-threaded images */
-	return (int) plat_fpga_calc_core_pos(mpidr);
+	int retval = get_cpuid_by_mpidr(mpidr);
+	NOTICE("MPIDR[0x%x] --> ID[%i]\n", (unsigned int)mpidr, retval);
+	return retval;
 }
