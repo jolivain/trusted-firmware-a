f35619b0cf0ddd8bd0aa4c03d65452d6a54760ab
Jan Kiszka <jan.kiszka@siemens.com>
2020-05-17 11:12:07 +0200

    ti: k3: Add support for IOT2050 board
    The only difference to the generic K3 board is the output UART.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Change-Id: Ifb85e9ec5e1f44c55a0ab85c2fe9d2ca9adb18ff

---
+++ plat/ti/k3/board/iot2050/board.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (c) Siemens AG, 2020
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+PLAT_INCLUDES += -Iplat/ti/k3/board/iot2050/include
+
+include plat/ti/k3/board/generic/board.mk
---
+++ plat/ti/k3/board/iot2050/include/board_def.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Siemens AG, 2020
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef BOARD_DEF_H
+#include_next <board_def.h>
+
+#define K3_USART_BASE			0x02810000
+
+#endif /* BOARD_DEF_H */
