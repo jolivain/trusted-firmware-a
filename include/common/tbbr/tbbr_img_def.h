52dcb18552144bc8dc5cf7ccc5b7b37ebe18066a
Manish Pandey <manish.pandey2@arm.com>
2020-05-27 22:40:10 +0100

    dualroot: add chain of trust for secure partitions
    A new certificate "sip-sp-cert" has been added for Silicon Provider(SiP)
    owned Secure Partitions(SP). A similar support for Platform owned SP can
    be added in future. The certificate is also protected against anti-
    rollback using the trusted Non-Volatile counter.
    
    To avoid deviating from TBBR spec, support for SP CoT is only provided
    in dualroot.
    Secure Partition content certificate is assigned image ID 31 and SP
    images follows after it.
    
    The CoT for secure partition look like below.
    +------------------+       +-------------------+
    | ROTPK/ROTPK Hash |------>| Trusted Key       |
    +------------------+       | Certificate       |
                               | (Auth Image)      |
                              /+-------------------+
                             /                   |
                            /                    |
                           /                     |
                          /                      |
                         L                       v
    +------------------+       +-------------------+
    | Trusted World    |------>| SiP owned SPs     |
    | Public Key       |       | Content Cert      |
    +------------------+       | (Auth Image)      |
                            /   +-------------------+
                           /                      |
                          /                      v|
    +------------------+ L     +-------------------+
    | SP_PKG1 Hash     |------>| SP_PKG1           |
    |                  |       | (Data Image)      |
    +------------------+       +-------------------+
            .                           .
            .                           .
            .                           .
    +------------------+       +-------------------+
    | SP_PKG8 Hash     |------>| SP_PKG8           |
    |                  |       | (Data Image)      |
    +------------------+       +-------------------+
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: Ia31546bac1327a3e0b5d37e8b99c808442d5e53f

--- include/common/tbbr/tbbr_img_def.h
+++ include/common/tbbr/tbbr_img_def.h
@@ -7,6 +7,14 @@
 #ifndef TBBR_IMG_DEF_H
 #define TBBR_IMG_DEF_H
 
+#if defined(SPD_spmd)
+#define SP_CONTENT_CERT_ID		U(31)
+#define MAX_SP_IDS			U(8)
+#define SPM_IMAGES			(MAX_SP_IDS + U(1))
+#else
+#define SPM_IMAGES			0
+#endif
+
 #include <export/common/tbbr/tbbr_img_def_exp.h>
 
 #endif /* TBBR_IMG_DEF_H */
