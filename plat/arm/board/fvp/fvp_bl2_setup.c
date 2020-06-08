90ade44690b0a88fc44f290b757791214fac6d68
Alexei Fedorov <Alexei.Fedorov@arm.com>
2020-06-08 19:17:59 +0100

    TF-A FVP: Add support for Measured Boot
    This patch adds support for Measured Boot functionality
    to FVP platform code.
    
    Change-Id: I087e1423afcb269d6cfe79c1af9c348931991292
    Signed-off-by: Alexei Fedorov <Alexei.Fedorov@arm.com>

--- plat/arm/board/fvp/fvp_bl2_setup.c
+++ plat/arm/board/fvp/fvp_bl2_setup.c
@@ -4,7 +4,18 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <common/debug.h>
+
+#if MEASURED_BOOT
+#include <common/desc_image_load.h>
+#endif
+
 #include <drivers/arm/sp804_delay_timer.h>
+
+#if MEASURED_BOOT
+#include <drivers/measured_boot/measured_boot.h>
+#endif
+
 #include <plat/arm/common/plat_arm.h>
 #include <plat/common/platform.h>
 #include <platform_def.h>
@@ -26,3 +37,30 @@ void bl2_platform_setup(void)
 	/* Initialize System level generic or SP804 timer */
 	fvp_timer_init();
 }
+
+#if MEASURED_BOOT
+int fvp_bl2_plat_handle_post_image_load(unsigned int image_id)
+{
+	bl_mem_params_node_t *bl_mem_params = get_bl_mem_params_node(image_id);
+	image_info_t image_info = bl_mem_params->image_info;
+	int err;
+
+	if ((image_info.h.attr & IMAGE_ATTRIB_SKIP_LOADING) == 0) {
+		/* Calculate image hash and record data in Event Log */
+		err = tpm_record_measurement(image_info.image_base,
+					     image_info.image_size, image_id);
+		if (err != 0) {
+			ERROR("BL2: Failed to record image id %u (%i)\n",
+							image_id, err);
+			return err;
+		}
+	}
+
+	return arm_bl2_handle_post_image_load(image_id);
+}
+
+int arm_bl2_plat_handle_post_image_load(unsigned int image_id)
+{
+	return fvp_bl2_plat_handle_post_image_load(image_id);
+}
+#endif	/* MEASURED_BOOT */
