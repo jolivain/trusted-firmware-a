b5698e3e41ea9c11a58480a4d98054ff40d61311
James Liao <jamesjj.liao@mediatek.com>
2020-06-15 16:41:03 +0800

    mediatek: mt8192: Add MCDI drivers
    Add MCDI related drivers to handle CPU powered on/off in CPU suspend.
    
    Change-Id: I5110461e8eef86f8383b45f197ec5cb10dbfeb3e
    Signed-off-by: James Liao <jamesjj.liao@mediatek.com>

---
+++ plat/mediatek/mt8192/drivers/mcdi/mt_mcdi.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __MT_MCDI_H__
+#define __MT_MCDI_H__
+
+int mcdi_try_init(void);
+
+#endif
