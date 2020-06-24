0f21722ce6674cb32dd3eb8fc537ff45cc64d95a
Jack Bond-Preston <jack.bond-preston@arm.com>
2020-06-24 13:43:20 +0100

    CMake: Add support for enabling RAS on FVP platform
    Allows enabling of RAS extension, by handling RAS_EXTENSION and
    EL3_EXCEPTION_HANDLING config flags.
    Signed-off-by: Jack Bond-Preston <jack.bond-preston@arm.com>
    Change-Id: I2a8ee305620238c85dfa5c8e054f3a7309c51c5a

--- plat/arm/common/arm_common.cmake
+++ plat/arm/common/arm_common.cmake
@@ -75,10 +75,17 @@ stgt_add_src(NAME bl31 SRC
 	${TFA_ROOT_DIR}/plat/arm/common/arm_topology.c
 	${TFA_ROOT_DIR}/plat/common/plat_psci_common.c
 
-	#TODO: if EL3_EXCEPTION_HANDLING=1
 	#TODO: if SDEI_SUPPORT=1
-	#TODO: if RAS_EXTENSION=1
 	#TODO: if ENABLE_SPM=1
+)
+
+stgt_add_src_cond(NAME bl31 KEY RAS_EXTENSION VAL 1 SRC
+	${TFA_ROOT_DIR}/lib/extensions/ras/std_err_record.c
+	${TFA_ROOT_DIR}/lib/extensions/ras/ras_common.c
+)
+
+stgt_add_src_cond(NAME bl31 KEY EL3_EXCEPTION_HANDLING VAL 1 SRC
+	${TFA_ROOT_DIR}/plat/arm/common/aarch64/arm_ehf.c
 )
 
 stgt_add_src_cond(NAME bl31 KEY ENABLE_PMF VAL 1 SRC
