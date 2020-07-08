325c770821129bef80c9d0a1a4bcb29ccc8323fc
Saurabh Gorecha <sgorecha@codeaurora.org>
2020-07-09 02:20:08 +0530

    qti: Add RNG driver
    This patch adds RNG driver and use it to generate random number for stack
    protection.
    
    Change-Id: I73d79e68d08b5aa902dc7fad48e17a03f996178d
    Signed-off-by: Saurabh Gorecha <sgorecha@codeaurora.org>

---
+++ plat/qti/common/inc/qti_rng.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef QTI_RNG_H
+#define QTI_RNG_H
+
+int qti_rng_get_data(uint8_t *out, uint32_t out_len);
+
+#endif /* QTI_RNG_H */
