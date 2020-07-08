977a4b3d2f13e2d1323a88ce72da79e80b63ec1f
Saurabh Gorecha <sgorecha@codeaurora.org>
2020-07-09 02:20:08 +0530

    qti: Add RNG driver
    This patch adds RNG driver and use it to generate random number for stack protection.
    
    Change-Id: I73d79e68d08b5aa902dc7fad48e17a03f996178d
    Signed-off-by: Saurabh Gorecha <sgorecha@codeaurora.org>

--- plat/qti/common/src/qti_stack_protector.c
+++ plat/qti/common/src/qti_stack_protector.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -9,6 +9,7 @@
 #include <platform_def.h>
 #include <stdint.h>
 
+#include "qti_rng.h"
 #include "qtiseclib_interface.h"
 
 u_register_t plat_get_stack_protector_canary(void)
@@ -17,7 +18,7 @@ u_register_t plat_get_stack_protector_canary(void)
 
 	/* get random data , the below API doesn't return random = 0 in success
 	 * case */
-	qtiseclib_prng_get_data((uint8_t *) &random, sizeof(random));
+	qti_rng_get_data((uint8_t *) &random, sizeof(random));
 	assert(0x0 != random);
 
 	return random;
