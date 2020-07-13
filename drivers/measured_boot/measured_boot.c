dfcb90c15268619b1de72b7bc7f201deaf517d94
Alexei Fedorov <Alexei.Fedorov@arm.com>
2020-07-13 13:58:06 +0100

    TF-A: Add Event Log for Measured Boot
    This patch adds support for Event Log generation required
    for Measured Boot functionality.
    
    Change-Id: I34f05a33565e6659e78499d62cc6fb00b7d6c2dc
    Signed-off-by: Alexei Fedorov <Alexei.Fedorov@arm.com>

---
+++ drivers/measured_boot/measured_boot.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <assert.h>
+
+#include <common/debug.h>
+#include <drivers/measured_boot/measured_boot.h>
+
+void measured_boot_init(void)
+{
+	event_log_init();
+}
+
+void measured_boot_finish(void)
+{
+	uint8_t *log_addr;
+	size_t log_size;
+	int rc;
+
+	rc = event_log_finalise(&log_addr, &log_size);
+	if (rc != 0) {
+		panic();
+	}
+
+	dump_event_log(log_addr, log_size);
+}
+
+
