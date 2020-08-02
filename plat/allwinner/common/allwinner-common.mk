11fce1e4914d20b44690ab0c58a57eb6e61fcb0d
Andre Przywara <andre.przywara@arm.com>
2020-08-03 00:25:03 +0100

    plat/allwinner: Use common gicv2.mk
    Compiling BL31 for the Allwinner platform now produces a message about
    the deprecation of gic_common.c.
    Follow the advice and use include gicv2.mk instead.
    
    Change-Id: Iee46e21a630bfa831d28059f09aa7b049eb554bb
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- plat/allwinner/common/allwinner-common.mk
+++ plat/allwinner/common/allwinner-common.mk
@@ -19,12 +19,12 @@ PLAT_BL_COMMON_SOURCES	:=	drivers/ti/uart/${ARCH}/16550_console.S	\
 				${AW_PLAT}/common/plat_helpers.S	\
 				${AW_PLAT}/common/sunxi_common.c
 
+include drivers/arm/gic/v2/gicv2.mk
+
 BL31_SOURCES		+=	drivers/allwinner/axp/common.c		\
 				drivers/allwinner/sunxi_msgbox.c	\
 				drivers/arm/css/scpi/css_scpi.c		\
-				drivers/arm/gic/common/gic_common.c	\
-				drivers/arm/gic/v2/gicv2_helpers.c	\
-				drivers/arm/gic/v2/gicv2_main.c		\
+				${GICV2_SOURCES}			\
 				drivers/delay_timer/delay_timer.c	\
 				drivers/delay_timer/generic_delay_timer.c \
 				lib/cpus/${ARCH}/cortex_a53.S		\
