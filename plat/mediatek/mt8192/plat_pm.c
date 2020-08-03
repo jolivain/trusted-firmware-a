f820478f739e18af22cd07daed031f2022390953
Roger Lu <roger.lu@mediatek.com>
2020-08-03 10:35:16 +0800

    mediatek: mt8192: add spm suspend driver
    support infra power off/on
    
    Signed-off-by: Roger Lu <roger.lu@mediatek.com>
    Change-Id: Id13a06d4132f00fb60066de75920ecac18306e32

--- plat/mediatek/mt8192/plat_pm.c
+++ plat/mediatek/mt8192/plat_pm.c
@@ -22,6 +22,7 @@
 #include <plat_params.h>
 #include <plat_pm.h>
 #include <pmic.h>
+#include <uart.h>
 
 /*
  * Cluster state request:
@@ -84,18 +85,16 @@ static void plat_cpu_pwron_common(unsigned int cpu,
 
 	coordinate_cluster_pwron();
 
-	/* Enable the GIC CPU interface */
-	gicv3_rdistif_on(plat_my_core_pos());
-	gicv3_cpuif_enable(plat_my_core_pos());
-	mt_gic_rdistif_init();
-
 	/*
 	 * If mcusys do power down before then restore
 	 * all CPUs' GIC Redistributors
 	 */
-	if (IS_MCUSYS_OFF_STATE(state)) {
+	if (IS_MCUSYS_OFF_STATE(state) || IS_SYSTEM_SUSPEND_STATE(state)) {
 		mt_gic_rdistif_restore_all();
 	} else {
+		gicv3_rdistif_on(plat_my_core_pos());
+		gicv3_cpuif_enable(plat_my_core_pos());
+		mt_gic_rdistif_init();
 		mt_gic_rdistif_restore();
 	}
 
@@ -244,6 +243,9 @@ static void plat_power_domain_suspend(const psci_power_state_t *state)
 
 	plat_mt_pm_invoke_no_check(pwr_prompt, cpu, state);
 
+	if (!mt_console_uart_cg_status())
+		console_switch_state(CONSOLE_FLAG_BOOT);
+
 	/* Perform the common CPU specific operations */
 	plat_cpu_pwrdwn_common(cpu, state, plat_power_state[cpu]);
 
@@ -252,10 +254,13 @@ static void plat_power_domain_suspend(const psci_power_state_t *state)
 		plat_cluster_pwrdwn_common(cpu, state, plat_power_state[cpu]);
 	}
 
-	if (IS_MCUSYS_OFF_STATE(state)) {
+	if (IS_MCUSYS_OFF_STATE(state) || IS_SYSTEM_SUSPEND_STATE(state)) {
 		/* Perform the common mcusys specific operations */
 		plat_mcusys_pwrdwn_common(cpu, state, plat_power_state[cpu]);
 	}
+
+	if (!mt_console_uart_cg_status())
+		console_switch_state(CONSOLE_FLAG_RUNTIME);
 }
 
 static void plat_power_domain_suspend_finish(const psci_power_state_t *state)
@@ -264,7 +269,10 @@ static void plat_power_domain_suspend_finish(const psci_power_state_t *state)
 
 	assert(cpu < PLATFORM_CORE_COUNT);
 
-	if (IS_MCUSYS_OFF_STATE(state)) {
+	if (!mt_console_uart_cg_status())
+		console_switch_state(CONSOLE_FLAG_BOOT);
+
+	if (IS_MCUSYS_OFF_STATE(state) || IS_SYSTEM_SUSPEND_STATE(state)) {
 		/* Perform the common mcusys specific operations */
 		plat_mcusys_pwron_common(cpu, state, plat_power_state[cpu]);
 	}
@@ -278,6 +286,9 @@ static void plat_power_domain_suspend_finish(const psci_power_state_t *state)
 	plat_cpu_pwron_common(cpu, state, plat_power_state[cpu]);
 
 	plat_mt_pm_invoke_no_check(pwr_reflect, cpu, state);
+
+	if (!mt_console_uart_cg_status())
+		console_switch_state(CONSOLE_FLAG_RUNTIME);
 }
 
 static int plat_validate_power_state(unsigned int power_state,
