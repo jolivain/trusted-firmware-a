68ed992ef78c9a67d2e7b541be14d09c74c9799d
Roger Lu <roger.lu@mediatek.com>
2020-08-03 10:35:16 +0800

    mediatek: mt8192: add spm suspend driver
    support infra power off/on
    
    Signed-off-by: Roger Lu <roger.lu@mediatek.com>
    Change-Id: Id13a06d4132f00fb60066de75920ecac18306e32

---
+++ plat/mediatek/mt8192/drivers/spm/notifier/mt_spm_notifier.h
@@ -0,0 +1,22 @@
+#ifndef __MT_SPM_SSPM_NOTIFIER_H__
+#define __MT_SPM_SSPM_NOTIFIER_H__
+
+
+#ifndef ATF_PLAT_SPM_SSPM_NOTIFIER_UNSUPPORT
+enum MT_SPM_SSPM_NOTIFY_ID {
+	MT_SPM_NOTIFY_LP_ENTER,
+	MT_SPM_NOTIFY_LP_LEAVE,
+};
+
+int mt_spm_sspm_notify(int type, const void *val);
+
+static inline
+int mt_spm_sspm_notify_u32(int type, unsigned int val)
+{
+	unsigned r_val = val;
+
+	return mt_spm_sspm_notify(type, (const void *)&r_val);
+}
+#endif
+
+#endif
