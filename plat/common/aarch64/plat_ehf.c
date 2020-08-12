c0e0bb265a86500c35969a424d0e10b417b1f95c
Sandeep Tripathy <sandeep.tripathy@broadcom.com>
2020-08-12 18:42:13 +0530

    RFC: ehf: use common priority level enumuration
    'EHF' is used by RAS, SDEI, SPM_MM common frameworks.
    If platform needs to plug-in specific handlers then
    'PLAT_EHF_DESC' can be used to populate platform specific
    priority levels.
    
    Signed-off-by: Sandeep Tripathy <sandeep.tripathy@broadcom.com>
    Change-Id: I37af7e0e48111f87b6982604bf5c15db3e05755d

--- plat/common/aarch64/plat_ehf.c
+++ plat/common/aarch64/plat_ehf.c
@@ -11,23 +11,27 @@
 /*
  * Enumeration of priority levels on ARM platforms.
  */
-ehf_pri_desc_t arm_exceptions[] = {
+ehf_pri_desc_t plat_exceptions[] = {
 #if RAS_EXTENSION
 	/* RAS Priority */
-	EHF_PRI_DESC(ARM_PRI_BITS, PLAT_RAS_PRI),
+	EHF_PRI_DESC(PLAT_PRI_BITS, PLAT_RAS_PRI),
 #endif
 
 #if SDEI_SUPPORT
 	/* Critical priority SDEI */
-	EHF_PRI_DESC(ARM_PRI_BITS, PLAT_SDEI_CRITICAL_PRI),
+	EHF_PRI_DESC(PLAT_PRI_BITS, PLAT_SDEI_CRITICAL_PRI),
 
 	/* Normal priority SDEI */
-	EHF_PRI_DESC(ARM_PRI_BITS, PLAT_SDEI_NORMAL_PRI),
+	EHF_PRI_DESC(PLAT_PRI_BITS, PLAT_SDEI_NORMAL_PRI),
 #endif
 #if SPM_MM
-	EHF_PRI_DESC(ARM_PRI_BITS, PLAT_SP_PRI),
+	EHF_PRI_DESC(PLAT_PRI_BITS, PLAT_SP_PRI),
+#endif
+
+#ifdef PLAT_EHF_DESC
+	PLAT_EHF_DESC,
 #endif
 };
 
 /* Plug in ARM exceptions to Exception Handling Framework. */
-EHF_REGISTER_PRIORITIES(arm_exceptions, ARRAY_SIZE(arm_exceptions), ARM_PRI_BITS);
+EHF_REGISTER_PRIORITIES(plat_exceptions, ARRAY_SIZE(plat_exceptions), PLAT_PRI_BITS);
