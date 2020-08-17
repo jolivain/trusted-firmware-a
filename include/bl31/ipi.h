efa585c1da577d43583650bae9565317b97a736f
Sandeep Tripathy <sandeep.tripathy@broadcom.com>
2020-08-17 19:16:52 +0530

    ipi: add basic ipi feature using EHF
    It is useful to have framework to be able to send IPI to target
    core in a platform independent manner. There are generic usecases
    like 'IPI_CPU_STOP' where one can force stop the target core.
    Current implementation uses EHF and requires platform to provide
    the priority level for IPI.
    
    'PLAT_IPI_PRI' to be defined by platforms those want to use
    'IPI_SUPPORT'.
    'PLAT_IPI_CPU_STOP' to be defined by platform to be a G0 SGI.
    'plat_ipi_cpu_stop()' is weakly defined to power down the core.
    
    Signed-off-by: Sandeep Tripathy <sandeep.tripathy@broadcom.com>
    Change-Id: I11c1854031959225b697faeffe827d8081485f8b

---
+++ include/bl31/ipi.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020 Broadcom
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef IPI_H
+#define IPI_H
+
+#include <cdefs.h>
+#include <stdint.h>
+
+void ipi_send_cpu_stop(u_register_t target_mpidr);
+void ipi_init(void);
+
+#endif /* IPI_H*/
