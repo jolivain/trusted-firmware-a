1d9abd4292041835da68838824df4a0695989c9f
Sandeep Tripathy <sandeep.tripathy@broadcom.com>
2020-08-17 19:16:52 +0530

    ipi: add basic ipi feature using EHF
    It is useful to have framework to be able to send IPI to target
    core in a platform independent manner. There are generic usecases
    like 'IPI_CPU_STOP' where one can force stop the target core.
    Current implementation uses EHF and requires platform to provide
    the priority level for IPI.
    
    'PLAT_IPI_PRI' to be defined by platforms those want to use
    'IPI_SUPPORT'.
    'PLAT_IPI_CPU_STOP' to be defined by platform to be a G0 SGI.
    'plat_ipi_cpu_stop()' is weakly defined to power down the core.
    
    Signed-off-by: Sandeep Tripathy <sandeep.tripathy@broadcom.com>
    Change-Id: I11c1854031959225b697faeffe827d8081485f8b

---
+++ bl31/ipi.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2020 Broadcom
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <arch_helpers.h>
+#include <bl31/ehf.h>
+#include <bl31/interrupt_mgmt.h>
+#include <common/bl_common.h>
+#include <common/debug.h>
+#include <common/interrupt_props.h>
+#include <lib/psci/psci.h>
+#include <platform_def.h>
+#include <plat/common/platform.h>
+
+#if defined(PLAT_IPI_CPU_STOP)
+#pragma weak plat_ipi_cpu_stop
+
+void plat_ipi_cpu_stop(void)
+{
+	/* Power down the core */
+	psci_cpu_off();
+}
+
+void ipi_send_cpu_stop(u_register_t target)
+{
+	plat_ic_raise_el3_sgi(PLAT_IPI_CPU_STOP, target);
+}
+#endif
+
+static int ipi_handler(unsigned int id, unsigned int flags,
+		       void *handle, void *cookie)
+{
+	switch (id) {
+#if defined(PLAT_IPI_CPU_STOP)
+	case PLAT_IPI_CPU_STOP:
+		plat_ipi_cpu_stop();
+		break;
+#endif
+	default:
+		ERROR("Unknown IPI 0x%x\n", id);
+		break;
+	}
+
+	plat_ic_end_of_interrupt(id);
+
+	return 0;
+}
+
+__init void ipi_init(void)
+{
+#if defined(PLAT_IPI_PRI)
+	ehf_register_priority_handler(PLAT_IPI_PRI, ipi_handler);
+#endif
+}
