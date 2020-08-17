44489715140ebee3620d5572cb5dfe3e0b512469
Sandeep Tripathy <sandeep.tripathy@broadcom.com>
2020-08-17 20:22:13 +0530

    psci: utility api to invoke stop for other cores
    The API can be used to invoke a 'stop_func' callback for all
    other cores from any initiating core. Optionally it can also
    wait for other cores to power down. There may be various use
    of such API by platform. Ex: Platform may use this to power
    down all other cores from a crashed core.
    
    Signed-off-by: Sandeep Tripathy <sandeep.tripathy@broadcom.com>
    Change-Id: I4f9dc8a38d419f299c021535d5f1bcc6883106f9

--- lib/psci/psci_common.c
+++ lib/psci/psci_common.c
@@ -12,6 +12,7 @@
 #include <common/bl_common.h>
 #include <common/debug.h>
 #include <context.h>
+#include <drivers/delay_timer.h>
 #include <lib/el3_runtime/context_mgmt.h>
 #include <lib/utils.h>
 #include <plat/common/platform.h>
@@ -972,4 +973,45 @@ void psci_do_pwrdown_sequence(unsigned int power_level)
 	 */
 	psci_do_pwrdown_cache_maintenance(power_level);
 #endif
+}
+
+/*******************************************************************************
+ * This function invokes the callback 'stop_func()' with the 'mpidr' of each
+ * online PE. Caller can pass suitable method to stop a remote core.
+ *
+ * 'wait_ms' is the timeout value in mili-seconds for the other cores to
+ * transition to power down state. Passing '0' makes it non-blocking.
+ *
+ * The function returns '1' if some cores failed to stop within the given
+ * timeout.
+ ******************************************************************************/
+int psci_stop_other_cores(int wait_ms,
+				   void (*stop_func)(u_register_t mpidr))
+{
+	int idx, this_cpu_idx;
+
+	this_cpu_idx = plat_my_core_pos();
+
+	/* Invoke stop_func for each core */
+	for (idx = 0; idx < psci_plat_core_count; idx++) {
+		if ((idx != this_cpu_idx) &&
+		    (psci_get_aff_info_state_by_idx(idx) == AFF_STATE_ON)) {
+			stop_func(psci_cpu_pd_nodes[idx].mpidr);
+		}
+	}
+
+	/* Need to wait for others cores to shutdown */
+	if (wait_ms != 0) {
+		while ((wait_ms-- != 0) && !psci_is_last_on_cpu()) {
+			mdelay(1);
+		}
+
+		if (!psci_is_last_on_cpu()) {
+			WARN("Failed to stop all cores!\n");
+			psci_print_power_domain_map();
+			return 1;
+		}
+	}
+
+	return 0;
 }
