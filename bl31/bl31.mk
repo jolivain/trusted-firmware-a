e502b7338e57e8f7d69923737c57979c03e4fff1
Sandeep Tripathy <sandeep.tripathy@broadcom.com>
2020-08-17 19:16:52 +0530

    ipi: add basic ipi feature using EHF
    It is useful to have framework to be able to send IPI to target
    core in a platform independent manner. There are generic usecases
    like 'IPI_CPU_STOP' where one can force stop the target core.
    Current implementation uses EHF and requires platform to provide
    the priority level for IPI.
    
    'PLAT_IPI_PRI' to be defined by platforms those want to use
    'IPI_SUPPORT'.
    'PLAT_IPI_CPU_STOP' to be defined by platform to be a G0 SGI.
    'plat_ipi_cpu_stop()' is weakly defined to power down the core.
    
    Signed-off-by: Sandeep Tripathy <sandeep.tripathy@broadcom.com>
    Change-Id: I11c1854031959225b697faeffe827d8081485f8b

--- bl31/bl31.mk
+++ bl31/bl31.mk
@@ -54,6 +54,13 @@ ifeq (${EL3_EXCEPTION_HANDLING},1)
 BL31_SOURCES		+=	bl31/ehf.c
 endif
 
+ifeq (${IPI_SUPPORT},1)
+ifeq (${EL3_EXCEPTION_HANDLING},0)
+  $(error EL3_EXCEPTION_HANDLING must be 1 for IPI support)
+endif
+BL31_SOURCES		+=	bl31/ipi.c
+endif
+
 ifeq (${SDEI_SUPPORT},1)
 ifeq (${EL3_EXCEPTION_HANDLING},0)
   $(error EL3_EXCEPTION_HANDLING must be 1 for SDEI support)
@@ -100,6 +107,7 @@ $(eval $(call assert_booleans,\
 	CRASH_REPORTING \
 	EL3_EXCEPTION_HANDLING \
 	SDEI_SUPPORT \
+	IPI_SUPPORT \
 )))
 
 $(eval $(call add_defines,\
@@ -107,4 +115,5 @@ $(eval $(call add_defines,\
         CRASH_REPORTING \
         EL3_EXCEPTION_HANDLING \
         SDEI_SUPPORT \
+	IPI_SUPPORT \
 )))
