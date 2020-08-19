6af577c07c22e64b557989e454f2323a5a01945b
Nina Wu <nina-cm.wu@mediatek.com>
2020-08-19 17:20:15 +0800

    mediatek: mt8192: Add SiP service
    Add the basic SiP service
    
    Change-Id: Ib7f2380aab910adf8d33498a79ecd287273907c5
    Signed-off-by: Nina Wu <nina-cm.wu@mediatek.com>

---
+++ plat/mediatek/mt8192/plat_sip_calls.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2015, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <common/debug.h>
+#include <common/runtime_svc.h>
+
+uintptr_t mediatek_plat_sip_handler(uint32_t smc_fid,
+				u_register_t x1,
+				u_register_t x2,
+				u_register_t x3,
+				u_register_t x4,
+				void *cookie,
+				void *handle,
+				u_register_t flags)
+{
+	//uint64_t ret;
+
+	switch (smc_fid) {
+	default:
+		ERROR("%s: unhandled SMC (0x%x)\n", __func__, smc_fid);
+		break;
+	}
+
+	SMC_RET1(handle, SMC_UNK);
+}
