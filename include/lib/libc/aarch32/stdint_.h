7885131edcd1c171e5c77729e51c51d24edc5d01
Scott Branden <scott.branden@broadcom.com>
2020-08-25 13:49:32 -0700

    libc: use long for 64-bit types on aarch64
    Use long instead of long long on aarch64 for 64_t stdint types.
    Introduce inttypes.h to properly support printf format specifiers for
    fixed width types for such change.
    
    Change-Id: I0bca594687a996fde0a9702d7a383055b99f10a1
    Signed-off-by: Scott Branden <scott.branden@broadcom.com>

---
+++ include/lib/libc/aarch32/stdint_.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018-2020, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef STDINT__H
+#define STDINT__H
+
+#define INT64_MAX  LLONG_MAX
+#define INT64_MIN  LLONG_MIN
+#define UINT64_MAX ULLONG_MAX
+
+#define INT64_C(x) x ## LL
+#define UINT64_C(x) x ## ULL
+
+typedef long long int64_t;
+typedef unsigned long long uint64_t;
+typedef long long int64_least_t;
+typedef unsigned long long uint64_least_t;
+typedef long long int64_fast_t;
+typedef unsigned long long uint64_fast_t;
+
+#endif
