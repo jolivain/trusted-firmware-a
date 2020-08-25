212ed28612f43bc2ea434dd5b8043b9779dfdefe
Scott Branden <scott.branden@broadcom.com>
2020-08-25 13:49:32 -0700

    libc: use long for 64-bit types on aarch64
    Use long instead of long long on aarch64 for 64_t stdint types.
    Introduce inttypes.h to properly support printf format specifiers for
    fixed width types for such change.
    
    Change-Id: I0bca594687a996fde0a9702d7a383055b99f10a1
    Signed-off-by: Scott Branden <scott.branden@broadcom.com>

--- services/std_svc/sdei/sdei_intr_mgmt.c
+++ services/std_svc/sdei/sdei_intr_mgmt.c
@@ -5,6 +5,8 @@
  */
 
 #include <assert.h>
+#include <inttypes.h>
+#include <stdint.h>
 #include <string.h>
 
 #include <arch_helpers.h>
@@ -394,8 +396,8 @@ int sdei_intr_handler(uint32_t intr_raw, uint32_t flags, void *handle,
 		 * Interrupts received while this PE was masked can't be
 		 * dispatched.
 		 */
-		SDEI_LOG("interrupt %u on %llx while PE masked\n", map->intr,
-				mpidr);
+		SDEI_LOG("interrupt %u on %" PRIx64 " while PE masked\n",
+			 map->intr, mpidr);
 		if (is_event_shared(map))
 			sdei_map_lock(map);
 
@@ -466,8 +468,8 @@ int sdei_intr_handler(uint32_t intr_raw, uint32_t flags, void *handle,
 	if (is_event_shared(map))
 		sdei_map_unlock(map);
 
-	SDEI_LOG("ACK %llx, ev:%d ss:%d spsr:%lx ELR:%lx\n", mpidr, map->ev_num,
-			sec_state, read_spsr_el3(), read_elr_el3());
+	SDEI_LOG("ACK %" PRIx64 ", ev:%d ss:%d spsr:%lx ELR:%lx\n",
+		 mpidr, map->ev_num, sec_state, read_spsr_el3(), read_elr_el3());
 
 	ctx = handle;
 
