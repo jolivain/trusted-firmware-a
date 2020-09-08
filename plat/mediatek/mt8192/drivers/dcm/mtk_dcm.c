2d90d9f669399621db68dc308c32a720728523bd
Nina Wu <nina-cm.wu@mediatek.com>
2020-09-08 14:36:07 +0800

    mediatek: mt8192: dcm: Add mcusys related dcm drivers
    1. Add mcusys related dcm drivers
    2. Turn on mcusys-related dcm by default
    
    Change-Id: Ibbee37c87cc38e7a6cd7c93c2fc0817aad6dbe95
    Signed-off-by: Nina Wu <nina-cm.wu@mediatek.com>

---
+++ plat/mediatek/mt8192/drivers/dcm/mtk_dcm.c
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2020, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <mtk_dcm.h>
+#include <mtk_dcm_utils.h>
+
+static int dcm_armcore(E_DCM_MODE mode)
+{
+	dcm_mp_cpusys_top_bus_pll_div_dcm(mode);
+	dcm_mp_cpusys_top_cpu_pll_div_0_dcm(mode);
+	dcm_mp_cpusys_top_cpu_pll_div_1_dcm(mode);
+
+	return 0;
+}
+
+static int dcm_mcusys(E_DCM_MODE on)
+{
+	dcm_mp_cpusys_top_adb_dcm(on);
+	dcm_mp_cpusys_top_apb_dcm(on);
+	dcm_mp_cpusys_top_cpubiu_dcm(on);
+	dcm_mp_cpusys_top_misc_dcm(on);
+	dcm_mp_cpusys_top_mp0_qdcm(on);
+	dcm_cpccfg_reg_emi_wfifo(on);
+	dcm_mp_cpusys_top_last_cor_idle_dcm(on);
+
+	return 0;
+}
+
+static int dcm_stall(E_DCM_MODE on)
+{
+	dcm_mp_cpusys_top_core_stall_dcm(on);
+	dcm_mp_cpusys_top_fcm_stall_dcm(on);
+
+	return 0;
+}
+
+static int check_dcm_state(void)
+{
+	bool ret = true;
+
+	ret &= dcm_mp_cpusys_top_bus_pll_div_dcm_is_on();
+	ret &= dcm_mp_cpusys_top_cpu_pll_div_0_dcm_is_on();
+	ret &= dcm_mp_cpusys_top_cpu_pll_div_1_dcm_is_on();
+
+	ret &= dcm_mp_cpusys_top_adb_dcm_is_on();
+	ret &= dcm_mp_cpusys_top_apb_dcm_is_on();
+	ret &= dcm_mp_cpusys_top_cpubiu_dcm_is_on();
+	ret &= dcm_mp_cpusys_top_misc_dcm_is_on();
+	ret &= dcm_mp_cpusys_top_mp0_qdcm_is_on();
+	ret &= dcm_cpccfg_reg_emi_wfifo_is_on();
+	ret &= dcm_mp_cpusys_top_last_cor_idle_dcm_is_on();
+
+	ret &= dcm_mp_cpusys_top_core_stall_dcm_is_on();
+	ret &= dcm_mp_cpusys_top_fcm_stall_dcm_is_on();
+
+	return (int)ret;
+}
+
+int dcm_set_default(void)
+{
+	dcm_armcore(DCM_ON);
+	dcm_mcusys(DCM_ON);
+	dcm_stall(DCM_ON);
+
+	return check_dcm_state();
+}
