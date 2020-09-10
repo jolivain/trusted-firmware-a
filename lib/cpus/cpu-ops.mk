dd8ab66b6e7c354c5b66c191f864d279c9d663b3
johpow01 <john.powell@arm.com>
2020-09-10 13:39:26 -0500

    Workaround for Cortex A77 erratum 1925769
    Cortex A77 erratum 1925769 is a Cat B erratum, present in older
    revisions of the Cortex A77 processor core.  The workaround is to
    set bit 8 in the ECTLR_EL1 register, there is a small performance cost
    (<0.5%) for setting this bit.
    
    SDEN can be found here:
    https://documentation-service.arm.com/static/5f7c35d0d3be967f7be46d33
    
    Signed-off-by: John Powell <john.powell@arm.com>
    Change-Id: I9cf0e0b5dc1e3e32e24279d2632c759cc7bd7ce9

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -282,6 +282,10 @@ ERRATA_A76_1165522	?=0
 # only to revision <= r1p1 of the Cortex A77 cpu.
 ERRATA_A77_1800714	?=0
 
+# Flag to apply erratum 1925769 workaround during reset. This erratum applies
+# only to revision <= r1p1 of the Cortex A77 cpu.
+ERRATA_A77_1925769	?=0
+
 # Flag to apply erratum 1688305 workaround during reset. This erratum applies
 # to revisions r0p0 - r1p0 of the A78 cpu.
 ERRATA_A78_1688305	?=0
@@ -550,6 +554,10 @@ $(eval $(call add_define,ERRATA_A76_1165522))
 # Process ERRATA_A77_1800714 flag
 $(eval $(call assert_boolean,ERRATA_A77_1800714))
 $(eval $(call add_define,ERRATA_A77_1800714))
+
+# Process ERRATA_A77_1925769 flag
+$(eval $(call assert_boolean,ERRATA_A77_1925769))
+$(eval $(call add_define,ERRATA_A77_1925769))
 
 # Process ERRATA_A78_1688305 flag
 $(eval $(call assert_boolean,ERRATA_A78_1688305))
