4e9ad04f3d418a9d1446f32f4169edf596406581
Po Xu <jg_poxu@mediatek.com>
2020-09-18 09:32:31 +0800

    mediatek: mt8192: add GPIO driver support
    add GPIO driver
    
    Change-Id: I67a9abef078e7a62b34dfbd366b45c03892800cd
    Signed-off-by: Po Xu <jg_poxu@mediatek.com>

--- plat/mediatek/mt8192/bl31_plat_setup.c
+++ plat/mediatek/mt8192/bl31_plat_setup.c
@@ -12,6 +12,7 @@
 #include <common/debug.h>
 #include <common/desc_image_load.h>
 #include <drivers/ti/uart/uart_16550.h>
+#include <gpio/mtgpio.h>
 #include <lib/coreboot.h>
 
 /* Platform Includes */
@@ -83,6 +84,7 @@ void bl31_platform_setup(void)
 	/* Initialize the GIC driver, CPU and distributor interfaces */
 	mt_gic_driver_init();
 	mt_gic_init();
+	plat_mt8192_gpio_init();
 }
 
 /*******************************************************************************
