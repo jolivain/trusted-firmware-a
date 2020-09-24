dc0f6f5b5ab80e96c1212dad0e57c32d89fd6ac6
Bharat Gooty <bharat.gooty@broadcom.com>
2020-09-24 13:02:39 +0530

    driver: brcm: add mdio driver
    Change-Id: Id873670f68a4c584e3b7b586cab28565bb5a1c27
    Signed-off-by: Bharat Gooty <bharat.gooty@broadcom.com>

--- plat/brcm/board/stingray/platform.mk
+++ plat/brcm/board/stingray/platform.mk
@@ -190,7 +190,8 @@ PLAT_BL_COMMON_SOURCES	+=	lib/cpus/aarch64/cortex_a72.S \
 				plat/${SOC_DIR}/src/tz_sec.c \
 				drivers/arm/tzc/tzc400.c \
 				plat/${SOC_DIR}/driver/plat_emmc.c \
-				plat/${SOC_DIR}/src/topology.c
+				plat/${SOC_DIR}/src/topology.c \
+				drivers/brcm/mdio/mdio.c
 
 ifeq (${USE_CHIMP},yes)
 PLAT_BL_COMMON_SOURCES	+=	drivers/brcm/chimp.c
