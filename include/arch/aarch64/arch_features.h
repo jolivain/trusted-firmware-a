427cbc664fcb889483d1609626a50b279949723c
johpow01 <john.powell@arm.com>
2020-10-02 13:41:11 -0500

    Enable v8.6 AMU enhancements (FEAT_AMUv1p1)
    ARMv8.6 adds virtual offset registers to support virtualization of the
    event counters in EL1 and EL0.  This patch enables support for this
    feature in EL3 firmware.
    
    Signed-off-by: John Powell <john.powell@arm.com>
    Change-Id: I7ee1f3d9f554930bf5ef6f3d492e932e6d95b217

--- include/arch/aarch64/arch_features.h
+++ include/arch/aarch64/arch_features.h
@@ -76,6 +76,12 @@ static inline unsigned long int get_armv8_6_ecv_support(void)
 		ID_AA64MMFR0_EL1_ECV_MASK);
 }
 
+static inline bool is_armv8_6_feat_amuv1p1_present(void)
+{
+	return (((read_id_aa64pfr0_el1() >> ID_AA64PFR0_AMU_SHIFT) &
+		ID_AA64PFR0_AMU_MASK) >= ID_AA64PFR0_AMU_V1P1);
+}
+
 /*
  * Return MPAM version:
  *
