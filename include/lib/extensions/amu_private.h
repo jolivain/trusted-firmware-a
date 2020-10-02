7965af7e05d9510cbc0359b3fbc96ea1582dc56c
johpow01 <john.powell@arm.com>
2020-10-02 13:41:11 -0500

    Enable v8.6 AMU enhancements
    ARMv8.6 adds virtual offset registers to support virtualization of the
    event counters in EL1 and EL0.  This patch enables support for this
    feature in EL3 firmware.
    
    Signed-off-by: John Powell <john.powell@arm.com>
    Change-Id: I7ee1f3d9f554930bf5ef6f3d492e932e6d95b217

--- include/lib/extensions/amu_private.h
+++ include/lib/extensions/amu_private.h
@@ -16,4 +16,14 @@ uint64_t amu_group1_cnt_read_internal(unsigned int idx);
 void amu_group1_cnt_write_internal(unsigned int idx, uint64_t val);
 void amu_group1_set_evtype_internal(unsigned int idx, unsigned int val);
 
+uint64_t amu_group0_voffset_read_internal(unsigned int idx);
+void amu_group0_voffset_write_internal(unsigned int idx, uint64_t val);
+
+uint64_t amu_group1_voffset_read_internal(unsigned int idx);
+void amu_group1_voffset_write_internal(unsigned int idx, uint64_t val);
+
+unsigned int amu_read_amcg1idr_el0(void);
+uint64_t amu_read_amcr(void);
+void amu_write_amcr(uint64_t val);
+
 #endif /* AMU_PRIVATE_H */
