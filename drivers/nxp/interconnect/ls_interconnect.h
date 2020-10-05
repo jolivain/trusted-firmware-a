5b7b6385bc68b0ddd8e029b67da58e4565063922
Pankaj Gupta <pankaj.gupta@nxp.com>
2020-10-06 00:10:22 +0530

    nxp:interconnect api based on arm ccn or cci drivr
    CCN API(s) to be used NXP SoC(s) are added.
    These API(s) based on ARM CCN driver
    - driver/arm/ccn
    
    CCI API(s) to be used NXP SoC(s) are added.
    These API(s) based on ARM CCI driver
    - driver/arm/cci
    
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Change-Id: I7682c4c9bd42f63542b3ffd3cb6c5d2effe4ae0a

---
+++ drivers/nxp/interconnect/ls_interconnect.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef LS_INTERCONNECT_H
+#define LS_INTERCONNECT_H
+
+/* Interconnect CCI/CCN functions */
+void plat_ls_interconnect_enter_coherency(unsigned int num_clusters);
+
+#endif
