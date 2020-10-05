aa5b8f9f39606e67038ff05db378316853e1af3d
Pankaj Gupta <pankaj.gupta@nxp.com>
2020-10-05 20:58:04 +0530

    nxp lx2160a-rdb: new plat based on soc lx2160a
    New NXP platform lx2160a-rdb(Reference Design Board):
    - Based SoC lx2160a
    - Board specific tuning for DDR init.
    - Board specific Flash details.
    
    Signed-off-by: Udit Agarwal Gupta <udit.agarwal@nxp.com>
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Change-Id: I9c10dac9d5e67d44a2d94a7a27812220fdcc6ae3

---
+++ plat/nxp/soc-lx2160/lx2160ardb/platform_def.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __PLATFORM_DEF_H__
+#define __PLATFORM_DEF_H__
+
+#include <plat_def.h>
+#include <plat_default_def.h>
+
+#endif
