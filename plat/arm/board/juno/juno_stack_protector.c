6ae0d98681083a299679a731897e826eb05c46cf
Andre Przywara <andre.przywara@arm.com>
2020-10-08 00:45:22 +0100

    plat/arm: juno: Use TRNG entropy source for SMCCC TRNG interface
    Now that we have a framework for the SMCCC TRNG interface, and the
    existing Juno entropy code has been prepared, add the few remaining bits
    to implement this interface for the Juno Trusted Entropy Source.
    
    We retire the existing Juno specific RNG interface, and use the generic
    one for the stack canary generation.
    
    Change-Id: Ib6a6e5568cb8e0059d71740e2d18d6817b07127d
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- plat/arm/board/juno/juno_stack_protector.c
+++ plat/arm/board/juno/juno_stack_protector.c
@@ -8,14 +8,13 @@
 #include <common/debug.h>
 #include <lib/utils.h>
 #include <platform_def.h>
-
-#include "juno_decl.h"
+#include <plat/common/plat_trng.h>
 
 u_register_t plat_get_stack_protector_canary(void)
 {
 	uint64_t entropy;
 
-	if (!juno_getentropy(&entropy)) {
+	if (!plat_get_entropy(&entropy)) {
 		ERROR("Not enough entropy to initialize canary value\n");
 		panic();
 	}
