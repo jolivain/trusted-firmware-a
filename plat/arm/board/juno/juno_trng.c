231072df5b39db885e48ecead1bb46753c5dd603
Andre Przywara <andre.przywara@arm.com>
2020-10-08 00:43:50 +0100

    plat/arm: juno: Refactor juno_getentropy()
    Currently we use the Juno's TRNG hardware entropy source to initialise
    the stack canary. The current function allows to fill a buffer of any
    size, but we will actually only ever request 16 bytes, as this is what
    the hardware implements. Out of this, we only need at most 64 bits for
    the canary.
    
    In preparation for the introduction of the SMCCC TRNG interface, we
    can simplify this Juno specific interface by making it compatible with
    the generic one: We just deliver 64 bits of entropy on each call.
    This reduces the complexity of the code. As the raw entropy register
    readouts seem to be biased, it makes sense to do some conditioning
    inside the juno_getentropy() function already.
    Also initialise the TRNG hardware, if not already done.
    
    Change-Id: I11b977ddc5417d52ac38709a9a7b61499eee481f
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- plat/arm/board/juno/juno_trng.c
+++ plat/arm/board/juno/juno_trng.c
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2017-2018, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2017-2020, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <string.h>
 
 #include <lib/mmio.h>
@@ -13,71 +15,75 @@
 
 #include "juno_decl.h"
 
-#define NSAMPLE_CLOCKS	1 /* min 1 cycle, max 231 cycles */
-#define NRETRIES	5
+#define NSAMPLE_CLOCKS	1U /* min 1 cycle, max 231 cycles */
+#define NRETRIES	5U
 
-static inline int output_valid(void)
+static bool juno_trng_initialized = false;
+
+static bool output_valid(void)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0; i < NRETRIES; i++) {
+	for (i = 0U; i < NRETRIES; i++) {
 		uint32_t val;
 
 		val = mmio_read_32(TRNG_BASE + TRNG_STATUS);
-		if (val & 1U)
-			break;
+		if ((val & 1U) != 0U) {
+			return true;
+		}
 	}
-	if (i >= NRETRIES)
-		return 0; /* No output data available. */
-	return 1;
+	return false; /* No output data available. */
 }
 
 /*
- * This function fills `buf` with `len` bytes of entropy.
+ * This function fills `buf` with 8 bytes of entropy.
  * It uses the Trusted Entropy Source peripheral on Juno.
  * Returns 0 when the buffer has been filled with entropy
  * successfully and -1 otherwise.
+ * 	   'false' otherwise.
  */
-int juno_getentropy(void *buf, size_t len)
+bool juno_getentropy(uint64_t *buf)
 {
-	uint8_t *bp = buf;
+	uint64_t ret;
 
 	assert(buf);
-	assert(len);
-	assert(!check_uptr_overflow((uintptr_t)bp, len));
+	assert(!check_uptr_overflow((uintptr_t)buf, sizeof(*buf)));
 
-	/* Disable interrupt mode. */
-	mmio_write_32(TRNG_BASE + TRNG_INTMASK, 0);
-	/* Program TRNG to sample for `NSAMPLE_CLOCKS`. */
-	mmio_write_32(TRNG_BASE + TRNG_CONFIG, NSAMPLE_CLOCKS);
-
-	while (len > 0) {
-		int i;
-
-		/* Start TRNG. */
-		mmio_write_32(TRNG_BASE + TRNG_CONTROL, 1);
-
-		/* Check if output is valid. */
-		if (!output_valid())
-			return -1;
-
-		/* Fill entropy buffer. */
-		for (i = 0; i < TRNG_NOUTPUTS; i++) {
-			size_t n;
-			uint32_t val;
-
-			val = mmio_read_32(TRNG_BASE + i * sizeof(uint32_t));
-			n = MIN(len, sizeof(uint32_t));
-			memcpy(bp, &val, n);
-			bp += n;
-			len -= n;
-			if (len == 0)
-				break;
-		}
-
+	if (!juno_trng_initialized) {
+		/* Disable interrupt mode. */
+		mmio_write_32(TRNG_BASE + TRNG_INTMASK, 0U);
+		/* Program TRNG to sample for `NSAMPLE_CLOCKS`. */
+		mmio_write_32(TRNG_BASE + TRNG_CONFIG, NSAMPLE_CLOCKS);
+		/* Abort any potentially pending sampling. */
+		mmio_write_32(TRNG_BASE + TRNG_CONTROL, 2U);
 		/* Reset TRNG outputs. */
-		mmio_write_32(TRNG_BASE + TRNG_STATUS, 1);
+		mmio_write_32(TRNG_BASE + TRNG_STATUS, 1U);
+
+		juno_trng_initialized = true;
 	}
 
-	return 0;
+	if (!output_valid()) {
+		/* Start TRNG. */
+		mmio_write_32(TRNG_BASE + TRNG_CONTROL, 1U);
+
+		if (!output_valid()) {
+			return false;
+		}
+	}
+
+	/* XOR each two 32-bit registers together, combine the pairs */
+	ret = mmio_read_32(TRNG_BASE + 0);
+	ret ^= mmio_read_32(TRNG_BASE + 4);
+	ret <<= 32;
+
+	ret |= mmio_read_32(TRNG_BASE + 8);
+	ret ^= mmio_read_32(TRNG_BASE + 12);
+	*buf = ret;
+
+	/* Acknowledge current cycle, clear output registers. */
+	mmio_write_32(TRNG_BASE + TRNG_STATUS, 1U);
+	/* Trigger next TRNG cycle. */
+	mmio_write_32(TRNG_BASE + TRNG_CONTROL, 1U);
+
+	return true;
 }
