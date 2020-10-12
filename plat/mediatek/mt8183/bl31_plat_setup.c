e03d947ea618f691f64a6c1b6a8b6cb1f6dfa58b
Fengquan Chen <fengquan.chen@mediatek.com>
2020-10-12 16:33:25 +0800

    mediatek: mt8183: add timer V20 compensation
    add timer driver.
    
    Signed-off-by: Fengquan Chen <fengquan.chen@mediatek.com>
    Change-Id: I60a7273f922233a618a6163b802c0858ed89f75f

--- plat/mediatek/mt8183/bl31_plat_setup.c
+++ plat/mediatek/mt8183/bl31_plat_setup.c
@@ -28,6 +28,7 @@
 #include <scu.h>
 #include <spm.h>
 #include <drivers/ti/uart/uart_16550.h>
+#include <mt_timer.h>
 
 static entry_point_info_t bl32_ep_info;
 static entry_point_info_t bl33_ep_info;
@@ -147,6 +148,8 @@ void bl31_platform_setup(void)
 	/* Initialize the GIC driver, CPU and distributor interfaces */
 	mt_gic_driver_init();
 	mt_gic_init();
+
+	mt_systimer_init();
 
 	/* Init mcsi SF */
 	plat_mtk_cci_init_sf();
