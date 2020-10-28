82648a5a75a7041c16fc29a09c134896ee4304b6
Tomas Pilar <tomas@nuviainc.com>
2020-10-28 15:35:53 +0000

    plat/qemu: Use RNDR in stack protector
    When getting a stack protector canary value, check
    if cpu supports FEAT_RNG and use that. Fallback to
    old method of using a (hardcoded value ^ timer).
    
    Signed-off-by: Tomas Pilar <tomas@nuviainc.com>
    Change-Id: If7d802062f01f73d37a639843cb7a2eb81d4be86

--- plat/qemu/common/qemu_stack_protector.c
+++ plat/qemu/common/qemu_stack_protector.c
@@ -7,12 +7,20 @@
 #include <stdint.h>
 
 #include <arch_helpers.h>
+#include <arch_features.h>
 #include <plat/common/platform.h>
 
 #define RANDOM_CANARY_VALUE ((u_register_t) 3288484550995823360ULL)
 
 u_register_t plat_get_stack_protector_canary(void)
 {
+#if ENABLE_FEAT_RNG
+	/* Use the RNDR instruction if the CPU supports it */
+	if (is_armv8_5_rng_present()) {
+		return read_rndr();
+	}
+#endif
+	
 	/*
 	 * Ideally, a random number should be returned instead of the
 	 * combination of a timer's value and a compile-time constant.
