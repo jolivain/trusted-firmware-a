75fa4d6f3c151e60f34434926e1a15641fbcde3f
Biju Das <biju.das.jz@bp.renesas.com>
2020-11-09 09:36:46 +0000

    renesas: rzg: Add support for DRAM initialization
    Add support for initializing DRAM on RZ/G2M SoC.
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Reviewed-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Change-Id: I99f1a6971a061a44687af498d55306a93e4fc8f7

---
+++ drivers/renesas/rzg/ddr/ddr.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2015-2020, Renesas Electronics Corporation. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include drivers/renesas/rzg/ddr/ddr_b/ddr_b.mk
+
+BL2_SOURCES += drivers/renesas/rzg/ddr/dram_sub_func.c
