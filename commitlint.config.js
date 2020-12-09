c8d22c408ec143c87635526bdc2c3dd51476690f
Chris Kay <chris.kay@arm.com>
2020-12-09 16:52:03 +0000

    build(hooks): add commitlint hook
    This change adds a configuration for commitlint - a tool designed to
    enforce a particular commit message style - and run it as part of Git's
    commit-msg hook. This validates commits immediately after the editor has
    been exited, and the configuration is derived from the configuration we
    provide to Commitizen.
    
    While the configuration provided enforces a maximum header length, it
    only warns about a breach of the maximum body line length. This is
    predominantly because URLs may span longer than the given 72 characters.
    
    Change-Id: Ib5e90472fd1f1da9c2bff47703c9682232ee5679
    Signed-off-by: Chris Kay <chris.kay@arm.com>

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+
+"use strict";
+
+const config = require("./.cz.json");
+
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "header-max-length": [1, "always", config.maxHeaderWidth], /* Warning */
+        "body-max-line-length": [1, "always", config.maxLineWidth] /* Warning */
+    }
+};
