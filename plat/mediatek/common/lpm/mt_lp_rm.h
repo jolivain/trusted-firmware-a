3f1d8e432bdf3df516bb00cfe285a1bd2b486502
Roger Lu <roger.lu@mediatek.com>
2020-12-14 16:53:22 +0800

    mediatek: mt8192: Add lpm driver
    Low Power Management (LPM) helps find a suitable configuration
    for letting system entering idle or suspend with the most
    resources off.
    
    Change-Id: Ie6a7063b666cf338cff5bc972c9025b26de482eb
    Signed-off-by: Roger Lu <roger.lu@mediatek.com>

---
+++ plat/mediatek/common/lpm/mt_lp_rm.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2020, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MT_LP_RM
+#define MT_LP_RM
+
+#include <stdbool.h>
+
+#define MT_RM_STATUS_OK		0
+#define MT_RM_STATUS_BAD	-1
+
+enum PLAT_MT_LPM_RC_TYPE {
+	PLAT_RC_UPDATE_CONDITION,
+	PLAT_RC_UPDATE_REMAIN_IRQS
+};
+
+struct mt_resource_constraint {
+	int level;
+	int (*init)(void);
+	bool (*is_valid)(int cpu, int stateid);
+	int (*update)(int stateid, int type, const void *p);
+	int (*run)(int cpu, int stateid);
+	int (*reset)(int cpu, int stateid);
+	unsigned int (*allow)(int stateid);
+};
+
+struct mt_resource_manager {
+	int (*update)(struct mt_resource_constraint **con,
+		      int stateid, void *priv);
+	struct mt_resource_constraint **consts;
+};
+
+extern int mt_lp_rm_register(struct mt_resource_manager *rm);
+extern int mt_lp_rm_find_and_run_constraint(int idx, int cpuid,
+					    int stateid, void *priv);
+extern int mt_lp_rm_reset_constraint(int constraint_id, int cpuid, int stateid);
+extern int mt_lp_rm_do_update(int stateid, int type, void const *p);
+#endif /* MT_LP_RM */
