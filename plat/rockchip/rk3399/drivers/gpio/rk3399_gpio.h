27bd6912b77094f22eec042c3a1ce3664f78cd52
Jona Stubbe <tf-a@jona-stubbe.de>
2020-12-22 13:58:56 +0100

    fix(plat/rockchip/rk3399): mux out any GPIO used for poweroff and reset
    The Pinebook Pro and RockPro64 use GPIO1A6, the TSADC thermal shutdown pin
    as a hard reset signal. The previous code accounted for this kind of use,
    but only for poweroff, not reset, and only for GPIO1A6. Add a GPIO IOMUX
    function that can be used for all GPIOs and use it in poweroff and reset.
    
    Signed-off-by: Jona Stubbe <tf-a@jona-stubbe.de>
    Change-Id: I4468e2998212b3ac6db08caf59e0c4224fcfb24c
    Signed-off-by: Jimmy Brisson <jimmy.brisson@arm.com>

---
+++ plat/rockchip/rk3399/drivers/gpio/rk3399_gpio.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020, Jona Stubbe. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef RK3399_GPIO_H
+#define RK3399_GPIO_H
+
+void rk3399_mux_out_gpio(int gpio);
+
+#endif /* RK3399_GPIO_H */
