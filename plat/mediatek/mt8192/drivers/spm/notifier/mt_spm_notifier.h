67f888c2d45ae43cc8368064135dc59153d88eca
Roger Lu <roger.lu@mediatek.com>
2021-01-04 00:02:34 +0800

    mediatek: mt8192: Add spm suspend driver
    Supports dram/mainpll/26m off when system suspend
    
    Signed-off-by: Roger Lu <roger.lu@mediatek.com>
    Change-Id: Id13a06d4132f00fb60066de75920ecac18306e32

---
+++ plat/mediatek/mt8192/drivers/spm/notifier/mt_spm_notifier.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MT_SPM_SSPM_NOTIFIER_H
+#define MT_SPM_SSPM_NOTIFIER_H
+
+enum MT_SPM_SSPM_NOTIFY_ID {
+	MT_SPM_NOTIFY_LP_ENTER,
+	MT_SPM_NOTIFY_LP_LEAVE,
+};
+
+int mt_spm_sspm_notify(int type, const void *val);
+
+static inline int mt_spm_sspm_notify_u32(int type, unsigned int val)
+{
+	unsigned r_val = val;
+
+	return mt_spm_sspm_notify(type, (const void *)&r_val);
+}
+#endif /* MT_SPM_SSPM_NOTIFIER_H */
