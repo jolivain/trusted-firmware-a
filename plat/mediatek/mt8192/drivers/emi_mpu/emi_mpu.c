6a241409170a0bac266f78867eb327b15c224de4
Xi Chen <xixi.chen@mediatek.com>
2021-01-06 16:05:01 +0800

    mediatek: mt8192: only enable Domain1:PCIe access 0x80000000:64MB
    Signed-off-by: Xi Chen <xixi.chen@mediatek.com>
    Change-Id: Ic4f9e6d85bfd1cebdb24ffc1d14309c89c103b2a

--- plat/mediatek/mt8192/drivers/emi_mpu/emi_mpu.c
+++ plat/mediatek/mt8192/drivers/emi_mpu/emi_mpu.c
@@ -94,7 +94,7 @@ void emi_mpu_init(void)
 	/* Set permission */
 	struct emi_region_info_t region_info;
 
-	/* PCE-e protect address(TODO) */
+	/* PCE-e protect address(64MB) */
 	region_info.start = 0x80000000ULL;
 	region_info.end = 0x83FF0000ULL;
 	region_info.region = 1;
@@ -102,8 +102,7 @@ void emi_mpu_init(void)
 			      FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
 			      FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
 			      FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
-			      FORBIDDEN, FORBIDDEN, NO_PROT,
-			      NO_PROT /*FORBIDDEN*/);
+			      FORBIDDEN, FORBIDDEN, NO_PROT, FORBIDDEN);
 	emi_mpu_set_protection(&region_info);
 
 	/* Forbidden All */
