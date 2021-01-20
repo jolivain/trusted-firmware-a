a79e26de1b56fb127e3e48e11bf3184bb84cbc63
Jimmy Brisson <jimmy.brisson@arm.com>
2021-01-20 15:34:51 -0600

    cert-tool: avoid duplicates in extension stack
    This bug manifests itself as a segfault triggered by a double-free.
    
    I noticed that right before the double-free, the sk list contained 2
    elements with the same address.
    
        (gdb) p sk_X509_EXTENSION_value(sk, 1)
        $34 = (X509_EXTENSION *) 0x431ad0
        (gdb) p sk_X509_EXTENSION_value(sk, 0)
        $35 = (X509_EXTENSION *) 0x431ad0
        (gdb) p sk_X509_EXTENSION_num(sk)
        $36 = 2
    
    This caused confusion; this should never happen.
    
    I figured that this was caused by a ext_new_xxxx function freeing
    something before it is added to the list, so I put a breakpoint on
    each of them to step through. I was suprised to find that none of my
    breakpoints triggered for the second element of the iteration through
    the outer loop just before the double-free.
    
    Looking through the code, I noticed that it's possible to avoid doing
    a ext_new_xxxx, when either:
       * ext->type == NVCOUNTER and ext->arg == NULL
       * ext->type == HASH and ext->arg == NULL and ext->optional == false
    So I put a breakpoint on both.
    
    It turns out that it was the HASH version, but I added a fix for both.
    This should no longer cause double-frees by adding the same ext twice.
    
    Change-Id: Idae2a24ecd964b0a3929e6193c7f85ec769f6470
    Signed-off-by: Jimmy Brisson <jimmy.brisson@arm.com>

--- tools/cert_create/src/main.c
+++ tools/cert_create/src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2020, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2015-2021, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -496,6 +496,9 @@ int main(int argc, char *argv[])
 					nvctr = atoi(ext->arg);
 					CHECK_NULL(cert_ext, ext_new_nvcounter(ext_nid,
 						EXT_CRIT, nvctr));
+				} else {
+					ERROR("NVCounter does not have a value\n");
+					exit(1);
 				}
 				break;
 			case EXT_TYPE_HASH:
@@ -505,7 +508,7 @@ int main(int argc, char *argv[])
 						memset(md, 0x0, SHA512_DIGEST_LENGTH);
 					} else {
 						/* Do not include this hash in the certificate */
-						break;
+						continue;
 					}
 				} else {
 					/* Calculate the hash of the file */
