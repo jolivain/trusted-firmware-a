a3de43fa5ee85da1be036e8f79f09bfea4022fc8
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2021-01-23 10:55:12 +0000

    plat/arm: fvp: Do not map GIC region in BL1 and BL2
    GIC memory region is not getting used in BL1 and BL2.
    Hence avoid its mapping in BL1 and BL2 that freed some
    page table entries to map other memory regions in the
    future.
    
    Retains mapping of CCN interconnect region in BL1 and BL2
    overlapped with the GIC memory region.
    
    Change-Id: I880dd0690f94b140e59e4ff0c0d436961b9cb0a7
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>

--- plat/arm/board/fvp/fvp_common.c
+++ plat/arm/board/fvp/fvp_common.c
@@ -70,7 +70,9 @@ const mmap_region_t plat_arm_mmap[] = {
 	V2M_MAP_FLASH0_RW,
 	V2M_MAP_IOFPGA,
 	MAP_DEVICE0,
+#if FVP_INTERCONNECT_DRIVER == FVP_CCN
 	MAP_DEVICE1,
+#endif
 #if TRUSTED_BOARD_BOOT
 	/* To access the Root of Trust Public Key registers. */
 	MAP_DEVICE2,
@@ -86,7 +88,9 @@ const mmap_region_t plat_arm_mmap[] = {
 	V2M_MAP_FLASH0_RW,
 	V2M_MAP_IOFPGA,
 	MAP_DEVICE0,
+#if FVP_INTERCONNECT_DRIVER == FVP_CCN
 	MAP_DEVICE1,
+#endif
 	ARM_MAP_NS_DRAM1,
 #ifdef __aarch64__
 	ARM_MAP_DRAM2,
