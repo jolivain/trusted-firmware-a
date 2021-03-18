f64c04a303e4460391bf746425bdd222703bc8a2
David Horstmann <david.horstmann@arm.com>
2021-03-18 15:20:54 +0000

    arm_fpga: Enable MMU
    Enable the MMU for the arm_fpga platform.
    
    Signed-off-by: David Horstmann <david.horstmann@arm.com>
    Change-Id: I33eff8d3808ed475ba1eb47e7fafdfdb1d5b2a99

---
+++ plat/arm/board/arm_fpga/fpga_common.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2021, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <lib/xlat_tables/xlat_tables_compat.h>
+
+#include "fpga_private.h"
+
+#define FPGA_PRELOADED_DTB						\
+				MAP_REGION_FLAT(			\
+					FPGA_PRELOADED_DTB_BASE,	\
+					FPGA_MAX_DTB_SIZE,		\
+					MT_MEMORY | MT_RW | MT_SECURE)
+
+/* Needs to be big enough to contain the fpga cmdline. */
+#define FPGA_CMD_LINE_SIZE	U(0x1000)
+/* The cmdline must be loaded in as device memory. */
+#define FPGA_CMD_LINE		MAP_REGION_FLAT(			\
+					FPGA_PRELOADED_CMD_LINE,	\
+					FPGA_CMD_LINE_SIZE,		\
+					MT_DEVICE | MT_RO | MT_SECURE)
+
+#define FPGA_UART_BASE		U(0x7ff80000)
+#define FPGA_UART_SIZE		U(0x1000)
+
+#define FPGA_UART		MAP_REGION_FLAT(			\
+					FPGA_UART_BASE,			\
+					FPGA_UART_SIZE,			\
+					MT_DEVICE | MT_RW | MT_SECURE)
+
+#define FPGA_GICD_BASE		U(0x30000000)
+#define FPGA_GICD_SIZE		U(0x10000)
+
+#define FPGA_GICD		MAP_REGION_FLAT(			\
+					FPGA_GICD_BASE,			\
+					FPGA_GICD_SIZE,			\
+					MT_DEVICE | MT_RW | MT_SECURE)
+
+#define FPGA_GICR_BASE		U(0x30040000)
+#define FPGA_GICR_SIZE		(U(0x20000) * PLATFORM_CORE_COUNT)
+
+#define FPGA_GICR		MAP_REGION_FLAT(			\
+					FPGA_GICR_BASE,			\
+					FPGA_GICR_SIZE,			\
+					MT_DEVICE | MT_RW | MT_SECURE)
+
+const mmap_region_t plat_arm_mmap[] = {
+	ARM_MAP_SHARED_RAM,
+	FPGA_UART,
+	FPGA_GICD,
+	FPGA_GICR,
+	FPGA_PRELOADED_DTB,
+	FPGA_CMD_LINE,
+	{0}
+};
