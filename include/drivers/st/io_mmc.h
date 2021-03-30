5d2269b9b5e96e7ea786817a60fc4a2f4d230d54
Vyacheslav Yurkov <uvv.mail@gmail.com>
2021-03-30 09:12:40 +0200

    stm32mp1: added STM32MP_EMMC_BOOT option
    Added a new STM32MP_EMMC_BOOT option, which is used to look for SSBL in the
    same eMMC boot partition TF-A booted from at a fixed 256k offset. In
    case STM32 image header is not found, the boot process rolls back to a
    GPT partition look-up scheme.
    
    Signed-off-by: Vyacheslav Yurkov <uvv.mail@gmail.com>
    Change-Id: I85a87dc9ae7f2b915ed8e584be80f4b3588efc48

--- include/drivers/st/io_mmc.h
+++ include/drivers/st/io_mmc.h
@@ -9,6 +9,10 @@
 
 #include <drivers/io/io_driver.h>
 
+typedef struct io_mmc_dev_spec {
+	bool use_boot_part;
+} io_mmc_dev_spec_t;
+
 int register_io_dev_mmc(const io_dev_connector_t **dev_con);
 
 #endif /* IO_MMC_H */
