e23ad6aff4706192b4f8eeec40864f5b204499c9
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2021-04-22 14:41:27 +0100

    feat(plat/arm): add Zlib support provision
    Added a provision to enable/disable the support of zip library in
    BL2 for Arm platforms.
    
    Zip library provides CRC calculation support, will be used in
    in the upcoming firmware update patches.
    
    Change-Id: Ia2ae801f62d2003e89a9c3e6d77469b5312614b3
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>

--- lib/zlib/tf_gunzip.c
+++ lib/zlib/tf_gunzip.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2018-2021, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -99,4 +99,16 @@ int gunzip(uintptr_t *in_buf, size_t in_len, uintptr_t *out_buf,
 	inflateEnd(&stream);
 
 	return ret;
+}
+
+/* tf_crc32 - wrapper function to calculate CRC
+ * @crc: previous accumulated CRC
+ * @buf: buffer base address
+ * @size: size of the buffer
+ *
+ * Return calculated CRC value
+ */
+int tf_crc32(unsigned long crc, unsigned char *buf, size_t size)
+{
+	return crc32(crc, buf, size);
 }
