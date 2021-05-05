bb0c5ccf4e9e78459a68ea8a2040296fac05e12e
Chris Kay <chris.kay@arm.com>
2021-05-05 13:38:30 +0100

    feat(mpmm): add support for MPMM
    MPMM - the Maximum Power Mitigation Mechanism - is an optional
    microarchitectural feature present on some Armv9-A cores, introduced
    with the Cortex-X2, Cortex-A710 and Cortex-A510 cores.
    
    MPMM allows the SoC firmware to detect and limit high activity events
    to assist in SoC processor power domain dynamic power budgeting and
    limit the triggering of whole-rail (i.e. clock chopping) responses to
    overcurrent conditions.
    
    This feature is enabled via the `ENABLE_MPMM` build option.
    Configuration can be done via FCONF by enabling `ENABLE_MPMM_FCONF`, or
    by via the plaform-implemented `plat_mpmm_topology` function.
    
    Change-Id: I77da82808ad4744ece8263f0bf215c5a091c3167
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- include/arch/aarch64/arch_helpers.h
+++ include/arch/aarch64/arch_helpers.h
@@ -540,6 +540,10 @@ DEFINE_RENAME_SYSREG_RW_FUNCS(hcrx_el2, HCRX_EL2)
 /* DynamIQ Shared Unit power management */
 DEFINE_RENAME_SYSREG_RW_FUNCS(clusterpwrdn_el1, CLUSTERPWRDN_EL1)
 
+/* CPU Power/Performance Management registers */
+DEFINE_RENAME_SYSREG_RW_FUNCS(cpuppmcr_el3, CPUPPMCR_EL3)
+DEFINE_RENAME_SYSREG_RW_FUNCS(cpumpmmcr_el3, CPUMPMMCR_EL3)
+
 #define IS_IN_EL(x) \
 	(GET_EL(read_CurrentEl()) == MODE_EL##x)
 
