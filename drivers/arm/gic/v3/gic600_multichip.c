9ef99afc005d2e53fae231632f64a6ef6b8dca59
Andre Przywara <andre.przywara@arm.com>
2021-05-18 15:46:58 +0100

    feat(gicv3): multichip: detect GIC-700 at runtime
    At the moment we have a GIC_ENABLE_V4_EXTN build time variable to
    determine whether the GIC interrupt controller is compliant to version
    4.0 of the GIC spec or not.
    In case of the GIC-600 multichip support we were somewhat abusing that
    flag to differentiate between a GIC-700 and GIC-600 implementation
    being used in the system.
    
    To avoid a build time dependency on this flag, look at the GICD_IIDR
    register and check if the hardware is a GIC-600 or not, to make this
    decision at runtime. We then use the values for either GIC-700 or the
    GIC-600, respectively.
    
    Change-Id: I8c09ec1cd6fd60d28da879ed55ffef5506f9869d
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- drivers/arm/gic/v3/gic600_multichip.c
+++ drivers/arm/gic/v3/gic600_multichip.c
@@ -13,6 +13,7 @@
 #include <common/debug.h>
 #include <drivers/arm/gic600_multichip.h>
 #include <drivers/arm/gicv3.h>
+#include <drivers/arm/arm_gicv3_common.h>
 
 #include "../common/gic_common_private.h"
 #include "gic600_multichip_private.h"
@@ -73,6 +74,7 @@ static void set_gicd_chipr_n(uintptr_t base,
 				unsigned int spi_id_max)
 {
 	unsigned int spi_block_min, spi_blocks;
+	unsigned int gicd_iidr_val = gicd_read_iidr(base);
 	uint64_t chipr_n_val;
 
 	/*
@@ -100,8 +102,16 @@ static void set_gicd_chipr_n(uintptr_t base,
 	spi_block_min = SPI_BLOCK_MIN_VALUE(spi_id_min);
 	spi_blocks    = SPI_BLOCKS_VALUE(spi_id_min, spi_id_max);
 
-	chipr_n_val = (GICD_CHIPR_VALUE(chip_addr, spi_block_min, spi_blocks)) |
-		GICD_CHIPRx_SOCKET_STATE;
+	if ((gicd_iidr_val & IIDR_MODEL_MASK) == IIDR_MODEL_ARM_GIC_600) {
+		chipr_n_val = GICD_CHIPR_VALUE_GIC_600(chip_addr,
+						       spi_block_min,
+						       spi_blocks);
+	} else {
+		chipr_n_val = GICD_CHIPR_VALUE_GIC_700(chip_addr,
+						       spi_block_min,
+						       spi_blocks);
+	}
+	chipr_n_val |= GICD_CHIPRx_SOCKET_STATE;
 
 	/*
 	 * Wait for DCHIPR.PUP to be zero before commencing writes to
