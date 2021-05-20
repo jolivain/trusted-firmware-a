51450866557605fb3775f201ff6fb2b6910cafbd
Chris Kay <chris.kay@arm.com>
2021-05-20 13:22:43 +0100

    build(fconf)!: clean up source collection
    Including the FCONF Makefile today automatically places the FCONF
    sources into the source list of the BL1 and BL2 images. This may be
    undesirable if, for instance, FCONF is only required for BL31.
    
    This change moves the BL1 and BL2 source appends out of the common
    Makefile to where they are required.
    
    BREAKING CHANGE: FCONF is no longer added to BL1 and BL2 automatically
    when the FCONF Makefile (`fconf.mk`) is included. When including this
    Makefile, consider whether you need to add `${FCONF_SOURCES}` and
    `${FCONF_DYN_SOURCES}` to `BL1_SOURCES` and `BL2_SOURCES`.
    
    Change-Id: Ic028eabb7437ae95a57c5bcb7821044d31755c77
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- lib/fconf/fconf.mk
+++ lib/fconf/fconf.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019-2020, ARM Limited. All rights reserved.
+# Copyright (c) 2019-2021, ARM Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -7,6 +7,3 @@
 # Add Firmware Configuration files
 FCONF_SOURCES		:=	lib/fconf/fconf.c
 FCONF_DYN_SOURCES	:=	lib/fconf/fconf_dyn_cfg_getter.c
-
-BL1_SOURCES		+=	${FCONF_SOURCES} ${FCONF_DYN_SOURCES}
-BL2_SOURCES		+=	${FCONF_SOURCES} ${FCONF_DYN_SOURCES}
