582a1759129daf8b51935f76088562500bf8ac72
Chris Kay <chris.kay@arm.com>
2021-05-27 17:57:19 +0100

    feat(mpmm): add support for MPMM
    MPMM - the Maximum Power Mitigation Mechanism - is an optional
    microarchitectural feature present on some Armv9-A cores, introduced
    with the Cortex-X2, Cortex-A710 and Cortex-A510 cores.
    
    Change-Id: I77da82808ad4744ece8263f0bf215c5a091c3167
    Signed-off-by: Chris Kay <chris.kay@arm.com>

---
+++ lib/mpmm/mpmm.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (c) 2021, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include lib/extensions/amu/amu.mk
+include lib/fconf/fconf.mk
+
+ifneq ($(ARCH),aarch64)
+	$(error MPMM is only supported when building for AArch64)
+endif
+
+ifeq (${ENABLE_MPMM},1)
+	ifneq (${ENABLE_AMU},1)
+		$(error MPMM support requires AMU support (see `ENABLE_AMU`))
+	endif
+
+	ifneq (${ENABLE_AMU_FCONF},1)
+		$(error MPMM support requires AMU FCONF support (see `ENABLE_AMU_FCONF`))
+	endif
+endif
+
+MPMM_SOURCES	+=	lib/mpmm/mpmm.c \
+			lib/mpmm/mpmm_fconf.c
+
+MPMM_SOURCES	+=	${AMU_SOURCES} \
+			${FCONF_SOURCES} ${FCONF_DYN_SOURCES}
