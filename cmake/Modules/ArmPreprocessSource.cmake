a2a23d3493a9fa3c93bf33ee5b4688500ec424af
Chris Kay <chris.kay@arm.com>
2021-06-25 16:31:11 +0100

    build(cmake): [wip] add source preprocessing helper
    CMake offers no way to preprocess files, which means that we have to do
    this ourself through an amalgamation of manual compiler-specific logic.
    
    This commit adds a single function to preprocess an arbitrary file using
    the preprocessor for a given language (assembler, C, or C++) and output
    the result to another file. This preprocessing is done at build-time,
    and the resulting file can be used as a dependency for other targets.
    
    Change-Id: Ia80e3336f3647acfeba002dda4bc0b2428d1c2a6
    Signed-off-by: Chris Kay <chris.kay@arm.com>

---
+++ cmake/Modules/ArmPreprocessSource.cmake
@@ -0,0 +1,186 @@
+#[=======================================================================[.rst:
+ArmPreprocessSource
+-------------------
+
+.. default-domain:: cmake
+
+.. command:: arm_preprocess_source
+
+Preprocess a file.
+
+.. code:: cmake
+
+    arm_preprocess_source([INHIBIT_LINEMARKERS]
+        OUTPUT <output> TARGET <target> SOURCE <source> LANGUAGE <language>)
+
+Creates a target ``<target>`` which preprocesses a source file ``<source>`` and
+outputs the result to ``<output>``.
+
+To pass preprocessor definitions, include directories or command line options to
+the preprocessor, you can apply the following properties to the target
+``<target>``:
+
+ - :prop_tgt:`COMPILE_OPTIONS <prop_tgt:COMPILE_OPTIONS>`
+ - :prop_tgt:`COMPILE_DEFINITIONS <prop_tgt:COMPILE_DEFINITIONS>`
+ - :prop_tgt:`INCLUDE_DIRECTORIES <prop_tgt:INCLUDE_DIRECTORIES>`
+
+For example, if you wish to preprocess a file ``foo.c`` with the preprocessor
+definition ``-DFOO=BAR`` you might use:
+
+.. code:: cmake
+
+    arm_preprocess_source(
+        TARGET foo
+        SOURCE "foo.c"
+        OUTPUT "foo.c.i"
+        LANGUAGE C)
+
+    set_target_properties(foo
+        PROPERTIES COMPILE_DEFINITIONS "FOO=BAR")
+
+.. note::
+
+    The created target automatically inherits :variable:`CMAKE_<LANG>_FLAGS
+    <variable:CMAKE_<LANG>_FLAGS>` and :variable:`CMAKE_<LANG>_FLAGS_<CONFIG>
+    <variable:CMAKE_<LANG>_FLAGS_<CONFIG>>`.
+#]=======================================================================]
+
+include_guard()
+
+include(ArmAssert)
+
+function(arm_preprocess_source)
+    set(options "INHIBIT_LINEMARKERS")
+    set(single-args "TARGET;SOURCE;OUTPUT;LANGUAGE")
+    set(multi-args "")
+
+    cmake_parse_arguments(PARSE_ARGV 0 _ARM_PREPROCESS_SOURCE
+        "${options}" "${single-args}" "${multi-args}")
+
+    arm_assert(
+        CONDITION DEFINED _ARM_PREPROCESS_SOURCE_TARGET
+        MESSAGE "No value was given for the `TARGET` argument.")
+
+    arm_assert(
+        CONDITION DEFINED _ARM_PREPROCESS_SOURCE_SOURCE
+        MESSAGE "No value was given for the `SOURCE` argument.")
+
+    arm_assert(
+        CONDITION DEFINED _ARM_PREPROCESS_SOURCE_OUTPUT
+        MESSAGE "No value was given for the `OUTPUT` argument.")
+
+    arm_assert(
+        CONDITION DEFINED _ARM_PREPROCESS_SOURCE_LANGUAGE
+        MESSAGE "No value was given for the `LANGUAGE` argument.")
+
+    set(target "${_ARM_PREPROCESS_SOURCE_TARGET}")
+    set(source "${_ARM_PREPROCESS_SOURCE_SOURCE}")
+    set(output "${_ARM_PREPROCESS_SOURCE_OUTPUT}")
+    set(language "${_ARM_PREPROCESS_SOURCE_LANGUAGE}")
+
+    set(inhibit-linemarkers "${_ARM_PREPROCESS_SOURCE_INHIBIT_LINEMARKERS}")
+
+    #
+    # Make the source path absolute so that there are no issues with path
+    # resolution during preprocessing.
+    #
+
+    cmake_path(ABSOLUTE_PATH source NORMALIZE
+        BASE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+    cmake_path(ABSOLUTE_PATH output NORMALIZE
+        BASE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
+
+    #
+    # CMake's toolchain detection logic creates a special variable -
+    # `CMAKE_<LANGUAGE>_CREATE_PREPROCESSED_SOURCE` - which describes the
+    # command line required to preprocess a source file for that language. For
+    # example, here it is for the GNU C preprocessor:
+    #
+    #     <CMAKE_C_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -E <SOURCE> >
+    #     <PREPROCESSED_SOURCE>
+    #
+    # We do some processing on this variable to convert these
+    # bracket-surrounded names to variables we set. For example, `<DEFINES>`
+    # is replaced with `@DEFINES@`. We then need to do some string replacement
+    # magic to expand that string out to the value of the actual variable.
+    #
+    # The values for some of these, namely include directories, definitions
+    # and other compiler options, come from properties set on the target by
+    # the caller. These are typically taken from the target that this
+    # preprocessed source file belongs to.
+    #
+
+    arm_assert(
+        CONDITION DEFINED CMAKE_${language}_CREATE_PREPROCESSED_SOURCE
+        MESSAGE "Unable to determine the preprocessor command line for the "
+                "${language} language. Please report this to the developer.")
+
+    set(command "${CMAKE_${language}_CREATE_PREPROCESSED_SOURCE}")
+
+    #
+    # Ensure the paths we provide to the command line are in OS-native form. We
+    # don't want the compiler complaining that the paths are malformed.
+    #
+
+    cmake_path(NATIVE_PATH source NORMALIZE SOURCE)
+    cmake_path(NATIVE_PATH output NORMALIZE PREPROCESSED_SOURCE)
+
+    #
+    # Split up the command into a list.
+    #
+
+    separate_arguments(command NATIVE_COMMAND "${command}")
+
+    set(FLAGS "$<TARGET_PROPERTY:${target},COMPILE_OPTIONS>")
+    set(DEFINES "$<TARGET_PROPERTY:${target},COMPILE_DEFINITIONS>")
+    set(INCLUDES "$<TARGET_PROPERTY:${target},INCLUDE_DIRECTORIES>")
+
+    #
+    # Until we come across an exception to the rule, use `-D` for definitions
+    # and `-I` for include directories.
+    #
+
+    set(DEFINES "$<$<BOOL:${DEFINES}>:-D$<JOIN:${DEFINES},$<SEMICOLON>-D>>")
+    set(INCLUDES "$<$<BOOL:${INCLUDES}>:-I$<JOIN:${INCLUDES},$<SEMICOLON>-I>>")
+
+    #
+    # Apply compiler-specific behaviours. The default assumption is that we're
+    # using a toolchain that behaves like GCC, as many tools attempt to emulate
+    # it.
+    #
+
+    if(language STREQUAL "ASM")
+        list(APPEND FLAGS "-x" "assembler-with-cpp")
+    elseif(language STREQUAL "C")
+        list(APPEND FLAGS "-x" "c")
+    elseif(language STREQUAL "CXX")
+        list(APPEND FLAGS "-x" "c++")
+    endif()
+
+    if(inhibit-linemarkers)
+        list(APPEND FLAGS "-P")
+    endif()
+
+    #
+    # Here's the magic part. For every angle bracket-wrapped value in the
+    # command, replace it with its at-variable form, then replace it with the
+    # value of that variable.
+    #
+
+    string(REGEX REPLACE "<([[A-Z_]+)>" [[@\1@]] command "${command}")
+    arm_expand(OUTPUT command STRING "${command}" ATONLY)
+
+    #
+    # Finally, add the command which generates the preprocessed file.
+    #
+
+    add_custom_target(${target}
+        SOURCES "${source}"
+        DEPENDS "${output}")
+
+    add_custom_command(
+        OUTPUT "${output}"
+        MAIN_DEPENDENCY "${source}"
+        COMMAND "${command}"
+        VERBATIM COMMAND_EXPAND_LISTS)
+endfunction()
