21c5645586fa5b5bb9e7041fc9282b417f107976
Sughosh Ganu <sughosh.ganu@linaro.org>
2021-07-02 16:00:40 +0530

    feat(fwu): add basic definitions for GUID handling
    The FWU metadata structure uses GUID's to identify the updatable
    firmware images. Add the type for using the GUID datatype defined by
    the UEFI specification. Also add some basic helper functions and
    macros that would be used for working with the GUID datatype.
    
    With the FWU feature enabled, these would then be used for image
    identification and booting of images from a particular
    bank(partition).
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>
    Change-Id: Ia54c0402d72b503d6abd1d94bc751cc14602cd39

---
+++ include/drivers/partition/efi.h
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2021, Linaro Limited
+ */
+
+#ifndef EFI_H
+#define EFI_H
+
+#include <cdefs.h>
+#include <stdint.h>
+
+typedef struct {
+	uint8_t b[16];
+} efi_guid_t __aligned(8);
+
+static inline int guidcmp(const void *g1, const void *g2)
+{
+	return memcmp(g1, g2, sizeof(efi_guid_t));
+}
+
+static inline void *guidcpy(void *dst, const void *src)
+{
+	return memcpy(dst, src, sizeof(efi_guid_t));
+}
+
+#define EFI_GUID(a, b, c, d0, d1, d2, d3, d4, d5, d6, d7) \
+	{{ (a) & 0xff, ((a) >> 8) & 0xff, ((a) >> 16) & 0xff, \
+		((a) >> 24) & 0xff, \
+		(b) & 0xff, ((b) >> 8) & 0xff, \
+		(c) & 0xff, ((c) >> 8) & 0xff, \
+		(d0), (d1), (d2), (d3), (d4), (d5), (d6), (d7) } }
+
+#define NULL_GUID \
+	EFI_GUID(0x00000000, 0x0000, 0x0000, 0x00, 0x00, \
+		 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
+
+#endif /* EFI_H */
