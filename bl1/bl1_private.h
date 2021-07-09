95d4b95df8e212f60a6bee3ba331d94582a34936
Zelalem Aweke <zelalem.aweke@arm.com>
2021-07-09 11:37:10 -0500

    feat(rme): run BL2 in root world when FEAT_RME is enabled
    This patch enables BL2 to run in root world (EL3) which is
    needed as per the security model of RME-enabled systems.
    
    Using the existing BL2_AT_EL3 TF-A build option is not convenient
    because that option assumes TF-A BL1 doesn't exist, which is not
    the case for RME-enabled systems. For the purposes of RME, we use
    a normal BL1 image but we also want to run BL2 in EL3 as normally as
    possible, therefore rather than use the special bl2_entrypoint
    function in bl2_el3_entrypoint.S, we use a new bl2_entrypoint
    function (in bl2_rme_entrypoint.S) which doesn't need reset or
    mailbox initialization code seen in the el3_entrypoint_common macro.
    
    The patch also cleans up bl2_el3_entrypoint.S, moving the
    bl2_run_next_image function to its own file to avoid duplicating
    code.
    
    Signed-off-by: Zelalem Aweke <zelalem.aweke@arm.com>
    Change-Id: I99821b4cd550cadcb701f4c0c4dc36da81c7ef55

--- bl1/bl1_private.h
+++ bl1/bl1_private.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2013-2021, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -11,6 +11,8 @@
 
 #include <common/bl_common.h>
 
+extern entry_point_info_t *bl2_ep_info;
+
 /******************************************
  * Function prototypes
  *****************************************/
@@ -18,6 +20,9 @@ void bl1_arch_setup(void);
 void bl1_arch_next_el_setup(void);
 
 void bl1_prepare_next_image(unsigned int image_id);
+#if ENABLE_RME
+void bl1_run_bl2_in_root(void);
+#endif
 
 u_register_t bl1_fwu_smc_handler(unsigned int smc_fid,
 		u_register_t x1,
