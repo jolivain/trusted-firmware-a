469fdac008087dd4bb37a3fb0b435efaa8c369ca
Chris Kay <chris.kay@arm.com>
2021-07-19 16:57:32 +0100

    build(cmake): add assertion helper
    Adds a very small assertion helper function to simplify validation in
    some of the larger functions introduced in later commits.
    
    This function simply takes a condition and a message, and will abort the
    build with the message if the condition fails.
    
    Signed-off-by: Chris Kay <chris.kay@arm.com>
    Change-Id: Ib33c3580c0e464ec945a56501d68fbccaae7ed72

---
+++ cmake/Modules/ArmAssert.cmake
@@ -0,0 +1,50 @@
+#[=======================================================================[.rst:
+ArmAssert
+---------
+
+.. default-domain:: cmake
+
+.. command:: arm_assert
+
+Assert an invariant, and fail the build if the invariant is broken.
+
+.. code:: cmake
+
+    arm_assert(
+        CONDITION <condition>
+        MESSAGE <message)
+
+This function takes a condition ``<condition>`` in :ref:`Condition Syntax`,
+evaluates it, and fails the build with the message ``<message>`` if evaluation
+does not yield a truthy result.
+
+Example
+^^^^^^^
+
+.. code:: cmake
+
+    arm_assert(
+        CONDITION STACK GREATER_EQUAL 256
+        MESSAGE "The stack must be at least 256 bytes.")
+
+    # ... is functionally identical to...
+
+    if(NOT STACK GREATER_EQUAL 256)
+        message(FATAL_ERROR "The stack must be at least 256 bytes.")
+    endif()
+#]=======================================================================]
+
+include_guard()
+
+function(arm_assert)
+    set(options "")
+    set(single-args "")
+    set(multi-args "CONDITION;MESSAGE")
+
+    cmake_parse_arguments(PARSE_ARGV 0 _ARM_ASSERT
+        "${options}" "${single-args}" "${multi-args}")
+
+    if(NOT (${_ARM_ASSERT_CONDITION}))
+        message(FATAL_ERROR ${_ARM_ASSERT_MESSAGE})
+    endif()
+endfunction()
