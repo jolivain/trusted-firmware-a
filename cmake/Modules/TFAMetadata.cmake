b759e21888960101cbabb51fa5b1d7bb15508557
Chris Kay <chris.kay@arm.com>
2021-07-21 12:29:13 +0100

    build(cmake): add platform selection logic
    This change adds the initial platform selection logic to the CMake-based
    build system. This supports loading in-tree platforms only - support for
    out-of-tree platforms is added later.
    
    Platform selection at the user level is controlled by the `TFA_PLATFORM`
    cache variable. The valid values for this option are derived from the
    global metadata file.
    
    This adds the `platforms` key to the root of the global metadata, which
    accepts a path to the platform directory. For example:
    
    ```json
    {
      "platforms": {
        "FVP": "plat/arm/board/fvp",
        "Juno": "plat/arm/board/juno",
        ...
      }
    }
    ```
    
    Change-Id: I596a3e1c2573b6ec4a117073665b87d6c9a0ba6f
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- cmake/Modules/TFAMetadata.cmake
+++ cmake/Modules/TFAMetadata.cmake
@@ -9,6 +9,22 @@ Metadata management utilities for TF-A.
 TF-A uses a set of JSON-formatted metadata files to manage some of its
 configuration data. This module provides a stable CMake interface for retrieving
 values from these metadata files.
+
+.. command:: tfa_platforms
+
+.. code:: cmake
+
+    tfa_platforms(<out-var>)
+
+Return the list of supported platforms in ``<out-var>``.
+
+.. command:: tfa_platform_path
+
+.. code:: cmake
+
+    tfa_platform_path(<out-var> PLATFORM <platform>)
+
+Return the path to the platform ``<platform>`` in ``<out-var>``.
 #]=======================================================================]
 
 include_guard()
@@ -36,6 +52,24 @@ arm_expand(OUTPUT _TFA_METADATA STRING "${_TFA_METADATA}")
 tfa_json_getter(tfa_metadata
     JSON "${_TFA_METADATA}")
 
+tfa_json_getter(tfa_metadata_platforms
+    JSON "${_TFA_METADATA}" PARENT tfa_metadata
+    PATH "platforms")
+
+tfa_json_getter(tfa_metadata_platforms_platform
+    JSON "${_TFA_METADATA}" PARENT tfa_metadata_platforms
+    PATH "@PLATFORM@" ARGUMENTS PLATFORM
+    ERROR_MESSAGE "No such platform: @PLATFORM@.")
+
 #
 # External global metadata API.
 #
+
+tfa_json_getter(tfa_platforms
+    JSON "${_TFA_METADATA}" PARENT tfa_metadata_platforms
+    DECODE MEMBERS)
+
+tfa_json_getter(tfa_platform_path
+    JSON "${_TFA_METADATA}" PARENT tfa_metadata_platforms_platform
+    DECODE STRING)
+
