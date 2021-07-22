3f2d3a66ab250d97df9c581d9f0c704532b6ddae
Icenowy Zheng <icenowy@sipeed.com>
2021-07-22 09:41:16 +0800

    feat(plat/allwinner): add R329 support
    Allwinner R329 is a new dual-core Corte-A53 SoC. Add basical TF-A
    support for it, to provide a PSCI implemention containing CPU
    boot/shutdown and SoC reset/shutdown.
    
    Change-Id: I0fa37ee9b4a8e0e1137bf7cf7d614b6ca9624bfe
    Signed-off-by: Icenowy Zheng <icenowy@sipeed.com>

---
+++ plat/allwinner/sun50i_r329/include/sunxi_mmap.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2017-2019, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SUNXI_MMAP_H
+#define SUNXI_MMAP_H
+
+/* Memory regions */
+#define SUNXI_ROM_BASE			0x00000000
+#define SUNXI_ROM_SIZE			0x00010000
+#define SUNXI_SRAM_BASE			0x00100000
+#define SUNXI_SRAM_SIZE			0x001d0000
+/*
+ * Technically there's only SRAM A2, but part of SRAM A2 is used for SPL,
+ * and the memory map also shows a 2*64K/27*64K division.
+ *
+ * Fake the first 128K SRAM A2 as SRAM A1.
+ */
+#define SUNXI_SRAM_A1_BASE		0x00100000
+#define SUNXI_SRAM_A1_SIZE		0x00020000
+#define SUNXI_SRAM_A2_BASE		0x00120000
+#define SUNXI_SRAM_A2_SIZE		0x001b0000
+#define SUNXI_DEV_BASE			0x01000000
+#define SUNXI_DEV_SIZE			0x09000000
+#define SUNXI_DRAM_BASE			0x40000000
+#define SUNXI_DRAM_VIRT_BASE		0x0a000000
+
+/* Memory-mapped devices */
+#define SUNXI_WDOG_BASE			0x020000a0
+#define SUNXI_R_WDOG_BASE		SUNXI_WDOG_BASE
+#define SUNXI_PIO_BASE			0x02000400
+#define SUNXI_SPC_BASE			0x02000800
+#define SUNXI_CCU_BASE			0x02001000
+#define SUNXI_UART0_BASE		0x02500000
+#define SUNXI_SYSCON_BASE		0x03000000
+#define SUNXI_DMA_BASE			0x03002000
+#define SUNXI_SID_BASE			0x03006000
+#define SUNXI_GICD_BASE			0x03021000
+#define SUNXI_GICC_BASE			0x03022000
+#define SUNXI_SPI0_BASE			0x04025000
+#define SUNXI_R_CPUCFG_BASE		0x07000400
+#define SUNXI_R_PRCM_BASE		0x07010000
+#define SUNXI_R_PIO_BASE		0x07022000
+#define SUNXI_R_UART_BASE		0x07080000
+#define SUNXI_R_I2C_BASE		0x07081400
+#define SUNXI_CPUCFG_BASE		0x08100000
+#define SUNXI_C0_CPUXCFG_BASE		0x09010000
+
+#endif /* SUNXI_MMAP_H */
