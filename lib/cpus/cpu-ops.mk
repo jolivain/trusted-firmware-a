fe84c7fd792dd98278c5e233aab077519e610473
nayanpatel-arm <nayankumar.patel@arm.com>
2021-08-06 16:39:48 -0700

    errata: workaround for Cortex-A710 errata 1987031
    Cortex-A710 erratum 1987031 is a Cat B erratum present in r0p0, r1p0,
    and r2p0 of the Cortex-A710 processor core, and it is still open.
    
    A710 SDEN: https://documentation-service.arm.com/static/61099dc59ebe3a7dbd3a8a88?token=
    
    Signed-off-by: nayanpatel-arm <nayankumar.patel@arm.com>
    Change-Id: I9bcff306f82328ad5a0f6e9836020d23c07f7179

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -392,6 +392,10 @@ ERRATA_V1_1925756	?=0
 # to revisions r1p0 and r1p1 of the Neoverse V1 cpu.
 ERRATA_V1_1940577	?=0
 
+# Flag to apply erratum 1987031 workaround during reset. This erratum applies
+# to revisions r0p0, r1p0 and r2p0 of the Cortex-A710 cpu.
+ERRATA_A710_1987031	?=0
+
 # Flag to apply DSU erratum 798953. This erratum applies to DSUs revision r0p0.
 # Applying the workaround results in higher DSU power consumption on idle.
 ERRATA_DSU_798953	?=0
@@ -716,6 +720,10 @@ $(eval $(call add_define,ERRATA_V1_1925756))
 # Process ERRATA_V1_1940577 flag
 $(eval $(call assert_boolean,ERRATA_V1_1940577))
 $(eval $(call add_define,ERRATA_V1_1940577))
+
+# Process ERRATA_A710_1987031 flag
+$(eval $(call assert_boolean,ERRATA_A710_1987031))
+$(eval $(call add_define,ERRATA_A710_1987031))
 
 # Process ERRATA_DSU_798953 flag
 $(eval $(call assert_boolean,ERRATA_DSU_798953))
