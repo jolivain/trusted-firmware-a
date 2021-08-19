4c4e290d2c0d228163fb4194878cbc1c05050fbf
Chris Kay <chris.kay@arm.com>
2021-08-19 11:21:52 +0100

    feat(amu): enable auxiliary counters through FCONF
    This change makes AMU auxiliary counters configurable on a per-core
    basis through a new FCONF populator.
    
    These auxiliary counters must be provided via the hardware
    configuration device tree blob. This is done through a new phandle
    property for `cpu` nodes (`amu`):
    
    ```
    cpu0: cpu@0 {
        ...
    
        amu = <&cpu0_amu>;
    };
    ```
    
    Multiple cores may share an `amu` handle if they implement the
    same set of auxiliary counters.
    
    AMU counters are described through the use of a new `amus` node:
    
    ```
    amus {
        cpu0_amu: amu@0 {
            #address-cells = <1>;
            #size-cells = <0>;
    
            counter@0 {
                reg = <0>;
    
                enable-at-el3;
            };
    
            counter@n {
                reg = <n>;
    
                ...
            };
        };
    };
    ```
    
    This structure describes the **auxiliary** (group 1) AMU counters.
    Architected counters have architecturally-defined behaviour, and as
    such do not require DTB entries.
    
    These `counter` nodes support two properties:
    
    - The `reg` property represents the counter register index.
    - The presence of the `enable-at-el3` property determines whether
      the firmware should enable the counter prior to exiting EL3.
    
    Change-Id: Ie43aee010518c5725a3b338a4899b0857caf4c28
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- lib/extensions/amu/amu.mk
+++ lib/extensions/amu/amu.mk
@@ -6,5 +6,9 @@
 
 include lib/fconf/fconf.mk
 
-AMU_SOURCES	+=	lib/extensions/amu/${ARCH}/amu.c \
+AMU_SOURCES	+=	lib/extensions/amu/amu_fconf.c \
+			lib/extensions/amu/${ARCH}/amu.c \
 			lib/extensions/amu/${ARCH}/amu_helpers.S
+
+AMU_SOURCES	+=	${FCONF_SOURCES} ${FCONF_DYN_SOURCES}
+AMU_SOURCES	+=	common/fdt_wrappers.c
