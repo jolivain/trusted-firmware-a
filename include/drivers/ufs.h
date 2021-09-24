3c33f994254d119b6910ddc5479353a43cdb4010
Jorge Troncoso <jatron@google.com>
2021-09-23 17:14:29 -0700

    refactor(ufs): Reuse ufshc_send_uic_cmd
    This change aims to make the UFS code more robust by removing asserts
    and adding retry logic. We also reduce repetition by reusing
    ufshc_send_uic_cmd for DME_GET and DME_SET commands.
    
    Signed-off-by: Jorge Troncoso <jatron@google.com>
    Change-Id: Id70aa1687d5ca78dc7d47234372255ac5a04a612

--- include/drivers/ufs.h
+++ include/drivers/ufs.h
@@ -254,6 +254,9 @@
 #define UFS_VENDOR_SKHYNIX		U(0x1AD)
 
 #define MAX_MODEL_LEN 16
+
+#define UFS_UIC_COMMAND_RETRIES		10
+
 /**
  * ufs_dev_desc - ufs device details from the device descriptor
  * @wmanufacturerid: card details
