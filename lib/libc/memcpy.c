38c5c767bc880a48ba2720d242ba292ebbb84cd6
Nathan Dunne <Nathan.Dunne@arm.com>
2021-09-27 10:33:07 +0100

    libc memcpy performance improvement
    Using asm implementations of memcpy for libc when pointers
    are aligned. 16 bytes aligned for aarch64 and 8 bytes aligned
    for aarch32.
    
    Makefile is also updated to align images by default.
    
    Signed-off-by: Nathan Dunne <Nathan.Dunne@arm.com>
    Change-Id: I6df42dec29fdecf9184addac278bf1550237e370

--- lib/libc/memcpy.c
+++ lib/libc/memcpy.c
@@ -5,15 +5,39 @@
  */
 
 #include <stddef.h>
+#include <stdint.h>
 #include <string.h>
+
+#if __aarch64__
+/* AARCH64 */
+#define ALIGN 16
+extern void memcpy16(void *dest, const void *src, unsigned long length);
+
+#else
+/* AARCH32 */
+#define ALIGN 8
+extern void memcpy8(void *dest, const void *src, unsigned int length);
+#endif
+
+#define is_aligned(pointer) ( ((uintptr_t)pointer & (ALIGN - 1)) == 0 )
 
 void *memcpy(void *dst, const void *src, size_t len)
 {
-	const char *s = src;
-	char *d = dst;
+	if (len) {
+		if (is_aligned(dst) && is_aligned(src)) {
+#if __aarch64__
+			memcpy16(dst, src, len);
+#else
+			memcpy8(dst, src, len);
+#endif
+		}
+		else {
+			const char *s = src;
+			char *d = dst;
 
-	while (len--)
-		*d++ = *s++;
-
+			while (len--)
+				*d++ = *s++;
+		}
+	}
 	return dst;
 }
