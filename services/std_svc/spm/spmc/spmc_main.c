286819d29ec34f78cf3341442d510d4ad100c770
Achin Gupta <achin.gupta@arm.com>
2021-10-04 20:08:55 +0100

    feat(spmc): add support for FF-A power mgmt. messages in the EL3 SPMC
    This patch adds support for forwarding the following PSCI messages
    received by the SPMC at EL3 to the S-EL1 SP if the SP has indicated
    that it wishes to receive the appropriate message via its manifest.
    
    1. A PSCI CPU_OFF message in response to a cpu hot unplug request
       from the OS
    2. A message to indicate warm boot of a cpu in response to a cpu
       hot plug request from the OS
    
    This patch also implements the FFA_SECONDARY_EP_REGISTER function to
    enable the SP specify its secondary entrypoint.
    
    Signed-off-by: Achin Gupta <achin.gupta@arm.com>
    Signed-off-by: Marc Bonnici <marc.bonnici@arm.com>
    Change-Id: I375d0655b2c6fc27445facc39213d1d0678557f4

--- services/std_svc/spm/spmc/spmc_main.c
+++ services/std_svc/spm/spmc/spmc_main.c
@@ -230,13 +230,21 @@ static uint64_t spmc_smc_return(uint32_t smc_fid,
  ******************************************************************************/
 static inline bool direct_msg_validate_arg2(uint64_t x2)
 {
-	/*
-	 * We currently only support partition messages, therefore ensure x2 is
-	 * not set.
-	 */
-	if (x2 != (uint64_t) 0) {
-		VERBOSE("Arg2 MBZ for partition messages (0x%lx).\n", x2);
-		return false;
+	/* Check message type. */
+	if (x2 & FFA_DIRECT_FRAMEWORK_MSG_MASK) {
+		/* We have a framework message, ensure it is a PM message. */
+		if (x2 & (~FFA_PM_MSG_MASK &
+			  ~(FFA_DIRECT_FRAMEWORK_MSG_MASK))) {
+			VERBOSE("Unknown Framework message 0x%lx.\n", x2);
+			return false;
+		}
+	} else {
+		/* We have a partition messages, ensure x2 is not set. */
+		if (x2 != (uint64_t) 0) {
+			VERBOSE("Arg2 MBZ for partition messages. (0x%lx).\n",
+				x2);
+			return false;
+		}
 	}
 	return true;
 }
@@ -1084,6 +1092,7 @@ static uint64_t ffa_features_handler(uint32_t smc_fid,
 			/* Execution stops here. */
 
 		/* Supported features only from the secure world. */
+		case FFA_SECONDARY_EP_REGISTER_SMC64:
 		case FFA_MSG_SEND_DIRECT_RESP_SMC32:
 		case FFA_MSG_SEND_DIRECT_RESP_SMC64:
 		case FFA_MSG_WAIT:
@@ -1213,6 +1222,88 @@ static uint64_t rx_release_handler(uint32_t smc_fid,
 
 	mbox->state = MAILBOX_STATE_EMPTY;
 	spin_unlock(&mbox->lock);
+
+	SMC_RET1(handle, FFA_SUCCESS_SMC32);
+}
+
+/*
+ * Perform initial validation on the provided secondary entry point.
+ * For now ensure it does not lie within the BL31 Image or the SP's
+ * RX/TX buffers as these are mapped within EL3.
+ * TODO: perform validation for additional invalid memory regions.
+ */
+static int validate_secondary_ep(uintptr_t ep, struct secure_partition_desc *sp)
+{
+	struct mailbox *mb;
+	uintptr_t buffer_size;
+	uintptr_t sp_rx_buffer;
+	uintptr_t sp_tx_buffer;
+	uintptr_t sp_rx_buffer_limit;
+	uintptr_t sp_tx_buffer_limit;
+
+	mb = &sp->mailbox;
+	buffer_size = (uintptr_t) (mb->rxtx_page_count * FFA_PAGE_SIZE);
+	sp_rx_buffer = (uintptr_t) mb->rx_buffer;
+	sp_tx_buffer = (uintptr_t) mb->tx_buffer;
+	sp_rx_buffer_limit = sp_rx_buffer + buffer_size;
+	sp_tx_buffer_limit = sp_tx_buffer + buffer_size;
+
+	/*
+	 * Check if the entry point lies within BL31, or the
+	 * SP's RX or TX buffer.
+	 */
+	if ((ep >= BL31_BASE && ep < BL31_LIMIT) ||
+	    (ep >= sp_rx_buffer && ep < sp_rx_buffer_limit) ||
+	    (ep >= sp_tx_buffer && ep < sp_tx_buffer_limit)) {
+		return -EINVAL;
+	}
+	return 0;
+}
+
+/*******************************************************************************
+ * This function handles the FFA_SECONDARY_EP_REGISTER SMC to allow an SP to
+ *  register an entry point for initialization during a secondary cold boot.
+ ******************************************************************************/
+static uint64_t ffa_sec_ep_register_handler(uint32_t smc_fid,
+					    bool secure_origin,
+					    uint64_t x1,
+					    uint64_t x2,
+					    uint64_t x3,
+					    uint64_t x4,
+					    void *cookie,
+					    void *handle,
+					    uint64_t flags)
+{
+	struct secure_partition_desc *sp;
+
+	/* This request cannot originate from the Normal world. */
+	if (!secure_origin) {
+		return spmc_ffa_error_return(handle, FFA_ERROR_NOT_SUPPORTED);
+	}
+
+	/* Get the context of the current SP. */
+	sp = spmc_get_current_sp_ctx();
+	if (sp == NULL) {
+		return spmc_ffa_error_return(handle,
+					     FFA_ERROR_INVALID_PARAMETER);
+	}
+
+	/* Only an S-EL1 SP should be invoking this ABI. */
+	if (sp->runtime_el != S_EL1) {
+		return spmc_ffa_error_return(handle, FFA_ERROR_DENIED);
+	}
+
+	/* Perform initial validation of the secondary entry point. */
+	if (validate_secondary_ep(x1, sp)) {
+		return spmc_ffa_error_return(handle,
+					     FFA_ERROR_INVALID_PARAMETER);
+	}
+
+	/* Lock and update the secondary entrypoint in SP context. */
+	spin_lock(&sp->secondary_ep_lock);
+	sp->secondary_ep = x1;
+	VERBOSE("%s %lx\n", __func__, sp->secondary_ep);
+	spin_unlock(&sp->secondary_ep_lock);
 
 	SMC_RET1(handle, FFA_SUCCESS_SMC32);
 }
@@ -1610,6 +1701,9 @@ int32_t spmc_setup(void)
 		return ret;
 	}
 
+	/* Register power management hooks with PSCI */
+	psci_register_spd_pm_hook(&spmc_pm);
+
 	/* Register init function for deferred init.  */
 	bl31_register_bl32_init(&sp_init);
 
@@ -1644,6 +1738,11 @@ uint64_t spmc_smc_handler(uint32_t smc_fid,
 	case FFA_FEATURES:
 		return ffa_features_handler(smc_fid, secure_origin, x1, x2, x3,
 					    x4, cookie, handle, flags);
+
+	case FFA_SECONDARY_EP_REGISTER_SMC64:
+		return ffa_sec_ep_register_handler(smc_fid, secure_origin, x1,
+						   x2, x3, x4, cookie, handle,
+						   flags);
 
 	case FFA_MSG_SEND_DIRECT_REQ_SMC32:
 	case FFA_MSG_SEND_DIRECT_REQ_SMC64:
