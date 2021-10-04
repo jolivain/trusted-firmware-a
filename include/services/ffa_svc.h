c0fbc83fa55e621d9e271d053e5e7c4003137507
Achin Gupta <achin.gupta@arm.com>
2021-10-04 20:13:36 +0100

    feat(spmc): add FF-A support to the TSP
    This patch adds the FF-A programming model in the test
    secure payload to ensure that it can be used to the
    following spec features.
    
    1. SP initialisation on the primary and secondary cpus
    2. An event loop to receive direct requests and respond
       with direct responses
    3. Ability to receive messages that indicate power on
       and off of a cpu
    4. Ability to handle a secure interrupt and relinquish
       control in response to a non-secure interrupt.
    
    Signed-off-by: Achin Gupta <achin.gupta@arm.com>
    Signed-off-by: Marc Bonnici <marc.bonnici@arm.com>
    Change-Id: I81cf744904d5cdc0b27862b5e4bc6f2cfe58a13a

--- include/services/ffa_svc.h
+++ include/services/ffa_svc.h
@@ -67,6 +67,11 @@
 #define FFA_PM_MSG_WB_REQ	0x1 /* Warm boot request */
 #define FFA_PM_MSG_PM_RESP	0x2 /* Response to a PSCI or warmboot request */
 
+/* FF-A warm boot types. */
+#define FFA_WB_TYPE_S2RAM	0x0
+#define FFA_WB_TYPE_NOTS2RAM	0x1
+
+
 /* Get FFA fastcall std FID from function number */
 #define FFA_FID(smc_cc, func_num)			\
 		((SMC_TYPE_FAST << FUNCID_TYPE_SHIFT) |	\
