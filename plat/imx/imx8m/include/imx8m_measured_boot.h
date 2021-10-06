90fccc9c82ca719035ef3fe0c25d679d5fc053ea
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2021-10-06 09:27:00 +0800

    feat(plat/imx/imx8m/imx8mm): add support for measured boot
    Add helper functions to generate event log for imx8mm
    when MEASURED_BOOT=1.
    
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
    Change-Id: Ifc947d749055787fbda0b39170aa2eb8865b7802

---
+++ plat/imx/imx8m/include/imx8m_measured_boot.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, Linaro
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef IMX8M_MEASURED_BOOT_H
+#define IMX8M_MEASURED_BOOT_H
+
+#include <stdint.h>
+#include <arch_helpers.h>
+
+int imx8m_set_nt_fw_info(
+/*
+ * Currently OP-TEE does not support reading DTBs from Secure memory
+ * and this option should be removed when feature is supported.
+ */
+#ifdef SPD_opteed
+			uintptr_t log_addr,
+#endif
+			size_t log_size, uintptr_t *ns_log_addr);
+
+#endif /* IMX8M_MEASURED_BOOT_H */
