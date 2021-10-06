90231b46a031a6f7962f088ae2e519c8c48a14a5
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2021-10-06 09:27:00 +0800

    feat(plat/imx/imx8m/imx8mm): add support for measured boot
    Add helper functions to generate event log for imx8mm
    when MEASURED_BOOT=1.
    
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
    Change-Id: Ifc947d749055787fbda0b39170aa2eb8865b7802

---
+++ plat/imx/imx8m/include/imx8m_measured_boot.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Linaro
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef IMX8M_MEASURED_BOOT_H
+#define IMX8M_MEASURED_BOOT_H
+
+#include <stdint.h>
+#include <arch_helpers.h>
+
+int imx8m_set_nt_fw_info(size_t log_size, uintptr_t *ns_log_addr);
+
+#endif /* IMX8M_MEASURED_BOOT_H */
