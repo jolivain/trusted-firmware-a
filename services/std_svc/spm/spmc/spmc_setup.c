f5c807a0549601ccd6d81ea4bfb940662cc84dc1
Achin Gupta <achin.gupta@arm.com>
2021-10-19 12:21:16 +0100

    feat(spmc): add support for FFA boot protocol
    A partition can request the use of the FFA boot protocol via
    an entry in it's manifest along with the register (0-3)
    that should be used to populate a point to a data structure
    containing boot related information. Currently the boot
    information consists of an allocated memory region
    containing the SP's manifest, allowing it to map and parse
    any extra information as required.
    
    Note: This data structure is subject to change from feedback
    and will be updated accordingly.
    
    Signed-off-by: Achin Gupta <achin.gupta@arm.com>
    Signed-off-by: Marc Bonnici <marc.bonnici@arm.com>
    Change-Id: I67692553a90a7e7d94c64fe275edd247b512efca

--- services/std_svc/spm/spmc/spmc_setup.c
+++ services/std_svc/spm/spmc/spmc_setup.c
@@ -12,6 +12,8 @@
 #include <arch_helpers.h>
 #include <context.h>
 #include <common/debug.h>
+#include <common/fdt_wrappers.h>
+#include <libfdt.h>
 #include <lib/el3_runtime/context_mgmt.h>
 #include <lib/xlat_tables/xlat_tables_v2.h>
 #include <lib/utils.h>
@@ -19,10 +21,77 @@
 #include <plat/common/common_def.h>
 #include <plat/common/platform.h>
 #include <services/ffa_svc.h>
+#include <tools_share/firmware_image_package.h>
 
 #include "spm_common.h"
 #include "spmc.h"
 
+/*
+ * Statically allocate a page of memory for passing boot information to an SP.
+ */
+static uint8_t ffa_boot_info_mem[PAGE_SIZE] __aligned(PAGE_SIZE);
+
+/*
+ * This function creates a initialization descriptor in the memory reserved
+ * for passing boot information to an SP. It then copies the partition manifest
+ * into this region and ensures that its reference in the initialization descriptor
+ * is updated. Finally, it calls another helper function to populate any generic
+ * boot information in the copied manifest inplace.
+ */
+static void spmc_create_boot_info(entry_point_info_t *ep_info)
+{
+	struct ffa_init_desc *ffa_boot_info;
+	unsigned int max_manifest_sz;
+	uintptr_t manifest_addr;
+	uuid_t manifest_uuid = UUID_TOS_FW_CONFIG;
+
+	/*
+	 * Calculate the maximum size of the manifest that can be accommodated
+	 * in the boot information memory region.
+	 */
+	max_manifest_sz = sizeof(ffa_boot_info_mem) - (sizeof(struct ffa_init_desc)
+						       + sizeof(ffa_nvs_tuple_t));
+
+	/*
+	 * Check if the manifest will fit into the boot info memory region else
+	 * bail.
+	 */
+	if (ep_info->args.arg1 > max_manifest_sz) {
+		WARN("Unable to copy manifest into boot information. ");
+		WARN("Max sz = %u bytes. Manifest sz = %lu bytes\n",
+		     max_manifest_sz, ep_info->args.arg1);
+		return;
+	}
+
+	/* Create initialisation descriptor in boot info region */
+	ffa_boot_info = (struct ffa_init_desc *) ffa_boot_info_mem;
+
+	/* Set the magic number "FF-A" */
+	ffa_boot_info->magic = FFA_INIT_DESC_MAGIC;
+
+	/* Set the count. Currently 1 since only the manifest is specified */
+	ffa_boot_info->count = 1;
+
+	/* Copy the UUID of the manifest into the boot information */
+	memcpy((void *) ffa_boot_info->nvp[0].name, (void *) &manifest_uuid, sizeof(uuid_t));
+
+	/*
+	 * Copy the manifest into boot info region after the initialization
+	 * descriptor.
+	 */
+	manifest_addr = (uintptr_t) (ffa_boot_info_mem + sizeof(struct ffa_init_desc)
+				     + sizeof(ffa_nvs_tuple_t));
+	memcpy((void *) manifest_addr, (void *) ep_info->args.arg0, ep_info->args.arg1);
+
+	/* Set the address and size of the manifest */
+	ffa_boot_info->nvp[0].value = manifest_addr;
+	ffa_boot_info->nvp[0].size = ep_info->args.arg1;
+
+	INFO("SP boot info @ 0x%lx, manifest @ 0x%lx, size %lu bytes\n",
+	     (uintptr_t) ffa_boot_info_mem,
+	     ffa_boot_info->nvp[0].value,
+	     ffa_boot_info->nvp[0].size);
+}
 
 unsigned int get_ec_index(sp_desc_t *sp)
 {
@@ -30,7 +99,9 @@ unsigned int get_ec_index(sp_desc_t *sp)
 }
 
 /* SEL1 partition specific initialisation. */
-void spmc_el1_sp_setup(sp_desc_t *sp, entry_point_info_t *ep_info)
+void spmc_el1_sp_setup(sp_desc_t *sp,
+		       entry_point_info_t *ep_info,
+		       int32_t boot_info_reg)
 {
 	/* Sanity check input arguments */
 	assert(sp != NULL);
@@ -63,7 +134,9 @@ void spmc_el1_sp_setup(sp_desc_t *sp, entry_point_info_t *ep_info)
 }
 
 /* Common initialisation for all SPs. */
-void spmc_sp_common_setup(sp_desc_t *sp, entry_point_info_t *ep_info)
+void spmc_sp_common_setup(sp_desc_t *sp,
+			  entry_point_info_t *ep_info,
+			  int32_t boot_info_reg)
 {
 	cpu_context_t *cpu_ctx;
 	uint16_t sp_id;
@@ -89,6 +162,34 @@ void spmc_sp_common_setup(sp_desc_t *sp, entry_point_info_t *ep_info)
 	 */
 	ep_info->spsr =	SPSR_64(MODE_EL1, MODE_SP_ELX,
 				DISABLE_ALL_EXCEPTIONS);
+
+	/* Zero the general purpose registers before we populate as required. */
+	zeromem(&ep_info->args, sizeof(ep_info->args));
+
+	/* Check if the SP wants to use the FF-A boot protocol. */
+	if (boot_info_reg >= 0) {
+		/*
+		 * Create a boot information descriptor, copy the partition manifest and
+		 * populate any fields in it inplace.
+		 */
+		spmc_create_boot_info(ep_info);
+
+		/* Pass the address of the boot information in the boot_info_reg */
+		switch (boot_info_reg) {
+		case 0:
+			ep_info->args.arg0 = (uintptr_t) ffa_boot_info_mem;
+			break;
+		case 1:
+			ep_info->args.arg1 = (uintptr_t) ffa_boot_info_mem;
+			break;
+		case 2:
+			ep_info->args.arg2 = (uintptr_t) ffa_boot_info_mem;
+			break;
+		case 3:
+			ep_info->args.arg3 = (uintptr_t) ffa_boot_info_mem;
+			break;
+		}
+	}
 
 	/*
 	 * Initialise the SP context based upon the entrypoint information
