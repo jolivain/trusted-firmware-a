ea2d07b258aed9cc29493950a9bc1e81dbad852a
Achin Gupta <achin.gupta@arm.com>
2021-10-19 12:21:16 +0100

    feat(spmc): add support for FFA boot protocol
    A partition can request the use of the FFA boot protocol via
    an entry in it's manifest along with the register (0-3)
    that should be used to populate a point to a data structure
    containing boot related information. Currently the boot
    information consists of an allocated memory region
    containing the SP's manifest, allowing it to map and parse
    any extra information as required.
    
    Note: This data structure is subject to change from feedback
    and will be updated accordingly.
    
    Signed-off-by: Achin Gupta <achin.gupta@arm.com>
    Signed-off-by: Marc Bonnici <marc.bonnici@arm.com>
    Change-Id: I67692553a90a7e7d94c64fe275edd247b512efca

--- services/std_svc/spm/spmc/spmc_main.c
+++ services/std_svc/spm/spmc/spmc_main.c
@@ -1298,7 +1298,8 @@ static uint64_t ffa_sec_ep_register_handler(uint32_t smc_fid,
  ******************************************************************************/
 static int sp_manifest_parse(void *sp_manifest, int offset,
 			     sp_desc_t *sp,
-			     entry_point_info_t *ep_info)
+			     entry_point_info_t *ep_info,
+			     int32_t *boot_info_reg)
 {
 	int32_t ret, node;
 	uint32_t config_32;
@@ -1413,6 +1414,20 @@ static int sp_manifest_parse(void *sp_manifest, int offset,
 		sp->pwr_mgmt_msgs = config_32;
 	}
 
+	ret = fdt_read_uint32(sp_manifest, node,
+			      "gp-register-num", &config_32);
+	if (ret != 0) {
+		WARN("Missing boot information register.\n");
+	} else {
+		/* Check if a register number between 0-3 is specified. */
+		if (config_32 < 4) {
+			*boot_info_reg = config_32;
+		} else {
+			WARN("Incorrect boot information register (%u).\n",
+			     config_32);
+		}
+	}
+
 	return 0;
 }
 
@@ -1428,7 +1443,7 @@ static int find_and_prepare_sp_context(void)
 	uintptr_t manifest_base;
 	uintptr_t manifest_base_align;
 	entry_point_info_t *next_image_ep_info;
-	int32_t ret;
+	int32_t ret, boot_info_reg = -1;
 	sp_desc_t *sp;
 
 	next_image_ep_info = bl31_plat_get_next_image_ep_info(SECURE);
@@ -1486,7 +1501,7 @@ static int find_and_prepare_sp_context(void)
 	SET_PARAM_HEAD(next_image_ep_info, PARAM_EP, VERSION_1, SECURE | EP_ST_ENABLE);
 
 	/* Parse the SP manifest. */
-	ret = sp_manifest_parse(sp_manifest, ret, sp, next_image_ep_info);
+	ret = sp_manifest_parse(sp_manifest, ret, sp, next_image_ep_info, &boot_info_reg);
 	if (ret != 0) {
 		ERROR(" Error in Secure Partition(SP) manifest parsing.\n");
 		return ret;
@@ -1499,10 +1514,10 @@ static int find_and_prepare_sp_context(void)
 	}
 
 	/* Perform any common initialisation. */
-	spmc_sp_common_setup(sp, next_image_ep_info);
+	spmc_sp_common_setup(sp, next_image_ep_info, boot_info_reg);
 
 	/* Perform any initialisation specific to S-EL1 SP's. */
-	spmc_el1_sp_setup(sp, next_image_ep_info);
+	spmc_el1_sp_setup(sp, next_image_ep_info, boot_info_reg);
 
 	/* Initialize the SP context with the required ep info. */
 	spmc_sp_common_ep_commit(sp, next_image_ep_info);
