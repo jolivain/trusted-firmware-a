44fc7c5cc8890f6f34735e7778b0742805faa804
Flora Fu <flora.fu@mediatek.com>
2021-11-08 16:37:39 +0800

    feat(plat/mediatek/apu): add mt8195 APU clock and pll Sip call
    The clock and pll of mt8195 can be locked into security access
    by device apc. Add clock and pll related SiP call for the access
    from Kernel space.
    
    Signed-off-by: Flora Fu <flora.fu@mediatek.com>
    Change-Id: I0c1f7d6c6abdd3b976492a0b776dc5b1d1f1512b

--- plat/mediatek/mt8195/drivers/apusys/mtk_apusys.h
+++ plat/mediatek/mt8195/drivers/apusys/mtk_apusys.h
@@ -12,6 +12,10 @@
 /* setup the SMC command ops */
 #define MTK_SIP_APU_START_MCU	0x00U
 #define MTK_SIP_APU_STOP_MCU	0x01U
+#define MTK_SIP_APUPWR_BUS_PROT_CG_ON	0x02U
+#define MTK_SIP_APUPWR_BULK_PLL		0x03U
+#define MTK_SIP_APUPWR_ACC_INIT_ALL	0x04U
+#define MTK_SIP_APUPWR_ACC_TOP		0x05U
 
 /* AO Register */
 #define AO_MD32_PRE_DEFINE	(APUSYS_APU_S_S_4_BASE + 0x00)
