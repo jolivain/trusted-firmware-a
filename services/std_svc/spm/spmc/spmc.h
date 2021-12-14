77395ef89dfc8f8305e124cd45bac8bed7535c28
Marc Bonnici <marc.bonnici@arm.com>
2021-12-14 18:26:10 +0000

    feat(spmc): allow for an SP to subscribe to PM messages
    Enable an SP to subscribe to power management messages.
    Each bit in the supplied value represents a different
    power management message.
    
    Note: Currently only CPU_OFF is supported.
    
    Signed-off-by: Marc Bonnici <marc.bonnici@arm.com>
    Change-Id: I8ef953820ffd8ec3ef22dbb1d2b890a2b3d6d187

--- services/std_svc/spm/spmc/spmc.h
+++ services/std_svc/spm/spmc/spmc.h
@@ -51,6 +51,11 @@
 #define FFA_RXTX_PAGE_COUNT_MASK 0x1F
 
 /*
+ * Defines to allow an SP to subscribe for power management messages
+ */
+#define FFA_PM_MSG_SUB_CPU_OFF		0x1
+
+/*
  * Runtime states of an execution context as per the FF-A v1.1 specification.
  */
 enum runtime_states {
@@ -178,6 +183,12 @@ typedef struct secure_partition_desc {
 	 * Lock to protect the secondary entrypoint update in a SP descriptor.
 	 */
 	spinlock_t secondary_ep_lock;
+
+	/*
+	 * Store whether the SP has subscribed to any power management messages.
+	 */
+	uint16_t pwr_mgmt_msgs;
+
 } sp_desc_t;
 
 /*
