7032defb00268c784df290295dfe4557d5a3846a
Federico Recanati <federico.recanati@arm.com>
2021-12-23 11:01:11 +0100

    fix(fvp): extend memory map to include all DRAM memory regions
    Currently only the lowest 2 DRAM region were configured in the
    TrustZone Controller, but the platform supports 6 regions spanning the
    whole address space.
    Configuring all of them to allow tests to access memory also in those
    higher memory regions.
    
    FVP memory map:
    https://developer.arm.com/documentation/100964/1116/Base-Platform/Base---memory/Base-Platform-memory-map
    Note that last row is wrong, describing a non-existing 56bit address,
    all region labels should be shifted upward.
    Issue has been reported and next release will be correct.
    
    Change-Id: I695fe8e24aff67d75e74635ba32a133342289eb4
    Signed-off-by: Federico Recanati <federico.recanati@arm.com>

--- plat/arm/board/fvp/fvp_security.c
+++ plat/arm/board/fvp/fvp_security.c
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2014-2020, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2014-2022, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <plat/arm/common/arm_config.h>
 #include <plat/arm/common/plat_arm.h>
+#include <platform_def.h>
 
 /*
  * We assume that all security programming is done by the primary core.
@@ -21,6 +22,24 @@ void plat_arm_security_setup(void)
 	 * configurations, those would be configured here.
 	 */
 
+	const arm_tzc_regions_info_t fvp_tzc_regions[] = {
+		{ARM_AP_TZC_DRAM1_BASE, ARM_EL3_TZC_DRAM1_END + ARM_L1_GPT_SIZE,
+		 TZC_REGION_S_RDWR, 0},
+		{ARM_NS_DRAM1_BASE, ARM_NS_DRAM1_END, ARM_TZC_NS_DRAM_S_ACCESS,
+		 PLAT_ARM_TZC_NS_DEV_ACCESS},
+		{ARM_DRAM2_BASE, ARM_DRAM2_END, ARM_TZC_NS_DRAM_S_ACCESS,
+		 PLAT_ARM_TZC_NS_DEV_ACCESS},
+		{FVP_DRAM3_BASE, FVP_DRAM3_END,
+		 ARM_TZC_NS_DRAM_S_ACCESS, PLAT_ARM_TZC_NS_DEV_ACCESS},
+		{FVP_DRAM4_BASE, FVP_DRAM4_END,
+		 ARM_TZC_NS_DRAM_S_ACCESS, PLAT_ARM_TZC_NS_DEV_ACCESS},
+		{FVP_DRAM5_BASE, FVP_DRAM5_END,
+		 ARM_TZC_NS_DRAM_S_ACCESS, PLAT_ARM_TZC_NS_DEV_ACCESS},
+		{FVP_DRAM6_BASE, FVP_DRAM6_END,
+		 ARM_TZC_NS_DRAM_S_ACCESS, PLAT_ARM_TZC_NS_DEV_ACCESS},
+		{0}
+	};
+
 	if ((get_arm_config()->flags & ARM_CONFIG_HAS_TZC) != 0U)
-		arm_tzc400_setup(PLAT_ARM_TZC_BASE, NULL);
+		arm_tzc400_setup(PLAT_ARM_TZC_BASE, fvp_tzc_regions);
 }
