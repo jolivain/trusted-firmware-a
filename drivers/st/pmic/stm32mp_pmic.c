1f75cf51aec170b6072115e92314a18b7f78144e
Yann Gautier <yann.gautier@st.com>
2022-01-06 09:35:35 +0100

    fix(st-pmic): correct verbose message
    Replace %d with %u in log, to avoid warning when
    -Wformat-signedness is enabled.
    
    Change-Id: Ied5823520181f225ae09bd164e2e52e9a7692c60
    Signed-off-by: Yann Gautier <yann.gautier@st.com>

--- drivers/st/pmic/stm32mp_pmic.c
+++ drivers/st/pmic/stm32mp_pmic.c
@@ -337,7 +337,7 @@ enum {
 
 static int pmic_set_state(const struct regul_description *desc, bool enable)
 {
-	VERBOSE("%s: set state to %u\n", desc->node_name, enable);
+	VERBOSE("%s: set state to %d\n", desc->node_name, enable);
 
 	if (enable == STATE_ENABLE) {
 		return stpmic1_regulator_enable(desc->node_name);
