59bf2f07af9cba53a70a7a85b519ca58a5f796ce
johpow01 <john.powell@arm.com>
2022-01-07 17:12:31 -0600

    fix(errata): workaround for Cortex A510 erratum 2042739
    Cortex A510 erratum 2042739 is a Cat B erratum present in the A510 core.
    It applies to revisions r0p0 - r0p2 and is fixed in r0p3.
    
    SDEN can be found here:
    https://developer.arm.com/documentation/SDEN2397239
    
    Signed-off-by: John Powell <john.powell@arm.com>
    Change-Id: I1d2ebee3914396e1e298eb45bdab35ce9e194ad9

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -508,12 +508,16 @@ ERRATA_X2_2058056	?=0
 ERRATA_X2_2083908	?=0
 
 # Flag to apply erratum 1922240 workaround during reset. This erratum applies
-# to revision r0p0 of the Cortex-A510 cpu and is fixed in r0p1
+# to revision r0p0 of the Cortex-A510 cpu and is fixed in r0p1.
 ERRATA_A510_1922240	?=0
 
 # Flag to apply erratum 2288014 workaround during reset. This erratum applies
-# to revision r0p0 of the Cortex-A510 cpu and is fixed in r0p1
+# to revision r0p0 of the Cortex-A510 cpu and is fixed in r0p1.
 ERRATA_A510_2288014	?=0
+
+# Flag to apply erratum 2042739 workaround during reset. This erratum applies
+# to revisions r0p0 - r0p2 of the Cortex-A510 cpu and is fixed in r0p3.
+ERRATA_A510_2042739	?=0
 
 # Flag to apply DSU erratum 798953. This erratum applies to DSUs revision r0p0.
 # Applying the workaround results in higher DSU power consumption on idle.
@@ -959,6 +963,10 @@ $(eval $(call add_define,ERRATA_A510_1922240))
 # Process ERRATA_A510_2288014 flag
 $(eval $(call assert_boolean,ERRATA_A510_2288014))
 $(eval $(call add_define,ERRATA_A510_2288014))
+
+# Process ERRATA_A510_2042739 flag
+$(eval $(call assert_boolean,ERRATA_A510_2042739))
+$(eval $(call add_define,ERRATA_A510_2042739))
 
 # Process ERRATA_DSU_798953 flag
 $(eval $(call assert_boolean,ERRATA_DSU_798953))
