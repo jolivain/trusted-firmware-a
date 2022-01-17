64ef0aed25d223ca1dc4d3edb3f6cbaa3fc378f5
Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
2022-01-17 18:57:17 +0000

    refactor(detect-arch-feat): add arch-features detection mechanism
    This patch adds architectural features detection procedure to ensure
    features enabled are present in the given hardware implementation.
    It verifies whether the arch build flags passed during compilation,
    match the supported features by reading all the feature specific
    ID registers at once and panics in case of mismatch.
    Henceforth the build flags are validated at an early phase prior to
    their usage, thereby preventing any undefined action under their control.
    
    This implementation is based on tristate approach for each feature and
    currently FEAT_STATE=0 and FEAT_STATE=1 are covered as part of this patch.
    
    FEAT_STATE=2 is planned for phase-2 implementation and will be taken
    care separately.
    
    Reference:
    https://confluence.arm.com/display/BSGSoftware/TF-A+Architectural+Features+Detection
    
    Signed-off-by: Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
    Change-Id: Ia23d95430fe82d417a938b672bfb5edc401b0f43

---
+++ include/common/feat_detect.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef FEAT_DETECT
+#define FEAT_DETECT
+
+#include <arch_features.h>
+#include <common/debug.h>
+
+/* Function Prototypes */
+void detect_arch_features(void);
+
+/* Macro Definitions */
+#define FEAT_STATE_1	1
+#define FEAT_STATE_2	2
+#define feat_detect_panic(a, b)		((a) ? (void)0 : features_panic(b))
+
+/*******************************************************************************
+ * Function : features_panic
+ * Customised panic module with error logging mechanism to list all the features
+ * not supported by the PE.
+ ******************************************************************************/
+static inline void features_panic(char *feat_name)
+{
+	ERROR("FEAT_%s not supported by the PE\n", feat_name);
+	panic();
+}
+
+#endif /* FEAT_DETECT */
