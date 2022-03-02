8676e212a226b93747a82572c530fd2214915cdb
Sieu Mun Tang <sieu.mun.tang@intel.com>
2022-03-02 11:04:09 +0800

    build(intel): initial commit for crypto driver
    This patch adds driver for Intel FPGA's Crypto Services.
    These services are provided by Intel platform
    Secure Device Manager(SDM) and are made accessible by
    processor components (ie ATF).
    Below is the list of enabled features:
    - Send SDM certificates
    - Efuse provision data dump
    - Encryption/decryption service
    - Hardware IP random number generator
    
    Signed-off-by: Abdul Halim, Muhammad Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
    Signed-off-by: Sieu Mun Tang <sieu.mun.tang@intel.com>
    Change-Id: If7604cd1cacf27a38a9a29ec6b85b07385e1ea26

--- plat/intel/soc/common/socfpga_sip_svc.c
+++ plat/intel/soc/common/socfpga_sip_svc.c
@@ -10,6 +10,7 @@
 #include <lib/mmio.h>
 #include <tools_share/uuid.h>
 
+#include "socfpga_fcs.h"
 #include "socfpga_mailbox.h"
 #include "socfpga_reset_manager.h"
 #include "socfpga_sip_svc.h"
@@ -430,9 +431,9 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 			 u_register_t flags)
 {
 	uint32_t retval = 0;
-	uint32_t status = INTEL_SIP_SMC_STATUS_OK;
 	uint32_t completed_addr[3];
 	uint64_t rsu_respbuf[9];
+	int status = INTEL_SIP_SMC_STATUS_OK;
 	int mbox_status;
 	unsigned int len_in_resp;
 	u_register_t x5, x6;
