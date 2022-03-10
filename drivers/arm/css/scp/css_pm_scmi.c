65543e48bea8bb34ccd27084880ac12b03eedd83
Pranav Madhu <pranav.madhu@arm.com>
2022-03-10 23:42:39 +0530

    feat(css): disconnect all CPUs from redistributor on power down
    For shutdown and reboot scenarios, only one core will invoke the PSCI
    and enter trusted firmware. In this case, the secondary CPUs will be in
    non-secure context and in WFI. In platforms with GIC v3 and above,
    it is required to disconnect the secondaty CPUs from the redistributor.
    
    Change-Id: I2a8ace4d38dd883cb6d6c02017c27b083fb41394
    Signed-off-by: Pranav Madhu <pranav.madhu@arm.com>

--- drivers/arm/css/scp/css_pm_scmi.c
+++ drivers/arm/css/scp/css_pm_scmi.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2020, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2017-2022, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -289,6 +289,19 @@ int css_scp_get_power_state(u_register_t mpidr, unsigned int power_level)
 void __dead2 css_scp_system_off(int state)
 {
 	int ret;
+	int core_pos;
+
+	/*
+	 * Mark all secondary CPUs as asleep to prevent pending interrupt from
+	 * waking.
+	 */
+	for (core_pos = 0; core_pos < PLATFORM_CORE_COUNT; core_pos++) {
+		if (core_pos == plat_my_core_pos()) {
+			/* Skip for primary core */
+			continue;
+		}
+		plat_arm_gic_mark_core_asleep(core_pos);
+	}
 
 	/*
 	 * Disable GIC CPU interface to prevent pending interrupt from waking
