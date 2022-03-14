29c854eec26e16c29f23bf402cb768fcda75af97
Sieu Mun Tang <sieu.mun.tang@intel.com>
2022-03-15 03:10:10 +0800

    feat(intel): add support for F2S and S2F bridge SMC with mask to enable, disable and reset bridge
    This adds F2S and S2F bridge enable, disable and reset
    sequence to enable, disable and reset properly the bridges
    in SMC call or during reset.
    
    The reset is also maskable as the SMC from uboot can
    pass in the bridge mask when requesting for bridge
    enable or disable.
    
    Signed-off-by: Loh Tien Hock <tien.hock.loh@intel.com>
    Signed-off-by: Sieu Mun Tang <sieu.mun.tang@intel.com>
    Change-Id: Ie144518c591664ef880016c9b3706968411bbf21

--- plat/intel/soc/stratix10/include/socfpga_plat_def.h
+++ plat/intel/soc/stratix10/include/socfpga_plat_def.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2019-2022, ARM Limited and Contributors. All rights reserved.
- * Copyright (c) 2019-2022, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2019-2022, Intel Corporation. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -19,6 +19,8 @@
 #define INTEL_SIP_SMC_FPGA_CONFIG_SIZE		0x1000000
 
 /* Register Mapping */
+#define SOCFPGA_F2SDRAMMGR_REG_BASE		U(0xf8024000)
+
 #define SOCFPGA_MMC_REG_BASE                    0xff808000
 
 #define SOCFPGA_RSTMGR_REG_BASE			0xffd11000
