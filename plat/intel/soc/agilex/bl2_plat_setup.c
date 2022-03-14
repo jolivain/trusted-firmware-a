c6d17c384f1ca800b6b0d69aa158525ceb250a29
Sieu Mun Tang <sieu.mun.tang@intel.com>
2022-03-15 03:10:10 +0800

    feat(intel): add support for F2S and S2F bridge SMC with mask to enable, disable and reset bridge
    This adds F2S and S2F bridge enable, disable and reset
    sequence to enable, disable and reset properly the bridges
    in SMC call or during reset.
    
    The reset is also maskable as the SMC from uboot can
    pass in the bridge mask when requesting for bridge
    enable or disable.
    
    Signed-off-by: Loh Tien Hock <tien.hock.loh@intel.com>
    Signed-off-by: Sieu Mun Tang <sieu.mun.tang@intel.com>
    Change-Id: Ie144518c591664ef880016c9b3706968411bbf21

--- plat/intel/soc/agilex/bl2_plat_setup.c
+++ plat/intel/soc/agilex/bl2_plat_setup.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2019-2021, ARM Limited and Contributors. All rights reserved.
- * Copyright (c) 2019-2021, Intel Corporation. All rights reserved.
+ * Copyright (c) 2019-2022, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2019-2022, Intel Corporation. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -28,6 +28,7 @@
 #include "socfpga_private.h"
 #include "socfpga_reset_manager.h"
 #include "socfpga_system_manager.h"
+#include "socfpga_f2sdram_manager.h"
 #include "wdt/watchdog.h"
 
 static struct mmc_device_info mmc_info;
@@ -81,8 +82,10 @@ void bl2_el3_early_platform_setup(u_register_t x0, u_register_t x1,
 	mailbox_init();
 	agx_mmc_init();
 
-	if (!intel_mailbox_is_fpga_not_ready())
-		socfpga_bridges_enable();
+	if (!intel_mailbox_is_fpga_not_ready()) {
+		socfpga_bridges_enable(SOC2FPGA_MASK | LWHPS2FPGA_MASK |
+			FPGA2SOC_MASK);
+	}
 }
 
 
