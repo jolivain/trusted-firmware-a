bdfaff319b4ca05ac67a253e0a32b5cafb70de27
Sieu Mun Tang <sieu.mun.tang@intel.com>
2022-03-17 04:25:21 +0800

    fix(intel): update encryption and decryption command logic
    This change is to re-align HPS cryption logic with
    underlying Secure Device Manager's (SDM) mailbox API.
    
    Signed-off-by: Abdul Halim, Muhammad Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
    Signed-off-by: Sieu Mun Tang <sieu.mun.tang@intel.com>
    Change-Id: I8fc90982d3cddceaf401c1a112ff8e20861bf4c5

--- plat/intel/soc/common/socfpga_sip_svc.c
+++ plat/intel/soc/common/socfpga_sip_svc.c
@@ -542,6 +542,19 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 					     &len_in_resp);
 		SMC_RET3(handle, status, mbox_status, len_in_resp);
 
+	case INTEL_SIP_SMC_FCS_CRYPTION:
+		x5 = SMC_GET_GP(handle, CTX_GPREG_X5);
+
+		if (x1 == FCS_MODE_DECRYPT) {
+			status = intel_fcs_decryption(x2, x3, x4, x5, &send_id);
+		} else if (x1 == FCS_MODE_ENCRYPT) {
+			status = intel_fcs_encryption(x2, x3, x4, x5, &send_id);
+		} else {
+			status = INTEL_SIP_SMC_STATUS_REJECTED;
+		}
+
+		SMC_RET3(handle, status, x4, x5);
+
 	default:
 		return socfpga_sip_handler(smc_fid, x1, x2, x3, x4,
 			cookie, handle, flags);
