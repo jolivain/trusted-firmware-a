ca7685b3f47cb5df26215b229dd9d82c222b3bfb
laurenw-arm <lauren.wehrmeister@arm.com>
2022-04-21 15:49:00 -0500

    feat(cca-cot): introduce new "cca" chain of trust
    This chain of trust defines 3 independent signing domains:
    
    1) CCA supply chain. It is rooted in the Silicon ROTPK, just as in the
    TBBR CoT.
    
    2) Non-CCA Secure World supply chain. It is rooted in a new key called
    Secure World ROTPK, or SWD_ROTPK for short.
    
    3) OEM supply chain. It is rooted in the Platform ROTPK, or PROTPK.
    
    Signed-off-by: Lauren Wehrmeister <lauren.wehrmeister@arm.com>
    Change-Id: I6ffef3f53d710e6a2072fb4374401249122a2805

---
+++ include/tools_share/cca_oid.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CCA_OID_H
+#define CCA_OID_H
+
+/* Reuse the Object IDs defined by TBBR for certificate extensions. */
+#include "tbbr_oid.h"
+
+/*
+ * Platform root-of-trust public key.
+ * Arbitrary value that does not conflict with any of the TBBR reserved OIDs.
+ */
+#define PROT_PK_OID				"1.3.6.1.4.1.4128.2100.1102"
+/* Secure World root-of-trust public key. */
+#define SWD_ROT_PK_OID				"1.3.6.1.4.1.4128.2100.1103"
+/* Core Secure World public key */
+#define CORE_SWD_PK_OID				"1.3.6.1.4.1.4128.2100.1104"
+/* OEM public key */
+#define OEM_PK_OID				"1.3.6.1.4.1.4128.2100.1105"
+/* Realm Monitor Manager (RMM) Hash */
+#define RMM_HASH_OID				"1.3.6.1.4.1.4128.2100.1106"
+
+#endif /* CCA_OID_H */
