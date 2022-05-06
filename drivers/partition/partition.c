6cf8f71295c1524fda29c41e33f5a4e0ea42ba47
Rohit Ner <rohitner@google.com>
2022-05-06 07:58:21 +0000

    feat(partition): verify crc while loading gpt header
    This change adds necessary functions for calculating 32-bit crc to use
    them for calculating crc of gpt header and comparing it with the actual
    value.
    
    Signed-off-by: Rohit Ner <rohitner@google.com>
    Change-Id: I49bca7aab2c3884881c4b7d90d31786a895290e6

--- drivers/partition/partition.c
+++ drivers/partition/partition.c
@@ -75,6 +75,42 @@ static int load_mbr_header(uintptr_t image_handle, mbr_entry_t *mbr_entry)
 	return 0;
 }
 
+static uint8_t reverse_4bit_table[] = {
+	0x0, 0x8, 0x4, 0xC,
+	0x2, 0xA, 0x6, 0xE,
+	0x1, 0x9, 0x5, 0xD,
+	0x3, 0xB, 0x7, 0xF};
+
+static uint8_t reverse_byte(uint8_t val)
+{
+	return (reverse_4bit_table[val & 0xF] << 4) | reverse_4bit_table[val >> 4];
+}
+
+static uint32_t reverse_uint32(uint32_t val)
+{
+	return (reverse_byte(val) << 24) | (reverse_byte(val >> 8) << 16) |
+		   (reverse_byte(val >> 16) << 8) | reverse_byte(val >> 24);
+}
+
+uint32_t crc32(uint8_t *buf, size_t size)
+{
+	uint32_t crc = 0xFFFFFFFF;
+
+	for (size_t i = 0; i < size; ++i) {
+		crc = crc ^ ((uint32_t)reverse_byte(buf[i]) << 24);
+		for (int j = 0; j < 8; ++j) {
+			if (crc & 0x80000000) {
+				crc = (crc << 1) ^ 0x04C11DB7;
+			}
+			else {
+				crc <<= 1;
+			}
+		}
+	}
+
+	return reverse_uint32(~crc);
+}
+
 /*
  * Load GPT header and check the GPT signature.
  * If partition numbers could be found, check & update it.
@@ -84,6 +120,7 @@ static int load_gpt_header(uintptr_t image_handle)
 	gpt_header_t header;
 	size_t bytes_read;
 	int result;
+	uint32_t header_crc, calc_crc;
 
 	result = io_seek(image_handle, IO_SEEK_SET, GPT_HEADER_OFFSET);
 	if (result != 0) {
@@ -98,6 +135,22 @@ static int load_gpt_header(uintptr_t image_handle)
 		   sizeof(header.signature)) != 0) {
 		return -EINVAL;
 	}
+
+	/*
+	 * UEFI Spec 2.8 March 2019 Page 119: HeaderCRC32 value is
+	 * computed by setting this field to 0, and computing the
+	 * 32-bit CRC for HeaderSize bytes.
+	 */
+	header_crc = header.header_crc;
+	header.header_crc = 0U;
+
+	calc_crc = crc32((uint8_t *)&header, DEFAULT_GPT_HEADER_SIZE);
+	if (header_crc != calc_crc) {
+		ERROR("Invalid GPT Header CRC: Expected 0x%x but got 0x%x.\n", header_crc, calc_crc);
+		return -EINVAL;
+	}
+
+	header.header_crc = header_crc;
 
 	/* partition numbers can't exceed PLAT_PARTITION_MAX_ENTRIES */
 	list.entry_count = header.list_num;
