8341ffc42492ded5844d99ecf39a5e3e9f9908c3
Leon Chen <leon.chen@mediatek.com>
2022-05-20 09:59:07 +0800

    feat(vendor event): extend SiP vendor subscription events
    Leverage pubsub event framework to customize vendor's
    event for better software modularization instead of adding
    call entries in abstraction layer for customized platform function
    with wrap-up define.
    
    Signed-off-by: Leon Chen <leon.chen@mediatek.com>
    Change-Id: I48be2303c45f759776fa2baa1c21130c1a8f0fa3

---
+++ plat/mediatek/include/vendor_pubsub_events.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+REGISTER_PUBSUB_EVENT(lpm_publish_event);
+REGISTER_PUBSUB_EVENT(suspend_publish_event);
+REGISTER_PUBSUB_EVENT(mt_cpupm_publish_pwr_on);
+REGISTER_PUBSUB_EVENT(mt_cpupm_publish_pwr_off);
+REGISTER_PUBSUB_EVENT(mt_cpupm_publish_afflv_pwr_on);
+REGISTER_PUBSUB_EVENT(mt_cpupm_publish_afflv_pwr_off);
+REGISTER_PUBSUB_EVENT(publish_check_wakeup_irq);
+REGISTER_PUBSUB_EVENT(watchdog_timeout);
