e95d80e3fb340fe268b3580970cda20f7f8f36c5
Ahmad Fatoum <a.fatoum@pengutronix.de>
2022-05-23 17:06:37 +0200

    refactor(mmc): export user/boot partition switch functions
    At the moment, mmc_boot_part_read_blocks() takes care to switch
    to the boot partition before transfer and back afterwards.
    This can introduce large overhead when reading small chunks.
    Give consumers of the API more control by exporting
    mmc_part_switch_current_boot() and mmc_part_switch_user().
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Change-Id: Ib641f188071bb8e0196f4af495ec9ad4a292284f

--- include/drivers/mmc.h
+++ include/drivers/mmc.h
@@ -236,6 +236,8 @@ size_t mmc_erase_blocks(int lba, size_t size);
 size_t mmc_rpmb_read_blocks(int lba, uintptr_t buf, size_t size);
 size_t mmc_rpmb_write_blocks(int lba, const uintptr_t buf, size_t size);
 size_t mmc_rpmb_erase_blocks(int lba, size_t size);
+int mmc_part_switch_current_boot(void);
+int mmc_part_switch_user(void);
 size_t mmc_boot_part_read_blocks(int lba, uintptr_t buf, size_t size);
 int mmc_init(const struct mmc_ops *ops_ptr, unsigned int clk,
 	     unsigned int width, unsigned int flags,
