07c29b89006bd8aeff8e488445394edc7f652d58
Leon Chen <leon.chen@mediatek.com>
2022-05-29 22:25:44 +0800

    refactor(SiP id): partition MTK SiP SMC ID
    Manage MTK SiP SMC ID with macros for 32/64 bit and
    function declaration code generation.
    Partition SMC ID with different exception level sources.
    
    Signed-off-by: Leon Chen <leon.chen@mediatek.com>
    Change-Id: I8966cd94f0d825e7ebae08833d2bd9fceedfd45e

--- plat/mediatek/mt8183/platform.mk
+++ plat/mediatek/mt8183/platform.mk
@@ -8,7 +8,8 @@ MTK_PLAT      := plat/mediatek
 MTK_PLAT_SOC  := ${MTK_PLAT}/${PLAT}
 
 PLAT_INCLUDES := -I${MTK_PLAT}/common/                            \
-                 -I${MTK_PLAT}/common/drivers/uart/                \
+                 -I${MTK_PLAT}/common/drivers/uart/               \
+                 -I${MTK_PLAT}/include/                           \
                  -I${MTK_PLAT_SOC}/drivers/                       \
                  -I${MTK_PLAT_SOC}/drivers/emi_mpu/               \
                  -I${MTK_PLAT_SOC}/drivers/devapc/                \
