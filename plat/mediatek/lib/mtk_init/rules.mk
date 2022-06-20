dcbc71635d2b963284329eee545a831c2a34aefd
Leon Chen <leon.chen@mediatek.com>
2022-06-20 10:25:35 +0800

    feat(mtk init): introduce mtk initialization framework
    Provide six initcall levels for drivers/modules initialize HW
    controllers or runtime arguments during cold boot.
    
    The initcall level cold boot execution order:
    
    -MTK_EARLY_PLAT_INIT
    Call before MMU enabled.
    
    -MTK_ARCH_INIT
    MMU Enabled, arch related init(GiC init, interrupt type registration).
    
    -MTK_PLAT_SETUP_0_INIT
    MTK driver init.
    
    -MTK_PLAT_SETUP_1_INIT
    MTK driver init.
    
    -MTK_PLAT_RUNTIME_INIT
    MTK driver init. After this initcall, TF-A handovers to MTK 2nd bootloader.
    
    -MTK_PLAT_BL33_DEFER_INIT
    MTK 2nd bootloader trap to TF-A before handover to rich OS.
    This initcall executed in the trap handler(boot_to_kernel).
    
    Signed-off-by: Leon Chen <leon.chen@mediatek.com>
    Change-Id: Icd7fe95372441db73c975ccb6ce77a6c529df1cc

---
+++ plat/mediatek/lib/mtk_init/rules.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (c) 2022, MediaTek Inc. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# Prologue, init variable
+LOCAL_DIR := $(call GET_LOCAL_DIR)
+
+# Define your module name
+MODULE := mtk_init
+
+# Add your source code here
+LOCAL_SRCS-y := $(LOCAL_DIR)/mtk_init.c
+LOCAL_SRCS-y += $(LOCAL_DIR)/mtk_mmap_init.c
+
+# Extra linker script
+ifneq ($(MTK_EXTRA_LINKERFILE),)
+# Local linker script added
+$(eval EXTRA_LINKERFILE += ${LOCAL_DIR}/mtk_mmap_pool.ld)
+
+# Generate final linker script by C preprocessor compiler
+$(eval MTK_LINKERFILE_SOURCE += ${LOCAL_DIR}/mtk_init.ld.S)
+$(eval MTK_LINKERFILE_SOURCE += ${LOCAL_DIR}/mtk_ro.ld.S)
+endif
+
+# Epilogue, Build as module
+$(eval $(call MAKE_MODULE,$(MODULE),$(LOCAL_SRCS-y),$(MTK_BL)))
