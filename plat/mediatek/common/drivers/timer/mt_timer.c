4693da317a405bd8b85c03f92284bdc93a93211f
Rex-BC Chen <rex-bc.chen@mediatek.com>
2022-07-08 13:16:17 +0800

    feat(mt8188): initialize systimer
    Add systimer to support timer function.
    
    TEST=build pass
    BUG=b:236331724
    
    Signed-off-by: Bo-Chen Chen <rex-bc.chen@mediatek.com>
    Change-Id: Ibe6b96a162caa8804bebb7ff7de326ebcb2a6daa

--- plat/mediatek/common/drivers/timer/mt_timer.c
+++ plat/mediatek/common/drivers/timer/mt_timer.c
@@ -1,14 +1,15 @@
 /*
- * Copyright (c) 2020, MediaTek Inc. All rights reserved.
+ * Copyright (c) 2020-2022, MediaTek Inc. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <arch_helpers.h>
+#include <common/debug.h>
 #include <lib/mmio.h>
-#include <mt_timer.h>
+#include <lib/mtk_init/mtk_init.h>
+#include "mt_timer.h"
 #include <platform_def.h>
-
 
 uint64_t normal_time_base;
 uint64_t atf_time_base;
@@ -30,9 +31,14 @@ uint64_t sched_clock(void)
 	return cval;
 }
 
-void mt_systimer_init(void)
+int mt_systimer_init(void)
 {
+	INFO("systimer init\n");
+
 	/* Enable access in NS mode */
 	mmio_write_32(CNTWACR_REG, CNT_WRITE_ACCESS_CTL_MASK);
 	mmio_write_32(CNTRACR_REG, CNT_READ_ACCESS_CTL_MASK);
+
+	return 0;
 }
+MTK_PLAT_SETUP_0_INIT(mt_systimer_init);
