b8c22a1067b335d551bb8d8487365348025e0e60
Akram Ahmad <Akram.Ahmad@arm.com>
2022-07-21 14:01:33 +0100

    fix(errata): workaround for Cortex-A510 erratum 2347730
    Cortex-A510 erratum 2347730 is a Cat B erratum that affects
    revisions r0p0, r0p1, r0p2, r0p3, r1p0 and r1p1. It is
    fixed in r1p2. The workaround is to set CPUACTLR_EL1[17]
    to 1, which will disable specific microarchitectural clock
    gating behaviour.
    
    SDEN can be found here:
    https://developer.arm.com/documentation/SDEN1873351/latest
    https://developer.arm.com/documentation/SDEN1873361/latest
    
    Signed-off-by: Akram Ahmad <Akram.Ahmad@arm.com>
    Change-Id: I115386284c2d91bd61515142f971e2e72de43e68

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -657,6 +657,11 @@ ERRATA_A510_2172148	?=0
 # to revisions r0p0, r0p1, r0p2, r0p3, r1p0, and r1p1. It is fixed in r1p2.
 ERRATA_A510_2371937	?=0
 
+# Flag to apply erratum 2347730 workaround during reset. This erratum applies
+# to revisions r0p0, r0p1, r0p2, r0p3, r1p0 and r1p1 of the Cortex-A510 CPU,
+# and is fixed in r1p2.
+ERRATA_A510_2347730	?=0
+
 # Flag to apply DSU erratum 798953. This erratum applies to DSUs revision r0p0.
 # Applying the workaround results in higher DSU power consumption on idle.
 ERRATA_DSU_798953	?=0
@@ -1238,6 +1243,10 @@ $(eval $(call add_define,ERRATA_A510_2172148))
 # Process ERRATA_A510_2371937 flag
 $(eval $(call assert_boolean,ERRATA_A510_2371937))
 $(eval $(call add_define,ERRATA_A510_2371937))
+
+# Process ERRATA_A510_2347730 flag
+$(eval $(call assert_boolean,ERRATA_A510_2347730))
+$(eval $(call add_define,ERRATA_A510_2347730))
 
 # Process ERRATA_DSU_798953 flag
 $(eval $(call assert_boolean,ERRATA_DSU_798953))
