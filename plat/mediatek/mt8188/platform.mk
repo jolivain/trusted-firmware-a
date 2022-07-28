85f9446bca9e60dca899a887d6b5b676b8aca4a0
Hui Liu <hui.liu@mediatek.corp-partner.google.com>
2022-07-28 20:28:32 +0800

    feat(mt8188): add pmic and pwrap support
    Add PWRAP and PMIC driver to support power-off.
    
    TEST=build pass.
    BUG=b:233720142
    
    Signed-off-by: Hui Liu <hui.liu@mediatek.corp-partner.google.com>
    Change-Id: Id9951134925f6cb5f8d304a7b8e7901837809bd9

--- plat/mediatek/mt8188/platform.mk
+++ plat/mediatek/mt8188/platform.mk
@@ -26,6 +26,7 @@ MODULES-y += $(MTK_PLAT)/common/drivers/cirq
 MODULES-y += $(MTK_PLAT)/common/drivers/dp
 MODULES-y += $(MTK_PLAT)/common/drivers/gic600
 MODULES-y += $(MTK_PLAT)/common/drivers/iommu
+MODULES-y += $(MTK_PLAT)/common/drivers/pmic_wrap
 MODULES-y += $(MTK_PLAT)/common/drivers/timer
 
 PLAT_BL_COMMON_SOURCES := common/desc_image_load.c \
