a2fda7a4d3b8bdf8e5f44eb96b8627b54c5b0c8b
Fengquan Chen <fengquan.chen@mediatek.corp-partner.google.com>
2022-08-17 10:42:15 +0800

    feat(mt8188): add DFD control in SiP service
    DFD (Design for Debug) is a debugging tool, which scans flip-flops and
    dumps to internal RAM on the WDT reset. After system reboots, those
    values could be showed for debugging.
    
    TEST=build pass.
    BUG=b:244216434
    
    Signed-off-by: Fengquan Chen <fengquan.chen@mediatek.corp-partner.google.com>
    Change-Id: I468036131e941a46bc1ec12d33105146000730d8

---
+++ plat/mediatek/drivers/dfd/dfd.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef DFD_H
+#define DFD_H
+
+#include <arch_helpers.h>
+#include <lib/mmio.h>
+
+void dfd_resume(void);
+
+#endif /* DFD_H */
