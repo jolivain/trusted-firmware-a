ae08dbf157344afa81796b96178db20e342dbfa4
Bo-Chen Chen <rex-bc.chen@mediatek.com>
2022-09-01 16:32:46 +0800

    refator(mediatek): move common drivers of pmic to common folder
    There are some definitions and functions for mt8192 and mt8195, so
    move them to common/drivers/pmic_wrap.
    
    Signed-off-by: Bo-Chen Chen <rex-bc.chen@mediatek.com>
    Change-Id: I8c12d15f1da79ab5767ac02b3ab70e8508155ee8

--- plat/mediatek/mt8192/platform.mk
+++ plat/mediatek/mt8192/platform.mk
@@ -50,6 +50,7 @@ BL31_SOURCES    += common/desc_image_load.c                              \
                    plat/common/plat_gicv3.c                              \
                    ${MTK_PLAT}/common/drivers/gic600/mt_gic_v3.c         \
                    ${MTK_PLAT}/common/drivers/gpio/mtgpio_common.c       \
+		   ${MTK_PLAT}/common/drivers/pmic_wrap/pmic.c           \
                    ${MTK_PLAT}/common/drivers/pmic_wrap/pmic_wrap_init_v2.c \
                    ${MTK_PLAT}/common/drivers/rtc/rtc_common.c           \
                    ${MTK_PLAT}/common/drivers/rtc/rtc_mt6359p.c          \
@@ -63,7 +64,6 @@ BL31_SOURCES    += common/desc_image_load.c                              \
                    ${MTK_PLAT_SOC}/aarch64/platform_common.c             \
                    ${MTK_PLAT_SOC}/aarch64/plat_helpers.S                \
                    ${MTK_PLAT_SOC}/bl31_plat_setup.c                     \
-                   ${MTK_PLAT_SOC}/drivers/pmic/pmic.c                   \
                    ${MTK_PLAT_SOC}/plat_pm.c                             \
                    ${MTK_PLAT_SOC}/plat_topology.c                       \
                    ${MTK_PLAT_SOC}/plat_sip_calls.c                      \
--- plat/mediatek/mt8195/platform.mk
+++ plat/mediatek/mt8195/platform.mk
@@ -50,6 +50,7 @@ BL31_SOURCES += common/desc_image_load.c                              \
                 plat/common/plat_gicv3.c                              \
                 ${MTK_PLAT}/common/drivers/gic600/mt_gic_v3.c         \
                 ${MTK_PLAT}/common/drivers/gpio/mtgpio_common.c       \
+		${MTK_PLAT}/common/drivers/pmic_wrap/pmic.c           \
                 ${MTK_PLAT}/common/drivers/pmic_wrap/pmic_wrap_init_v2.c \
                 ${MTK_PLAT}/common/drivers/rtc/rtc_common.c           \
                 ${MTK_PLAT}/common/drivers/rtc/rtc_mt6359p.c          \
@@ -77,7 +78,6 @@ BL31_SOURCES += common/desc_image_load.c                              \
                 ${MTK_PLAT_SOC}/drivers/mcdi/mt_mcdi.c                \
 		${MTK_PLAT_SOC}/drivers/mcdi/mt_lp_irqremain.c        \
                 ${MTK_PLAT_SOC}/drivers/gpio/mtgpio.c                 \
-                ${MTK_PLAT_SOC}/drivers/pmic/pmic.c                   \
                 ${MTK_PLAT_SOC}/drivers/ptp3/mtk_ptp3_main.c          \
                 ${MTK_PLAT_SOC}/drivers/spmc/mtspmc.c                 \
                 ${MTK_PLAT_SOC}/plat_pm.c                             \
