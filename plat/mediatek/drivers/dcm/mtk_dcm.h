3e50768997d1e3925843c8a7969f0111fe5b3b72
Garmin Chang <garmin.chang@mediatek.com>
2022-09-05 17:44:02 +0800

    feat(mt8188): add DCM driver
    DCM means dynamic clock management, and it can dynamically
    slow down or gate clocks during CPU or bus idle.
    
    1. Add MCUSYS related DCM drivers.
    2. Enable MCUSYS related DCM by default.
    
    Signed-off-by: Garmin Chang <garmin.chang@mediatek.com>
    Change-Id: I131354d72bbc190af504e9639bcc85a720e2bb17

---
+++ plat/mediatek/drivers/dcm/mtk_dcm.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MTK_DCM_H
+#define MTK_DCM_H
+
+#include <stdbool.h>
+
+int dcm_set_init(void);
+void dcm_set_defaulta(void);
+
+#endif /* #ifndef MTK_DCM_H */
