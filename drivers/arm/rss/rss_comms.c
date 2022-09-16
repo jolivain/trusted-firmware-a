cc0e9df3c160d1b2e444ee0d812f770bf1562fb0
Tamas Ban <tamas.ban@arm.com>
2022-09-16 13:42:29 +0200

    fix(rss): clear the message buffer
    Clear the MHU message buffer to remove assets from memory.
    
    Signed-off-by: Tamas Ban <tamas.ban@arm.com>
    Change-Id: I730e7b044eaf0bf517532a12146e4f542949544e

--- drivers/arm/rss/rss_comms.c
+++ drivers/arm/rss/rss_comms.c
@@ -142,6 +142,9 @@ psa_status_t psa_call(psa_handle_t handle, int32_t type, const psa_invec *in_vec
 		VERBOSE("out_vec[%lu].buf=%p\n", idx, (void *)out_vec[idx].base);
 	}
 
+	/* Clear the MHU message buffer to remove assets from memory */
+	memset(&io_buf.reply, 0x0, reply_size);
+
 	seq_num++;
 
 	return return_val;
