7ff35d337c7bf1a82224a444f745d4b95f5d38ba
Manish Pandey <manish.pandey2@arm.com>
2022-10-10 11:43:08 +0100

    fix(ras): restrict RAS support for NS world
    Current RAS framework in TF-A only supports handling errors originating
    from NS world but the HANDLE_EA_EL3_FIRST flag configures it for all
    lower Els. To make the current design of RAS explicit, rename this macro
    to HANDLE_EA_EL3_FIRST_NS and set EA bit in scr_el3 only when
    switching to NS world.
    
    Note: I am unaware of any platform which traps errors originating in
    Secure world to EL3, if there is any such platform then it need to
    be explicitly implemented in TF-A
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: If58eb201d8fa792c16325c85c26056e9b409b750

--- plat/common/aarch64/plat_common.c
+++ plat/common/aarch64/plat_common.c
@@ -95,12 +95,11 @@ void plat_default_ea_handler(unsigned int ea_reason, uint64_t syndrome, void *co
 	ERROR("Unhandled External Abort received on 0x%lx from %s\n",
 		read_mpidr_el1(), get_el_str(level));
 	ERROR("exception reason=%u syndrome=0x%" PRIx64 "\n", ea_reason, syndrome);
-#if HANDLE_EA_EL3_FIRST
-	/* Skip backtrace for lower EL */
+
+	/* Skip backtrace and print crash dump of lower EL*/
 	if (level != MODE_EL3) {
 		console_flush();
 		do_panic();
 	}
-#endif
 	panic();
 }
