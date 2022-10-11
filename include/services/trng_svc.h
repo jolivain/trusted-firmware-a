828cef81fc40106bf623a53adf72fe9176678e14
Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
2022-10-11 17:16:07 +0100

    refactor(trng): cleanup the existing TRNG support
    This patch adds the following changes to complete the existing
    TRNG implementation:
    
    1. Adds a feature specific scope for buildlog generation.
    2. Updates the docs on the build flag "TRNG_SUPPORT" and its values.
    3. Makefile update and improves the existing comments at few sections
    for better understanding of the underlying logic.
    
    Change-Id: I3f72f0ccd5c94005a2df87158cf23199d2160d37
    Signed-off-by: Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>

--- include/services/trng_svc.h
+++ include/services/trng_svc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, ARM Limited. All rights reserved.
+ * Copyright (c) 2021-2022, ARM Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -17,7 +17,7 @@
 #define ARM_TRNG_FEATURES	U(0x84000051)
 #define ARM_TRNG_GET_UUID	U(0x84000052)
 #define ARM_TRNG_RND32		U(0x84000053)
-#define ARM_TRNG_RND64		U(0xc4000053)
+#define ARM_TRNG_RND64		U(0xC4000053)
 
 /* TRNG version numbers */
 #define TRNG_VERSION_MAJOR	(0x1)
@@ -29,6 +29,10 @@
 #define TRNG_E_INVALID_PARAMS	(-2)
 #define TRNG_E_NO_ENTROPY	(-3)
 #define TRNG_E_NOT_IMPLEMENTED	(-4)
+
+/* TRNG Entropy Bit Numbers */
+#define TRNG_RND32_ENTROPY_MAXBITS	(96U)
+#define TRNG_RND64_ENTROPY_MAXBITS	(192U)
 
 #if TRNG_SUPPORT
 void trng_setup(void);
@@ -42,7 +46,9 @@ static inline bool is_trng_fid(uint32_t smc_fid)
 {
 	return false;
 }
-#endif
+#endif /* TRNG_SUPPORT */
+
+/* Handler to be called to handle TRNG smc calls */
 uintptr_t trng_smc_handler(
 	uint32_t smc_fid,
 	u_register_t x1,
