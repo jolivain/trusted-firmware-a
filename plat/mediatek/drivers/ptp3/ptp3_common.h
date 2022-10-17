ed5306f65764f959535bc8e79fb502917db31354
Riven Chen <riven.chen@mediatek.corp-partner.google.com>
2022-10-17 13:11:27 +0800

    feat(mt8188): add support for PTP3
    Add PTP3 driver to protect CPU from excessive voltage drop in CPU
    heavy loading.
    
    Signed-off-by: Riven Chen <riven.chen@mediatek.corp-partner.google.com>
    Change-Id: I394096be43e1d1d615f99b22f38f0b3ae0bb40c1

---
+++ plat/mediatek/drivers/ptp3/ptp3_common.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PTP3_COMMON_H
+#define PTP3_COMMON_H
+
+#include <lib/mmio.h>
+#include <lib/utils_def.h>
+
+/* config enum */
+enum PTP3_CFG {
+	PTP3_CFG_ADDR,
+	PTP3_CFG_VALUE,
+	NR_PTP3_CFG,
+};
+
+#endif /* PTP3_COMMON_H */
