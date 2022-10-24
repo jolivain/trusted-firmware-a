2724632ec65d210ed85e429583a05af16f690e80
Mate Toth-Pal <mate.toth-pal@arm.com>
2022-10-24 15:15:10 +0200

    feat(lib/psa): add read_measurement API
    This API is added for testing purposes. It makes possible to write test
    cases that read measurements back after extending them, and compare
    them to expected results.
    
    Change-Id: Iec447d972fdd54a56ab933a065476e0f4d35a6fc
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>

--- lib/psa/measured_boot_private.h
+++ lib/psa/measured_boot_private.h
@@ -12,6 +12,22 @@
 
 /* Measured boot message types that distinguish its services */
 #define RSS_MEASURED_BOOT_EXTEND	1002U
+#define RSS_MEASURED_BOOT_READ		1001U
+
+struct measured_boot_read_iovec_in_t {
+    uint8_t index;
+    uint8_t sw_type_size;
+    uint8_t version_size;
+};
+
+struct measured_boot_read_iovec_out_t {
+    uint8_t  is_locked;
+    uint32_t measurement_algo;
+    uint8_t  sw_type[SW_TYPE_MAX_SIZE];
+    uint8_t  sw_type_len;
+    uint8_t  version[VERSION_MAX_SIZE];
+    uint8_t  version_len;
+};
 
 struct measured_boot_extend_iovec_t {
 	uint8_t  index;
