786debff95f8c2c23ad8444219117458e5a0b17d
Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
2022-11-08 10:31:07 +0000

    feat(sme): enable SME2 functionality for NS world
    FEAT_SME2 is an extension of FEAT_SME and an optional feature
    From v9.2 version. Its an extension of SME, wherein its not only
    process matrix operations efficiently, beyond that it provides
    outer-product instructions to accelerate matrix operations.
    It affords instructions for multi-vcector operations.
    Further, it adds an 512 bit architectural register ZT0.
    
    This patch implements all the changes introduced with FEAT_SME2
    to ensure that the instructions are allowed to access ZT0
    register from Non-secure lower exception levels.
    
    Additionally, it adds support to ensure FEAT_SME2 is aligned
    with the existing FEATURE DETECTION mechanism, and documented.
    
    Change-Id: Iee0f61943304a9cfc3db8f986047b1321d0a6463
    Signed-off-by: Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>

--- lib/extensions/sme/sme.c
+++ lib/extensions/sme/sme.c
@@ -48,6 +48,17 @@ void sme_enable(cpu_context_t *context)
 		VERBOSE("[SME] FA64 enabled\n");
 		reg |= SMCR_ELX_FA64_BIT;
 	}
+
+	/*
+	 * Enable access to ZT0 register.
+	 * Make sure FEAT_SME2 is supported by the hardware before continuing.
+	 * If supported, Set the EZT0 bit in SMCR_EL3 to allow instructions to
+	 * access ZT0 register without trapping.
+	 */
+	if (is_feat_sme2_supported()) {
+		VERBOSE("SME2 enabled\n");
+		reg |= SMCR_ELX_EZT0_BIT;
+	}
 	write_smcr_el3(reg);
 
 	/* Reset CPTR_EL3 value. */
