1912a61450ca37d27c80e3311a61e5cafa148219
Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
2022-11-08 10:31:07 +0000

    feat(sme): enable SME2 functionality for NS world
    FEAT_SME2 is an extension of FEAT_SME and an optional feature
    From v9.2 version. Its an extension of SME, wherein its not only
    process matrix operations efficiently, beyond that it provides
    outer-product instructions to accelerate matrix operations.
    It affords instructions for multi-vcector operations.
    Further, it adds an 512 bit architectural register ZT0.
    
    This patch implements all the changes introduced with FEAT_SME2
    to ensure that the instructions are allowed to access ZT0
    register from Non-secure lower exception levels.
    
    Additionally, it adds support to ensure FEAT_SME2 is aligned
    with the existing FEATURE DETECION mechanism, and documented.
    
    Change-Id: Iee0f61943304a9cfc3db8f986047b1321d0a6463
    Signed-off-by: Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>

--- include/arch/aarch64/arch_features.h
+++ include/arch/aarch64/arch_features.h
@@ -409,6 +409,7 @@ static inline bool is_feat_trbe_supported(void)
 /*******************************************************************************
  * Function to identify the presence of FEAT_SMEx (Scalar Matrix Extension)
  ******************************************************************************/
+
 static inline bool feat_sme_fa64_supported(void)
 {
 	uint64_t features;
@@ -433,6 +434,19 @@ static inline bool is_feat_sme_supported(void)
 	}
 
 	return read_feat_sme_id_field() >= ID_AA64PFR1_EL1_SME_SUPPORTED;
+}
+
+static inline bool is_feat_sme2_supported(void)
+{
+	if (ENABLE_SME2_FOR_NS == FEAT_STATE_DISABLED) {
+		return false;
+	}
+
+	if (ENABLE_SME2_FOR_NS == FEAT_STATE_ALWAYS) {
+		return true;
+	}
+
+	return read_feat_sme_id_field() >= ID_AA64PFR1_EL1_SME2_SUPPORTED;
 }
 
 #endif /* ARCH_FEATURES_H */
