76686675b54b5b9b99abdc7b4ab3819b36243225
Mohamed Elzahhar <Mohamed.Elzahhar@arm.com>
2022-11-16 12:05:37 +0000

    feat(ethos-n): add support for NPU to cert_create
    Add Juno specific Makefile to the certificate tool build. That
    Makefile is included by the certificate tool Makefile to add
    information about the authentication data for the
    Arm(R) Ethos(TM)-N NPU's firmware binary.
    
    Signed-off-by: Mohamed Elzahhar <Mohamed.Elzahhar@arm.com>
    Signed-off-by: Rob Hughes <robert.hughes@arm.com>
    Signed-off-by: Mikael Olsson <mikael.olsson@arm.com>
    Change-Id: Ie4b6a1c29d73b3ed5041b57f2cd88033be18a63a

---
+++ plat/arm/board/juno/certificate/src/juno_tbb_key.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <juno_tbb_key.h>
+#include <ethosn_cert.h>
+
+static key_t juno_plat_tbb_keys[] = {
+	ETHOSN_NPU_FW_CONTENT_CERT_KEY_DEF
+};
+
+PLAT_REGISTER_KEYS(juno_plat_tbb_keys);
