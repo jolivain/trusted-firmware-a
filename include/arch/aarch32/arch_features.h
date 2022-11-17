568f7f61ee3ad715cd50d819626895ec1de3a5ae
Andre Przywara <andre.przywara@arm.com>
2022-11-17 17:30:43 +0000

    refactor(trf): enable FEAT_TRF for FEAT_STATE_CHECKED
    At the moment we only support FEAT_TRF to be either unconditionally
    compiled in, or to be not supported at all.
    
    Add support for runtime detection (ENABLE_TRF_FOR_NS=2), by splitting
    is_feat_trf_present() into an ID register reading function and a second
    function to report the support status. That function considers both
    build time settings and runtime information (if needed), and is used
    before we access TRF related registers.
    Also move the context saving code from assembly to C, and use the new
    is_feat_trf_supported() function to guard its execution.
    
    The FVP platform decided to compile in support unconditionally (=1),
    even though FEAT_TRF is an ARMv8.4 feature, so is not available with the
    FVP model's default command line.
    Change that to the now supported dynamic option (=2), so the right
    decision can be made by the code at runtime.
    
    Change-Id: Ia97b01adbe24970a4d837afd463dc5506b7295a3
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- include/arch/aarch32/arch_features.h
+++ include/arch/aarch32/arch_features.h
@@ -24,4 +24,22 @@ static inline bool is_armv8_2_ttcnp_present(void)
 	return ISOLATE_FIELD(read_id_mmfr4(), ID_MMFR4_CNP) != 0U;
 }
 
+static inline unsigned int read_feat_trf_id_field(void)
+{
+	return ISOLATE_FIELD(read_id_dfr0(), ID_DFR0_TRACEFILT_SHIFT);
+}
+
+static inline bool is_feat_trf_supported(void)
+{
+	if (ENABLE_TRF_FOR_NS == FEAT_STATE_DISABLED) {
+		return false;
+	}
+
+	if (ENABLE_TRF_FOR_NS == FEAT_STATE_ALWAYS) {
+		return true;
+	}
+
+	return read_feat_trf_id_field() != 0U;
+}
+
 #endif /* ARCH_FEATURES_H */
