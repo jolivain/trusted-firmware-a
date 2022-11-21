14df25ce821a3d07dac2c7955e889ef2d4a383df
Demi Marie Obenour <demi@invisiblethingslab.com>
2022-11-21 00:22:50 -0500

    fix(auth/mbedtls): Require bit strings to have no unused bits
    This is already checked by the crypto module or by mbedTLS, but checking
    it in the X.509 parser is harmless.
    
    Change-Id: Ifdbe3b4c6d04481bb8e93106ee04b49a70f50d5d
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

--- drivers/auth/mbedtls/mbedtls_x509_parser.c
+++ drivers/auth/mbedtls/mbedtls_x509_parser.c
@@ -284,7 +284,7 @@ static int cert_parse(void *img, unsigned int img_len)
 	/*
 	 * subjectPublicKey BIT STRING
 	 */
-	ret = mbedtls_asn1_get_tag(&p, end, &len, MBEDTLS_ASN1_BIT_STRING);
+	ret = mbedtls_asn1_get_bitstring_null(&p, end, &len);
 	if (ret != 0) {
 		return IMG_PARSER_ERR_FORMAT;
 	}
@@ -423,7 +423,7 @@ static int cert_parse(void *img, unsigned int img_len)
 	 * signatureValue       BIT STRING
 	 */
 	signature.p = p;
-	ret = mbedtls_asn1_get_tag(&p, end, &len, MBEDTLS_ASN1_BIT_STRING);
+	ret = mbedtls_asn1_get_bitstring_null(&p, end, &len);
 	if (ret != 0) {
 		return IMG_PARSER_ERR_FORMAT;
 	}
