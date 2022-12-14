ad93ceb1444b6f45f009d8ed3a1c808afde4c7c5
Andre Przywara <andre.przywara@arm.com>
2022-12-14 16:53:16 +0000

    feat(cpufeat): include optional features in arch_features.mk
    Many architecture features are introduced in a certain architecture
    revision, but remain optional. This includes MPAM and SPE, for instance.
    
    Since we now support runtime detection, default those feature to being
    enabled (but runtime-checked) when compiling for a particular
    architecture revision.
    Platforms can override this in their Makefiles when desired.
    
    Change-Id: Icdaec231ace35b90f46637cd9b2e83d0283e73b2
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- make_helpers/arch_features.mk
+++ make_helpers/arch_features.mk
@@ -6,6 +6,9 @@
 
 # This file lists all the checks related to the Architectural Feature
 # Enablement flags, based on the Architectural version.
+# For features that become mandatory with a certain architecture revision,
+# we set them to "1" (unconditionally enable), for features that are introduced,
+# but remain optional we use "2", to trigger the runtime ID register test.
 
 # Enable the features which are mandatory from ARCH version 8.1 and upwards.
 ifeq "8.1" "$(word 1, $(sort 8.1 $(ARM_ARCH_MAJOR).$(ARM_ARCH_MINOR)))"
@@ -13,10 +16,21 @@ ENABLE_FEAT_PAN		=	1
 ENABLE_FEAT_VHE		=	1
 endif
 
-# Enable the features which are mandatory from ARCH version 8.4 and upwards.
+# Enable the features which are optional from ARCH version 8.2 and upwards.
+ifeq "8.2" "$(word 1, $(sort 8.2 $(ARM_ARCH_MAJOR).$(ARM_ARCH_MINOR)))"
+ENABLE_MPAM_FOR_LOWER_ELS	=	2
+endif
+
+# Enable the features which are optional from ARCH version 8.3 and upwards.
+ifeq "8.3" "$(word 1, $(sort 8.3 $(ARM_ARCH_MAJOR).$(ARM_ARCH_MINOR)))"
+ENABLE_SPE_FOR_LOWER_ELS	=	2
+endif
+
+# Features introduced with ARCH version 8.4
 ifeq "8.4" "$(word 1, $(sort 8.4 $(ARM_ARCH_MAJOR).$(ARM_ARCH_MINOR)))"
 ENABLE_FEAT_DIT		=	1
 ENABLE_FEAT_SEL2	=	1
+ENABLE_TRF_FOR_NS	=	2
 endif
 
 # Enable the features which are mandatory from ARCH version 8.5 and upwards.
