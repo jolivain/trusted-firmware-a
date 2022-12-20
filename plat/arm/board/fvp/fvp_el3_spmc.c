d64dc28f0fe546d95a25758db30e118ec0dbab52
Shruti Gupta <shruti.gupta@arm.com>
2022-12-20 18:01:43 +0000

    fix(fvp): move datastore at start of DRAM
    Workaround for https://jira.arm.com/browse/GENFW-6950: datastore used by
    EL3 SPMC is not relocated at boot time. Move the datastore to a fixed
    address instead.
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: If22d2fc8deacc74c73d7dc51bb70093935d9fa2b

--- plat/arm/board/fvp/fvp_el3_spmc.c
+++ plat/arm/board/fvp/fvp_el3_spmc.c
@@ -7,23 +7,9 @@
 
 #include <platform_def.h>
 
-/*
- * On the FVP platform when using the EL3 SPMC implementation allocate the
- * datastore for tracking shared memory descriptors in the TZC DRAM section
- * to ensure sufficient storage can be allocated.
- * Provide an implementation of the accessor method to allow the datastore
- * details to be retrieved by the SPMC.
- * The SPMC will take care of initializing the memory region.
- */
-
-#define PLAT_SPMC_SHMEM_DATASTORE_SIZE 512 * 1024
-
-__section(".arm_el3_tzc_dram") static uint8_t
-plat_spmc_shmem_datastore[PLAT_SPMC_SHMEM_DATASTORE_SIZE];
-
 int plat_spmc_shmem_datastore_get(uint8_t **datastore, size_t *size)
 {
-	*datastore = plat_spmc_shmem_datastore;
+	*datastore = (uint8_t *)ARM_EL3_TZC_DRAM1_BASE;
 	*size = PLAT_SPMC_SHMEM_DATASTORE_SIZE;
 	return 0;
 }
