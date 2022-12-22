d328ff92aa3009602ce1e3c3f47672d6616eacc8
Jay Buddhabhatti <jay.buddhabhatti@amd.com>
2022-12-22 03:40:21 -0800

    feat(versal): move pm_client_set_wakeup_sources() to common place
    Moved pm_client_set_wakeup_sources() to make common for both Versal and
    Versal NET platforms.
    
    Signed-off-by: Jay Buddhabhatti <jay.buddhabhatti@amd.com>
    Change-Id: I79c6634064d45f73fdaa627bed181934e39e5e76

--- plat/xilinx/versal_net/versal_net_gicv3.c
+++ plat/xilinx/versal_net/versal_net_gicv3.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2018-2019, ARM Limited and Contributors. All rights reserved.
  * Copyright (c) 2018-2022, Xilinx, Inc. All rights reserved.
- * Copyright (C) 2022-2023, Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Advanced Micro Devices, Inc. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -63,8 +63,8 @@ static uint32_t versal_net_gicv3_mpidr_hash(u_register_t mpidr)
 }
 
 static const gicv3_driver_data_t versal_net_gic_data __unused = {
-	.gicd_base = PLAT_VERSAL_NET_GICD_BASE,
-	.gicr_base = PLAT_VERSAL_NET_GICR_BASE,
+	.gicd_base = PLAT_GICD_BASE_VALUE,
+	.gicr_base = 0U,
 	.interrupt_props = versal_net_interrupt_props,
 	.interrupt_props_num = ARRAY_SIZE(versal_net_interrupt_props),
 	.rdistif_num = PLATFORM_CORE_COUNT,
