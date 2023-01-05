d3c7e66bb9d806ba3c7f306b6c90b4de1ffe72ed
Harrison Mutai <harrison.mutai@arm.com>
2023-01-05 16:50:32 +0000

    tools: add memory mapping script
    Add new script to present memory usage and footprint. This script is
    intended to replace the `print_memory_map.py` script which outputs a
    table showing the virtual address map, with section and module mappings.
    This script makes clearer the actual memory consumption of a build by
    presenting it in a tabular format. Alongside this makes the virtual
    address map more readable.
    
    Change-Id: I6646f817a1d38d6184b837b78039b7465a533c5c
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

---
+++ tools/memory/src/memmap.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2023, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+from pathlib import Path
+
+import click
+from buildparser import TfaBuildParser
+from printer import TfaPrettyPrinter
+
+
+@click.command()
+@click.option(
+    "-r",
+    "--root",
+    type=Path,
+    default=None,
+    help="Root containing build output.",
+)
+@click.option(
+    "-p",
+    "--platform",
+    default="fvp",
+    help="The platform targeted for analysis (default is fvp).",
+)
+@click.option(
+    "-b",
+    "--build-type",
+    default="release",
+    help="The target build type (typically debug or release).",
+)
+@click.option(
+    "-f",
+    "--footprint",
+    is_flag=True,
+    help="Generate a high level view of memory usage by memory types.",
+)
+@click.option(
+    "-t",
+    "--tree",
+    is_flag=True,
+    help="Generate a hierarchical view of the modules, segments and sections.",
+)
+@click.option(
+    "-d",
+    "--depth",
+    default=3,
+    help="Generate a virtual address map of important TF symbols.",
+)
+@click.option(
+    "-s",
+    "--symbols",
+    is_flag=True,
+    help="Generate a virtual address map of important TF symbols.",
+)
+@click.option(
+    "-x",
+    is_flag=True,
+    default=False,
+    help="Display numbers in hex.",
+)
+def main(root, platform, build_type, footprint, tree, symbols, depth, x):
+    """A tool for visualizing the contents of ELF format TF-A files."""
+    build_path = Path(root) if root else Path("build/", platform, build_type)
+    click.echo(f"build-path: {build_path.resolve()}")
+
+    parser = TfaBuildParser(build_path)
+    printer = TfaPrettyPrinter(num_as_hex=x)
+
+    modules = parser.get_sorted_mod_names()
+
+    if footprint:
+        printer.print_footprint(parser.get_mem_usage_dict())
+
+    if tree:
+        printer.print_mem_tree(
+            parser.get_mem_tree_as_dict(), modules, depth=depth
+        )
+
+    if symbols:
+        expr = (
+            r"(.*)(TEXT|BSS|RODATA|STACKS|_OPS|PMF|XLAT|GOT|FCONF"
+            r"|R.M)(.*)(START|END)__$"
+        )
+        printer.print_symbol_table(parser.get_sorted_symbols(expr), modules)
+
+
+if __name__ == "__main__":
+    main()
