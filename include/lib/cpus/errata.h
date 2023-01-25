eec724b259ac6d6b1e4c3c7ab0b8daa4d62a985e
Boyan Karatotev <boyan.karatotev@arm.com>
2023-01-25 18:50:10 +0000

    refactor(cpus): convert print_errata_status to C
    The function is called in a fully initialised C environment and calls
    into other C functions. The Aarch differences are minimal and are hidden
    by the pre-existing headers. Converting it results into cleaner code
    that is the same across both architectures.
    
    To avoid having to do very ugly pointer arithmetic, define a C struct
    for the cpu_ops for both architectures.
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    Change-Id: Idc07c4064e03143c88a4a0e2d10ceda70ba19a50

--- include/lib/cpus/errata.h
+++ include/lib/cpus/errata.h
@@ -9,19 +9,8 @@
 
 #ifndef __ASSEMBLER__
 
-#include <arch.h>
-#include <arch_helpers.h>
-#include <lib/spinlock.h>
-#include <lib/utils_def.h>
-
-#if DEBUG
 void print_errata_status(void);
-#else
-static inline void print_errata_status(void) {}
-#endif
-
 void errata_print_msg(unsigned int status, const char *cpu, const char *id);
-int errata_needs_reporting(spinlock_t *lock, uint32_t *reported);
 
 #endif /* __ASSEMBLER__ */
 
