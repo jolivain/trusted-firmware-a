93a3b0085b2222d39de40dd743b14ceebe33d42e
Boyan Karatotev <boyan.karatotev@arm.com>
2023-01-27 09:38:15 +0000

    feat(errata_framework): add a way to automatically report errata
    Using the errata framework per-cpu data structure, errata can all be
    printed automatically through a single standard errata reporter which
    can replace the cpu-specific ones.
    
    This reporter can also enforce the ordering requirement of errata.
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    Change-Id: I7d2d5ac5bcb9d21aed0d560d7d23919a323ffdab

--- include/lib/cpus/errata.h
+++ include/lib/cpus/errata.h
@@ -17,6 +17,22 @@ static inline void print_errata_status(void) {}
 
 void errata_print_msg(unsigned int status, const char *cpu, const char *id);
 
+/*
+ * NOTE that this structure will be different on Aarch32 and Aarch64. The
+ * uintptr_t will reflect the change and the alignment will be correct in both.
+ */
+struct erratum_entry {
+	uintptr_t (*wa_func)(uint64_t cpu_rev);
+	uintptr_t (*check_func)(uint64_t cpu_rev);
+	/* Will fit CVEs with up to 10 character in the ID field */
+	uint32_t erratum_id;
+	/* we denote errata with 0, CVEs have their year here */
+	uint16_t cve;
+	uint8_t chosen;
+	/* TODO(errata ABI): placeholder for the mitigated field */
+	uint8_t _mitigated;
+} __packed;
+
 #endif /* __ASSEMBLER__ */
 
 /* Errata status */
@@ -26,5 +42,8 @@ void errata_print_msg(unsigned int status, const char *cpu, const char *id);
 
 /* Macro to get CPU revision code for checking errata version compatibility. */
 #define CPU_REV(r, p)		((r << 4) | p)
+
+#define ERRATUM(id)		0, id
+#define CVE(year, id)		year, id
 
 #endif /* ERRATA_REPORT_H */
