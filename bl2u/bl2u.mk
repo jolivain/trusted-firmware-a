8155368e9774fe10720dc938f985dd90737e8dcd
Harrison Mutai <harrison.mutai@arm.com>
2023-02-06 15:34:48 +0000

    build(bl2u): migrate to a shared linker script
    Refactor the BL2U linker script to utilise the generic
    ``bootloader.ld.S`` linker script.
    
    Change-Id: Iae5a4d6d6207dae3a3ad1b845051ebf95334c78a
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- bl2u/bl2u.mk
+++ bl2u/bl2u.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
-BL2U_SOURCES		+=	bl2u/bl2u_main.c			\
+BL2U_SOURCES	+=	bl2u/bl2u_main.c			\
 				bl2u/${ARCH}/bl2u_entrypoint.S		\
 				plat/common/${ARCH}/platform_up_stack.S
 
@@ -12,4 +12,5 @@ ifeq (${ARCH},aarch64)
 BL2U_SOURCES		+=	common/aarch64/early_exceptions.S
 endif
 
-BL2U_DEFAULT_LINKER_SCRIPT_SOURCE := bl2u/bl2u.ld.S
+BL2U_DEFAULT_LINKER_SCRIPT_SOURCE := bootloader/bootloader.ld.S
+BL2U_LINKER_SCRIPT_SOURCES := bl2u/bl2u.ld.S
