b9645fb6cc90be88ececdab82f745ee37f711988
Boyan Karatotev <boyan.karatotev@arm.com>
2023-02-13 15:08:09 +0000

    fix(trbe): set MDCR_EL3.NSTBE explicitly
    With the introduction of FEAT_RME MDCR_EL3 bits NSTB and NSTBE depend on
    each other. The enable code relies on the register being initialised to
    zero and omits to reset NSTBE. However, this is not obvious. Reset the
    bit explicitly to document this.
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    Change-Id: I3d24b45d3ea80882c8e450b2d9db9d5531facec1

--- lib/extensions/trbe/trbe.c
+++ lib/extensions/trbe/trbe.c
@@ -25,6 +25,10 @@ void trbe_enable(void)
 
 	if (is_feat_trbe_present()) {
 		/*
+		 * MDCR_EL3.NSTBE = 0b0
+		 * Trace Buffer owning Security state is Non-secure state. If
+		 * FEAT_RME is not implemented, this field is RES0.
+		 *
 		 * MDCR_EL3.NSTB = 0b11
 		 * Allow access of trace buffer control registers from NS-EL1
 		 * and NS-EL2, tracing is prohibited in Secure and Realm state
@@ -32,6 +36,7 @@ void trbe_enable(void)
 		 */
 		val = read_mdcr_el3();
 		val |= MDCR_NSTB(MDCR_NSTB_EL1);
+		val &= ~(MDCR_NSTBE_BIT);
 		write_mdcr_el3(val);
 	}
 }
