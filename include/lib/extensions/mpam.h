930094bae38b564635c7f83b0df4012b612d6864
Boyan Karatotev <boyan.karatotev@arm.com>
2023-02-16 15:12:45 +0000

    refactor(cpufeat): separate the EL2 and EL3 enablement code
    Combining the EL2 and EL3 enablement code necessitates that it must be
    called at el3_exit, which is the only place with enough context to make
    the decision of what needs to be set. Decouple them to allow them to be
    called from elsewhere.
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    Change-Id: I147764c42771e7d4100699ec8fae98dac0a505c0

--- include/lib/extensions/mpam.h
+++ include/lib/extensions/mpam.h
@@ -10,9 +10,13 @@
 #include <stdbool.h>
 
 #if ENABLE_MPAM_FOR_LOWER_ELS
-void mpam_enable(bool el2_unused);
+void mpam_enable_el3(void);
+void mpam_enable_el2(void);
 #else
-static inline void mpam_enable(bool el2_unused)
+static inline void mpam_enable_el3(void)
+{
+}
+static inline void mpam_enable_el2(void)
 {
 }
 #endif
