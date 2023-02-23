55e774c104acccb4f400701c49b74a713ae1ba6d
Harrison Mutai <harrison.mutai@arm.com>
2023-02-23 11:30:17 +0000

    feat(memmap): add tabular tabular memory use data
    Add support for tabulating static memory consumption data from ELF
    binaries. This relies on static symbols, defined in the linker files,
    that provide information about the memory ranges.
    
    Change-Id: Ie19cd2b80a7b591607640feeb84c63266963ea4d
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- tools/memory/src/elfparser.py
+++ tools/memory/src/elfparser.py
@@ -21,6 +21,47 @@ class TfaElfParser:
             for sym in elf.get_section_by_name(".symtab").iter_symbols()
         }
 
+        self._memory_layout = self.get_memory_layout_from_symbols()
+
+    def get_memory_layout_from_symbols(self, expr=None) -> dict:
+        """Retrieve information about the memory configuration from the symbol
+        table.
+        """
+        assert len(self._symbols), "Symbol table is empty!"
+
+        if not expr:
+            expr = r".*(.?R.M)_REGION.*(START|END|LENGTH)"
+
+        region_symbols = dict(self.get_filtered_symbols(expr))
+        memory_layout = {}
+
+        for k, v in region_symbols.items():
+            region, _, attr = tuple(k.lower().strip("__").split("_"))
+            if region not in memory_layout:
+                memory_layout[region] = {}
+
+            memory_layout[region][attr] = v
+
+        return memory_layout
+
+    def get_elf_memory_layout(self):
+        """Get the total memory consumed by this module from the memory
+        configuration.
+            {"rom": {"start": 0x0, "end": 0xFF, "length": ... }
+        """
+        mem_dict = {}
+
+        for mem, attrs in self._memory_layout.items():
+            limit = attrs["start"] + attrs["length"]
+            mem_dict[mem] = {
+                "start": attrs["start"],
+                "limit": limit,
+                "size": attrs["end"] - attrs["start"],
+                "free": limit - attrs["end"],
+                "total":  attrs["length"]
+            }
+        return mem_dict
+
     def get_filtered_symbols(self, expr):
         """Filter the symbol table by name."""
         return filter(lambda sym: re.match(expr, sym[0]), self._symbols.items())
