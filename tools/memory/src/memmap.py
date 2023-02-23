3a003753b9e9565030d1ab3ff1c76055cb8c94c2
Harrison Mutai <harrison.mutai@arm.com>
2023-02-23 11:30:55 +0000

    feat(memmap): add topological memory view
    Present memory usage in hierarchical view. This view maps modules to
    their respective segments and sections.
    
    Change-Id: I5c374b46738edbc83133441ff3f4268f08cb011d
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- tools/memory/src/memmap.py
+++ tools/memory/src/memmap.py
@@ -38,6 +38,18 @@ from printer import TfaPrettyPrinter
     help="Generate a high level view of memory usage by memory types.",
 )
 @click.option(
+    "-t",
+    "--tree",
+    is_flag=True,
+    help="Generate a hierarchical view of the modules, segments and sections.",
+)
+@click.option(
+    "-d",
+    "--depth",
+    default=3,
+    help="Generate a virtual address map of important TF symbols.",
+)
+@click.option(
     "-s",
     "--symbols",
     is_flag=True,
@@ -50,7 +62,7 @@ from printer import TfaPrettyPrinter
     default=False,
     help="Display numbers in hex.",
 )
-def main(root, platform, build_type, footprint, symbols, width, x):
+def main(root, platform, build_type, footprint, tree, symbols, depth, width, x):
     build_path = Path(root) if root else Path("build/", platform, build_type)
     click.echo(f"build-path: {build_path.resolve()}")
 
@@ -61,6 +73,11 @@ def main(root, platform, build_type, footprint, symbols, width, x):
 
     if footprint:
         printer.print_footprint(parser.get_mem_usage_dict())
+
+    if tree:
+        printer.print_mem_tree(
+            parser.get_mem_tree_as_dict(), modules, depth=depth
+        )
 
     if symbols:
         expr = (
