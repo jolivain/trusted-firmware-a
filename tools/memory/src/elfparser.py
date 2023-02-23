3c98da830b8e31228662310c906b278470c4c38e
Harrison Mutai <harrison.mutai@arm.com>
2023-02-23 11:30:17 +0000

    feat(memmap): add tabular tabular memory use data
    Add support for tabulating static memory consumption data from ELF
    binaries. This relies on static symbols, defined in the linker files,
    that provide information about the memory ranges.
    
    Change-Id: Ie19cd2b80a7b591607640feeb84c63266963ea4d
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- tools/memory/src/elfparser.py
+++ tools/memory/src/elfparser.py
@@ -30,6 +30,32 @@ class TfaElfParser:
             for sym in elf.get_section_by_name(".symtab").iter_symbols()
         }
 
+        self.set_mem_cfg()
+
+    def set_mem_cfg(self, expr=None):
+        """Retrieve information about the memory configuration from the symbol
+        table.
+        """
+        assert len(self._symbols), "Symbol table is empty!"
+
+        if not expr:
+            expr = r".*(.?R.M)_REGION.*(START|END|LENGTH)"
+        region_symbols = dict(self.get_filtered_symbols(expr))
+
+        for k, v in region_symbols.items():
+            mem_type, _, param = tuple(k.lower().strip("__").split("_"))
+            if mem_type not in self._mem_cfg.keys():
+                self._mem_cfg[mem_type] = {}
+
+            self._mem_cfg[mem_type][param] = v
+
+    def get_mem_cfg(self):
+        """Get the total memory consumed by this module from the memory
+        configuration.
+            {"rom": {"start": 0x0, "end": 0xFF, "length": ... }
+        """
+        return self._mem_cfg
+
     def get_filtered_symbols(self, expr):
         """Filter the symbol table by name."""
         return filter(lambda sym: re.match(expr, sym[0]), self._symbols.items())
