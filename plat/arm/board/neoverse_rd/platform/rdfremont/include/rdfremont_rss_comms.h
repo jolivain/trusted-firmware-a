69f53a10bc5f1f9b58a9c3c172613b07314b9209
Vivek Gautam <vivek.gautam@arm.com>
2023-03-09 20:40:12 +0000

    feat(rdfremont): helper to initialize rss-comms with AP-RSS MHUv3
    Add a helper function to initialize rss_comms on RD-Fremont platforms
    with AP-RSS MHUv3 postbox and mailbox register frames.
    
    Signed-off-by: Rohit Mathew <rohit.mathew@arm.com>
    Signed-off-by: Vivek Gautam <vivek.gautam@arm.com>
    Change-Id: Ic390517a8810df195a2582793b81afdbff5ffa15

---
+++ plat/arm/board/neoverse_rd/platform/rdfremont/include/rdfremont_rss_comms.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef RDFREMONT_RSS_COMMS_H
+#define RDFREMONT_RSS_COMMS_H
+
+int plat_rss_comms_init(void);
+
+#endif /* RDFREMONT_RSS_COMMS_H */
