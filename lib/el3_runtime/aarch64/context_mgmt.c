02cb0f3bc718f8a8b4a54d97184dda33c81a63f2
Mark Brown <broonie@kernel.org>
2023-03-14 20:48:43 +0000

    feat(pie/por): support permission indirection and overlay
    Arm v8.9 introduces a series of features providing a new way to set memory
    permissions. Instead of directly encoding the permissions in the page
    tables the PTEs contain indexes into an array of permissions stored in
    system registers, allowing greater flexibility and density of encoding.
    
    Enable access to these features for EL2 and below, context switching the
    newly added EL2 registers as appropriate. Since all of FEAT_S[12]P[IO]E
    are separately discoverable we have separate build time options for
    enabling them, but note that there is overlap in the registers that they
    implement and the enable bit required for lower EL access.
    
    Change the FVP platform to default to handling them as dynamic options so
    the right decision can be made by the code at runtime.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Change-Id: Icf89e444e39e1af768739668b505661df18fb234

--- lib/el3_runtime/aarch64/context_mgmt.c
+++ lib/el3_runtime/aarch64/context_mgmt.c
@@ -355,6 +355,15 @@ static void setup_context_common(cpu_context_t *ctx, const entry_point_info_t *e
 	}
 
 	/*
+	 * SCR_EL3.PIEN: Enable permission indirection and overlay
+	 * registers for AArch64 if present.
+	 */
+	if ((is_feat_sxpie_supported() || is_feat_sxpoe_supported()) &&
+	    (GET_RW(ep->spsr) == MODE_RW_64)) {
+		scr_el3 |= SCR_PIEN_BIT;
+	}
+
+	/*
 	 * CPTR_EL3 was initialized out of reset, copy that value to the
 	 * context register.
 	 */
@@ -894,6 +903,12 @@ void cm_el2_sysregs_context_save(uint32_t security_state)
 		if (is_feat_tcr2_supported()) {
 			write_ctx_reg(el2_sysregs_ctx, CTX_TCR2_EL2, read_tcr2_el2());
 		}
+		if (is_feat_sxpie_supported()) {
+			write_ctx_reg(el2_sysregs_ctx, CTX_PIR_EL2, read_pir_el2());
+		}
+		if (is_feat_sxpoe_supported()) {
+			write_ctx_reg(el2_sysregs_ctx, CTX_POR_EL2, read_por_el2());
+		}
 	}
 }
 
@@ -956,6 +971,12 @@ void cm_el2_sysregs_context_restore(uint32_t security_state)
 		}
 		if (is_feat_tcr2_supported()) {
 			write_tcr2_el2(read_ctx_reg(el2_sysregs_ctx, CTX_TCR2_EL2));
+		}
+		if (is_feat_sxpie_supported()) {
+			write_pir_el2(read_ctx_reg(el2_sysregs_ctx, CTX_PIR_EL2));
+		}
+		if (is_feat_sxpoe_supported()) {
+			write_por_el2(read_ctx_reg(el2_sysregs_ctx, CTX_POR_EL2));
 		}
 	}
 }
