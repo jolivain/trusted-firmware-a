e38427d4d0aaf97449e628a1950fb1f92a906d53
Vivek Gautam <vivek.gautam@arm.com>
2023-03-28 21:47:05 +0100

    feat(rdfremont): fetch attestation key and token from RSS
    Use the delegated attestation driver to fetch platform attestation token
    and Realm attestation key from RSS over the AP-RSS comms interface.
    
    Signed-off-by: Rohit Mathew <Rohit.Mathew@arm.com>
    Signed-off-by: Vivek Gautam <vivek.gautam@arm.com>
    Change-Id: Id0cfd82ef79598cd8368ba017c145bf34d502e65

--- plat/arm/board/neoverse_rd/platform/rdfremont/rdfremont_plat_attest_token.c
+++ plat/arm/board/neoverse_rd/platform/rdfremont/rdfremont_plat_attest_token.c
@@ -4,13 +4,25 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <errno.h>
 #include <stdint.h>
 
+#include <common/debug.h>
+#include <drivers/delay_timer.h>
 #include <plat/common/platform.h>
+#include <psa/error.h>
+#include <rss_delegated_attestation.h>
 
-/* Placeholder implementation of the API to retrieve attestation token */
 int plat_rmmd_get_cca_attest_token(uintptr_t buf, size_t *len,
 			       uintptr_t hash, size_t hash_size)
 {
+	psa_status_t ret;
+
+	ret = delegated_attestation_get_token(buf, len, hash, hash_size);
+	if (ret != PSA_SUCCESS) {
+		ERROR("Unable to fetch CCA attestation token\n");
+		return -1;
+	}
+
 	return 0;
 }
