1f9cb3972514b06ef93aa77ae020647533ea53bf
Vivek Gautam <vivek.gautam@arm.com>
2023-03-28 21:47:05 +0100

    feat(rdfremont): fetch attestation key and token from RSE
    Use the delegated attestation driver to fetch platform attestation token
    and Realm attestation key from RSE over the AP-RSE comms interface.
    
    Signed-off-by: Rohit Mathew <Rohit.Mathew@arm.com>
    Signed-off-by: Vivek Gautam <vivek.gautam@arm.com>
    Change-Id: Id0cfd82ef79598cd8368ba017c145bf34d502e65

--- plat/arm/board/neoverse_rd/platform/rdfremont/rdfremont_realm_attest_key.c
+++ plat/arm/board/neoverse_rd/platform/rdfremont/rdfremont_realm_attest_key.c
@@ -4,13 +4,25 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <errno.h>
 #include <stdint.h>
 
+#include <common/debug.h>
+#include <drivers/delay_timer.h>
 #include <plat/common/platform.h>
+#include <psa/error.h>
+#include <rse_delegated_attestation.h>
 
-/* Placeholder implementation of the API to retrieve attestation key */
 int plat_rmmd_get_cca_realm_attest_key(uintptr_t buf, size_t *len,
 				       unsigned int type)
 {
+	psa_status_t ret;
+
+	ret = delegated_attestation_get_key(buf, len, type);
+	if (ret != PSA_SUCCESS) {
+		ERROR("Unable to fetch CCA attestation key\n");
+		return -1;
+	}
+
 	return 0;
 }
