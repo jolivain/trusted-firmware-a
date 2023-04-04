db4dccf37d78a1ae205a40a0cfd040dcfe02eb56
Dongjiu Geng <gengdongjiu1@gmail.com>
2023-04-04 19:24:11 +0800

    feat(plat/qemu): add sdei support for QEMU
    Add sdei support for QEMU, this is to let jailhouse Hypervisor
    use SDEI to do hypervisor management, after physical IRQ
    has been disabled routing.
    
    Note: To enable SDEI in QEMU, it needs to set "SDEI_SUPPORT=1
    EL3_EXCEPTION_HANDLING=1" when compiling.
    
    Signed-off-by: Dongjiu Geng <gengdongjiu1@gmail.com>
    Change-Id: Ia7f9c5a0db36da03e5c6e6fb1270281f19924d77

---
+++ plat/qemu/common/qemu_sdei.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2017-2020, ARM Limited and Contributors. All rights reserved.
+ * Copyright 2020 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/* SDEI configuration for ARM platforms */
+
+#include <bl31/ehf.h>
+#include <common/debug.h>
+#include <services/sdei.h>
+#include <lib/utils_def.h>
+
+#include <platform_def.h>
+
+/* Private event mappings */
+static sdei_ev_map_t qemu_sdei_private[] = {
+	SDEI_DEFINE_EVENT_0(PLAT_SDEI_SGI_PRIVATE),
+};
+
+/* Shared event mappings */
+static sdei_ev_map_t qemu_sdei_shared[] = {
+};
+
+void plat_sdei_setup(void)
+{
+	INFO("SDEI platform setup\n");
+}
+
+/* Export Arm SDEI events */
+REGISTER_SDEI_MAP(qemu_sdei_private, qemu_sdei_shared);
