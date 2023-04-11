a98622ea94968c4ad38b29a2fe42e7f1cb750f8e
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2023-04-11 16:13:09 +0100

    feat(fvp): implement platform function to measure and publish Public Key
    Implemented 'plat_mboot_measure_key' platform function for FVP platform
    to measure and publish the public key information via RSS.
    
    Change-Id: I0c9d6d6ac3650a939437e9331ed3c9246f242830
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>

--- plat/arm/board/fvp/fvp_common_measured_boot.c
+++ plat/arm/board/fvp/fvp_common_measured_boot.c
@@ -45,3 +45,9 @@ int plat_mboot_measure_image(unsigned int image_id, image_info_t *image_data)
 
 	return rc;
 }
+
+int plat_mboot_measure_key(void *pk_oid, void *pk_ptr, unsigned int pk_len)
+{
+	return rss_mboot_set_signer_id(fvp_rss_mboot_metadata, pk_oid, pk_ptr,
+				       pk_len);
+}
