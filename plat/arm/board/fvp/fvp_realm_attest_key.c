a1402de06d6826f6b54f6240da00f0ffc398b64b
David Vincze <david.vincze@arm.com>
2023-04-11 09:55:34 +0200

    fix(fvp): update realm attestation key parameters
    Use (only) valid parameters for retrieving the realm attestation
    key even if they are currently ignored in lower SW layers.
    
    Change-Id: I5df90a3437af5559fbf059228a2413762f8e00d3
    Signed-off-by: David Vincze <david.vincze@arm.com>

--- plat/arm/board/fvp/fvp_realm_attest_key.c
+++ plat/arm/board/fvp/fvp_realm_attest_key.c
@@ -5,6 +5,10 @@
  */
 
 #include <assert.h>
+
+#include <psa/crypto_types.h>
+#include <psa/crypto_values.h>
+
 #include <delegated_attestation.h>
 #include <psa/error.h>
 #include <services/rmmd_svc.h>
@@ -21,10 +25,18 @@ int plat_rmmd_get_cca_realm_attest_key(uintptr_t buf, size_t *len,
 {
 	psa_status_t ret;
 
+	/*
+	 * As currently the only supported elliptic curve type is
+	 * ATTEST_KEY_CURVE_ECC_SECP384R1, the parameters of the
+	 * PSA Delegated Attestation interface are fixed.
+	 * ecc_curve:	PSA_ECC_FAMILY_SECP_R1
+	 * key_bits:	384
+	 */
 	assert(type == ATTEST_KEY_CURVE_ECC_SECP384R1);
 
-	ret = rss_delegated_attest_get_delegated_key(0U, 0U, (uint8_t *)buf,
-						     *len, len, 0U);
+	ret = rss_delegated_attest_get_delegated_key(PSA_ECC_FAMILY_SECP_R1, 384,
+						     (uint8_t *)buf, *len, len,
+						     PSA_ALG_SHA_256);
 
 	return ret;
 }
