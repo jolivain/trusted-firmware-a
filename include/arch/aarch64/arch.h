c81f70501b1d11ddb2b6879f87b5718624e90689
Boyan Karatotev <boyan.karatotev@arm.com>
2023-04-20 11:00:50 +0100

    refactor(cm): clean up SCR_EL3 and CPTR_EL3 initialization
    As with MDCR_EL3, setting some bits of these registers is redundant at
    reset since they do not matter for EL3 execution and the registers get
    context switched so they get overwritten anyway.
    
    The SCR_EL3.{TWE, TWI, SMD, API, APK} bits only affect lower ELs so
    their place is in context management. The API and APK bits are a bit
    special as they would get implicitly unset for secure world when
    CTX_INCLUDE_PAUTH_REGS is unset. This is now explicit with their normal
    world values being undefined as they don't matter.
    
    The CPTR_EL3.{ESM, EZ, TAM} bits are set by the relevant
    feat_enable()s in lib/extensions so they can be skipped too.
    
    CPTR_EL3.TAM is special as it's needed for access to generic gloating
    point registers even when SVE is not present. So keep it but move to
    context management.
    
    This leaves CPTR_EL3.TCPAC which affects several extensions. This bit
    was set centrally for convenience. However, it is each feature's
    responsibility as it only affects EL2.
    
    Finally, setting CPTR_EL3 to a fresh RESET_VAL for each security state
    prevents any bits from leaking between them.
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    Change-Id: Ie7095e967bd4a6d6ca6acf314c7086d89fec8900

--- include/arch/aarch64/arch.h
+++ include/arch/aarch64/arch.h
@@ -672,13 +672,13 @@
 #define EVNTI_MASK		U(0xf)
 
 /* CPTR_EL3 definitions */
-#define TCPAC_BIT		(U(1) << 31)
-#define TAM_SHIFT		U(30)
-#define TAM_BIT			(U(1) << TAM_SHIFT)
-#define TTA_BIT			(U(1) << 20)
-#define ESM_BIT			(U(1) << 12)
-#define TFP_BIT			(U(1) << 10)
-#define CPTR_EZ_BIT		(U(1) << 8)
+#define TCPAC_BIT		(ULL(1) << 31)
+#define TAM_SHIFT		ULL(30)
+#define TAM_BIT			(ULL(1) << TAM_SHIFT)
+#define TTA_BIT			(ULL(1) << 20)
+#define ESM_BIT			(ULL(1) << 12)
+#define TFP_BIT			(ULL(1) << 10)
+#define CPTR_EZ_BIT		(ULL(1) << 8)
 #define CPTR_EL3_RESET_VAL	((TCPAC_BIT | TAM_BIT | TTA_BIT | TFP_BIT) & \
 				~(CPTR_EZ_BIT | ESM_BIT))
 
