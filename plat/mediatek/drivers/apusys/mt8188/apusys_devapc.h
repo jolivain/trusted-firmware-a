707aba8898f2fbe22b7636fc519ad8ff1269a35c
Karl Li <karl.li@mediatek.corp-partner.google.com>
2023-04-21 11:43:24 +0800

    feat(mt8188): add apusys ao devapc setting
    Apusys ao devapc is a set of control registers inside APU, and it
    controls the access permission of APU ao domain.
    Moreover, apusys ao devapc must be set after apusys power init, so
    we need to place the drivers in TF-A instead of coreboot.
    
    Change-Id: Ife849c32d4dd9dca15432d4b8a51753fde61b148
    Signed-off-by: Karl Li <karl.li@mediatek.com>
    Signed-off-by: Chungying Lu <chungying.lu@mediatek.com>

---
+++ plat/mediatek/drivers/apusys/mt8188/apusys_devapc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef APUSYS_DEVAPC_H
+#define APUSYS_DEVAPC_H
+
+#include "../devapc/apusys_dapc_v1.h"
+
+enum apusys_apc_err_status apusys_devapc_ao_init(void);
+
+#endif /* APUSYS_DEVAPC_H */
