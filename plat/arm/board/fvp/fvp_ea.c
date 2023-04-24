a0e5fd0e3370c30ae16bcb2e42006d31b0a03b1e
Manish Pandey <manish.pandey2@arm.com>
2023-04-24 10:46:21 +0100

    feat(fvp): introduce PLATFORM_TEST_LOWER_EL_EA config
    FVP currently does not have proper handling of lower EL External aborts
    and it ends up with default handling, which ends up in EL3 panic.
    
    To test these scenarios sensibly we need a proper handling but that too
    when FVP is under test so that we do not change the default behaviour.
    
    Intorduce PLATFORM_TEST_LOWER_EL_EA config which will be enabled in CI
    scripts and implement a proper handling for Sync EA and SErrors from
    lower EL.
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: Ib130154206b17f72c49c9f07de2d92f35a97ab0b

---
+++ plat/arm/board/fvp/fvp_ea.c
@@ -0,0 +1,46 @@
+/*
+* Copyright (c) 2023, Arm Limited. All rights reserved.
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#include <inttypes.h>
+#include <stdint.h>
+
+#include <common/bl_common.h>
+#include <context.h>
+#include <common/debug.h>
+#include <arch_helpers.h>
+#include <plat/common/platform.h>
+#include <bl31/ea_handle.h>
+#include <lib/el3_runtime/context_mgmt.h>
+
+/*
+ * This source file with custom plat_ea_handler function is compiled only when
+ * building TF-A with compile option HANDLE_EA_EL3_FIRST_NS=1
+ */
+void plat_ea_handler(unsigned int ea_reason, uint64_t syndrome, void *cookie,
+		void *handle, uint64_t flags)
+{
+	u_register_t elr_el3;
+	cpu_context_t *ctx = cm_get_context(NON_SECURE);
+	el3_state_t *state = get_el3state_ctx(ctx);
+
+
+	unsigned int level = (unsigned int)GET_EL(read_spsr_el3());
+
+	if (level < MODE_EL3) {
+		if (ea_reason == ERROR_EA_SYNC) {
+			INFO("Recieved sync EA from lower EL\n");
+			elr_el3 = read_ctx_reg(state, CTX_ELR_EL3);
+			elr_el3 += 4;
+			write_ctx_reg(state, CTX_ELR_EL3, elr_el3);
+			return;
+		} else if (ea_reason == ERROR_EA_ASYNC) {
+			INFO("Recieved Serror from lower EL\n");
+			return;
+		}
+	}
+
+	plat_default_ea_handler(ea_reason, syndrome, cookie, handle, flags);
+}
