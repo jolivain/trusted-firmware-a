266b5380d54991bdae7e1341bb8c48457373222b
Manish Pandey <manish.pandey2@arm.com>
2023-04-24 10:46:21 +0100

    feat(fvp): introduce PLATFORM_TEST_LOWER_EL_EA config
    FVP currently does not have proper handling of lower EL External aborts
    and it ends up with default handling, which ends up in EL3 panic.
    
    To test these scenarios sensibly we need a proper handling but that too
    when FVP is under test so that we do not change the default behaviour.
    
    Intorduce PLATFORM_TEST_LOWER_EL_EA config which will be enabled in CI
    scripts and implement a proper handling for Sync EA and SErrors from
    lower EL.
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: Ib130154206b17f72c49c9f07de2d92f35a97ab0b

--- plat/arm/board/fvp/platform.mk
+++ plat/arm/board/fvp/platform.mk
@@ -509,3 +509,8 @@ PLAT_BL_COMMON_SOURCES	+=	plat/arm/board/fvp/fvp_el3_spmc.c
 endif
 
 PSCI_OS_INIT_MODE	:=	1
+
+$(eval $(call add_define,PLATFORM_TEST_LOWER_EL_EA))
+ifeq (${PLATFORM_TEST_LOWER_EL_EA}, 1)
+BL31_SOURCES	+= plat/arm/board/fvp/fvp_ea.c
+endif
