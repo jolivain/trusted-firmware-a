5101c8e07e07067a77b977baa4113cd19191c04a
Manish Pandey <manish.pandey2@arm.com>
2023-04-24 14:58:55 +0100

    feat(fvp): introduce PLATFORM_TEST_RAS_FFH config
    While doing RAS related tests there were few patches related with
    fault injection and handling were applied through CI hooks.
    These patches were invisible as they were applied and removed after the
    build is done.
    
    This patch introduces build macro PLATFORM_TEST_RAS_FFH and moves the
    patches applied through CI under this.
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: Iddba52f3ebf21f575a473e50c607a944391156b9

--- plat/arm/board/fvp/platform.mk
+++ plat/arm/board/fvp/platform.mk
@@ -509,3 +509,8 @@ PLAT_BL_COMMON_SOURCES	+=	plat/arm/board/fvp/fvp_el3_spmc.c
 endif
 
 PSCI_OS_INIT_MODE	:=	1
+
+$(eval $(call add_define,PLATFORM_TEST_RAS_FFH))
+ifeq (${PLATFORM_TEST_RAS_FFH}, 1)
+BL31_SOURCES	+= plat/arm/board/fvp/aarch64/fvp_fault.S
+endif
