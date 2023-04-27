5e4d1121b614d6e728f6582ebc8b70e0d48b772c
Karl Li <karl.li@mediatek.corp-partner.google.com>
2023-04-27 14:00:10 +0800

    feat(mt8188): enable apusys mailbox mpu protect
    Enable apusys mailbox mpu protect.
    
    Change-Id: Idbf67084037b7ecf4926f57a901075f98540ee57
    Signed-off-by: Karl Li <karl.li@mediatek.com>
    Signed-off-by: Chungying Lu <chungying.lu@mediatek.com>

--- plat/mediatek/drivers/apusys/rules.mk
+++ plat/mediatek/drivers/apusys/rules.mk
@@ -10,11 +10,13 @@ MODULE := apusys
 
 LOCAL_SRCS-y:= ${LOCAL_DIR}/apusys.c
 
-PLAT_INCLUDES += -I${LOCAL_DIR} -I${LOCAL_DIR}/${MTK_SOC} -I${LOCAL_DIR}/security_ctrl/1.0
+PLAT_INCLUDES += -I${LOCAL_DIR} -I${LOCAL_DIR}/${MTK_SOC} -I${LOCAL_DIR}/security_ctrl/1.0 \
+		 -I${LOCAL_DIR}/apusys_rv/2.0
 
 $(eval $(call MAKE_MODULE,$(MODULE),$(LOCAL_SRCS-y),$(MTK_BL)))
 
 SUB_RULES-y := ${LOCAL_DIR}/${MTK_SOC}
 SUB_RULES-y += ${LOCAL_DIR}/devapc
+SUB_RULES-y += ${LOCAL_DIR}/apusys_rv/2.0
 
 $(eval $(call INCLUDE_MAKEFILE,$(SUB_RULES-y)))
