32e22c235cab9e2b611b2a69b9ac4a492bd3a5d2
Tamas Ban <tamas.ban@arm.com>
2023-05-08 13:51:27 +0200

    refactor(n1sdp): update SDS driver API and move region definition
    Signed-off-by: Tamas Ban <tamas.ban@arm.com>
    Change-Id: I3447855fbe7427376d5f7aa0ba7356fe2f14d567

--- plat/arm/board/n1sdp/n1sdp_bl2_setup.c
+++ plat/arm/board/n1sdp/n1sdp_bl2_setup.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Arm Limited. All rights reserved.
+ * Copyright (c) 2022-2023, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -60,13 +60,14 @@ void bl2_platform_setup(void)
 	int ret;
 	struct n1sdp_plat_info plat_info;
 
-	ret = sds_init();
+	ret = sds_init(SDS_SCP_AP_REGION_ID);
 	if (ret != SDS_OK) {
 		ERROR("SDS initialization failed\n");
 		panic();
 	}
 
-	ret = sds_struct_read(N1SDP_SDS_PLATFORM_INFO_STRUCT_ID,
+	ret = sds_struct_read(SDS_SCP_AP_REGION_ID,
+				N1SDP_SDS_PLATFORM_INFO_STRUCT_ID,
 				N1SDP_SDS_PLATFORM_INFO_OFFSET,
 				&plat_info,
 				N1SDP_SDS_PLATFORM_INFO_SIZE,
