f9b1e5d256fddfb0af94a07751baf4e8d29a33c3
Prasad Kummari <prasad.kummari@amd.com>
2023-05-11 14:58:13 +0530

    fix(zynqmp): handling of type el3 interrrupts
    The array type_el3_interrupt_table is defined for MAX_INTR_EL3(128)
    elements and only two interrupts - ARM_IRQ_SEC_SGI_7(15), IRQ_TTC3_1(77)
    are being handled. Current implementation is consuming 1024 bytes which
    can be optimized for the number of interrupts to be handled.
    The current array is replaced with the array of struct
    zynmp_intr_info_type_el3_t (id and handler as member) and with
    maximum number of interrupts to be handled as  the size of array
    (MAX_INTR_EL3 = 2). User is expected to adjust MAX_INTR_EL3 based on
    how many interrupts are handled in TF-A.
    With the updated implementation, a reduction of 960 bytes is observed.
    Versal and Versal NET are using similar implementation introduced by
    commit e497421d7f1e ("feat(versal): add infrastructure to handle
    multiple interrupts") and commit 0654ab7f7544 ("feat(versal-net): add
    support  for platform management").
    
    Signed-off-by: Prasad Kummari <prasad.kummari@amd.com>
    Change-Id: I07aa388d38ac3ff3c0d25decbe0719087b27ee18

--- plat/xilinx/zynqmp/include/plat_private.h
+++ plat/xilinx/zynqmp/include/plat_private.h
@@ -21,8 +21,12 @@ uint32_t zynqmp_calc_core_pos(u_register_t mpidr);
 uint32_t zynqmp_get_uart_clk(void);
 uint32_t zynqmp_get_bootmode(void);
 
-
 #if ZYNQMP_WDT_RESTART
+typedef struct zynqmp_intr_info_type_el3 {
+                uint32_t id;
+                interrupt_type_handler_t handler;
+} zynmp_intr_info_type_el3_t;
+
 /*
  * Register handler to specific GIC entrance
  * for INTR_TYPE_EL3 type of interrupt
