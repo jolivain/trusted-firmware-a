1d0498c9d2201cb2a4569180202274b563279ff8
Boyan Karatotev <boyan.karatotev@arm.com>
2023-06-02 11:11:52 +0100

    refactor(cm): context switch MDCR_EL3
    MDCR_EL3 is not context switched and a single immutable value is
    expected to cater for all 4 worlds. This is not true, as not all
    extensions have realm world disables, e.g. FEAT_BRBE.
    
    Give MDCR_EL3 a per-world value. As all values, except those relating to
    FEAT_BRBE, are identical for all worlds, factor them out into the
    various xxx_common() functions.
    
    Doing this requires considering the settings in el3_arch_init_common
    where all bits but the SDD bit relate to higher ELs, so they too can be
    set in the cm_setup_context_common(). The SDD bit is special in that it
    affects "secure state" which on older ARMv8 revisions might mean EL3 so
    it needs to be set at reset and kept for each world. This bit is
    expected to eventually be part of the root context.
    
    Finally, add a brbe_disable() and use it for the realm context. Although
    not strictly necessary, it makes the prohibition explicit and removes
    the constraint of not building TF-A with FEAT_RME and FEAT_BRBE enabled
    simultaneously.
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    Change-Id: I8c21320329f229a298aff01e51cf18014bfd20fc

--- include/lib/extensions/trbe.h
+++ include/lib/extensions/trbe.h
@@ -7,11 +7,13 @@
 #ifndef TRBE_H
 #define TRBE_H
 
+#include <context.h>
+
 #if ENABLE_TRBE_FOR_NS
-void trbe_enable_el3(void);
+void trbe_enable(cpu_context_t *ctx);
 void trbe_enable_el2(void);
 #else
-static inline void trbe_enable_el3(void)
+static inline void trbe_enable(cpu_context_t *ctx)
 {
 }
 static inline void trbe_enable_el2(void)
