bdaec2ca9b9a40065de6367ad2d73404ee8cde6a
Tamas Ban <tamas.ban@arm.com>
2023-06-06 14:38:16 +0200

    feat(dpe): add DPE driver to measured boot
    Implement a DPE specific backend within the
    generic measured boot framework.
    
    Signed-off-by: Tamas Ban <tamas.ban@arm.com>
    Change-Id: Ia3a0eac0ee6f7b4b337a93d08286613e7c8186b4

---
+++ include/drivers/measured_boot/rss/dice_prot_env.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef DICE_PROT_ENV_H
+#define DICE_PROT_ENV_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/* TODO: Due to size related defines, might move them to a separate header */
+#include <measured_boot.h>
+
+#define DPE_INVALID_ID	UINT32_MAX
+
+struct dpe_metadata {
+	unsigned int id;
+	uint8_t signer_id[SIGNER_ID_MAX_SIZE];
+	size_t  signer_id_size;
+	uint8_t version[VERSION_MAX_SIZE];
+	size_t  version_size;
+	uint8_t sw_type[SW_TYPE_MAX_SIZE];
+	size_t  sw_type_size;
+	bool allow_child_to_derive;
+	bool retain_parent_context;
+	bool create_certificate;
+};
+
+void dpe_init(void);
+struct dpe_metadata *plat_dpe_get_metadata(void);
+int dpe_measure_and_record(uintptr_t data_base, uint32_t data_size,
+			   uint32_t data_id);
+
+#endif /* DICE_PROT_ENV_H */
