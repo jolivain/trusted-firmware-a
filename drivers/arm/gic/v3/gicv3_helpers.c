45078ce056e18593d2ae95f4c869a6b72bd3e387
Sergey Nazarov <Sergey.Nazarov@baikalelectronics.ru>
2023-06-07 14:17:30 +0300

    fix(gicv3): use gicv3_get_multichip_base for SPI's only
    Call to gicv3_get_multichip_base will case ASSERT if it used not for SPI's.
    Put it after the check for an interrupt type.
    
    Signed-off-by: Sergey Nazarov <Sergey.Nazarov@baikalelectronics.ru>
    Change-Id: I2f91c169cc2eeefe1b40d96842bf51218ad1d348

--- drivers/arm/gic/v3/gicv3_helpers.c
+++ drivers/arm/gic/v3/gicv3_helpers.c
@@ -223,12 +223,13 @@ unsigned int gicv3_secure_spis_config_props(uintptr_t gicd_base,
 		current_prop = &interrupt_props[i];
 
 		unsigned int intr_num = current_prop->intr_num;
-		uintptr_t multichip_gicd_base = gicv3_get_multichip_base(intr_num, gicd_base);
 
 		/* Skip SGI, (E)PPI and LPI interrupts */
 		if (!IS_SPI(intr_num)) {
 			continue;
 		}
+
+		uintptr_t multichip_gicd_base = gicv3_get_multichip_base(intr_num, gicd_base);
 
 		/* Configure this interrupt as a secure interrupt */
 		gicd_clr_igroupr(multichip_gicd_base, intr_num);
