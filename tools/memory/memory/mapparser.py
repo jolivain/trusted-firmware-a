eb1c1ad43654972d0ddd129dbadd6f8fd5ab5439
Harrison Mutai <harrison.mutai@arm.com>
2023-06-07 11:28:16 +0100

    fix(memtool): reintroduce support for GNU map files
    The intial patch stack only supported ELF files, which proved particularly
    problematic when dealing with incomplete builds (i.e. build didn't complete
    due to linker errors). This adds support for GNU map files. Most analysis
    performed by the tool should be possible with map files alone.
    
    Change-Id: I89f775a98efc5aef6671a17d0e6e973df555a6fa
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

---
+++ tools/memory/memory/mapparser.py
@@ -0,0 +1,37 @@
+#
+# Copyright (c) 2023, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+from re import search
+from typing import TextIO
+
+
+class TfaMapParser:
+    """A class representing a map file built for TF-A.
+
+    Provides a basic interface for reading the symbol table. The constructor
+    accepts a file-like object with the contents an ELF file.
+    """
+
+    def __init__(self, map_file: TextIO):
+        self._symbols = self.read_symbols(map_file)
+
+    @property
+    def symbols(self):
+        return self._symbols.items()
+
+    @staticmethod
+    def read_symbols(file: TextIO, pattern: str = None) -> dict:
+        pattern = r"\b(0x\w*)\s*(\w*)\s=" if not pattern else pattern
+        symbols = {}
+
+        for line in file.readlines():
+            match = search(pattern, line)
+
+            if match is not None:
+                value, name = match.groups()
+                symbols[name] = int(value, 16)
+
+        return symbols
