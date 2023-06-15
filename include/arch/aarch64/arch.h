35c7385611452226f01c8ed38792bf734be672b2
Olivier Deprez <olivier.deprez@arm.com>
2023-06-15 13:26:24 +0200

    fix(sme): correct SMCR length bitmask
    SMCR_ELx.LEN is 4 bits wide. Fix it in the arch header.
    
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: Idb98bbdbf4eb775f9e592955593fb47a09b56639

--- include/arch/aarch64/arch.h
+++ include/arch/aarch64/arch.h
@@ -1067,7 +1067,7 @@
 
 /* SMCR_ELx definitions */
 #define SMCR_ELX_LEN_SHIFT		U(0)
-#define SMCR_ELX_LEN_MAX		U(0x1ff)
+#define SMCR_ELX_LEN_MAX		U(0xf)
 #define SMCR_ELX_FA64_BIT		(U(1) << 31)
 #define SMCR_ELX_EZT0_BIT		(U(1) << 30)
 
