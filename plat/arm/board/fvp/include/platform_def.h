4ae4c5e4e788b14fa3d249eab8a1af5c2e187c66
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2023-06-27 11:29:34 +0100

    fix(fvp): adjust BL31 maximum size as per total SRAM size
    Adjusted BL31 maximum size as per total SRAM size.
    
    Change-Id: Ifdfdedb8af3e001cebba8e60c973f3c72be11652
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>

--- plat/arm/board/fvp/include/platform_def.h
+++ plat/arm/board/fvp/include/platform_def.h
@@ -232,7 +232,9 @@ defined(IMAGE_BL2) && MEASURED_BOOT
  * calculated using the current BL31 PROGBITS debug size plus the sizes of
  * BL2 and BL1-RW
  */
-#define PLAT_ARM_MAX_BL31_SIZE		(UL(0x3D000) - ARM_L0_GPT_SIZE)
+#define PLAT_ARM_MAX_BL31_SIZE		(PLAT_ARM_TRUSTED_SRAM_SIZE - \
+					 ARM_SHARED_RAM_SIZE - \
+					 ARM_FW_CONFIGS_SIZE - ARM_L0_GPT_SIZE)
 #endif /* RESET_TO_BL31 */
 
 #ifndef __aarch64__
