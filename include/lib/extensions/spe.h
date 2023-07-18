36c35ab1c8ebb0bd8a6bb0fac9a2341b42c7aedf
Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
2023-07-18 14:48:09 +0100

    fix(spe): invoke spe_disable during power domain off/suspend
    spe_disable function, disables profiling and flushes all the buffers and
    hence need to be called on power-off/suspend path.
    It needs to be invoked as SPE feature writes to memory as part of
    regular operation and ignoring to disable before exiting coherency
    could potentially cause issues.
    
    Currently, this is handled only for the FVP. Other platforms need
    to replicate this behaviour and is covered as part of this patch.
    
    Calling it from a generic psci library code before the platform specific
    actions to turn off the CPUs, will make it applicable for all the
    platforms which have ported the PSCI library.
    
    Change-Id: I90b24c59480357e2ebfa3dfc356c719ca935c13d
    Signed-off-by: Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>

--- include/lib/extensions/spe.h
+++ include/lib/extensions/spe.h
@@ -9,10 +9,16 @@
 
 #include <stdbool.h>
 
+typedef struct spe_ctx {
+	u_register_t pmblimitr_el1;
+} spe_ctx_t;
+
 #if ENABLE_SPE_FOR_NS
 void spe_init_el3(void);
 void spe_init_el2_unused(void);
 void spe_disable(void);
+spe_ctx_t spe_context_save(void);
+void spe_context_restore(spe_ctx_t *spe_ctx);
 #else
 static inline void spe_init_el3(void)
 {
@@ -21,6 +27,14 @@ static inline void spe_init_el2_unused(void)
 {
 }
 static inline void spe_disable(void)
+{
+}
+static inline spe_ctx_t spe_context_save(void)
+{
+	spe_ctx_t ctx = {0};
+	return ctx;
+}
+static inline void spe_context_restore(spe_ctx_t *spe_ctx)
 {
 }
 #endif /* ENABLE_SPE_FOR_NS */
