3d2fb0cce958fec4b56d00a56a9982c13cc31a7d
Elizabeth Ho <elizabeth.ho@arm.com>
2023-07-18 14:10:25 +0100

    refactor(cm): move EL3 registers to global context
    Currently, EL3 context registers are duplicated per-world per-cpu.
    Some registers have the same value across all CPUs, so this patch
    moves these registers out into a per-world global context to reduce
    memory usage.
    
    Change-Id: I91294e3d5f4af21a58c23599af2bdbd2a747c54a
    Signed-off-by: Elizabeth Ho <elizabeth.ho@arm.com>
    Signed-off-by: Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>

--- lib/extensions/amu/aarch64/amu.c
+++ lib/extensions/amu/aarch64/amu.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2021, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2017-2023, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -69,6 +69,19 @@ static inline __unused void write_cptr_el2_tam(uint64_t value)
 		((value << CPTR_EL2_TAM_SHIFT) & CPTR_EL2_TAM_BIT));
 }
 
+<<<<<<< HEAD
+=======
+static inline __unused void ctx_write_cptr_el3_tam(global_context_t *global_ctx, uint64_t tam)
+{
+	uint64_t value = global_ctx->ctx_cptr_el3;
+
+	value &= ~TAM_BIT;
+	value |= (tam << TAM_SHIFT) & TAM_BIT;
+
+	global_ctx->ctx_cptr_el3 = value;
+}
+
+>>>>>>> 1364ed9c7... refactor(cm): move EL3 registers to global context
 static inline __unused void ctx_write_scr_el3_amvoffen(cpu_context_t *ctx, uint64_t amvoffen)
 {
 	uint64_t value = read_ctx_reg(get_el3state_ctx(ctx), CTX_SCR_EL3);
@@ -180,15 +193,6 @@ static __unused bool amu_group1_supported(void)
  */
 void amu_enable(cpu_context_t *ctx)
 {
-	/*
-	 * Set CPTR_EL3.TAM to zero so that any accesses to the Activity Monitor
-	 * registers do not trap to EL3.
-	 */
-	u_register_t cptr_el3 = read_ctx_reg(get_el3state_ctx(ctx), CTX_CPTR_EL3);
-
-	cptr_el3 &= ~TAM_BIT;
-	write_ctx_reg(get_el3state_ctx(ctx), CTX_CPTR_EL3, cptr_el3);
-
 	/* Initialize FEAT_AMUv1p1 features if present. */
 	if (is_feat_amuv1p1_supported()) {
 		/*
@@ -197,6 +201,15 @@ void amu_enable(cpu_context_t *ctx)
 		 */
 		ctx_write_scr_el3_amvoffen(ctx, 1U);
 	}
+}
+
+void amu_enable_global(global_context_t *global_ctx)
+{
+	/*
+	 * Set CPTR_EL3.TAM to zero so that any accesses to the Activity Monitor
+	 * registers do not trap to EL3.
+	 */
+	ctx_write_cptr_el3_tam(global_ctx, 0U);
 }
 
 void amu_init_el3(void)
