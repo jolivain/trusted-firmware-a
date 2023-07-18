541e844539c23f68bbe399078168b4e8c65d97ef
Harrison Mutai <harrison.mutai@arm.com>
2023-07-18 11:38:22 +0100

    feat(cpufeat): adds feature detection for FEAT_HAFT
    Adds feature detection for v8.9 feature FEAT_HAFT. Adds respective
    ID_AA64MMFR1_EL1 definitions and ENABLE_FEAT_HAFT define.
    
    Change-Id: Icff588c9621852526741c6104db8537fc658dc7e
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- include/arch/aarch64/arch_features.h
+++ include/arch/aarch64/arch_features.h
@@ -428,6 +428,24 @@ static inline bool is_feat_hcx_supported(void)
 	return read_feat_hcx_id_field() != 0U;
 }
 
+static inline unsigned int read_feat_haft_id_field(void)
+{
+	return ISOLATE_FIELD(read_id_aa64mmfr1_el1(), ID_AA64MMFR1_EL1_HAFDBS);
+}
+
+static inline bool is_feat_haft_supported(void)
+{
+	if (ENABLE_FEAT_HAFT == FEAT_STATE_DISABLED) {
+		return false;
+	}
+
+	if (ENABLE_FEAT_HAFT == FEAT_STATE_ALWAYS) {
+		return true;
+	}
+
+	return read_feat_haft_id_field() == ID_AA64MMFR1_EL1_HAFT_SUPPORTED;
+}
+
 static inline bool is_feat_rng_trap_present(void)
 {
 	return (((read_id_aa64pfr1_el1() >> ID_AA64PFR1_EL1_RNDR_TRAP_SHIFT) &
