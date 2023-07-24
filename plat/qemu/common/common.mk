0fdb51685949b867a24440be809aa93bdfb44a3a
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2023-07-24 21:08:16 +0200

    refactor(qemu) move CPU definitions into one place
    Keep list of supported cpu cores in one place for both platforms.
    qemu_sbsa does not handle some of them but with 256MB firmware space it
    does not matter.
    
    Change-Id: I5b8f7d18dc903e86e0cc7babbc2fb3f26a1bfdfa
    Signed-off-by: Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>

--- plat/qemu/common/common.mk
+++ plat/qemu/common/common.mk
@@ -7,3 +7,22 @@
 include lib/libfdt/libfdt.mk
 include common/fdt_wrappers.mk
 
+PLAT_INCLUDES		:=	-Iinclude/plat/arm/common/		\
+				-I${PLAT_QEMU_COMMON_PATH}/include	\
+				-I${PLAT_QEMU_PATH}/include		\
+				-Iinclude/common/tbbr
+ifeq (${ARM_ARCH_MAJOR},8)
+PLAT_INCLUDES		+=	-Iinclude/plat/arm/common/${ARCH}
+
+QEMU_CPU_LIBS		:=	lib/cpus/aarch64/aem_generic.S		\
+				lib/cpus/aarch64/cortex_a53.S		\
+				lib/cpus/aarch64/cortex_a57.S		\
+				lib/cpus/aarch64/cortex_a72.S		\
+				lib/cpus/aarch64/cortex_a76.S		\
+				lib/cpus/aarch64/neoverse_n_common.S	\
+				lib/cpus/aarch64/neoverse_n1.S		\
+				lib/cpus/aarch64/neoverse_v1.S		\
+				lib/cpus/aarch64/qemu_max.S
+else
+QEMU_CPU_LIBS		:=	lib/cpus/${ARCH}/cortex_a15.S
+endif
