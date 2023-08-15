2c02f35d0b4aed8333a201175f2a40c5e135fe0e
laurenw-arm <lauren.wehrmeister@arm.com>
2023-08-15 14:56:46 -0500

    feat(auth): ecdsa p384 key support
    Add ECDSA P384 to list of defined key algorithms supported on mbed TLS
    libraries - TF_MBEDTLS_ECDSA_P384. Use to enable support in mbed TLS by
    setting MBEDTLS_ECP_DP_SECP384R1_ENABLED.
    
    Selected by setting KEY_ALG=ecdsa-p384.
    
    Change-Id: I382f34fc4da98f166a2aada5d16fdf44632b47f5
    Signed-off-by: Lauren Wehrmeister <lauren.wehrmeister@arm.com>

--- drivers/auth/mbedtls/mbedtls_common.mk
+++ drivers/auth/mbedtls/mbedtls_common.mk
@@ -84,6 +84,8 @@ endif
 ifeq (${TF_MBEDTLS_KEY_ALG},)
     ifeq (${KEY_ALG}, ecdsa)
         TF_MBEDTLS_KEY_ALG		:=	ecdsa
+    else ifeq (${KEY_ALG}, ecdsa-p384)
+        TF_MBEDTLS_KEY_ALG		:=	ecdsa-p384
     else
         TF_MBEDTLS_KEY_ALG		:=	rsa
     endif
@@ -109,6 +111,8 @@ endif
 
 ifeq (${TF_MBEDTLS_KEY_ALG},ecdsa)
     TF_MBEDTLS_KEY_ALG_ID	:=	TF_MBEDTLS_ECDSA
+else ifeq (${TF_MBEDTLS_KEY_ALG},ecdsa-p384)
+    TF_MBEDTLS_KEY_ALG_ID	:=	TF_MBEDTLS_ECDSA_P384
 else ifeq (${TF_MBEDTLS_KEY_ALG},rsa)
     TF_MBEDTLS_KEY_ALG_ID	:=	TF_MBEDTLS_RSA
 else ifeq (${TF_MBEDTLS_KEY_ALG},rsa+ecdsa)
