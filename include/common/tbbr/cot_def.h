386664bb6e428e2f878a07a79adf0569569008b0
laurenw-arm <lauren.wehrmeister@arm.com>
2023-08-15 14:57:56 -0500

    feat(arm): ecdsa p384/p256 full key support
    Add full key support for ECDSA P384 and P256.
    
    New .S files and p384 pem file created along with new
    plat_get_rotpk_info() flag ARM_ROTPK_DEVEL_FULL_DEV_ECDSA_KEY_ID.
    
    Add expected PK_DER_LEN for ECDSA P384.
    
    Change-Id: I578b257eca41070bb4f4791ef429f2b8a66b1eb3
    Signed-off-by: Lauren Wehrmeister <lauren.wehrmeister@arm.com>

--- include/common/tbbr/cot_def.h
+++ include/common/tbbr/cot_def.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2023, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2015-2023, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -46,7 +46,11 @@
 #error "Invalid value for TF_MBEDTLS_KEY_SIZE"
 #endif
 #else /* Only using ECDSA keys. */
+#if (TF_MBEDTLS_KEY_ALG_ID == TF_MBEDTLS_ECDSA_P384)
+#define PK_DER_LEN                      120
+#else
 #define PK_DER_LEN                      92
+#endif
 #endif
 
 #if TF_MBEDTLS_HASH_ALG_ID == TF_MBEDTLS_SHA256
