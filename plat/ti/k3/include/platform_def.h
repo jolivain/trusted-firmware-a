d82fb052bf75ead7b433e13753bcf4ba4e90f038
Gregory CLEMENT <gregory.clement@bootlin.com>
2023-08-23 18:13:44 +0200

    feat(k3): map a memory region for suspend to ram
    For suspend to RAM support, BL31 may need access to a memory region
    to save itself before to enter sleep.
    
    Change-Id: I4f2ed958d86b60c72ecd07a385fcf7b4ff55e65a
    Signed-off-by: Gregory CLEMENT <gregory.clement@bootlin.com>
    Signed-off-by: Thomas Richard <thomas.richard@bootlin.com>

--- plat/ti/k3/include/platform_def.h
+++ plat/ti/k3/include/platform_def.h
@@ -82,11 +82,7 @@
  * runtime memory used, choose the smallest value needed to register the
  * required regions for each BL stage.
  */
-#if USE_COHERENT_MEM
-#define MAX_MMAP_REGIONS	11
-#else
-#define MAX_MMAP_REGIONS	10
-#endif
+#define MAX_MMAP_REGIONS	12
 
 /*
  * Defines the total size of the address space in bytes. For example, for a 32
