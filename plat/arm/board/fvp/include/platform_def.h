0a71b6db0738ba45cc66ab1cc1aeb0ffb0480f90
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2023-09-01 11:17:36 +0100

    fix(fvp): increase the maximum size of Event Log
    To make room for all image measurements using the
    RME+SPM+TBB+MEASURED_BOOT test configuration, the Event Log's maximum
    size has been significantly increased.
    
    Change-Id: I0b9948dab893e14677bca0afa07167648a6c2729
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>

--- plat/arm/board/fvp/include/platform_def.h
+++ plat/arm/board/fvp/include/platform_def.h
@@ -437,7 +437,11 @@ defined(IMAGE_BL2) && MEASURED_BOOT
 /*
  * Maximum size of Event Log buffer used in Measured Boot Event Log driver
  */
+#if ENABLE_RME && (defined(SPD_tspd) || defined(SPD_opteed) || defined(SPD_spmd))
+#define	PLAT_ARM_EVENT_LOG_MAX_SIZE		UL(0x800)
+#else
 #define	PLAT_ARM_EVENT_LOG_MAX_SIZE		UL(0x400)
+#endif
 
 /*
  * Maximum size of Event Log buffer used for DRTM
