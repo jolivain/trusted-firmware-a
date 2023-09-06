3e4031a4631ebb1a04022fc7f1f68733c0db3395
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2023-09-06 10:22:19 +0100

    feat(mbedtls-psa): register explicit PSA crypto driver
    An explicit MbedTLS PSA crypto driver is registered by compiling
    a new driver file, namely mbedtls_psa_crypto.c when PSA_CRYPTO=1.
    As of now, this file is the same as mbedtls_crypto.c, but subsequent
    patches will update crypto functions in this file to trigger
    PSA crypto APIs.
    
    Change-Id: I404c347990661d87dcf5d0501d238e36914ec3ee
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>

--- drivers/auth/mbedtls/mbedtls_crypto.mk
+++ drivers/auth/mbedtls/mbedtls_crypto.mk
@@ -1,11 +1,17 @@
 #
-# Copyright (c) 2015-2017, ARM Limited and Contributors. All rights reserved.
+# Copyright (c) 2015-2023, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
 include drivers/auth/mbedtls/mbedtls_common.mk
 
-MBEDTLS_SOURCES	+=		drivers/auth/mbedtls/mbedtls_crypto.c
+# Some of the PSA functions are declared in multiple header files, that
+# triggers this warning.
+TF_CFLAGS      +=      -Wno-error=redundant-decls
 
-
+ifeq (${PSA_CRYPTO},1)
+MBEDTLS_SOURCES +=              drivers/auth/mbedtls/mbedtls_psa_crypto.c
+else
+MBEDTLS_SOURCES +=              drivers/auth/mbedtls/mbedtls_crypto.c
+endif
