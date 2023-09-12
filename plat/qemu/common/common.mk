d937dbd86a6357b487abe0341c8312b7833c3d87
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2023-09-12 13:08:13 +0200

    feat(qemu): add "cortex-a710" cpu support
    Add support to qemu "cortex-a710" cpu for "qemu" platform.
    
    CPU is supported by qemu/virt only as qemu/sbsa-ref memory starts at
    2^40 which is limit for Cortex-A710.
    
    Switched 'qemu' platform to be built as armv9.0 to cover features of new
    cpu core.
    
    Signed-off-by: Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
    Change-Id: I035790eac41b2caf7f13167e53f48c16f0827754

--- plat/qemu/common/common.mk
+++ plat/qemu/common/common.mk
@@ -20,12 +20,15 @@ QEMU_CPU_LIBS		:=	lib/cpus/aarch64/aem_generic.S		\
 				lib/cpus/aarch64/cortex_a57.S		\
 				lib/cpus/aarch64/cortex_a72.S		\
 				lib/cpus/aarch64/cortex_a76.S		\
+				lib/cpus/aarch64/cortex_a710.S		\
 				lib/cpus/aarch64/neoverse_n_common.S	\
 				lib/cpus/aarch64/neoverse_n1.S		\
 				lib/cpus/aarch64/neoverse_v1.S		\
 				lib/cpus/aarch64/qemu_max.S
 
 PLAT_INCLUDES		+=	-Iinclude/plat/arm/common/${ARCH}
+
+ARM_ARCH_MAJOR		:=	9
 endif
 
 PLAT_BL_COMMON_SOURCES	:=	${PLAT_QEMU_COMMON_PATH}/qemu_common.c		\
