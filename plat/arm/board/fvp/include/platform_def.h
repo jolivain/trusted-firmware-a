5173ab4b93123e60286bd6e1b7fea87a3301b177
Yann Gautier <yann.gautier@st.com>
2023-10-20 14:49:17 +0200

    fix(fvp): increase BL2 size for CRYPTO_SUPPORT
    With MbedTLS v2.28.5 and using GCC 11.3, there is a compilation error
    for the config fvp-tbb-mbedtls-romlib-fwu:
    BL2 image has exceeded its limit.
    region `RAM' overflowed by 4096 bytes
    Increase the size of BL2 by 4kB: use the same size for CRYPTO_SUPPORT
    as for (TRUSTED_BOARD_BOOT && COT_DESC_IN_DTB).
    
    Signed-off-by: Yann Gautier <yann.gautier@st.com>
    Change-Id: I88d0cc7bae39a4c8027794ce49f9d0d19c2df489

--- plat/arm/board/fvp/include/platform_def.h
+++ plat/arm/board/fvp/include/platform_def.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2022, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2014-2023, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -191,10 +191,8 @@
  * PLAT_ARM_MAX_BL2_SIZE is calculated using the current BL2 debug size plus a
  * little space for growth.
  */
-#if TRUSTED_BOARD_BOOT && COT_DESC_IN_DTB
+#if (TRUSTED_BOARD_BOOT && COT_DESC_IN_DTB) || CRYPTO_SUPPORT
 # define PLAT_ARM_MAX_BL2_SIZE	(UL(0x1E000) - FVP_BL2_ROMLIB_OPTIMIZATION)
-#elif CRYPTO_SUPPORT
-# define PLAT_ARM_MAX_BL2_SIZE	(UL(0x1D000) - FVP_BL2_ROMLIB_OPTIMIZATION)
 #elif ARM_BL31_IN_DRAM
 /* When ARM_BL31_IN_DRAM is set, BL2 can use almost all of Trusted SRAM. */
 # define PLAT_ARM_MAX_BL2_SIZE	(UL(0x1F000) - FVP_BL2_ROMLIB_OPTIMIZATION)
