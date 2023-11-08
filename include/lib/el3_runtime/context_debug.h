3d524154457e9542f511ccfa8794518f84329735
Juan Pablo Conde <juanpablo.conde@arm.com>
2023-11-08 16:14:28 -0600

    feat(context-mgmt): report context memory usage
    This patch provides a reporting functionality to display the memory
    consumed by the context in each security state and for each exception
    level. Flag PLATFORM_REPORT_CTX_MEM_USE enables or disables this
    feature.
    
    Change-Id: I1515366bf87561dcedf2b3206be167804df681d4
    Signed-off-by: Juan Pablo Conde <juanpablo.conde@arm.com>
    Signed-off-by: Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>

---
+++ include/lib/el3_runtime/context_debug.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#if PLATFORM_REPORT_CTX_MEM_USE
+/********************************************************************************
+ * Reports the allocated memory for every security state and then reports the
+ * total system-wide allocated memory.
+ *******************************************************************************/
+#ifdef __aarch64__
+void report_ctx_memory_usage(void);
+#else
+static inline void report_ctx_memory_usage(void) {}
+#endif /* __aarch64__ */
+
+#endif /* PLATFORM_REPORT_CTX_MEM_USE */
