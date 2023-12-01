ea261eeec91d741eeb2f84dcfa647ecc1c04ce6d
Harrison Mutai <harrison.mutai@arm.com>
2023-12-01 15:50:00 +0000

    feat(handoff): support FW handoff b/w BL2 & BL31
    Add support for the firmware handoff framework between BL2 and BL31.
    Create a transfer list in trusted SRAM, leveraging the larger SRAM sizes
    in recent models. Load the HW_CONFIG as a TE along with entry point
    parameters for BL31 execution.
    
    Change-Id: I7c4c6e8353ca978a13520fb3e15fb2803f0f1d0e
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- plat/arm/board/fvp/fvp_common.c
+++ plat/arm/board/fvp/fvp_common.c
@@ -72,9 +72,14 @@ arm_config_t arm_config;
 
 #if TRANSFER_LIST
 #ifdef FW_NS_HANDOFF_BASE
-#define MAP_FW_NS_HANDOFF MAP_REGION_FLAT(FW_NS_HANDOFF_BASE, \
-					  FW_HANDOFF_SIZE,    \
-					  MT_MEMORY | MT_RW | MT_NS)
+#define MAP_FW_NS_HANDOFF                                             \
+	MAP_REGION_FLAT(FW_NS_HANDOFF_BASE, PLAT_ARM_FW_HANDOFF_SIZE, \
+			MT_MEMORY | MT_RW | MT_NS)
+#endif
+#ifdef PLAT_ARM_EL3_FW_HANDOFF_BASE
+#define MAP_EL3_FW_HANDOFF                            \
+	MAP_REGION_FLAT(PLAT_ARM_EL3_FW_HANDOFF_BASE, \
+			PLAT_ARM_FW_HANDOFF_SIZE, MT_MEMORY | MT_RW | EL3_PAS)
 #endif
 #endif
 
@@ -156,7 +161,10 @@ defined(SPD_spmd))
 	ARM_MAP_OPTEE_CORE_MEM,
 	ARM_OPTEE_PAGEABLE_LOAD_MEM,
 #endif
-	{0}
+#ifdef MAP_EL3_FW_HANDOFF
+	MAP_EL3_FW_HANDOFF,
+#endif
+	{ 0 }
 };
 #endif
 #ifdef IMAGE_BL2U
@@ -190,10 +198,10 @@ const mmap_region_t plat_arm_mmap[] = {
 	ARM_MAP_GPT_L1_DRAM,
 	ARM_MAP_EL3_RMM_SHARED_MEM,
 #endif
-#ifdef MAP_FW_NS_HANDOFF
-	MAP_FW_NS_HANDOFF,
+#ifdef MAP_EL3_FW_HANDOFF
+	MAP_EL3_FW_HANDOFF,
 #endif
-	{0}
+	{ 0 }
 };
 
 #if defined(IMAGE_BL31) && SPM_MM
