0bbc842035937e944393ef797a7075b0ef38118e
Chris Kay <chris.kay@arm.com>
2023-12-04 12:03:51 +0000

    build: use new toolchain variables for tools
    This change migrates the values of `CC`, `CPP`, `AS` and other toolchain
    variables to the new `$(toolchain)-$(tool)` variables, which were
    introduced by the toolchain refactor patch. These variables should be
    equivalent to the values that they're replacing.
    
    Change-Id: I644fe4ce82ef1894bed129ddb4b6ab94fb04985d
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- make_helpers/build_macros.mk
+++ make_helpers/build_macros.mk
@@ -658,7 +658,7 @@ $(eval DTBDEP := $(patsubst %.dtb,%.d,$(DOBJ)))
 $(DOBJ): $(2) $(filter-out %.d,$(MAKEFILE_LIST)) | fdt_dirs
 	$${ECHO} "  CPP     $$<"
 	$(eval DTBS       := $(addprefix $(1)/,$(call SOURCES_TO_DTBS,$(2))))
-	$$(Q)$$(PP) $$(DTC_CPPFLAGS) -MT $(DTBS) -MMD -MF $(DTSDEP) -o $(DPRE) $$<
+	$$(Q)$$(CPP) $$(DTC_CPPFLAGS) -MT $(DTBS) -MMD -MF $(DTSDEP) -o $(DPRE) $$<
 	$${ECHO} "  DTC     $$<"
 	$$(Q)$$(DTC) $$(DTC_FLAGS) -d $(DTBDEP) -o $$@ $(DPRE)
 
---
+++ make_helpers/toolchain-tools.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2023, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+toolchains := host
+
+host-cc := $(HOSTCC)
+host-cpp := $(HOSTCPP)
+
+host-as := $(HOSTAS)
+
+host-ld := $(HOSTLD)
+host-oc := $(HOSTOC)
+host-od := $(HOSTOD)
+host-ar := $(HOSTAR)
+
+host-dtc := $(HOSTDTC)
