3241943740c7f1094258e241638b693b47dcf28a
Manish Pandey <manish.pandey2@arm.com>
2023-12-08 20:13:29 +0000

    feat(el3-runtime): introduce UNDEF injection to lower EL
    For a feature to be used at lower ELs, EL3 generally needs to disable
    the trap so that lower ELs can access the system registers associated
    with the feature. Lower ELs generally check ID registers to dynamically
    detect if a feature is present (in HW) or not while EL3 Firmware relies
    statically on feature build macros to enable a feature.
    
    If a lower EL accesses a system register for a feature that EL3 FW is
    unaware of, EL3 traps the access and panics. This happens mostly with
    EL2 but sometime VM's can also cause EL3 panic.
    
    To provide platforms with capability to mitigate this problem, UNDEF
    injection support has been introduced which injects an synchronous
    exception in to the lower EL which is supposed to handle the exception.
    
    The current support is only provided for aarch64.
    
    The implementation does following on encountering sys reg trap
    
     - For exception happening at EL2/EL1 target is same EL but for
       exception happening at EL0 the target EL can be EL2 or EL1 depnending
       on FEAT_VHE and HCR.TGE/E2H bits. Once the EL is decided, PSTATE for
       target EL has be re-created by modifying SPSR_EL3.
     - update ELR_ELx with ELR_EL3
     - ESR_ELx with EC_UNKNOWN
     - ELR_EL3 to sync exception vector address of lower EL
     - Update the SPSR_EL3 (with exceptions masked)
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: I1b7bf6c043ce7aec1ee4fc1121c389b490b7bfb7

--- bl31/bl31_traps.c
+++ bl31/bl31_traps.c
@@ -7,8 +7,11 @@
  * Dispatch synchronous system register traps from lower ELs.
  */
 
+#include <arch_helpers.h>
+#include <arch_features.h>
 #include <bl31/sync_handle.h>
 #include <context.h>
+#include <lib/el3_runtime/context_mgmt.h>
 
 int handle_sysreg_trap(uint64_t esr_el3, cpu_context_t *ctx)
 {
@@ -27,4 +30,109 @@ int handle_sysreg_trap(uint64_t esr_el3, cpu_context_t *ctx)
 #endif
 
 	return TRAP_RET_UNHANDLED;
+}
+
+static bool is_el2_host(void)
+{
+	u_register_t hcr_el2 = read_hcr_el2();
+
+	return 	((read_feat_vhe_id_field() != 0U) &&
+		 (hcr_el2 & HCR_TGE_BIT) &&
+		 (hcr_el2 & HCR_E2H_BIT));
+}
+
+static unsigned int target_el(void)
+{
+	unsigned int level = (unsigned int)GET_EL(read_spsr_el3());
+	if (level == MODE_EL0) {
+		if (is_el2_host()) {
+			level = MODE_EL2;
+		} else {
+			level = MODE_EL1;
+		}
+	}
+	return level;
+}
+
+
+void inject_undef64(cpu_context_t *ctx)
+{
+	u_register_t esr = (EC_UNKNOWN << ESR_EC_SHIFT) | ESR_IL_BIT;
+	el3_state_t *state = get_el3state_ctx(ctx);
+	u_register_t elr_el3 = read_ctx_reg(state, CTX_ELR_EL3);
+	u_register_t old_spsr = read_ctx_reg(state, CTX_SPSR_EL3);
+	unsigned int to_el = target_el();
+	u_register_t new_spsr = 0;
+	u_register_t sctlr;
+
+	if (to_el == MODE_EL2) {
+		/* Set Exception Level to EL2 in AArch64 mode */
+		new_spsr |= (SPSR_M_AARCH64 << SPSR_M_SHIFT) | SPSR_M_EL2H;
+		sctlr = read_sctlr_el2();
+		write_elr_el2(elr_el3);
+		elr_el3 = read_vbar_el2() + 0x200; /* Sync exception address */
+		write_esr_el2(esr);
+		write_spsr_el2(old_spsr);
+	} else {
+		/* Set Exception Level to EL1 in AArch64 mode */
+		new_spsr |= (SPSR_M_AARCH64 << SPSR_M_SHIFT) | SPSR_M_EL1H;
+		sctlr = read_sctlr_el1();
+		write_elr_el1(elr_el3);
+		elr_el3 = read_vbar_el1() + 0x200; /* Sync exception address */
+		write_esr_el1(esr);
+		write_spsr_el1(old_spsr);
+	}
+
+	/* NZCV bits are unchanged */
+	new_spsr |= old_spsr & (SPSR_NZCV);
+
+	/* Mask all exceptions */
+	new_spsr |= SPSR_DAIF_MASK << SPSR_DAIF_SHIFT;
+
+	/* Clear PSTATE.IL bit */
+	new_spsr &= ~SPSR_IL_BIT;
+
+	/* Update PSTATE.PAN bit */
+	new_spsr |= old_spsr & SPSR_PAN_BIT;
+	if (is_feat_pan_supported() &&
+	    ((to_el == MODE_EL1) || is_el2_host()) &&
+	    ((sctlr & SCTLR_SPAN_BIT) == 0U)) {
+		new_spsr |= SPSR_PAN_BIT;
+	}
+
+	/* If SSBS is implemented, take the value from SCTLR.DSSBS */
+	new_spsr |= old_spsr & SPSR_SSBS_BIT_AARCH64;
+	u_register_t sbss_enabled = (read_id_aa64pfr1_el1() >> ID_AA64PFR1_EL1_SSBS_SHIFT)
+				    & ID_AA64PFR1_EL1_SSBS_MASK;
+	if (sbss_enabled != SSBS_UNAVAILABLE) {
+		new_spsr |= ((sctlr & SCTLR_DSSBS_BIT) >> SCTLR_DSSBS_SHIFT)
+			     << SPSR_SSBS_SHIFT_AARCH64;
+	}
+
+	/* If FEAT_MTE2 is implemented mask tag faults by setting TCO */
+	new_spsr |= old_spsr & SPSR_TCO_BIT_AARCH64;
+	if (get_armv8_5_mte_support() >= MTE_IMPLEMENTED_ELX) {
+		new_spsr |= SPSR_TCO_BIT_AARCH64;
+	}
+
+	/* If FEAT_BTI is present, disable branch target exception */
+	new_spsr |= old_spsr & (SPSR_BTYPE_MASK_AARCH64 << SPSR_BTYPE_SHIFT_AARCH64);
+	if (is_armv8_5_bti_present()) {
+		new_spsr &= ~(SPSR_BTYPE_MASK_AARCH64 << SPSR_BTYPE_SHIFT_AARCH64);
+	}
+
+	/* DIT bits are unchanged */
+	new_spsr |= old_spsr & SPSR_DIT_BIT;
+
+	/* Clear UAO bit if FEAT_UAO is present */
+	new_spsr |= old_spsr & SPSR_UAO_BIT_AARCH64;
+	if (is_armv8_2_uao_present()) {
+		new_spsr &= ~(SPSR_UAO_BIT_AARCH64);
+	}
+
+	/* TODO: Re-create other bits as TF-A feature awareness progresses */
+
+	write_ctx_reg(state, CTX_SPSR_EL3, new_spsr);
+	write_ctx_reg(state, CTX_ELR_EL3, elr_el3);
+	return;
 }
