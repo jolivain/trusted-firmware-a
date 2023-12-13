086ff81c5fe0e65bb01a6f0e2235402f7198028f
Chris Kay <chris.kay@arm.com>
2023-12-13 21:26:46 +0000

    build: introduce `merge` and `split` functions
    These functions are fairly simple:
    
      - `merge` joins all the values in a list into a single string
      - `split` splits a string into a list at a given delimiter
    
    Change-Id: I0021e0e9f2219b93f15b9dd12180d60922981f98
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- make_helpers/utilities.mk
+++ make_helpers/utilities.mk
@@ -7,3 +7,41 @@
 empty :=
 space := $(empty) $(empty)
 comma := ,
+
+#
+# Merge a list into a single string.
+#
+# This function takes the following arguments:
+#
+#   - $(1): the list to merge
+#   - $(2): the delimiter to use between elements (optional)
+#
+# Example usage:
+#
+#     $(call merge,a b c) # abc
+#     $(call merge,a b c,:) # a:b:c
+#
+# This is equivalent to doing a string substitution of all spaces in the list
+# variable with the delimiter.
+#
+
+merge = $(subst $(space),$(2),$(strip $(1)))
+
+#
+# Split a string at a delimiter.
+#
+# This function takes the following arguments:
+#
+#   - $(1): the string to split
+#   - $(2): the delimiter to split at
+#
+# Example usage:
+#
+#     $(call split,a-b-c,-) # a b c
+#     $(call split,a:b:c,:) # a b c
+#
+# This is equivalent to doing a string substitution of the delimiter with a
+# single space.
+#
+
+split = $(subst $(2),$(space),$(1))
