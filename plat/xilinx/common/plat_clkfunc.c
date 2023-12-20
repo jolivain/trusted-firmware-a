256d2d47532ecdef9135d63f9e198ca326a20f7a
Prasad Kummari <prasad.kummari@amd.com>
2023-12-20 11:01:24 +0530

    refactor(xilinx): move plat_get_syscnt_freq2 to common file
    The code in the AMD-Xilinx platform for Versal and Versal NET is being
    refactored to move the plat_get_syscnt_freq2() function to a common
    file. This common function is utilized for obtaining the CPU clock
    frequency from the platform.
    
    Change-Id: I7a4c3fa43a2941d51cacd259c57b24e545aea848
    Signed-off-by: Prasad Kummari <prasad.kummari@amd.com>

--- plat/xilinx/common/plat_clkfunc.c
+++ plat/xilinx/common/plat_clkfunc.c
@@ -4,10 +4,26 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 #include <common/debug.h>
+#include <lib/mmio.h>
 #include <plat/common/platform.h>
 
 #include <platform_def.h>
 #include <plat_private.h>
+
+uint32_t plat_get_syscnt_freq2(void)
+{
+	uint32_t counter_freq = 0;
+
+	counter_freq = mmio_read_32(IOU_SCNTRS_BASE +
+				    IOU_SCNTRS_BASE_FREQ_OFFSET);
+	if (counter_freq != 0U) {
+		return counter_freq;
+	} else {
+		INFO("Indicates counter frequency %dHz setting to %dHz\n",
+		     counter_freq, cpu_clock);
+		return cpu_clock;
+	}
+}
 
 void set_cnt_freq(void)
 {
