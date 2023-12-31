bab4c5db2f764d24f61857db70c7d62d9ef3cdc9
Rohit Mathew <Rohit.Mathew@arm.com>
2023-12-31 18:26:15 +0000

    refactor(neoverse_rd): remove soc_css.mk from common makefile
    The soc_css.mk file within the plat/arm/soc module currently implements
    initialization functions for the PCIe controller and NIC400 within the
    SOC specification. However, as none of the Neoverse reference design
    platforms necessitate the initialization of PCIe or NIC400, remove the
    soc_css.mk from the common makefile.
    
    Additionally, empty initialization functions for PCIe and NIC400 are
    added to satisfy the requirements of the plat/arm common code, which
    expects these functions to be present.
    
    Signed-off-by: Rohit Mathew <Rohit.Mathew@arm.com>
    Change-Id: Ia431af62f48fc224962d64902dd3acfbf0b93935

---
+++ plat/arm/board/neoverse_rd/common/nrd_bl1_setup.c
@@ -0,0 +1,20 @@
+
+/*
+ * Copyright (c) 2021, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <plat/arm/soc/common/soc_css.h>
+
+/*******************************************************************************
+ * Perform any BL1 specific platform actions.
+ ******************************************************************************/
+
+void soc_css_init_nic400(void)
+{
+}
+
+void soc_css_init_pcie(void)
+{
+}
