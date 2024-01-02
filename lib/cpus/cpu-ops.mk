2343884fda36af566a8d52c23a504dfd7f547aeb
Harrison Mutai <harrison.mutai@arm.com>
2024-01-02 16:55:44 +0000

    fix(cpus): workaround for Cortex-A715 erratum 2344187
    Cortex-A715 erratum 2344187 is a Cat B erratum that applies to r0p0,
    r1p0 and is fixed in r1p1. The workaround is to set GCR_EL1.RRND to
    0b1, and apply an implementation specific patch sequence.
    
    SDEN: https://developer.arm.com/documentation/SDEN2148827/latest
    
    Change-Id: I78ea39a91254765c964bff89f771af33b23f29c1
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -908,8 +908,12 @@ CPU_FLAG_LIST += ERRATA_V2_2801372
 CPU_FLAG_LIST += ERRATA_A715_2561034
 
 # Flag to apply erratum 2701951 workaround for non-arm interconnect ip.
-# This erratum applies to revisions r0p0, r1p0, and r1p1. Its is fixed in r1p2.
+# This erratum applies to revisions r0p0, r1p0, and r1p1. It is fixed in r1p2.
 CPU_FLAG_LIST += ERRATA_A715_2701951
+
+# Flag to apply erratum 2344187 workaround during reset. This erratum applies
+# to revisions r0p0, and r1p0. It is fixed in r1p1.
+CPU_FLAG_LIST += ERRATA_A715_2344187
 
 # Flag to apply DSU erratum 798953. This erratum applies to DSUs revision r0p0.
 # Applying the workaround results in higher DSU power consumption on idle.
