51efe1c2c6c61565cf0f0f4e97f63ec8d8d659b8
Stuart Yoder <stuart.yoder@arm.com>
2024-01-09 17:51:38 -0600

    feat(drtm): add additional return codes
    add additional return codes defined in the DRTM 1.0 spec
    
    Signed-off-by: Stuart Yoder <stuart.yoder@arm.com>
    Change-Id: I1620e098edf4f070ac759a26ce3c7272faf2d8b2

--- services/std_svc/drtm/drtm_main.h
+++ services/std_svc/drtm/drtm_main.h
@@ -55,6 +55,12 @@ enum drtm_retc {
 	NOT_FOUND = -4,
 	INTERNAL_ERROR = -5,
 	MEM_PROTECT_INVALID = -6,
+	COPROCESSOR_ERROR = -7,
+	OUT_OF_RESOURCE = -8,
+	INVALID_DATA = -9,
+	SECONDARY_PE_NOT_OFF = -10,
+	ALREADY_CLOSED = -11,
+	TPM_ERROR = -12
 };
 
 typedef struct {
