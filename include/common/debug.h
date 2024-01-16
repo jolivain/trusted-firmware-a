3a344654b9091c0815df47bdeedebde80a9223ea
Yann Gautier <yann.gautier@st.com>
2024-01-16 19:39:31 +0100

    feat(console): introduce EARLY_CONSOLE
    This is a generic porting of what was done on ST platforms with flag
    STM32MP_EARLY_CONSOLE. It creates the flag and the prototype for
    plat_setup_early_console(). This function depends on platform
    implementation. This function call is added at the beginning of each BL
    image early setup function.
    The patch also introduce extra log macros (EARLY_ERROR
    and other logs macros). This can replace normal log macros in code that
    will only be executed before the default console is enabled, and will
    do nothing when the EARLY_CONSOLE is not enabled. This can then save
    some space in memory.
    
    Signed-off-by: Yann Gautier <yann.gautier@st.com>
    Change-Id: I77bf0a0c4289b4c7df94e4bfb783a938e05bf023

--- include/common/debug.h
+++ include/common/debug.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2023, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2013-2024, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -90,6 +90,20 @@
 #else
 # define VERBOSE(...)	no_tf_log(LOG_MARKER_VERBOSE __VA_ARGS__)
 #endif
+
+#if EARLY_CONSOLE
+#define EARLY_ERROR(...)	ERROR(__VA_ARGS__)
+#define EARLY_NOTICE(...)	NOTICE(__VA_ARGS__)
+#define EARLY_WARN(...)		WARN(__VA_ARGS__)
+#define EARLY_INFO(...)		INFO(__VA_ARGS__)
+#define EARLY_VERBOSE(...)	VERBOSE(__VA_ARGS__)
+#else /* !EARLY_CONSOLE */
+#define EARLY_ERROR(...)	no_tf_log(LOG_MARKER_ERROR __VA_ARGS__)
+#define EARLY_NOTICE(...)	no_tf_log(LOG_MARKER_NOTICE __VA_ARGS__)
+#define EARLY_WARN(...)		no_tf_log(LOG_MARKER_WARNING __VA_ARGS__)
+#define EARLY_INFO(...)		no_tf_log(LOG_MARKER_INFO __VA_ARGS__)
+#define EARLY_VERBOSE(...)	no_tf_log(LOG_MARKER_VERBOSE __VA_ARGS__)
+#endif /* EARLY_CONSOLE */
 
 const char *get_el_str(unsigned int el);
 
