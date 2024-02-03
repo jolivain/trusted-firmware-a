1e6a0b9df2be45e30ae0fc9290283859a63d3f02
Rohit Mathew <Rohit.Mathew@arm.com>
2024-02-03 19:06:16 +0000

    refactor(sgi): replace build-option prefix to "NRD"
    As of now, CSS_SGI_PLATFORM_VARIANT and CSS_SGI_CHIP_COUNT are the
    external build option that "sgi" platforms support. As "sgi" has been
    renamed to "neoverse_rd" and the source files have been migrated out of
    the css directory, replace the prefix "CSS_SGI" with "NRD".
    
    Signed-off-by: Rohit Mathew <Rohit.Mathew@arm.com>
    Change-Id: I27989ff42404d823dd2a8cd22ff485497ccb62d4

--- plat/arm/board/neoverse_rd/common/nrd-common.mk
+++ plat/arm/board/neoverse_rd/common/nrd-common.mk
@@ -16,9 +16,9 @@ EL3_EXCEPTION_HANDLING		:=	0
 
 HANDLE_EA_EL3_FIRST_NS		:=	0
 
-CSS_SGI_CHIP_COUNT		:=	1
+NRD_CHIP_COUNT		:=	1
 
-CSS_SGI_PLATFORM_VARIANT	:=	0
+NRD_PLATFORM_VARIANT	:=	0
 
 # Do not enable SVE
 ENABLE_SVE_FOR_NS		:=	0
@@ -60,9 +60,9 @@ endif
 
 $(eval $(call add_define,SGI_PLAT))
 
-$(eval $(call add_define,CSS_SGI_CHIP_COUNT))
+$(eval $(call add_define,NRD_CHIP_COUNT))
 
-$(eval $(call add_define,CSS_SGI_PLATFORM_VARIANT))
+$(eval $(call add_define,NRD_PLATFORM_VARIANT))
 
 override CSS_LOAD_SCP_IMAGES	:=	0
 override NEED_BL2U		:=	no
