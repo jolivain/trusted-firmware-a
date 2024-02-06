cbc58de52b00559f6ce64f3d3947d2cb553ed478
Chris Kay <chris.kay@arm.com>
2024-02-06 14:33:12 +0000

    fix(bl1): add missing spinlock dependency
    The spinlock functions from `spinlock.S` are used by `errata_report.c`,
    which is pulled into BL1. In a normal build it appears that this
    function call undergoes dead code elimination so the link error is not
    reported, but when compiled with LTO enabled the linker reports an
    undefined reference.
    
    Change-Id: Id22ffa8c0c8d3ca4b4cd46f0f4aefa53907c8de5
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- bl1/bl1.mk
+++ bl1/bl1.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2013-2023, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2013-2024, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -12,6 +12,7 @@ BL1_SOURCES		+=	bl1/${ARCH}/bl1_arch_setup.c		\
 				lib/cpus/${ARCH}/cpu_helpers.S		\
 				lib/cpus/errata_report.c		\
 				lib/el3_runtime/${ARCH}/context_mgmt.c	\
+				lib/locks/exclusive/${ARCH}/spinlock.S	\
 				plat/common/plat_bl1_common.c		\
 				plat/common/${ARCH}/platform_up_stack.S \
 				${MBEDTLS_SOURCES}
