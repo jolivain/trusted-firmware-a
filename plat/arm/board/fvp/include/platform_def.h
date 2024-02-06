56c166c640eefedd023f926b010507fd5b1cf77f
Harrison Mutai <harrison.mutai@arm.com>
2024-02-06 17:38:40 +0000

    fix(bl2): make BL2 SRAM footprint flexible
    On FVP's the default SRAM size is severly restrictive. However, more
    recent models support larger SRAM configurations (> 256 Kb). We
    introduced the flag FVP_TRUSTED_SRAM_SIZE to allow for TF to handle
    different configurations.
    
    BL31 automatically benefits from this optimisation since it starts from
    the bottom of shared memory, and runs up to the end of SRAM. Increase
    the size of all BL2 builds in proportion to FVP_TRUSTED_SRAM_SIZE so
    that BL2 covers around a third of SRAM.
    
    Change-Id: Idf37e8cb86507ea44b97ac8b3b90fffefe13f57a
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- plat/arm/board/fvp/include/platform_def.h
+++ plat/arm/board/fvp/include/platform_def.h
@@ -217,8 +217,9 @@ defined(IMAGE_BL2) && MEASURED_BOOT
 #endif
 
 /*
- * Set the maximum size of BL2 to be close to half of the Trusted SRAM.
- * Maximum size of BL2 increases as Trusted SRAM size increases.
+ * Set the maximum size of BL2 to be close to a third of Trusted SRAM (half with
+ * CRYPTO_SUPPORT). Maximum size of BL2 increases as Trusted SRAM size
+ * increases.
  */
 #if CRYPTO_SUPPORT
 #if (TF_MBEDTLS_KEY_ALG_ID == TF_MBEDTLS_RSA_AND_ECDSA) || COT_DESC_IN_DTB
@@ -234,7 +235,9 @@ defined(IMAGE_BL2) && MEASURED_BOOT
 /* When ARM_BL31_IN_DRAM is set, BL2 can use almost all of Trusted SRAM. */
 # define PLAT_ARM_MAX_BL2_SIZE	(UL(0x1F000) - FVP_BL2_ROMLIB_OPTIMIZATION)
 #else
-# define PLAT_ARM_MAX_BL2_SIZE	(UL(0x13000) - FVP_BL2_ROMLIB_OPTIMIZATION)
+#define PLAT_ARM_MAX_BL2_SIZE                                   \
+	(((PLAT_ARM_TRUSTED_SRAM_SIZE / 3) & ~PAGE_SIZE_MASK) - \
+	 FVP_BL2_ROMLIB_OPTIMIZATION)
 #endif
 
 #if RESET_TO_BL31
