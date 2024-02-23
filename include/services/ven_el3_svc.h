fa4d10d61ac474047bcd54d176ee6348120ad1a5
Govindraj Raja <govindraj.raja@arm.com>
2024-02-23 16:50:52 -0600

    feat(smccc): support for vendor specific el3 service
    Add support for vendor specific el3 service, SMCCC 1.5 introduces
    support for vendor specific EL3 monitor calls.
    
    Ref: https://developer.arm.com/docs/den0028/latest
    
    Change-Id: Id8bc43842eecdb7a8a2ec7f31a631e88fe4fe0b4
    Signed-off-by: Govindraj Raja <govindraj.raja@arm.com>

---
+++ include/services/ven_el3_svc.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef VEN_EL3_SVC_H
+#define VEN_EL3_SVC_H
+
+/* VEN_EL3_SMC_32		U(0x87000000) */
+/* VEN_EL3_SMC_64		U(0xC7000000) */
+
+#define VEN_EL3_SVC_UID		U(0x8700ff01)
+/*				U(0x8200ff02) is reserved */
+#define VEN_EL3_SVC_VERSION	U(0x8700ff03)
+
+#define VEN_EL3_SVC_VERSION_MAJOR	U(0x0)
+#define VEN_EL3_SVC_VERSION_MINOR	U(0x1)
+
+#endif /* VEN_EL3_SVC_H */
