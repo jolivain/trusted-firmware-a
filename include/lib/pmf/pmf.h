03200d63d9b7f9b48ad1d62c67ccc8f5e9d1980d
Govindraj Raja <govindraj.raja@arm.com>
2024-03-07 16:22:36 -0600

    refactor(smccc): move pmf to vendor el3 calls
    Move pmf support to vendor-specific EL3 Monitor Service Calls. Remove
    pmf call count as it's not supported in vendor-specific el3 as per
    SMCCC Documentation 1.5:
    https://developer.arm.com/documentation/den0028/latest
    
    BREAKING CHANGE: This is a breaking change for users of pmf service
    calls, since from this patch it will be moved to new service range.
    
    Change-Id: Ie1e14aa601d4fc3db352cd5621d842017a18e9ec
    Signed-off-by: Govindraj Raja <govindraj.raja@arm.com>

--- include/lib/pmf/pmf.h
+++ include/lib/pmf/pmf.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2020, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2016-2024, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -38,8 +38,8 @@
 /*
  * Defines for PMF SMC function ids.
  */
-#define PMF_SMC_GET_TIMESTAMP_32	U(0x82000010)
-#define PMF_SMC_GET_TIMESTAMP_64	U(0xC2000010)
+#define PMF_SMC_GET_TIMESTAMP_32	U(0x87000020)
+#define PMF_SMC_GET_TIMESTAMP_64	U(0xC7000020)
 #define PMF_NUM_SMC_CALLS		2
 
 /*
