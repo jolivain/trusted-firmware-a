bb63f3b0d5ac9a844a73f14f77575c20cdc9ae40
Chris Kay <chris.kay@arm.com>
2024-03-08 16:08:31 +0000

    build: remove `MAKE_BUILD_STRINGS` function
    This function causes the build message to be generated and compiled in
    two different ways, with one way done inside `build_macros.mk` and the
    other done inside `windows.mk`, mostly because it's done by generating
    the C file on the command line.
    
    We can instead replace this whole build message generation sequence with
    a simple standard C compilation command and a normal C file.
    
    Change-Id: I8bc136380c9585ddeec9a11154ee39ef70526f81
    Signed-off-by: Chris Kay <chris.kay@arm.com>

---
+++ include/common/build_message.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+extern const char build_message[];
+extern const char version_string[];
+extern const char version[];
