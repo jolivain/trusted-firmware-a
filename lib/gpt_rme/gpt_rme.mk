7f39975cc967a755174c2a469235a95bdd02895c
AlexeiFedorov <Alexei.Fedorov@arm.com>
2024-03-13 17:07:03 +0000

    feat(gpt): add support for large GPT mappings
    This patch adds support for large GPT mappings using
    Contiguous descriptors. The maximum size of supported
    contiguous block in MB is defined in GPT_MAX_BLOCK
    build parameter and takes values 0, 2, 32 and 512 and
    by default set to 2 in make_helpers/defaults.mk.
    Setting GPT_MAX_BLOCK value to 0 disables use of
    Contiguous descriptors.
    gpt_tlbi_by_pa_ll() function is removed from
    lib/aarch64/misc_helpers.S, because TLBI RPALOS
    instruction is now executed in corresponding
    functions in gpt_rme.c.
    
    Change-Id: Ia9a59bde1741c5666b4ca1de9324e6dfd6f734eb
    Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>

--- lib/gpt_rme/gpt_rme.mk
+++ lib/gpt_rme/gpt_rme.mk
@@ -1,8 +1,16 @@
 #
-# Copyright (c) 2021, Arm Limited. All rights reserved.
+# Copyright (c) 2021-2024, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
+
+# Process RME_GPT_MAX_BLOCK value
+ifeq ($(filter 0 2 32 512, ${RME_GPT_MAX_BLOCK}),)
+    $(error "Invalid value for RME_GPT_MAX_BLOCK: ${RME_GPT_MAX_BLOCK}")
+endif
+
+# Pass RME_GPT_MAX_BLOCK to the build system
+$(eval $(call add_define,RME_GPT_MAX_BLOCK))
 
 GPT_LIB_SRCS	:=	$(addprefix lib/gpt_rme/,        \
 			gpt_rme.c)
