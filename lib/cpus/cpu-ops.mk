71744e23040980fbce7fa286eff3a2f6b22cead0
Sona Mathew <sonarebecca.mathew@arm.com>
2024-04-05 16:27:07 -0500

    fix(cpus): workaround for Cortex-X4 erratum 2763018
    Cortex-X4 erratum 2763018 is a Cat B erratum that is present
    in revisions r0p0, r0p1 and is fixed in r0p2.
    
    The workaround is to set bit[47] of CPUACTLR3_EL1 register.
    Setting this chicken bit might have a small impact on power
    and negligible impact on performance.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN2432808/latest
    
    Change-Id: Ia188e08c2eb2952923ec72e2a56efdeea836fe1e
    Signed-off-by: Sona Mathew <sonarebecca.mathew@arm.com>

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -818,10 +818,18 @@ CPU_FLAG_LIST += ERRATA_X3_2743088
 # to revisions r0p0, r1p0, r1p1 of the Cortex-X3 cpu, it is fixed in r1p2.
 CPU_FLAG_LIST += ERRATA_X3_2779509
 
+# Flag to apply erratum 2763018 workaround on reset. This erratum applies
+# to revisions r0p0, r1p0, r1p1 of the Cortex-X3 cpu, it is fixed in r1p2.
+CPU_FLAG_LIST += ERRATA_X3_2779509
+
 # Flag to apply erratum 2701112 workaround for platforms that do not use an
 # Arm interconnect IP. This erratum applies to revisions r0p0 of the Cortex-X4
 # cpu and is fixed in r0p1.
 CPU_FLAG_LIST += ERRATA_X4_2701112
+
+# Flag to apply erratum 2763018 workaround on reset. This erratum applies
+# to revisions r0p0 and r0p1 of the Cortex-X4 cpu. It is fixed in r0p2.
+CPU_FLAG_LIST += ERRATA_X4_2763018
 
 # Flag to apply erratum 1922240 workaround during reset. This erratum applies
 # to revision r0p0 of the Cortex-A510 cpu and is fixed in r0p1.
