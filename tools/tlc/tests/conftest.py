443eb60472db245df21b438d190839707dcb14be
Harrison Mutai <harrison.mutai@arm.com>
2024-05-10 16:54:29 +0000

    feat(tlc): add host tool for static TL generation
    Transfer List Compiler is a command line tool that enables the static
    generation of TL's compliant with version 0.9 of the firmware handoff
    specification. The intent of this tool is to support information passing
    via the firmware handoff framework to bootloaders that run without
    preceding images (i.e. `RESET_TO_BL31`).
    
    It currently allows for TL's to be statically generated from blobs of
    data, and modified by removing/adding TE's. Future work will provide
    support for TL generation from configuration file.
    
    Change-Id: Iff670842e34c9ad18eac935248ee2aece43dc533
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

---
+++ tools/tlc/tests/conftest.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# type: ignore[attr-defined]
+
+#
+# Copyright (c) 2024, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+import pytest
+from click.testing import CliRunner
+
+from tlc.cli import cli
+
+
+@pytest.fixture
+def tmptlstr(tmpdir):
+    return tmpdir.join("tl.bin").strpath
+
+
+@pytest.fixture
+def tmpfdt(tmpdir):
+    fdt = tmpdir.join("fdt.bin")
+    fdt.write_binary(b"\x00" * 100)
+    return fdt
+
+
+@pytest.fixture
+def tlcrunner(tmptlstr):
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        runner.invoke(cli, ["create", tmptlstr])
+    return runner
+
+
+@pytest.fixture
+def tlc_entries(tmpfdt):
+    return [(0, "/dev/null"), (1, tmpfdt.strpath), (0x102, tmpfdt.strpath)]
