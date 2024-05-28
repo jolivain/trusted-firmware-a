f6359349369bc4dfade26a6db1177783e268edff
Harrison Mutai <harrison.mutai@arm.com>
2024-05-28 14:35:41 +0000

    feat(mbedtls): migrate heap info to fw handoff
    Mbed-TLS requires platforms to allocate it a heap for it's own internal
    usage. This heap is typically between shared by BL1 and BL2 to conserve
    memory.The base address and size of the heap are conveyed from BL1 to
    BL2 through the config TB_FW_CONFIG.
    
    This slightly awkward approach necessitates declaring a placeholder node
    in the DTS. At runtime, this node is populated with the actual values of
    the heap information. Instead, since this dynamic information, and
    simple to represent through C structures, transmit it to later stages
    using the firmware handoff framework.
    
    Change-Id: Iff00dc742924a055b8bd304f15eec03ce3c6d1ef
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- include/plat/arm/common/plat_arm.h
+++ include/plat/arm/common/plat_arm.h
@@ -286,6 +286,9 @@ void arm_transfer_list_populate_ep_info(bl_mem_params_node_t *next_param_node,
 					struct transfer_list_header *ns_tl);
 void arm_transfer_list_copy_hw_config(struct transfer_list_header *secure_tl,
 				      struct transfer_list_header *ns_tl);
+struct transfer_list_entry *
+arm_transfer_list_set_heap_info(struct transfer_list_header *tl);
+void arm_transfer_list_get_heap_info(void **heap_addr, size_t *heap_size);
 
 /* TSP utility functions */
 void arm_tsp_early_platform_setup(void);
