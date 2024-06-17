3ff7ea9f6b04177a3fc8edfcb6afcbb2c34b3782
Charlie Bareham <charlie.bareham@arm.com>
2024-06-17 11:58:03 +0100

    feat: add creating transfer lists from yaml files
    This commit adds a command create-from-yaml to tlc, which
    creates a transfer list from a yaml file. It also changes
    the files structure of the fixtures in the unit tests so
    they are in a directory called trusted-firmware-a. This
    is necessary because blob file paths in the yaml file are
    relative to the root of TF-A.
    
    The blob files are not verified by TLC, so it can be used
    to load arbitrary binary information into the transfer
    list. The authenticity of the transfer list must be
    ensured by the loader.
    
    Change-Id: Idf704ce5d9b7e28b31f471ac337e4aef33d0ad8a
    Signed-off-by: Charlie Bareham <charlie.bareham@arm.com>

--- tools/tlc/tests/conftest.py
+++ tools/tlc/tests/conftest.py
@@ -7,22 +7,262 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
+import os
 import pytest
+import yaml
 from click.testing import CliRunner
 
 from tlc.cli import cli
 
 
 @pytest.fixture
-def tmptlstr(tmpdir):
-    return tmpdir.join("tl.bin").strpath
+def tmptfa(tmpdir):
+    """Fake trusted-firmware-a directory, containing all of the blob files"""
+    temp_tfa_root = tmpdir.join("trusted-firmware-a")
+    os.mkdir(temp_tfa_root.strpath)
+    return temp_tfa_root
 
 
 @pytest.fixture
-def tmpfdt(tmpdir):
-    fdt = tmpdir.join("fdt.bin")
+def tmptlstr(tmptfa):
+    return tmptfa.join("tl.bin").strpath
+
+
+@pytest.fixture
+def tmpfdt(tmptfa):
+    fdt = tmptfa.join("fdt.bin")
     fdt.write_binary(b"\x00" * 100)
     return fdt
+
+
+@pytest.fixture
+def tmpyamlconfig_fdt_entry(tmpdir, tmpfdt):
+    config_path = tmpdir.join("config.yaml")
+
+    config = {
+        "has_checksum": True,
+        "max_size": 0x1000,
+        "entries": [
+            {
+                "tag_id": 1,
+                "fdt": tmpfdt.basename,
+            },
+        ],
+    }
+
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
+
+    return config_path
+
+
+@pytest.fixture
+def tmpyamlconfig_empty_entry(tmpdir, tmpfdt):
+    config_path = tmpdir.join("config_empty_entry.yaml")
+
+    config = {
+        "has_checksum": True,
+        "max_size": 0x1000,
+        "entries": [
+            {
+                "tag_id": 0,
+            },
+        ],
+    }
+
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
+
+    return config_path
+
+
+@pytest.fixture
+def tmpyamlconfig_memory_layout_entry(tmpdir, tmpfdt):
+    config_path = tmpdir.join("config_empty_entry.yaml")
+
+    config = {
+        "has_checksum": True,
+        "max_size": 0x1000,
+        "entries": [
+            {
+                "tag_id": 0x104,
+                "addr": 0x0400100000000010,
+                "size": 0x0003300000000000,
+            },
+        ],
+    }
+
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
+
+    return config_path
+
+
+@pytest.fixture
+def tmpyamlconfig_ep_entry(tmpdir, tmpfdt):
+    config_path = tmpdir.join("config_ep_entry.yaml")
+
+    config = {
+        "has_checksum": True,
+        "max_size": 0x1000,
+        "entries": [
+            {
+                "tag_id": 0x102,
+                "ep_info": {
+                    "h": {
+                        "type": 0x01,
+                        "version": 0x02,
+                        "attr": 8,
+                    },
+                    "pc": 67239936,
+                    "spsr": 965,
+                    "args": [67112976, 67112960, 0, 0, 0, 0, 0, 0],
+                },
+            },
+        ],
+    }
+
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
+
+    return config_path
+
+
+@pytest.fixture
+def tmpyamlconfig_ep_entry_aarch32(tmpdir, tmpfdt):
+    config_path = tmpdir.join("config_ep_entry.yaml")
+
+    config = {
+        "execution_state": "aarch32",
+        "has_checksum": True,
+        "max_size": 0x1000,
+        "entries": [
+            {
+                "tag_id": 0x102,
+                "ep_info": {
+                    "h": {
+                        "type": 0x01,
+                        "version": 0x02,
+                        "attr": 8,
+                    },
+                    "pc": 67239936,
+                    "spsr": 467,
+                    "lr_svc": 0,
+                    "args": [67112968, 67112960, 0, 0],
+                },
+            },
+        ],
+    }
+
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
+
+    return config_path
+
+
+@pytest.fixture
+def tmpyamlconfig_hob_block_entry(tmpdir, tmpfdt):
+    config_path = tmpdir.join("config.yaml")
+
+    config = {
+        "has_checksum": True,
+        "max_size": 0x1000,
+        "entries": [
+            {
+                "tag_id": 2,
+                "hob_block": tmpfdt.basename,
+            },
+        ],
+    }
+
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
+
+    return config_path
+
+
+@pytest.fixture
+def tmpyamlconfig_hob_list_entry(tmpdir, tmpfdt):
+    config_path = tmpdir.join("config.yaml")
+
+    config = {
+        "has_checksum": True,
+        "max_size": 0x1000,
+        "entries": [
+            {
+                "tag_id": 3,
+                "hob_list": tmpfdt.basename,
+            },
+        ],
+    }
+
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
+
+    return config_path
+
+
+@pytest.fixture
+def tmpyamlconfig_acpi_entry(tmpdir, tmpfdt):
+    config_path = tmpdir.join("config.yaml")
+
+    config = {
+        "has_checksum": True,
+        "max_size": 0x1000,
+        "entries": [
+            {
+                "tag_id": 4,
+                "acpi_tables": tmpfdt.basename,
+            },
+        ],
+    }
+
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
+
+    return config_path
+
+
+@pytest.fixture
+def tmpyamlconfig_spmc_entry(tmpdir, tmpfdt):
+    config_path = tmpdir.join("config.yaml")
+
+    config = {
+        "has_checksum": True,
+        "max_size": 0x1000,
+        "entries": [
+            {
+                "tag_id": 0x101,
+                "spmc_man": tmpfdt.basename,
+            },
+        ],
+    }
+
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
+
+    return config_path
+
+
+@pytest.fixture
+def tmpyamlconfig_optee_entry(tmpdir, tmpfdt):
+    config_path = tmpdir.join("config.yaml")
+
+    config = {
+        "has_checksum": True,
+        "max_size": 0x1000,
+        "entries": [
+            {
+                "tag_id": 0x100,
+                "pp_addr": 100,
+            },
+        ],
+    }
+
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
+
+    return config_path
 
 
 @pytest.fixture
