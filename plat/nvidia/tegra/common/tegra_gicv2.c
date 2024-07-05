691d1b6ad805b9156d6869ca49a300d469611adb
Varun Wadekar <vwadekar@nvidia.com>
2024-07-05 17:20:18 +0000

    feat(tegra/gicv2): introduce plat_ic_* helpers for interrupt type
    This patch introduces the following platform helper functions
    for Tegra platforms.
    
    * plat_ic_has_interrupt_type
    * plat_ic_set_interrupt_type
    
    The helper functions treat EL3 and SEL1 interrupts types as
    group0 interrupts for Tegra. The plat_ic_has_interrupt_type
    is marked as weak in plat_gicv2.c to allow Tegra code to
    override it.
    
    Change-Id: If7b6e028433ecff89f4b414c027c787914db5763
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- plat/nvidia/tegra/common/tegra_gicv2.c
+++ plat/nvidia/tegra/common/tegra_gicv2.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
- * Copyright (c) 2020, NVIDIA Corporation. All rights reserved.
+ * Copyright (c) 2020-2024, NVIDIA Corporation. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -8,6 +8,7 @@
 #include <assert.h>
 #include <platform_def.h>
 
+#include <bl31/interrupt_mgmt.h>
 #include <common/bl_common.h>
 #include <drivers/arm/gicv2.h>
 #include <lib/utils.h>
@@ -69,4 +70,49 @@ void tegra_gic_pcpu_init(void)
 	gicv2_pcpu_distif_init();
 	gicv2_set_pe_target_mask(plat_my_core_pos());
 	gicv2_cpuif_enable();
+}
+
+/******************************************************************************
+ * Tegra helper to return supported interupt types
+ *****************************************************************************/
+bool plat_ic_has_interrupt_type(unsigned int type)
+{
+	bool has_interrupt_type = false;
+
+	switch (type) {
+	case INTR_TYPE_EL3:
+	case INTR_TYPE_S_EL1:
+	case INTR_TYPE_NS:
+		has_interrupt_type = true;
+		break;
+	default:
+		/* Do nothing in default case */
+		break;
+	}
+
+	return has_interrupt_type;
+}
+
+/******************************************************************************
+ * Tegra helper to set the interrupt type
+ *****************************************************************************/
+void plat_ic_set_interrupt_type(unsigned int id, unsigned int type)
+{
+	unsigned int gicv2_group = 0U;
+
+	/* Map canonical interrupt type to GICv2 type */
+	switch (type) {
+	case INTR_TYPE_EL3:
+	case INTR_TYPE_S_EL1:
+		gicv2_group = GICV2_INTR_GROUP0;
+		break;
+	case INTR_TYPE_NS:
+		gicv2_group = GICV2_INTR_GROUP1;
+		break;
+	default:
+		assert(false); /* Unreachable */
+		break;
+	}
+
+	gicv2_set_interrupt_group(id, gicv2_group);
 }
