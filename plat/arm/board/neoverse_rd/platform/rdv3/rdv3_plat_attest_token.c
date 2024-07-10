1293a755b508b8dc24a9d185856b431db6a37f0e
Juan Pablo Conde <juanpablo.conde@arm.com>
2024-07-10 14:33:42 -0500

    refactor(rmmd): plat token requests in pieces
    Until now, the attestation token size was limited by the size of the
    shared buffer between RMM and TF-A. With this change, RMM can now
    request the token in pieces, so they fit in the shared buffer. A new
    output parameter was added to the SMC call, which will return (along
    with the size of bytes copied into the buffer) the number of bytes
    of the token that remain to be retrieved.
    
    TF-A will keep an offset variable that will indicate the position in
    the token where the next call will retrieve bytes from. This offset
    will be increased on every call by adding the number number of bytes
    copied. If the received hash size is not 0, TF-A will reset the
    offset to 0 and copy from that position on.
    
    The SMC call will now return at most the size of the shared buffer
    in bytes on every call. Therefore, from now on, multiple SMC calls
    may be needed to be issued if the token size exceeds the shared
    buffer size.
    
    Change-Id: I591f7013d06f64e98afaf9535dbea6f815799723
    Signed-off-by: Juan Pablo Conde <juanpablo.conde@arm.com>

--- plat/arm/board/neoverse_rd/platform/rdv3/rdv3_plat_attest_token.c
+++ plat/arm/board/neoverse_rd/platform/rdv3/rdv3_plat_attest_token.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <assert.h>
 #include <errno.h>
 #include <stdint.h>
 
@@ -12,15 +13,22 @@
 #include <psa/error.h>
 
 int plat_rmmd_get_cca_attest_token(uintptr_t buf, size_t *len,
-				   uintptr_t hash, size_t hash_size)
+				   uintptr_t hash, size_t hash_size,
+				   size_t *remaining_len)
 {
 	psa_status_t ret;
+
+	assert(*len == SZ_4K);
 
 	ret = cca_attestation_get_plat_token(buf, len, hash, hash_size);
 	if (ret != PSA_SUCCESS) {
 		ERROR("Unable to fetch CCA attestation token\n");
 		return -1;
 	}
+
+	assert(*len <= SZ_4K);
+
+	*remaining_len = 0;
 
 	return 0;
 }
