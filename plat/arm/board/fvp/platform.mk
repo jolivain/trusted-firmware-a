1f0df07470fab22256114b532065a4030c44602c
Arvind Ram Prakash <arvind.ramprakash@arm.com>
2024-08-05 15:49:18 -0500

    fix(fvp): include Cortex-A520 library by default
    This patch adds Cortex-A520 by default.
    
    Change-Id: Iaeb6f7867b169fa02211ce93c6335fd73b93f352
    Signed-off-by: Arvind Ram Prakash <arvind.ramprakash@arm.com>

--- plat/arm/board/fvp/platform.mk
+++ plat/arm/board/fvp/platform.mk
@@ -192,6 +192,7 @@ else
 					lib/cpus/aarch64/neoverse_n_common.S	\
 					lib/cpus/aarch64/neoverse_n1.S		\
 					lib/cpus/aarch64/neoverse_n2.S		\
+					lib/cpus/aarch64/cortex_a520.S		\
 					lib/cpus/aarch64/neoverse_v1.S		\
 					lib/cpus/aarch64/neoverse_e1.S		\
 					lib/cpus/aarch64/cortex_x2.S		\
