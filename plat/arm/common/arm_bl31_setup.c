3aa168898d4d2b8895a8a0c759750e39818bc27f
Harrison Mutai <harrison.mutai@arm.com>
2024-08-28 13:27:19 +0000

    feat(handoff): make tl generation flexible
    Make the process of compiling a TL from DT source flexible. Provide a
    top level recipe to make it easier for developers to build a transfer
    list. Clean up integration of TLC into the build system.
    
    Change-Id: I4466e27a457dfd5bf709dc3a360a2b63bf6030ce
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- plat/arm/common/arm_bl31_setup.c
+++ plat/arm/common/arm_bl31_setup.c
@@ -20,6 +20,7 @@
 #include <lib/transfer_list.h>
 #endif
 #include <lib/xlat_tables/xlat_tables_compat.h>
+
 #include <plat/arm/common/plat_arm.h>
 #include <plat/common/platform.h>
 #include <platform_def.h>
@@ -56,33 +57,30 @@ CASSERT(BL31_BASE >= ARM_FW_CONFIG_LIMIT, assert_bl31_base_overflows);
 #pragma weak bl31_plat_get_next_image_ep_info
 #pragma weak bl31_plat_runtime_setup
 
-#define MAP_BL31_TOTAL		MAP_REGION_FLAT(			\
-					BL31_START,			\
-					BL31_END - BL31_START,		\
-					MT_MEMORY | MT_RW | EL3_PAS)
+#define MAP_BL31_TOTAL                                     \
+	MAP_REGION_FLAT(BL31_START, BL31_END - BL31_START, \
+			MT_MEMORY | MT_RW | EL3_PAS)
 #if RECLAIM_INIT_CODE
 IMPORT_SYM(unsigned long, __INIT_CODE_START__, BL_INIT_CODE_BASE);
 IMPORT_SYM(unsigned long, __INIT_CODE_END__, BL_CODE_END_UNALIGNED);
 IMPORT_SYM(unsigned long, __STACKS_END__, BL_STACKS_END_UNALIGNED);
 
-#define	BL_INIT_CODE_END	((BL_CODE_END_UNALIGNED + PAGE_SIZE - 1) & \
-					~(PAGE_SIZE - 1))
-#define	BL_STACKS_END		((BL_STACKS_END_UNALIGNED + PAGE_SIZE - 1) & \
-					~(PAGE_SIZE - 1))
+#define BL_INIT_CODE_END \
+	((BL_CODE_END_UNALIGNED + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
+#define BL_STACKS_END \
+	((BL_STACKS_END_UNALIGNED + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
 
-#define MAP_BL_INIT_CODE	MAP_REGION_FLAT(			\
-					BL_INIT_CODE_BASE,		\
-					BL_INIT_CODE_END		\
-						- BL_INIT_CODE_BASE,	\
-					MT_CODE | EL3_PAS)
+#define MAP_BL_INIT_CODE                                      \
+	MAP_REGION_FLAT(BL_INIT_CODE_BASE,                    \
+			BL_INIT_CODE_END - BL_INIT_CODE_BASE, \
+			MT_CODE | EL3_PAS)
 #endif
 
 #if SEPARATE_NOBITS_REGION
-#define MAP_BL31_NOBITS		MAP_REGION_FLAT(			\
-					BL31_NOBITS_BASE,		\
-					BL31_NOBITS_LIMIT 		\
-						- BL31_NOBITS_BASE,	\
-					MT_MEMORY | MT_RW | EL3_PAS)
+#define MAP_BL31_NOBITS                                       \
+	MAP_REGION_FLAT(BL31_NOBITS_BASE,                     \
+			BL31_NOBITS_LIMIT - BL31_NOBITS_BASE, \
+			MT_MEMORY | MT_RW | EL3_PAS)
 
 #endif
 /*******************************************************************************
@@ -147,8 +145,7 @@ void __init arm_bl31_early_platform_setup(u_register_t arg0, u_register_t arg1,
 	bl33_image_ep_info.spsr = arm_get_spsr_for_bl33_entry();
 	SET_SECURITY_STATE(bl33_image_ep_info.h.attr, NON_SECURE);
 
-	bl33_image_ep_info.args.arg0 =
-		FW_NS_HANDOFF_BASE + ARM_PRELOADED_DTB_OFFSET;
+	bl33_image_ep_info.args.arg0 = PLAT_ARM_TRANSFER_LIST_DTB_OFFSET;
 	bl33_image_ep_info.args.arg1 =
 		TRANSFER_LIST_HANDOFF_X1_VALUE(REGISTER_CONVENTION_VERSION);
 	bl33_image_ep_info.args.arg3 = FW_NS_HANDOFF_BASE;
@@ -188,8 +185,10 @@ void __init arm_bl31_early_platform_setup(u_register_t arg0, u_register_t arg1,
 #endif /* RESET_TO_BL31 */
 }
 #else
-void __init arm_bl31_early_platform_setup(void *from_bl2, uintptr_t soc_fw_config,
-				uintptr_t hw_config, void *plat_params_from_bl2)
+void __init arm_bl31_early_platform_setup(void *from_bl2,
+					  uintptr_t soc_fw_config,
+					  uintptr_t hw_config,
+					  void *plat_params_from_bl2)
 {
 	/* Initialize the console to provide early debug support */
 	arm_console_boot_init();
@@ -199,12 +198,9 @@ void __init arm_bl31_early_platform_setup(void *from_bl2, uintptr_t soc_fw_confi
 	assert(from_bl2 == NULL);
 	assert(plat_params_from_bl2 == NULL);
 
-# ifdef BL32_BASE
+#ifdef BL32_BASE
 	/* Populate entry point information for BL32 */
-	SET_PARAM_HEAD(&bl32_image_ep_info,
-				PARAM_EP,
-				VERSION_1,
-				0);
+	SET_PARAM_HEAD(&bl32_image_ep_info, PARAM_EP, VERSION_1, 0);
 	SET_SECURITY_STATE(bl32_image_ep_info.h.attr, SECURE);
 	bl32_image_ep_info.pc = BL32_BASE;
 	bl32_image_ep_info.spsr = arm_get_spsr_for_bl32_entry();
@@ -216,17 +212,14 @@ void __init arm_bl31_early_platform_setup(void *from_bl2, uintptr_t soc_fw_confi
 	 * shared SRAM is allocated to BL31, so to avoid overwriting of manifest
 	 * keep it in the last page.
 	 */
-	bl32_image_ep_info.args.arg0 = ARM_TRUSTED_SRAM_BASE +
-				PLAT_ARM_TRUSTED_SRAM_SIZE - PAGE_SIZE;
+	bl32_image_ep_info.args.arg0 =
+		ARM_TRUSTED_SRAM_BASE + PLAT_ARM_TRUSTED_SRAM_SIZE - PAGE_SIZE;
 #endif
 
-# endif /* BL32_BASE */
+#endif /* BL32_BASE */
 
 	/* Populate entry point information for BL33 */
-	SET_PARAM_HEAD(&bl33_image_ep_info,
-				PARAM_EP,
-				VERSION_1,
-				0);
+	SET_PARAM_HEAD(&bl33_image_ep_info, PARAM_EP, VERSION_1, 0);
 	/*
 	 * Tell BL31 where the non-trusted software image
 	 * is located and the entry state information
@@ -252,7 +245,7 @@ void __init arm_bl31_early_platform_setup(void *from_bl2, uintptr_t soc_fw_confi
 	 * In release builds, it's not used.
 	 */
 	assert(((unsigned long long)plat_params_from_bl2) ==
-		ARM_BL31_PLAT_PARAM_VAL);
+	       ARM_BL31_PLAT_PARAM_VAL);
 
 	/*
 	 * Check params passed from BL2 should not be NULL,
@@ -310,7 +303,7 @@ void __init arm_bl31_early_platform_setup(void *from_bl2, uintptr_t soc_fw_confi
 #endif
 #endif /* RESET_TO_BL31 */
 
-# if ARM_LINUX_KERNEL_AS_BL33
+#if ARM_LINUX_KERNEL_AS_BL33
 	/*
 	 * According to the file ``Documentation/arm64/booting.txt`` of the
 	 * Linux kernel tree, Linux expects the physical address of the device
@@ -329,12 +322,12 @@ void __init arm_bl31_early_platform_setup(void *from_bl2, uintptr_t soc_fw_confi
 	bl33_image_ep_info.args.arg1 = 0U;
 	bl33_image_ep_info.args.arg2 = 0U;
 	bl33_image_ep_info.args.arg3 = 0U;
-# endif
+#endif
 }
 #endif
 
 void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
-		u_register_t arg2, u_register_t arg3)
+				u_register_t arg2, u_register_t arg3)
 {
 #if TRANSFER_LIST
 	arm_bl31_early_platform_setup(arg0, arg1, arg2, arg3);
@@ -405,7 +398,7 @@ void arm_bl31_platform_setup(void)
 
 	/* Enable and initialize the System level generic timer */
 	mmio_write_32(ARM_SYS_CNTCTL_BASE + CNTCR_OFF,
-			CNTCR_FCREQ(0U) | CNTCR_EN);
+		      CNTCR_FCREQ(0U) | CNTCR_EN);
 
 	/* Allow access to the System counter timer module */
 	arm_configure_sys_timer();
@@ -478,19 +471,19 @@ void arm_free_init_memory(void)
 	if (BL_STACKS_END < BL_INIT_CODE_END) {
 		/* Reclaim some of the init section as stack if possible. */
 		if (BL_INIT_CODE_BASE < BL_STACKS_END) {
-			ret |= xlat_change_mem_attributes(BL_INIT_CODE_BASE,
-					BL_STACKS_END - BL_INIT_CODE_BASE,
-					MT_RW_DATA);
+			ret |= xlat_change_mem_attributes(
+				BL_INIT_CODE_BASE,
+				BL_STACKS_END - BL_INIT_CODE_BASE, MT_RW_DATA);
 		}
 		/* Make the rest of the init section read-only. */
-		ret |= xlat_change_mem_attributes(BL_STACKS_END,
-				BL_INIT_CODE_END - BL_STACKS_END,
-				MT_RO_DATA);
+		ret |= xlat_change_mem_attributes(
+			BL_STACKS_END, BL_INIT_CODE_END - BL_STACKS_END,
+			MT_RO_DATA);
 	} else {
 		/* The stacks cover the init section, so reclaim it all. */
-		ret |= xlat_change_mem_attributes(BL_INIT_CODE_BASE,
-				BL_INIT_CODE_END - BL_INIT_CODE_BASE,
-				MT_RW_DATA);
+		ret |= xlat_change_mem_attributes(
+			BL_INIT_CODE_BASE, BL_INIT_CODE_END - BL_INIT_CODE_BASE,
+			MT_RW_DATA);
 	}
 
 	if (ret != 0) {
@@ -518,27 +511,25 @@ void bl31_plat_runtime_setup(void)
  ******************************************************************************/
 void __init arm_bl31_plat_arch_setup(void)
 {
-	const mmap_region_t bl_regions[] = {
-		MAP_BL31_TOTAL,
+	const mmap_region_t bl_regions[] = { MAP_BL31_TOTAL,
 #if ENABLE_RME
-		ARM_MAP_L0_GPT_REGION,
+					     ARM_MAP_L0_GPT_REGION,
 #endif
 #if RECLAIM_INIT_CODE
-		MAP_BL_INIT_CODE,
+					     MAP_BL_INIT_CODE,
 #endif
 #if SEPARATE_NOBITS_REGION
-		MAP_BL31_NOBITS,
+					     MAP_BL31_NOBITS,
 #endif
-		ARM_MAP_BL_RO,
+					     ARM_MAP_BL_RO,
 #if USE_ROMLIB
-		ARM_MAP_ROMLIB_CODE,
-		ARM_MAP_ROMLIB_DATA,
+					     ARM_MAP_ROMLIB_CODE,
+					     ARM_MAP_ROMLIB_DATA,
 #endif
 #if USE_COHERENT_MEM
-		ARM_MAP_BL_COHERENT_RAM,
+					     ARM_MAP_BL_COHERENT_RAM,
 #endif
-		{0}
-	};
+					     { 0 } };
 
 	setup_page_tables(bl_regions, plat_arm_get_mmap());
 
